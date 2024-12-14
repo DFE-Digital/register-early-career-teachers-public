01217c68c8719bce66a5f9b948b2e65bad7aac97
Lorenzo Tello <ltello8a@gmail.com>
2024-12-14 09:37:05 +0000

    fully isolate each feature spec in its own Playwright context

--- spec/support/testing_with_playwright.rb
+++ spec/support/testing_with_playwright.rb
@@ -1,28 +1,20 @@
 require_relative 'rspec_playwright'
 
 RSpec.configure do |config|
+  config.add_setting :playwright_browser
   config.add_setting :playwright_page
-  config.add_setting :playwright_page_with_js_enabled
-  config.add_setting :playwright_page_with_js_disabled
 
   # Start/Reuse Playwright browser
   config.before(type: :feature) do
-    if Capybara.current_driver == :js_enabled
-      config.playwright_page_with_js_enabled ||= RSpecPlaywright.start_browser(javascript_enabled: true)
-      config.playwright_page = config.playwright_page_with_js_enabled
-    else
-      config.playwright_page_with_js_disabled ||= RSpecPlaywright.start_browser(javascript_enabled: false)
-      config.playwright_page = config.playwright_page_with_js_disabled
-    end
-  end
-
-  # Sign out the user after every feature spec.
-  config.after(type: :feature) do
-    config.playwright_page.goto(sign_out_path)
+    config.playwright_browser ||= RSpecPlaywright.start_browser
+    config.playwright_page = config.playwright_browser
+                                   .new_page(baseURL: Capybara.current_session.server.base_url,
+                                             javaScriptEnabled: Capybara.current_driver == :js_enabled)
+    config.playwright_page.set_default_timeout(RSpecPlaywright::DEFAULT_TIMEOUT)
   end
 
   # Close Playwright browsers
   config.after(:suite) do
-    RSpecPlaywright.close_browsers
+    RSpecPlaywright.close_browser
   end
 end
