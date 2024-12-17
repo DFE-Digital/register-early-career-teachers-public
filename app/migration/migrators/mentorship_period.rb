b920ac83a0e82f0cf81d9f70ba144866fb73f0f6
Tony Headford <tony@binarycircus.com>
2024-12-17 08:46:27 +0000

    check in case teachers profile was not migrated

--- app/migration/migrators/mentorship_period.rb
+++ app/migration/migrators/mentorship_period.rb
@@ -30,13 +30,18 @@ module Migrators
 
     def migrate_mentorships!(participant_profile:)
       teacher = ::Teacher.find_by!(legacy_ect_id: participant_profile.id)
+
       induction_records = InductionRecordSanitizer.new(participant_profile:)
       induction_records.validate!
 
       mentorship_period_data = MentorshipPeriodExtractor.new(induction_records:)
       Builders::MentorshipPeriods.new(teacher:, mentorship_period_data:).process!
     rescue ActiveRecord::ActiveRecordError, ::InductionRecordSanitizer::InductionRecordError => e
-      raise ChildRecordError.new(e.message, teacher)
+      if teacher.present?
+        raise ChildRecordError.new(e.message, teacher)
+      else
+        raise
+      end
     end
   end
 end
