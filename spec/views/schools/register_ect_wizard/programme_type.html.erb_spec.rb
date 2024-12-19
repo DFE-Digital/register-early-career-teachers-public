978364ae90fd82b635194863887b304ae10afd76
Toby Retallick <toby.retallick@gmail.com>
2024-12-19 11:11:02 +0000

    refactor: view specs

--- spec/views/schools/register_ect_wizard/programme_type.html.erb_spec.rb
+++ spec/views/schools/register_ect_wizard/programme_type.html.erb_spec.rb
@@ -4,21 +4,20 @@ RSpec.describe "schools/register_ect_wizard/programme_type.html.erb" do
   let(:step) { Schools::RegisterECTWizard::FindECTStep.new }
   let(:ect) { double(full_name: 'John Smith') }
   let(:title) { "What training programme will #{ect.full_name} follow?" }
-  let(:wizard) { Schools::RegisterECTWizard::Wizard.new(current_step: :find_ect, store: {}) }
+  let(:wizard) { Schools::RegisterECTWizard::Wizard.new(current_step: :programme_type, store: {}) }
 
-  it "sets the page title to 'What training programme will John Smith follow?'" do
+  before do
     assign(:wizard, wizard)
     assign(:ect, ect)
+  end
 
+  it "sets the page title to 'What training programme will John Smith follow?'" do
     render
 
     expect(sanitize(view.content_for(:page_title))).to eql(sanitize(title))
   end
 
   it "prefixes the page with 'Error:' when the programme type is not selected" do
-    assign(:wizard, wizard)
-    assign(:ect, ect)
-
     wizard.valid_step?
     render
 
@@ -26,18 +25,12 @@ RSpec.describe "schools/register_ect_wizard/programme_type.html.erb" do
   end
 
   it 'includes a back button that links to the start page of the register ECT journey' do
-    assign(:wizard, wizard)
-    assign(:ect, ect)
-
     render
 
     expect(view.content_for(:backlink_or_breadcrumb)).to have_link('Back', href: back_path)
   end
 
   it 'includes a continue button that posts to the programme type page' do
-    assign(:wizard, wizard)
-    assign(:ect, ect)
-
     render
 
     expect(rendered).to have_button('Continue')
