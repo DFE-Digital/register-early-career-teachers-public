953bdef5f26f69a9a0c7225f10cc8d5f46870dd3
Lorenzo Tello <ltello8a@gmail.com>
2024-12-20 03:17:23 +0000

    refactor SessionUser to instantiate the right type of user from the session. Also include the activity expiry logic.

--- app/services/sessions/session_user.rb
+++ app/services/sessions/session_user.rb
@@ -1,96 +1,37 @@
 module Sessions
   class SessionUser
-    attr_reader :provider, :name, :email, :appropriate_body_id, :school_urn, :dfe, :dfe_sign_in_user_id, :dfe_sign_in_organisation_id
+    attr_reader :email, :last_active_at
 
-    def initialize(provider:, email:, dfe_sign_in_user_id: nil, name: nil, appropriate_body_id: nil, school_urn: nil, dfe_sign_in_organisation_id: nil, dfe: false, last_active_at: Time.zone.now)
-      @dfe_sign_in_user_id = dfe_sign_in_user_id
-      @dfe_sign_in_organisation_id = dfe_sign_in_organisation_id
-      @provider = provider
-      @name = name
-      @email = email
-      @last_active_at = last_active_at
-      @appropriate_body_id = appropriate_body_id || find_appropriate_body_id
-      @school_urn = school_urn
-      @dfe = dfe
-    end
-
-    def last_active_at
-      # FIXME: why is this sometimes a string?
-      @last_active_at.is_a?(String) ? Time.zone.parse(@last_active_at) : @last_active_at
-    end
-
-    def record_new_activity!(session:, time: Time.zone.now)
-      session['user_session']['last_active_at'] = time
-
-      self
-    end
-
-    def dfe_user?
-      dfe
-    end
-
-    def appropriate_body_user?
-      appropriate_body_id.present?
-    end
-
-    def school_user?
-      school_urn.present?
-    end
+    MAX_SESSION_IDLE_TIME = 2.hours
 
     def self.from_session(user_session)
-      new(
-        provider: user_session['provider'],
-        name: user_session['name'],
-        email: user_session['email'],
-        appropriate_body_id: user_session['appropriate_body_id'],
-        last_active_at: user_session['last_active_at'],
-        school_urn: user_session['school_urn'],
-        dfe: user_session['dfe']
-      )
+      session_user_class = user_session['type'].constantize
+      session_user_class.new(**user_session.except('type').symbolize_keys)
     end
 
-    def self.from_dfe_sign_in(user_info)
-      new(
-        provider: user_info.provider,
-        name: user_info.info.then { |info| "#{info.first_name} #{info.last_name}" },
-        school_urn: user_info.extra.raw_info.organisation.urn,
-        dfe_sign_in_organisation_id: user_info.extra.raw_info.organisation.id,
-        dfe_sign_in_user_id: user_info.uid,
-        email: user_info.info.email
-      )
+    # User?
+    def appropriate_body_user? = false
+    def dfe_user? = false
+    def school_user? = false
+
+    # Activity
+    def expired?
+      last_active_at < MAX_SESSION_IDLE_TIME.ago
     end
 
-    def self.from_user_record(email:, provider:)
-      user = User.find_by!(email:)
-
-      new(
-        provider:,
-        name: user.name,
-        email: email,
-        dfe: true
-      )
+    def expires_at
+      (last_active_at + MAX_SESSION_IDLE_TIME) if last_active_at
     end
 
-    def to_h
-      {
-        "provider" => provider,
-        "email" => email,
-        "name" => name,
-        "last_active_at" => last_active_at,
-        "appropriate_body_id" => appropriate_body_id.presence,
-        "school_urn" => school_urn.presence,
-        "dfe" => dfe,
-        "dfe_sign_in_user_id" => dfe_sign_in_user_id,
-        "dfe_sign_in_organisation_id" => dfe_sign_in_organisation_id,
-      }
+    def record_new_activity(time)
+      @last_active_at = time
     end
 
   private
 
-    def find_appropriate_body_id
-      appropriate_body = AppropriateBodies::Search.new.find_by_dfe_sign_in_organisation_id(dfe_sign_in_organisation_id)
-
-      appropriate_body&.id
+    def initialize(email:, last_active_at: Time.zone.now)
+      @email = email
+      @last_active_at = last_active_at.is_a?(String) ? Time.zone.parse(last_active_at) : last_active_at
     end
   end
 end
