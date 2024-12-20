bdfdffc3931665213f608fa76106e7595f269f7d
Lorenzo Tello <ltello8a@gmail.com>
2024-12-20 03:27:07 +0000

    mock dfe_sign_in provider

---
+++ spec/support/requests/omniauth_helper.rb
@@ -0,0 +1,35 @@
+module OmniAuthHelper
+  def mock_dfe_sign_in_provider!(**args)
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:dfe_sign_in] = OmniAuth::AuthHash.new(
+      provider: 'dfe_sign_in',
+      callback_path: '/auth/dfe/callback',
+      uid: '1234567890',
+      info: {
+        email: args[:email],
+        first_name: args[:first_name],
+        last_name: args[:last_name],
+      },
+      extra: {
+        raw_info: {
+          organisation: {
+            id: args[:organisation_id],
+            urn: args[:organisation_urn],
+          }
+        }
+      },
+      credentials: {
+        token: 'mock_token',
+        refresh_token: 'mock_refresh_token',
+        expires_at: Time.current + 1.week
+      }
+    )
+  end
+
+  def stop_mocking_dfe_sign_in_provider!
+    OmniAuth.config.mock_auth[:dfe_sign_in] = nil
+    OmniAuth.config.test_mode = false
+  end
+end
+
+RSpec.configure { |config| config.include(OmniAuthHelper, type: :request) }
