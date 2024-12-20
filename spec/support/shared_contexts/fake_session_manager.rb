e10ba6c9a286251d69ac6fb01232c1ddb661b418
Lorenzo Tello <ltello8a@gmail.com>
2024-12-20 03:23:42 +0000

    simplify session manager

--- spec/support/shared_contexts/fake_session_manager.rb
+++ spec/support/shared_contexts/fake_session_manager.rb
@@ -1,20 +1,18 @@
 shared_context 'fake session manager for non-DfE user' do
   let(:user) { FactoryBot.create(:user) }
-  let(:session_manager) { instance_double(Sessions::SessionManager, provider: "developer", expires_at: 2.hours.from_now) }
+  let(:session_manager) { instance_double(Sessions::SessionManager, current_user: fake_user) }
   let(:fake_user) { double(Sessions::SessionUser, email: 'admin@example.org', name: 'sample admin', dfe_user?: false) }
 
   before do
     allow(Sessions::SessionManager).to receive(:new).and_return(session_manager)
-    allow(session_manager).to receive(:load_from_session).and_return(fake_user)
   end
 end
 
 shared_context 'fake session manager for DfE user' do
-  let(:session_manager) { instance_double(Sessions::SessionManager, provider: "developer", expires_at: 2.hours.from_now) }
+  let(:session_manager) { instance_double(Sessions::SessionManager, current_user: fake_user) }
   let(:fake_user) { double(Sessions::SessionUser, email: 'admin@example.org', name: 'sample admin', dfe_user?: true) }
 
   before do
     allow(Sessions::SessionManager).to receive(:new).and_return(session_manager)
-    allow(session_manager).to receive(:load_from_session).and_return(fake_user)
   end
 end
