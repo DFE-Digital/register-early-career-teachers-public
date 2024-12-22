903aab744748780cde7de99bb60dfe695f31c979
Lorenzo Tello <ltello8a@gmail.com>
2024-12-22 11:44:23 +0000

    tidy up login on requests specs

--- spec/factories/dfe_role_factory.rb
+++ spec/factories/dfe_role_factory.rb
@@ -1,6 +1,17 @@
 FactoryBot.define do
   factory(:dfe_role) do
     user { nil }
-    role_type { "admin" }
+
+    trait :admin do
+      role_type { 'admin' }
+    end
+
+    trait :super_admin do
+      role_type { 'super_admin' }
+    end
+
+    trait :finance do
+      role_type { 'finance' }
+    end
   end
 end
