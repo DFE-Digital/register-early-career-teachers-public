527eceed7add875036e913f47915b763b7bddf4e
Peter Yates <peter.yates@graphia.co.uk>
2024-12-31 11:23:41 +0000

    Make sign_in_with_xxx methods return sessions user
    Before this change the methods returned nil which made it difficult to
    ensure events were attributed to the right author. We won't have the
    actual user session object but the attributes will be the same, so we
    can compare them:
    
      expect(AppropriateBodies::ClaimAnECT::RegisterECT).to have_received(:new).with(
        appropriate_body:,
        pending_induction_submission:,
        author: instance_of(Sessions::Users::AppropriateBodyUser)
      ) do |arguments|
        arguments.fetch(:author).tap do |author|
          expect(author.name).to eql(user.name)
          expect(author.email).to eql(user.email)
        end
      end
    
    I resisted implementing a custom `def  ==(other)` because that might
    have unintended consequences elsewhere, perhaps worth exploring.

--- spec/support/requests/auth_helper.rb
+++ spec/support/requests/auth_helper.rb
@@ -54,40 +54,52 @@ private
   def sign_in_with_appropriate_body_user(appropriate_body:, email:, first_name:, last_name:, uid:)
     Rails.logger.debug("Signing in with dfe sign in as appropriate body user")
     allow(DfESignIn::APIClient).to receive(:new).and_return(DfESignIn::FakeAPIClient.new)
-    mock_dfe_sign_in_provider!(email:,
-                               uid:,
-                               first_name:,
-                               last_name:,
-                               organisation_id: appropriate_body.dfe_sign_in_organisation_id)
+    omniauth_hash = mock_dfe_sign_in_provider!(email:,
+                                               uid:,
+                                               first_name:,
+                                               last_name:,
+                                               organisation_id: appropriate_body.dfe_sign_in_organisation_id)
+
     post("/auth/dfe/callback")
     stop_mocking_dfe_sign_in_provider!
+
+    Sessions::Users::Builder.new(omniauth_payload: omniauth_hash).session_user
   end
 
   def sign_in_with_school_user(school:, email:, first_name:, last_name:, uid:)
     Rails.logger.debug("Signing in with dfe sign in as appropriate body user")
     allow(DfESignIn::APIClient).to receive(:new).and_return(DfESignIn::FakeAPIClient.new)
-    mock_dfe_sign_in_provider!(email:,
-                               uid:,
-                               first_name:,
-                               last_name:,
-                               organisation_urn: school.urn)
+    omniauth_hash = mock_dfe_sign_in_provider!(email:,
+                                               uid:,
+                                               first_name:,
+                                               last_name:,
+                                               organisation_urn: school.urn)
+
     post("/auth/dfe/callback")
     stop_mocking_dfe_sign_in_provider!
+
+    Sessions::Users::Builder.new(omniauth_payload: omniauth_hash).session_user
   end
 
   def sign_in_with_appropriate_body_persona(appropriate_body:, email:, name:)
     Rails.logger.debug("Signing in with persona as appropriate body user")
     post("/auth/persona/callback", params: { email:, name:, appropriate_body_id: appropriate_body.id })
+
+    Sessions::Users::AppropriateBodyPersona.new(appropriate_body_id: appropriate_body.id, email:, name:)
   end
 
   def sign_in_with_dfe_persona(user:)
     Rails.logger.debug("Signing in with persona as dfe user")
     post('/auth/persona/callback', params: { email: user.email, name: user.name, dfe_staff: true })
+
+    Sessions::Users::DfEPersona.new(email: user.email)
   end
 
   def sign_in_with_school_persona(school:, name:, email:)
     Rails.logger.debug("Signing in with persona as school user")
     post("/auth/persona/callback", params: { email:, name:, school_urn: school.urn })
+
+    Sessions::Users::SchoolPersona.new(school_urn: school.urn, email:, name:)
   end
 end
 
