23c6c18fc0bd463325afd2bb8abd80d5e4f3fd81
Peter Yates <peter.yates@graphia.co.uk>
2025-01-03 14:05:11 +0000

    Make the TRS API client return ITT data by default
    Co-authored-by: priyanahar2 <priya.nahar@education.gov.uk>

--- lib/trs/api_client.rb
+++ lib/trs/api_client.rb
@@ -11,8 +11,19 @@ module TRS
       end
     end
 
-    def find_teacher(trn:, date_of_birth: nil, national_insurance_number: nil)
-      params = { dateOfBirth: date_of_birth, nationalInsuranceNumber: national_insurance_number, include: 'Induction' }.compact
+    # available include values:
+    # * Induction
+    # * InitialTeacherTraining
+    # * NpqQualifications
+    # * MandatoryQualifications
+    # * PendingDetailChanges
+    # * HigherEducationQualifications
+    # * Sanctions
+    # * Alerts
+    # * PreviousNames
+    # * AllowIdSignInWithProhibitions
+    def find_teacher(trn:, date_of_birth: nil, national_insurance_number: nil, include: %w[Induction InitialTeacherTraining])
+      params = { dateOfBirth: date_of_birth, nationalInsuranceNumber: national_insurance_number, include: include.join(",") }.compact
       response = @connection.get(persons_path(trn), params)
 
       if response.success?
