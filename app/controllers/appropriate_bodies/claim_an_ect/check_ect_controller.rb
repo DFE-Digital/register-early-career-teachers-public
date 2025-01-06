ba9e541f21ccfea5e9f750064c22ca81ea595b2a
Peter Yates <peter.yates@graphia.co.uk>
2025-01-06 17:32:08 +0000

    Move the claim ECT button to the top of check page
    Previously we had the confirm details and continue buttons at the bottom
    of the page but now we're changing the journey from just claim to find
    and claim, we want to move it up. This is because the 'check' page
    contains far more data and the button would be pushed way below the
    fold.
    
    There are no form elements on this page any more so the error summary
    has been removed too.
    
    Fixes #280, #979

--- app/controllers/appropriate_bodies/claim_an_ect/check_ect_controller.rb
+++ app/controllers/appropriate_bodies/claim_an_ect/check_ect_controller.rb
@@ -12,7 +12,7 @@ module AppropriateBodies
         check_ect = AppropriateBodies::ClaimAnECT::CheckECT
           .new(appropriate_body: @appropriate_body, pending_induction_submission: @pending_induction_submission)
 
-        if check_ect.confirm_info_correct(confirmed?)
+        if check_ect.begin_claim!
           redirect_to(edit_ab_claim_an_ect_register_path(check_ect.pending_induction_submission))
         else
           @pending_induction_submission = check_ect.pending_induction_submission
@@ -24,12 +24,6 @@ module AppropriateBodies
       end
 
     private
-
-      def confirmed?
-        confirmed_param = params.require("pending_induction_submission").fetch("confirmed")
-
-        ActiveModel::Type::Boolean.new.cast(confirmed_param)
-      end
 
       def find_pending_induction_submission
         PendingInductionSubmissions::Search.new(appropriate_body: @appropriate_body).pending_induction_submissions.find(params[:id])
--- spec/services/appropriate_bodies/claim_an_ect/check_ect_spec.rb
+++ spec/services/appropriate_bodies/claim_an_ect/check_ect_spec.rb
@@ -11,36 +11,21 @@ RSpec.describe AppropriateBodies::ClaimAnECT::CheckECT do
     end
   end
 
-  describe "#confirm_info_correct" do
+  describe "#begin_claim" do
     context "when confirmed = true (box is checked)" do
-      before { subject.confirm_info_correct(true) }
-
       it "sets the confirmed attribute to true" do
+        subject.begin_claim!
         expect(subject.pending_induction_submission.confirmed).to be(true)
       end
 
       it "sets confirmed_at timestamp to now" do
+        subject.begin_claim!
         expect(subject.pending_induction_submission.confirmed_at).to be_within(1.second).of(Time.zone.now)
       end
 
       it "results in the pending_induction_submission being valid" do
+        subject.begin_claim!
         expect(subject.pending_induction_submission).to be_valid(:check_ect)
-      end
-    end
-
-    context "when confirmed = false (box isn't checked)" do
-      before { subject.confirm_info_correct(false) }
-
-      it "sets the confirmed attribute to false" do
-        expect(subject.pending_induction_submission.confirmed).to be(false)
-      end
-
-      it "leaves the confirmed_at timestamp blank" do
-        expect(subject.pending_induction_submission.confirmed_at).to be_nil
-      end
-
-      it "results in the pending_induction_submission being invalid" do
-        expect(subject.pending_induction_submission).to be_invalid(:check_ect)
       end
     end
   end
