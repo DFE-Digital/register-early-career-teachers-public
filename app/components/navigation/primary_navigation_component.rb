0069cd578613557481a8ee9f407c719d3169965e
Peter Yates <peter.yates@graphia.co.uk>
2025-01-09 17:53:54 +0000

    Refactor and simplify the primary nav
    We don't (yet) need to know anything about the user in order to render
    the nav. We're already guarding against the wrong kind of user accessing
    the page at the controller level so it doesn't make sense to do it here
    too.
    
    Now we use the path prefix to determine the 'area' and then use that
    area value to ensure we're:
    
    * setting the right serivce_url
    * adding the right nav items
    
    When no area is matched, i.e., when the user's on the landing page,
    they'll be assigned :none and nothing is rendered.
    
    The current page behaviour is already implemented in the service nav
    component[0] so we don't need to pass current (or active) in, plus we
    can pass in the whole set of nav items as an argument so don't need to
    loop.
    
    [0] https://govuk-components.netlify.app/components/service-navigation/#input-slim-with-navigation-items

--- app/components/navigation/primary_navigation_component.rb
+++ app/components/navigation/primary_navigation_component.rb
@@ -1,68 +1,52 @@
 module Navigation
   class PrimaryNavigationComponent < ViewComponent::Base
-    attr_accessor :current_path, :current_user
+    attr_accessor :current_path, :area
 
-    def initialize(current_path:, current_user:)
+    def initialize(current_path:)
       super
-      @current_user = current_user
+
       @current_path = current_path
+      @area = case current_path
+              when %r{\A/admin}            then :admin
+              when %r{\A/appropriate-body} then :appropriate_body
+              when %r{\A/schools}          then :school
+              else :none
+              end
     end
 
     def call
-      govuk_service_navigation(service_name: "Register early career teachers", service_url: service_url) do |service_navigation|
-        navigation_items.each do |item|
-          service_navigation.with_navigation_item(
-            text: item[:text],
-            href: item[:href],
-            current: current_page?(item[:href])
-          )
-        end
-      end
+      govuk_service_navigation(service_name:, service_url:, current_path:, navigation_items:)
     end
 
   private
 
+    def service_name
+      "Register early career teachers"
+    end
+
     def service_url
-      if current_path.start_with?("/admin")
-        "/admin"
-      else
-        "/"
-      end
+      {
+        admin: '/admin',
+        appropriate_body: '/appropriate-body',
+        school: '/schools/home/ects',
+        none: '/'
+      }.fetch(area)
     end
 
     def navigation_items
-      if admin_section? && admin_access?
-        admin_navigation_items
-      else
-        school_navigation_items
-      end
-    end
-
-    def admin_navigation_items
-      [
-        { text: "Teachers", href: "/admin/teachers" },
-        { text: "Organisations", href: "/admin/organisations" },
-        { text: "Admin users", href: "#" },
-      ]
-    end
-
-    def school_navigation_items
-      [
-        { text: "Your ECTs", href: schools_ects_home_path },
-        { text: "Your mentors", href: "#" }
-      ]
-    end
-
-    def admin_section?
-      current_path.start_with?("/admin")
-    end
-
-    def admin_access?
-      Admin::Access.new(current_user).can_access?
-    end
-
-    def current_page?(path)
-      current_path == path
+      {
+        admin: [
+          { text: "Teachers", href: admin_teachers_path },
+          { text: "Organisations", href: admin_organisations_path },
+          { text: "Admin users", href: "#" },
+        ],
+        school: [
+          { text: "Your ECTs", href: schools_ects_home_path },
+          { text: "Your mentors", href: "#" }
+        ],
+        appropriate_body: [],
+        none: []
+      }.fetch(area)
     end
   end
 end
