71626393131c527b508d0b498ec2e9c9d02ff562
Toby Retallick <toby.retallick@gmail.com>
2025-01-13 10:32:25 +0000

    test: add test coverage for the FindECTStep class

--- app/services/ect_at_school_periods/search.rb
+++ app/services/ect_at_school_periods/search.rb
@@ -3,6 +3,7 @@ module ECTAtSchoolPeriods
     def exists?(school_id:, trn:)
       ECTAtSchoolPeriod.joins(:teacher)
                        .where(teachers: { trn: }, school_id:)
+                       .ongoing
                        .exists?
     end
   end
