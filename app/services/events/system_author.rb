44453c62fcc34f6ed575ef35a0291c1646780297
Peter Yates <peter.yates@graphia.co.uk>
2025-01-15 11:36:55 +0000

    Add a Events::SystemAuthor author type
    This will be useful when we run automated tasks that update data and
    want to attribute it to something other than a Sessions::User

---
+++ app/services/events/system_author.rb
@@ -0,0 +1,5 @@
+class Events::SystemAuthor
+  def system_author_params
+    { author_type: 'system' }
+  end
+end
--- spec/services/teachers/refresh_trs_attributes_spec.rb
+++ spec/services/teachers/refresh_trs_attributes_spec.rb
@@ -1,4 +1,6 @@
 describe Teachers::RefreshTRSAttributes do
+  include ActiveJob::TestHelper
+
   describe '#refresh!' do
     include_context 'fake trs api client that finds teacher that has passed their induction'
 
@@ -17,6 +19,16 @@ describe Teachers::RefreshTRSAttributes do
       expect(teacher.trs_qts_status_description).to eql('Passed')
       expect(teacher.trs_initial_teacher_training_provider_name).to eql('Example Provider Ltd.')
       expect(teacher.trs_initial_teacher_training_end_date).to eql(Date.new(2021, 4, 5))
+    end
+
+    it 'adds a teacher_name_updated_by_trs event' do
+      expect(teacher.events).to be_empty
+
+      Teachers::RefreshTRSAttributes.new(teacher).refresh!
+
+      perform_enqueued_jobs
+
+      expect(teacher.events.last.event_type).to eql('teacher_name_updated_by_trs')
     end
   end
 end
