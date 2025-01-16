10f5f077518f81a09b3930c235d34fa1da9173d5
Peter Yates <peter.yates@graphia.co.uk>
2025-01-16 11:03:56 +0000

    Add organisations to the admin path
    In the nav we're nesting schools, ABs, DPs and LPs beneath
    organisations so it makes sense for us to do it in the routes too, to
    keep things consistent.
    
    Also, there's no /admin page, we render admin/teachers here
    instead. This means the nav won't know what's actually being shown
    (without us hardcoding extra behaviour) so let's redirect instead.

--- spec/requests/admin/root_spec.rb
+++ spec/requests/admin/root_spec.rb
@@ -2,30 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Admin root", type: :request do
   describe "GET /admin" do
-    it "redirects to sign-in" do
+    it "redirects to teachers path" do
       get "/admin"
-      expect(response).to redirect_to(sign_in_path)
-    end
-
-    context "with an authenticated non-DfE user" do
-      include_context 'sign in as non-DfE user'
-
-      it "requires authorisation" do
-        get "/admin"
-        expect(response.status).to eq(401)
-      end
-    end
-
-    context "with an authenticated DfE user" do
-      include_context 'sign in as DfE user'
-
-      it "shows the teachers search page" do
-        get "/admin"
-        expect(response.status).to eq(200)
-
-        expect(response.body).to include("Early career teachers")
-        expect(response.body).to include("Search by name or TRN")
-      end
+      expect(response).to redirect_to(admin_teachers_path)
     end
   end
 end
