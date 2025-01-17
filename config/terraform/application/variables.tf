89efc77fc85af2c3f0d8228d4136460701b4d9b8
Lorenzo Tello <ltello8a@gmail.com>
2025-01-17 09:23:27 +0000

    add snapshot database in production

--- config/terraform/application/variables.tf
+++ config/terraform/application/variables.tf
@@ -93,6 +93,10 @@ variable "postgres_flexible_server_sku" {
   default = "B_Standard_B1ms"
 }
 
+variable "postgres_snapshot_flexible_server_sku" {
+  default = "GP_Standard_D2ds_v4"
+}
+
 variable "postgres_enable_high_availability" {
   type    = bool
   default = false
