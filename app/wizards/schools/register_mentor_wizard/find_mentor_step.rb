94b567dc7b665be8988521143adac9692cb4400a
Lorenzo Tello <ltello8a@gmail.com>
2025-01-30 11:00:42 +0000

    add new step to the register-mentor journey to prevent ects to mentor themselves.

--- app/wizards/schools/register_mentor_wizard/find_mentor_step.rb
+++ app/wizards/schools/register_mentor_wizard/find_mentor_step.rb
@@ -12,6 +12,7 @@ module Schools
 
       def next_step
         return :trn_not_found unless mentor.in_trs?
+        return :cannot_mentor_themself if mentor.trn == ect.trn
         return :national_insurance_number unless mentor.matches_trs_dob?
         return :already_active_at_school if mentor.active_at_school?
         return :cannot_register_mentor if trs_teacher.prohibited_from_teaching?
@@ -22,7 +23,7 @@ module Schools
     private
 
       def persist
-        mentor.update(trn:,
+        mentor.update(trn: formatted_trn,
                       date_of_birth: date_of_birth.values.join("-"),
                       trs_date_of_birth: trs_teacher.date_of_birth,
                       trs_first_name: trs_teacher.first_name,
@@ -30,7 +31,11 @@ module Schools
       end
 
       def trs_teacher
-        @trs_teacher ||= fetch_trs_teacher(trn:)
+        @trs_teacher ||= fetch_trs_teacher(trn: formatted_trn)
+      end
+
+      def formatted_trn
+        @formatted_trn ||= Validation::TeacherReferenceNumber.new(trn).formatted_trn
       end
     end
   end
