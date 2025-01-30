e04e040ba009d4a069c04d80a5996e3a8906fbeb
Peter Yates <peter.yates@graphia.co.uk>
2025-01-30 13:54:23 +0000

    Add basic rack-attack config
    We're just throttling the /otp-sign-in and /sign-in routes for now with
    5 requests per IP every 20 seconds.

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,24 @@
+class Rack::Attack
+  PROTECTED_ROUTES = %w[
+    /otp-sign-in
+    /otp-sign-in/code
+    /otp-sign-in/verify
+    /sign-in
+    /auth
+  ].freeze
+
+  def self.protected_path?(request)
+    PROTECTED_ROUTES.any? { |route| request.path.start_with?(route) }
+  end
+
+  throttle("protected routes (OTP)", limit: 5, period: 20.seconds) do |request|
+    request.ip if protected_path?(request)
+  end
+end
+
+ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |_name, _start, _finish, request_id, payload|
+  ip = payload.fetch(:request).ip
+  path = payload.fetch(:request).fullpath
+
+  Rails.logger.warn("[rack-attack] Throttled request #{request_id} from #{ip} to '#{path}'")
+end
