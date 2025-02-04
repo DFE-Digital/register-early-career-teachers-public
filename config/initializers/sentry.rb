4f1bd4e801c5d11b112a277148b3d396fe02b90f
edujackedu <jack.scotti@education.gov.uk>
2025-02-04 13:52:58 +0000

    DELETE

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-if true
-  Sentry.init do |config|
-    config.dsn = Rails.application.config.sentry_dsn
-    config.breadcrumbs_logger = %i[active_support_logger http_logger]
-    config.release = ENV["COMMIT_SHA"]
+Sentry.init do |config|
+  config.dsn = Rails.application.config.sentry_dsn
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.release = ENV["COMMIT_SHA"]
 
-    config.enable_tracing = true
-    config.traces_sample_rate = 0.1
-    config.profiles_sample_rate = 0.1
-  end
+  config.enable_tracing = true
+  config.traces_sample_rate = 0.1
+  config.profiles_sample_rate = 0.1
 end
