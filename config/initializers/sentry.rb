3896653f4aeadf28b3fd225a478d28857823956f
edujackedu <jack.scotti@education.gov.uk>
2025-02-04 13:22:46 +0000

    REMOVE COMMIT

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.application.config.enable_sentry
+if true
   Sentry.init do |config|
     config.dsn = Rails.application.config.sentry_dsn
     config.breadcrumbs_logger = %i[active_support_logger http_logger]
