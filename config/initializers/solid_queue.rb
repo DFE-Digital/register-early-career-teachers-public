172c31a27b96186e01011b463ce289d84863f52b
edujackedu <jack.scotti@education.gov.uk>
2025-02-04 14:40:00 +0000

    wip

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,17 +1,17 @@
-if Rails.env.production?
-  module SupervisorSafety
-    def supervisees
-      return [] if supervisor.nil?
+# if Rails.env.production?
+#   module SupervisorSafety
+#     def supervisees
+#       return [] if supervisor.nil?
 
-      super
-    end
-  end
+#       super
+#     end
+#   end
 
-  module SolidQueue
-    class Process
-      class << self
-        prepend SupervisorSafety
-      end
-    end
-  end
-end
+#   module SolidQueue
+#     class Process
+#       class << self
+#         prepend SupervisorSafety
+#       end
+#     end
+#   end
+# end
