0a5661a1f977df370e46b4c904f25bad5155fde3
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-02-05 08:51:38 +0000

    Remove PendingInductionSubmission trs_induction_status character limit

--- db/migrate/20250131171406_increase_induction_status_limit.rb
+++
@@ -1,9 +0,0 @@
-class IncreaseInductionStatusLimit < ActiveRecord::Migration[8.0]
-  def up
-    change_column :pending_induction_submissions, :trs_induction_status, :string, limit: 18
-  end
-
-  def down
-    change_column :pending_induction_submissions, :trs_induction_status, :string, limit: 16
-  end
-end
