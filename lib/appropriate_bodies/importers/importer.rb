c0643ff4299ee1e7931a8197698455a1e0427c9b
Peter Yates <peter.yates@graphia.co.uk>
2025-02-07 14:29:32 +0000

    Move the actual importing to the import! method

--- lib/appropriate_bodies/importers/importer.rb
+++ lib/appropriate_bodies/importers/importer.rb
@@ -1,26 +1,24 @@
 module AppropriateBodies::Importers
   class Importer
-    # rubocop:disable Rails/Output
     def initialize(appropriate_body_csv, teachers_csv, induction_period_csv)
-      induction_periods_grouped_by_trn = InductionPeriodImporter.new(induction_period_csv).periods_by_trn
+      @induction_periods_grouped_by_trn = InductionPeriodImporter.new(induction_period_csv).periods_by_trn
 
-      active_teachers = induction_periods_grouped_by_trn.keys
-      teacher_importer_rows = TeacherImporter.new(teachers_csv, active_teachers).rows
+      @active_teachers = @induction_periods_grouped_by_trn.keys
+      @teacher_importer_rows = TeacherImporter.new(teachers_csv, @active_teachers).rows
 
-      active_abs = induction_periods_grouped_by_trn.flat_map { |_trn, ips| ips.map(&:legacy_appropriate_body_id) }.uniq
-      ab_importer_rows = AppropriateBodyImporter.new(appropriate_body_csv, active_abs).rows
+      @active_abs = @induction_periods_grouped_by_trn.flat_map { |_trn, ips| ips.map(&:legacy_appropriate_body_id) }.uniq
+      @ab_importer_rows = AppropriateBodyImporter.new(appropriate_body_csv, @active_abs).rows
+    end
 
-      puts "Active appropriate bodies: #{active_abs.count}"
+    def import!
+      Rails.logger.info("Active appropriate bodies: #{@active_abs.count}")
+      AppropriateBody.insert_all(@ab_importer_rows.select { |r| r.legacy_id.in?(@active_abs) }.map(&:to_h))
+      Rails.logger.info("Appropriate bodies inserted: #{AppropriateBody.count}")
 
-      AppropriateBody.insert_all(ab_importer_rows.select { |r| r.legacy_id.in?(active_abs) }.map(&:to_h))
-      puts "Appropriate bodies inserted: #{AppropriateBody.count}"
+      Rails.logger.info("Active Teachers: #{@teacher_importer_rows.count}")
+      Teacher.insert_all(@teacher_importer_rows.map(&:to_h))
+      Rails.logger.info("Teachers inserted: #{Teacher.count}")
 
-      puts "Active Teachers: #{teacher_importer_rows.count}"
-
-      Teacher.insert_all(teacher_importer_rows.map(&:to_h))
-      puts "Teachers inserted: #{Teacher.count}"
-
-      # TODO: insert induction periods
       teacher_trn_to_id = Teacher.all.select(:id, :trn).each_with_object({}) do |t, h|
         h[t[:trn]] = t[:id]
       end
@@ -31,7 +29,7 @@ module AppropriateBodies::Importers
 
       induction_period_rows = []
 
-      induction_periods_grouped_by_trn.each do |trn, induction_periods|
+      @induction_periods_grouped_by_trn.each do |trn, induction_periods|
         induction_periods.each do |ip|
           begin
             ip.teacher_id = teacher_trn_to_id.fetch(trn)
@@ -53,7 +51,7 @@ module AppropriateBodies::Importers
 
       InductionPeriod.insert_all(induction_period_rows.map(&:to_record))
 
-      induction_extensions = teacher_importer_rows.select { |tir| tir.extension_terms.present? }.map do |row|
+      induction_extensions = @teacher_importer_rows.select { |tir| tir.extension_terms.present? }.map do |row|
         {
           teacher_id: teacher_trn_to_id.fetch(row.trn),
           number_of_terms: row.extension_terms
@@ -61,10 +59,9 @@ module AppropriateBodies::Importers
       end
 
       InductionExtension.insert_all(induction_extensions)
-      puts "Induction extensions inserted: #{InductionExtension.count}"
+      Rails.logger.info("Induction extensions inserted: #{InductionExtension.count}")
 
       # TODO: insert events
     end
-    # rubocop:enable Rails/Output
   end
 end
