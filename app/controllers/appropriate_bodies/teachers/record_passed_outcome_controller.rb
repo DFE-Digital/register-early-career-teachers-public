a78807932d60967fd9988f41d30937f52aff5e1a
Peter Yates <peter.yates@graphia.co.uk>
2025-02-12 12:38:30 +0000

    Make pass induction conf page find teacher record

--- app/controllers/appropriate_bodies/teachers/record_passed_outcome_controller.rb
+++ app/controllers/appropriate_bodies/teachers/record_passed_outcome_controller.rb
@@ -2,13 +2,13 @@ module AppropriateBodies
   module Teachers
     class RecordPassedOutcomeController < AppropriateBodiesController
       def new
-        @teacher = find_teacher
+        @teacher = find_current_teacher
 
         @pending_induction_submission = PendingInductionSubmission.new
       end
 
       def create
-        @teacher = find_teacher
+        @teacher = find_current_teacher
         @pending_induction_submission = PendingInductionSubmissions::Build.closing_induction_period(
           ::Teachers::InductionPeriod.new(@teacher).active_induction_period,
           **pending_induction_submission_params,
@@ -26,7 +26,6 @@ module AppropriateBodies
           if @pending_induction_submission.save(context: :record_outcome) && record_outcome.pass!
             redirect_to(
               ab_teacher_record_passed_outcome_path(@teacher),
-              flash: { teacher_name: ::Teachers::Name.new(@teacher).full_name }
             )
           else
             render :new
@@ -35,7 +34,7 @@ module AppropriateBodies
       end
 
       def show
-        @teacher_name = flash['teacher_name']
+        @teacher = find_former_teacher
       end
 
     private
@@ -48,8 +47,12 @@ module AppropriateBodies
         { appropriate_body_id: @appropriate_body.id, trn: @teacher.trn, outcome: "pass" }
       end
 
-      def find_teacher
+      def find_current_teacher
         AppropriateBodies::ECTs.new(@appropriate_body).current.find_by!(id: params[:teacher_id])
+      end
+
+      def find_former_teacher
+        AppropriateBodies::ECTs.new(@appropriate_body).former.find_by!(id: params[:teacher_id])
       end
     end
   end
