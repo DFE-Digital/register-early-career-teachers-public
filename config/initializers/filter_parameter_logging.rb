62c416b78928d46827cb851259937dc1c85e5f34
Peter Yates <peter.yates@graphia.co.uk>
2025-02-14 15:17:47 +0000

    Add more fields to the filter params list
    Co-authored-by: tonyheadford <tony@binarycircus.com>
    Co-authored-by: leoapost <leoapost@gmail.com>
    Co-authored-by: peterdavidhamilton  <pete@peterdavidhamilton.com>

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -5,15 +5,22 @@
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += %i[
   _key
+  author_email
+  author_name
   certificate
   commit
+  corrected_name
   crypt
+  date_of_birth
   email
+  emails
   email_address
   first_name
   last_name
   name
+  national_insurance_number
   otp
+  otp_secret
   passw
   q
   salt
