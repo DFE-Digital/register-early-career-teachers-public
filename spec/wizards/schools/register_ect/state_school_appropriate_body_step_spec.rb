427a42f8574cd64789bda6506a84c45aa6274c11
Leonidas Apostolidis <leoapost@gmail.com>
2025-02-14 01:22:53 +0000

    Fix navigation in the register ect journey

--- spec/wizards/schools/register_ect/state_school_appropriate_body_step_spec.rb
+++ spec/wizards/schools/register_ect/state_school_appropriate_body_step_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Schools::RegisterECTWizard::StateSchoolAppropriateBodyStep, type:
     subject { wizard.current_step }
 
     it 'returns the previous step' do
-      expect(subject.previous_step).to eq(:start_date)
+      expect(subject.previous_step).to eq(:working_pattern)
     end
   end
 
