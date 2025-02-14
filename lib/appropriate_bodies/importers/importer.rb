7dff5a3dd7b1d4f5dd9372f11a9d673adb537b34
Peter Yates <peter.yates@graphia.co.uk>
2025-02-14 22:54:17 +0000

    Add cutoff date for old ABs
    For the list of appropriate body IDs provided in the cutoff CSV file we
    want to prevent their induction periods going beyond the date their
    appropriate body status ended (2024-08-31).
    
    There's one exception that started after the cutoff date, so we adjust
    it so it's 1 day long.
    
    Co-authored-by: joe-harrison-dfe <joe.harrison@education.gov.uk>

--- lib/appropriate_bodies/importers/importer.rb
+++ lib/appropriate_bodies/importers/importer.rb
@@ -1,7 +1,7 @@
 module AppropriateBodies::Importers
   class Importer
-    def initialize(appropriate_body_csv, teachers_csv, induction_period_csv, dfe_sign_in_mapping_csv, admin_csv)
-      @induction_periods_grouped_by_trn = InductionPeriodImporter.new(induction_period_csv).periods_by_trn
+    def initialize(appropriate_body_csv:, teachers_csv:, induction_period_csv:, dfe_sign_in_mapping_csv:, admin_csv:, cutoff_csv:)
+      @induction_periods_grouped_by_trn = InductionPeriodImporter.new(induction_period_csv, cutoff_csv).periods_by_trn
 
       @active_teachers = @induction_periods_grouped_by_trn.keys
       @teacher_importer_rows = TeacherImporter.new(teachers_csv, @active_teachers).rows
