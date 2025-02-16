7ce2a3aa26bd3dade4f72ba75e627740b57a7097
Lorenzo Tello <ltello8a@gmail.com>
2025-02-16 17:47:07 +0000

    extract superclass for the different validation dates in the journeys

--- app/validators/ect_start_date_validator.rb
+++ app/validators/ect_start_date_validator.rb
@@ -1,12 +1,12 @@
 class ECTStartDateValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     start_date = Schools::Validation::ECTStartDate.new(
-      ect_start_date_as_hash: value,
+      date_as_hash: value,
       current_date: options[:current_date]
     )
 
     unless start_date.valid?
-      record.errors.add(attribute, start_date.format_error)
+      record.errors.add(attribute, start_date.error_message)
     end
   end
 end
