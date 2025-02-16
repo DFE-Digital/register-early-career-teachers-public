b16c00269dccf61c7a11727fcc36aaf6dd74c165
Lorenzo Tello <ltello8a@gmail.com>
2025-02-16 17:47:07 +0000

    extract superclass for the different validation dates in the journeys

--- lib/schools/validation/teacher_reference_number.rb
+++ lib/schools/validation/teacher_reference_number.rb
@@ -4,11 +4,11 @@ module Schools
       MIN_UNPADDED_TRN_LENGTH = 5
       PADDED_TRN_LENGTH = 7
 
-      attr_reader :trn, :format_error
+      attr_reader :trn, :error_message
 
       def initialize(trn)
         @trn = trn
-        @format_error = nil
+        @error_message = nil
       end
 
       def formatted_trn
@@ -22,14 +22,14 @@ module Schools
     private
 
       def extract_trn_value
-        @format_error = "Enter the teacher reference number (TRN)" and return if trn.blank?
+        @error_message = "Enter the teacher reference number (TRN)" and return if trn.blank?
 
         # remove any characters that are not digits
         only_digits = trn.to_s.gsub(/[^\d]/, "")
 
-        @format_error = "Teacher reference number must include at least 5 digits" and return if only_digits.blank?
-        @format_error = "Teacher reference number must include at least 5 digits" and return if only_digits.length < MIN_UNPADDED_TRN_LENGTH
-        @format_error = "Teacher reference number cannot include more than 7 digits" and return if only_digits.length > PADDED_TRN_LENGTH
+        @error_message = "Teacher reference number must include at least 5 digits" and return if only_digits.blank?
+        @error_message = "Teacher reference number must include at least 5 digits" and return if only_digits.length < MIN_UNPADDED_TRN_LENGTH
+        @error_message = "Teacher reference number cannot include more than 7 digits" and return if only_digits.length > PADDED_TRN_LENGTH
 
         only_digits.rjust(PADDED_TRN_LENGTH, "0")
       end
