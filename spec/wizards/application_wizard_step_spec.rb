c26ef84905b4eb703e537bbd96b2c516f4cd2c5a
Lorenzo Tello <ltello8a@gmail.com>
2025-02-17 21:44:12 +0000

    add specs

---
+++ spec/wizards/application_wizard_step_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe ApplicationWizardStep, type: :model do
+  let(:permitted_params) { %i[param1 param2] }
+
+  before do
+    described_class.class_exec(permitted_params) { |permitted_params| attr_accessor(*permitted_params) }
+    allow(described_class).to receive(:permitted_params).and_return(permitted_params)
+  end
+
+  let(:instance) { described_class.new(params) }
+
+  describe '#initialize' do
+    context 'when permitted params are provided' do
+      let(:params) { { param1: 1 } }
+
+      before do
+        allow(instance).to receive(:pre_populate_attributes).and_call_original
+      end
+
+      it 'does not call pre_populate_attributes' do
+        expect(instance).not_to have_received(:pre_populate_attributes)
+      end
+    end
+
+    context 'when no permitted params are provided' do
+      let(:params) { {} }
+
+      before do
+        allow_any_instance_of(described_class).to receive(:pre_populate_attributes).and_return(nil)
+      end
+
+      it 'calls pre_populate_attributes' do
+        expect(instance).to have_received(:pre_populate_attributes)
+      end
+    end
+  end
+
+  describe '#pre_populate_attributes' do
+    let(:params) { { param1: 1 } }
+
+    it 'raises NotImplementedError' do
+      expect { instance.send(:pre_populate_attributes) }.to raise_error(NotImplementedError)
+    end
+  end
+end
--- spec/wizards/schools/register_mentor_wizard/national_insurance_number_step_spec.rb
+++ spec/wizards/schools/register_mentor_wizard/national_insurance_number_step_spec.rb
@@ -1,6 +1,37 @@
 describe Schools::RegisterMentorWizard::NationalInsuranceNumberStep, type: :model do
-  let(:wizard) { FactoryBot.build(:register_mentor_wizard, current_step: :national_insurance_number) }
+  let(:store) do
+    FactoryBot.build(:session_repository,
+                     trn: '1234567',
+                     trs_first_name: 'John',
+                     trs_last_name: 'Wayne',
+                     corrected_name: 'Jim Wayne',
+                     date_of_birth: '01/01/1990',
+                     email: 'initial@email.com',
+                     national_insurance_number: 'Ab123456A')
+  end
+  let(:wizard) { FactoryBot.build(:register_mentor_wizard, current_step: :national_insurance_number, store:) }
   subject { described_class.new(wizard:) }
+
+  describe '#initialisation' do
+    let(:national_insurance_number) { 'ZZ123456A' }
+    subject { described_class.new(wizard:, **params) }
+
+    context 'when the national insurance number is provided' do
+      let(:params) { { national_insurance_number: } }
+
+      it 'populate the instance from it' do
+        expect(subject.national_insurance_number).to eq(national_insurance_number)
+      end
+    end
+
+    context 'when no national_insurance_number is provided' do
+      let(:params) { {} }
+
+      it 'populate it from the wizard store' do
+        expect(subject.national_insurance_number).to eq('Ab123456A')
+      end
+    end
+  end
 
   describe 'validations' do
     ['Ab123456A',
@@ -58,6 +89,52 @@ describe Schools::RegisterMentorWizard::NationalInsuranceNumberStep, type: :mode
 
       it 'returns :already_active_at_school' do
         expect(subject.next_step).to eq(:already_active_at_school)
+      end
+    end
+
+    context 'when the mentor is already active at the school' do
+      let(:teacher) { FactoryBot.create(:teacher, trn: '1234568') }
+      let(:active_mentor_period) { FactoryBot.create(:mentor_at_school_period, :active, teacher:) }
+
+      before do
+        wizard.store.update!(trn: '1234568', school_urn: active_mentor_period.school.urn)
+        allow(::TRS::APIClient).to receive(:new).and_return(TRS::FakeAPIClient.new)
+        subject.save!
+      end
+
+      it 'returns :already_active_at_school' do
+        expect(subject.next_step).to eq(:already_active_at_school)
+      end
+    end
+
+    context 'when the mentor is prohibited from teaching' do
+      let(:teacher) { create(:teacher, trn: '1234568') }
+      before do
+        fake_client = TRS::FakeAPIClient.new
+
+        allow(fake_client).to receive(:find_teacher).and_return(
+          TRS::Teacher.new(
+            'trn' => '1234568',
+            'firstName' => 'Jane',
+            'lastName' => 'Smith',
+            'dateOfBirth' => '1977-02-03',
+            'alerts' => [
+              {
+                'alertType' => {
+                  'alertCategory' => {
+                    'alertCategoryId' => TRS::Teacher::PROHIBITED_FROM_TEACHING_CATEGORY_ID
+                  }
+                }
+              }
+            ]
+          )
+        )
+        allow(::TRS::APIClient).to receive(:new).and_return(fake_client)
+        subject.save!
+      end
+
+      it 'returns :cannot_register_mentor' do
+        expect(subject.next_step).to eq(:cannot_register_mentor)
       end
     end
 
