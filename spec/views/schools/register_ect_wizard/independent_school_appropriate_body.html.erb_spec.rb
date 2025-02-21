8e584b748d8f9c8c59dbd3e5bfd7f4cd5f55c0c2
Leonidas Apostolidis <leoapost@gmail.com>
2025-02-21 15:41:52 +0000

    Fix the previous step of Schools::RegisterECTWizard::IndependentSchoolAppropriateBodyStep

--- spec/views/schools/register_ect_wizard/independent_school_appropriate_body.html.erb_spec.rb
+++ spec/views/schools/register_ect_wizard/independent_school_appropriate_body.html.erb_spec.rb
@@ -3,8 +3,8 @@ require_relative './shared_examples/independent_school_appropriate_body_view'
 RSpec.describe "schools/register_ect_wizard/independent_school_appropriate_body.html.erb" do
   it_behaves_like 'an independent school appropriate body view',
                   current_step: :independent_school_appropriate_body,
-                  back_path: :schools_register_ect_wizard_start_date_path,
-                  back_step_name: 'start date',
+                  back_path: :schools_register_ect_wizard_working_pattern_path,
+                  back_step_name: 'working pattern',
                   continue_path: :schools_register_ect_wizard_independent_school_appropriate_body_path,
                   continue_step_name: 'independent school appropriate body'
 end
