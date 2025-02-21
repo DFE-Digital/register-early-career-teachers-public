415939702b2534ef82406f2d4f97bb9da79f4297
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-02-21 15:54:34 +0000

    Change pagination limit to 20

--- app/controllers/schools/mentors_controller.rb
+++ app/controllers/schools/mentors_controller.rb
@@ -2,9 +2,8 @@ module Schools
   class MentorsController < SchoolsController
     layout "full"
 
-    # TODO: increase the pagination limit to 20
     def index
-      @pagy, @mentors = pagy(Schools::Home.new(school:).mentors_with_ects, limit: 2)
+      @pagy, @mentors = pagy(Schools::Home.new(school:).mentors_with_ects, limit: 20)
     end
   end
 end
