004a00fa609689f1bdebf2cf9006719325c62454
Peter Yates <peter.yates@graphia.co.uk>
2025-02-28 11:02:28 +0000

    wip - rework subcomponents

--- app/components/teachers/details_component.rb
+++ app/components/teachers/details_component.rb
@@ -1,14 +1,30 @@
 module Teachers
   class DetailsComponent < ViewComponent::Base
-    renders_one :personal_details
-    renders_one :itt_details
-    renders_one :induction_summary
-    renders_one :current_induction_period
-    renders_one :past_induction_periods
+    renders_one :personal_details, -> do
+      Teachers::Details::PersonalDetailsComponent.new(teacher:)
+    end
+
+    renders_one :itt_details, -> do
+      Teachers::Details::ITTDetailsComponent.new(teacher:)
+    end
+
+    renders_one :induction_summary, -> do
+      Teachers::Details::InductionSummaryComponent.new(teacher:)
+    end
+
+    renders_one :current_induction_period, -> do
+      Teachers::Details::CurrentInductionPeriodComponent.new(teacher:)
+    end
+
+    renders_one :past_induction_periods, -> do
+      Teachers::Details::PastInductionPeriodsComponent.new(teacher:)
+    end
 
     attr_reader :teacher
 
-    def initialize(teacher:)
+    def initialize(mode:, teacher:)
+      fail unless mode.in?(%i[admin appropriate_body school])
+
       @teacher = teacher
     end
   end
