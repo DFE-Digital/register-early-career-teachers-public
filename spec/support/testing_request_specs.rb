5967639656133baa38e0dbf8cb79881bf89de665
edujackedu <jack.scotti@education.gov.uk>
2025-02-28 10:10:47 +0000

    Add separate settings file for request specs
    So that they can have their own timeout default

---
+++ spec/support/testing_request_specs.rb
@@ -0,0 +1,16 @@
+require 'timeout'
+
+RSpec.configure do |config|
+  # Set a timeout for request specs
+  config.add_setting :request_timeout, default: 3
+
+  # Set a timeout for each request spec
+  config.around(:each, type: :request) do |example|
+    Timeout.timeout(config.request_timeout) do
+      example.run
+    end
+  rescue Timeout::Error
+    example.example.set_exception(Timeout::Error.new("Example timed out after #{config.request_timeout} seconds"))
+    raise
+  end
+end
