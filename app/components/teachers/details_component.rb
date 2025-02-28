4561d2496b48849efde9eceeb85fe581743270b8
edujackedu <jack.scotti@education.gov.uk>
2025-02-28 14:24:07 +0000

    Fix specs

--- app/components/teachers/details_component.rb
+++ app/components/teachers/details_component.rb
@@ -1,31 +1,42 @@
 module Teachers
   class DetailsComponent < ViewComponent::Base
-    renders_one :personal_details, -> do
+    renders_one :personal_details, lambda {
       Teachers::Details::PersonalDetailsComponent.new(teacher:)
-    end
+    }
 
-    renders_one :itt_details, -> do
+    renders_one :itt_details, lambda {
       Teachers::Details::ITTDetailsComponent.new(teacher:)
-    end
+    }
 
-    renders_one :induction_summary, -> do
+    renders_one :induction_summary, lambda {
       Teachers::Details::InductionSummaryComponent.new(teacher:)
-    end
+    }
 
-    renders_one :current_induction_period, -> do
-      Teachers::Details::CurrentInductionPeriodComponent.new(teacher:)
-    end
+    renders_one :current_induction_period, lambda {
+      # Set enable_release based on mode and context
+      # Only appropriate bodies can release ECTs, and only in the teacher show context
+      enable_release = mode == :appropriate_body && context == :teacher_show
+      enable_edit = mode == :admin
 
-    renders_one :past_induction_periods, -> do
+      Teachers::Details::CurrentInductionPeriodComponent.new(
+        teacher:,
+        enable_release:,
+        enable_edit:
+      )
+    }
+
+    renders_one :past_induction_periods, lambda {
       Teachers::Details::PastInductionPeriodsComponent.new(teacher:)
-    end
+    }
 
-    attr_reader :teacher
+    attr_reader :teacher, :mode, :context
 
-    def initialize(mode:, teacher:)
+    def initialize(mode:, teacher:, context: :default)
       fail unless mode.in?(%i[admin appropriate_body school])
 
       @teacher = teacher
+      @mode = mode
+      @context = context
     end
   end
 end
