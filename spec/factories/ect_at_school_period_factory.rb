627502b5d2a900f2eb94f7979d63f3c707757e7a
Leonidas Apostolidis <leoapost@gmail.com>
2025-02-28 14:55:59 +0000

    Avoid overlaping when creating school period factories

--- spec/factories/ect_at_school_period_factory.rb
+++ spec/factories/ect_at_school_period_factory.rb
@@ -10,12 +10,8 @@ FactoryBot.define do
     email { Faker::Internet.email }
 
     trait :active do
+      started_on { generate(:base_ect_date) + 1.year }
       finished_on { nil }
-    end
-
-    trait :finished do
-      started_on { generate(:base_ect_date) - 10.days }
-      finished_on { started_on + 1.day }
     end
   end
 end
--- spec/services/schools/teacher_email_spec.rb
+++ spec/services/schools/teacher_email_spec.rb
@@ -37,7 +37,7 @@ describe Schools::TeacherEmail do
     context "when the email has been used by the same teacher in the past" do
       let(:teacher) { ongoing_ect_at_school_period.teacher }
       let(:trn) { teacher.trn }
-      let(:email) { FactoryBot.create(:ect_at_school_period, :finished, teacher:).email }
+      let(:email) { FactoryBot.create(:ect_at_school_period, teacher:).email }
 
       it "returns false" do
         expect(subject.is_currently_used?).to be false
