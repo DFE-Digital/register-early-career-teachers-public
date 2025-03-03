526194adb223145946bbedada468a4ce511d25fe
Lorenzo Tello <ltello8a@gmail.com>
2025-03-03 19:23:20 +0000

    add fields to store previous ECT programme choices

--- app/models/ect_at_school_period.rb
+++ app/models/ect_at_school_period.rb
@@ -1,6 +1,19 @@
 class ECTAtSchoolPeriod < ApplicationRecord
   include Interval
 
+  # Enums
+  enum :appropriate_body_type,
+       { teaching_induction_panel: "teaching_induction_panel",
+         teaching_school_hub: "teaching_school_hub" },
+       validate: { allow_nil: true },
+       suffix: :ab_type
+
+  enum :programme_type,
+       { provider_led: "provider_led",
+         school_led: "school_led" },
+       validate: true,
+       suffix: :programme_type
+
   # Associations
   belongs_to :school, inverse_of: :ect_at_school_periods
   belongs_to :teacher, inverse_of: :ect_at_school_periods
@@ -14,10 +27,55 @@ class ECTAtSchoolPeriod < ApplicationRecord
   has_many :events
 
   # Validations
-  validates :email, notify_email: true, allow_nil: true
-  validates :started_on, presence: true
-  validates :school_id, presence: true
-  validates :teacher_id, presence: true
+  validates :appropriate_body_id,
+            presence: {
+              message: "Must contain the id of an AppropriateBody",
+              if: -> { teaching_school_hub_ab_type? }
+            },
+            absence: {
+              message: "Must be nil",
+              unless: -> { teaching_school_hub_ab_type? }
+            }
+
+  validates :appropriate_body_type,
+            inclusion: {
+              in: ECTAtSchoolPeriod.appropriate_body_types.keys,
+              message: "Must be nil or #{ECTAtSchoolPeriod.appropriate_body_types.keys.join(' or ')}",
+              allow_nil: true
+            },
+            presence: {
+              message: "Must be 'teaching_school_hub'",
+              if: -> { appropriate_body_id }
+            }
+
+  validates :email,
+            notify_email: true,
+            allow_nil: true
+
+  validates :lead_provider_id,
+            presence: {
+              message: "Must contain the id of a LeadProvider",
+              if: -> { provider_led_programme_type? }
+            },
+            absence: {
+              message: "Must be nil",
+              unless: -> { provider_led_programme_type? }
+            }
+
+  validates :programme_type,
+            inclusion: {
+              in: School.chosen_programme_types.keys,
+              message: "Must be #{School.chosen_programme_types.keys.join(' or ')}"
+            }
+
+  validates :school_id,
+            presence: true
+
+  validates :started_on,
+            presence: true
+
+  validates :teacher_id,
+            presence: true
 
   validate :teacher_distinct_period
 
