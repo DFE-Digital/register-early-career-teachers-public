c36fa2135ec78322a0527cda0e1f10885a712e9d
Lorenzo Tello <ltello8a@gmail.com>
2025-03-03 19:35:22 +0000

    add new journey steps

---
+++ spec/wizards/schools/register_ect_wizard/use_previous_ect_choices_step_spec.rb
@@ -0,0 +1,74 @@
+RSpec.describe Schools::RegisterECTWizard::UsePreviousECTChoicesStep, type: :model do
+  let(:use_previous_ect_choices) { true }
+  let(:step_params) { {} }
+  let(:school) { FactoryBot.build(:school) }
+  let(:store) { FactoryBot.build(:session_repository, use_previous_ect_choices:) }
+  let(:wizard) { FactoryBot.build(:register_ect_wizard, current_step: :use_previous_ect_choices, school:, store:, step_params:) }
+  subject { wizard.current_step }
+
+  describe '#initialize' do
+    subject { described_class.new(wizard:, **params) }
+
+    context 'when use_previous_ect_choices is provided' do
+      let(:params) { { use_previous_ect_choices: false } }
+
+      it 'populate the instance from it' do
+        expect(subject.use_previous_ect_choices).to be_falsey
+      end
+    end
+
+    context 'when no use_previous_ect_choices is provided' do
+      let(:params) { {} }
+
+      it 'populate it from the wizard store' do
+        expect(subject.use_previous_ect_choices).to be_truthy
+      end
+    end
+  end
+
+  describe '#next_step' do
+    context 'when use_previous_ect_choices is true' do
+      let(:use_previous_ect_choices) { true }
+
+      it { expect(subject.next_step).to eq(:check_answers) }
+    end
+
+    context 'when use_previous_ect_choices is false' do
+      let(:use_previous_ect_choices) { false }
+
+      context 'for independent schools' do
+        before do
+          allow(school).to receive(:independent?).and_return(true)
+        end
+
+        it { expect(subject.next_step).to eq(:independent_school_appropriate_body) }
+      end
+
+      context 'for state-funded schools' do
+        before do
+          allow(school).to receive(:independent?).and_return(false)
+        end
+
+        it { expect(subject.next_step).to eq(:state_school_appropriate_body) }
+      end
+    end
+  end
+
+  describe '#previous_step' do
+    it { expect(subject.previous_step).to eq(:working_pattern) }
+  end
+
+  context '#save!' do
+    let(:step_params) do
+      ActionController::Parameters.new(
+        "use_previous_ect_choices" => {
+          "use_previous_ect_choices" => '0',
+        }
+      )
+    end
+
+    it 'updates the wizard ect use_previous_ect_choices' do
+      expect { subject.save! }.to change(subject.ect, :use_previous_ect_choices).from(true).to(false)
+    end
+  end
+end
