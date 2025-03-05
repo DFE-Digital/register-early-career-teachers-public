f00d314cb3f31f71ea60cd954729335952052ce2
Peter Yates <peter.yates@graphia.co.uk>
2025-03-05 12:50:33 +0000

    Add time formats specified by GOV.UK styleguide
    We already had existing date formats from the styleguide[0] but were
    missing the time ones.
    
    They work like this:
    
    register-early-career-teachers(dev)> 5.hours.ago.to_fs(:govuk)
    => "5 March 2025, 7:57am"
    register-early-career-teachers(dev)> 5.hours.ago.to_fs(:govuk_short)
    => "5 Mar 2025, 7:57am"
    
    See the relevant sections for more info
    
    https://www.gov.uk/guidance/style-guide/a-to-z#dates
    https://www.gov.uk/guidance/style-guide/a-to-z#times

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,4 +1,8 @@
-# https://www.gov.uk/guidance/style-guide/a-to-z-of-gov-uk-style#dates
+# https://www.gov.uk/guidance/style-guide/a-to-z#dates
 Date::DATE_FORMATS[:govuk]        = "%-d %B %Y" # 2 January 1998
 Date::DATE_FORMATS[:govuk_short]  = "%-d %b %Y" # 2 Jan 1998
 Date::DATE_FORMATS[:govuk_approx] = "%B %Y"     # January 1998
+
+# https://www.gov.uk/guidance/style-guide/a-to-z#times
+Time::DATE_FORMATS[:govuk] = %(#{Date::DATE_FORMATS[:govuk]}, %-l:%M%P)             # 2 January 1998, 5:30pm
+Time::DATE_FORMATS[:govuk_short] = %(#{Date::DATE_FORMATS[:govuk_short]}, %-l:%M%P) # 2 Jan 1998, 5:30pm
