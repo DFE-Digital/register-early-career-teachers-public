138738ddf2c4418687a339c95202ee31fd3839e9
Avin Hurry <avinhurry@icloud.com>
2025-03-12 16:02:58 +0000

    Add spec for number of rows

--- spec/components/schools/teacher_profile_summary_list_component_spec.rb
+++ spec/components/schools/teacher_profile_summary_list_component_spec.rb
@@ -44,4 +44,10 @@ RSpec.describe Schools::TeacherProfileSummaryListComponent, type: :component do
     expect(page).to have_selector(".govuk-summary-list__row", text: "Working pattern")
     expect(page).to have_text('Full time')
   end
+
+  describe '#rows' do
+    it 'returns the correct number of rows' do
+      expect(described_class.new(mentee).rows.count).to eq(5)
+    end
+  end
 end
