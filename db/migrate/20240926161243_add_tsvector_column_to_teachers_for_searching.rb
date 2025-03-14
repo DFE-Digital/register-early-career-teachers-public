c28e912ce9e26b9857ad4b921c15e2ff6b4ca128
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-03-14 12:12:22 +0000

    Remove individual rubocop disable comments

--- db/migrate/20240926161243_add_tsvector_column_to_teachers_for_searching.rb
+++ db/migrate/20240926161243_add_tsvector_column_to_teachers_for_searching.rb
@@ -4,15 +4,11 @@ class AddTsvectorColumnToTeachersForSearching < ActiveRecord::Migration[7.2]
 
     rename_column 'teachers', :name, :corrected_name
 
-    # rubocop:disable Rails/ReversibleMigration
     change_column 'teachers', :corrected_name, :string, null: true
-    # rubocop:enable Rails/ReversibleMigration
 
     change_table 'teachers', bulk: true do |t|
-      # rubocop:disable Rails/NotNullColumn
       t.string :first_name, null: false
       t.string :last_name, null: false
-      # rubocop:enable Rails/NotNullColumn
 
       tsvector_columns = %w[first_name last_name corrected_name].map { |col| "coalesce(#{col}, '')" }.join(" || ' ' || ")
       t.tsvector :search, type: :tsvector, as: "to_tsvector('english', #{tsvector_columns})", stored: true
