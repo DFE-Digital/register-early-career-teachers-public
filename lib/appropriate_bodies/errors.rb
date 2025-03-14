078f7251ee8c8fa9018b5455ed314f069f0df144
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-03-14 09:14:06 +0000

    Remove Rails.application.routes.url_helpers

--- lib/appropriate_bodies/errors.rb
+++ lib/appropriate_bodies/errors.rb
@@ -15,8 +15,6 @@ module AppropriateBodies
     end
 
     class TeacherHasActiveInductionPeriodWithAnotherAB < StandardError
-      include Rails.application.routes.url_helpers
-
       def template
         :induction_with_another_appropriate_body
       end
