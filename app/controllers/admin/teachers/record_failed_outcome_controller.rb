e60d07d27908fd34192e3d686cb4d218373d6f71
edujackedu <jack.scotti@education.gov.uk>
2025-03-17 19:35:41 +0000

    Add induction outcome to the admin console

---
+++ app/controllers/admin/teachers/record_failed_outcome_controller.rb
@@ -0,0 +1,49 @@
+module Admin
+  module Teachers
+    class RecordFailedOutcomeController < AdminController
+      def new
+        @teacher = Teacher.find(params[:teacher_id])
+        @pending_induction_submission = PendingInductionSubmission.new
+      end
+
+      def create
+        @teacher = Teacher.find(params[:teacher_id])
+        active_induction_period = ::Teachers::InductionPeriod.new(@teacher).active_induction_period
+        @pending_induction_submission = PendingInductionSubmissions::Build.closing_induction_period(
+          active_induction_period,
+          **pending_induction_submission_params,
+          **pending_induction_submission_attributes(active_induction_period)
+        ).pending_induction_submission
+
+        record_outcome = ::AppropriateBodies::RecordOutcome.new(
+          appropriate_body: active_induction_period.appropriate_body,
+          pending_induction_submission: @pending_induction_submission,
+          teacher: @teacher,
+          author: current_user
+        )
+
+        PendingInductionSubmission.transaction do
+          if @pending_induction_submission.save(context: :record_outcome) && record_outcome.fail!
+            redirect_to(admin_teacher_record_failed_outcome_path(@teacher))
+          else
+            render :new
+          end
+        end
+      end
+
+      def show
+        @teacher = Teacher.find(params[:teacher_id])
+      end
+
+    private
+
+      def pending_induction_submission_params
+        params.require(:pending_induction_submission).permit(:finished_on, :number_of_terms, :outcome)
+      end
+
+      def pending_induction_submission_attributes(active_induction_period)
+        { appropriate_body_id: active_induction_period.appropriate_body.id, trn: @teacher.trn, outcome: "fail" }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,8 @@ Rails.application.routes.draw do
     resources :teachers, only: %i[index show] do
       resource :timeline, only: %i[show], controller: 'teachers/timeline'
       resources :induction_periods, only: %i[new create edit update], path: 'induction-periods'
+      resource :record_passed_outcome, only: %i[new create show], path: 'record-passed-outcome', controller: 'teachers/record_passed_outcome'
+      resource :record_failed_outcome, only: %i[new create show], path: 'record-failed-outcome', controller: 'teachers/record_failed_outcome'
     end
   end
 
