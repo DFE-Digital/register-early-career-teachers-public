fd157585f988b0adbc4eccb7c1f50abc5e42b717
edujackedu <jack.scotti@education.gov.uk>
2025-03-20 15:02:18 +0000

    Update admin controllers to match AB controllers

--- app/controllers/admin/teachers/record_failed_outcome_controller.rb
+++ app/controllers/admin/teachers/record_failed_outcome_controller.rb
@@ -4,30 +4,39 @@ module Admin
       def new
         @teacher = Teacher.find(params[:teacher_id])
         @pending_induction_submission = PendingInductionSubmission.new
+
+        if current_teacher_ongoing_induction_period.blank?
+          redirect_to admin_teacher_path(@teacher), notice: "No active induction period found"
+        end
       end
 
       def create
         @teacher = Teacher.find(params[:teacher_id])
-        active_induction_period = ::Teachers::InductionPeriod.new(@teacher).active_induction_period
-        @pending_induction_submission = PendingInductionSubmissions::Build.closing_induction_period(
-          active_induction_period,
-          **pending_induction_submission_params,
-          **pending_induction_submission_attributes(active_induction_period)
-        ).pending_induction_submission
 
-        record_outcome = ::AppropriateBodies::RecordOutcome.new(
-          appropriate_body: active_induction_period.appropriate_body,
-          pending_induction_submission: @pending_induction_submission,
-          teacher: @teacher,
-          author: current_user
-        )
+        if current_teacher_ongoing_induction_period.present?
+          @pending_induction_submission = PendingInductionSubmissions::Build.closing_induction_period(
+            current_teacher_ongoing_induction_period,
+            **pending_induction_submission_params,
+            **pending_induction_submission_attributes
+          ).pending_induction_submission
 
-        PendingInductionSubmission.transaction do
-          if @pending_induction_submission.save(context: :record_outcome) && record_outcome.fail!
-            redirect_to(admin_teacher_record_failed_outcome_path(@teacher))
-          else
-            render :new
+          record_outcome = ::AppropriateBodies::RecordOutcome.new(
+            appropriate_body: current_teacher_ongoing_induction_period.appropriate_body,
+            pending_induction_submission: @pending_induction_submission,
+            teacher: @teacher,
+            author: current_user
+          )
+
+          PendingInductionSubmission.transaction do
+            if @pending_induction_submission.save(context: :record_outcome) && record_outcome.fail!
+              redirect_to(admin_teacher_record_failed_outcome_path(@teacher))
+            else
+              render :new
+            end
           end
+
+        else
+          redirect_to admin_teacher_path(@teacher)
         end
       end
 
@@ -41,8 +50,12 @@ module Admin
         params.require(:pending_induction_submission).permit(:finished_on, :number_of_terms, :outcome)
       end
 
-      def pending_induction_submission_attributes(active_induction_period)
-        { appropriate_body_id: active_induction_period.appropriate_body.id, trn: @teacher.trn, outcome: "fail" }
+      def pending_induction_submission_attributes
+        { appropriate_body_id: current_teacher_ongoing_induction_period.appropriate_body.id, trn: @teacher.trn, outcome: "fail" }
+      end
+
+      def current_teacher_ongoing_induction_period
+        @current_teacher_ongoing_induction_period ||= ::Teachers::InductionPeriod.new(@teacher).ongoing_induction_period
       end
     end
   end
