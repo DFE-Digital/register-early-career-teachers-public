c9454d2b4339c46464f205676bba57929c2dc72c
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-03-20 09:07:42 +0000

    Fix performance cops

--- app/validators/working_pattern_validator.rb
+++ app/validators/working_pattern_validator.rb
@@ -2,7 +2,7 @@ class WorkingPatternValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.blank?
       record.errors.add(attribute, "Select if the ECT's working pattern is full or part time")
-    elsif !::WORKING_PATTERNS.keys.include?(value.to_sym)
+    elsif !::WORKING_PATTERNS.key?(value.to_sym)
       record.errors.add(attribute, "'#{value}' is not a valid working pattern")
     end
   end
