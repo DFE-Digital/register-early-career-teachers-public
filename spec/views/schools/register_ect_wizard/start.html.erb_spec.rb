81ac4482f36c6dcdf49e45a2b7688711b1dbdd6e
Avin Hurry <avinhurry@icloud.com>
2025-03-25 20:15:21 +0000

    Update what-you-will-need page
    - Update content
    - Add back link
    - Add details component to show/hide text
    - Test backlink

--- spec/views/schools/register_ect_wizard/start.html.erb_spec.rb
+++ spec/views/schools/register_ect_wizard/start.html.erb_spec.rb
@@ -1,11 +1,17 @@
 RSpec.describe "schools/register_ect_wizard/start.html.erb" do
-  it "sets the page title" do
+  before do
     render
+  end
+
+  it "sets the page title" do
     expect(sanitize(view.content_for(:page_title))).to eql("What you’ll need to register a new ECT")
   end
 
+  it "sets the back link path" do
+    expect(view.content_for(:backlink_or_breadcrumb)).to include(schools_ects_home_path)
+  end
+
   it 'includes a continue button' do
-    render
     expect(rendered).to have_link('Continue', href: schools_register_ect_wizard_find_ect_path)
   end
 end
