62f236ae6cb417b3d3aa03ca488c2ef6a4d7785e
Leonidas Apostolidis <leoapost@gmail.com>
2025-03-26 12:27:12 +0000

    Rename the mapper for improved clarity

--- app/migration/school_period_extractor.rb
+++ app/migration/school_period_extractor.rb
@@ -33,7 +33,7 @@ private
     induction_records.each_with_object([]) do |induction_record, periods|
       record_school = induction_record.induction_programme.school_cohort.school
       extracted_training_programme = induction_record.induction_programme.training_programme
-      programme_type = Mappers::ProgrammeTypeMapper.new(extracted_training_programme).mapped_value
+      programme_type = Mappers::TrainingProgrammeTypeMapper.new(extracted_training_programme).mapped_value
 
       if current_school != record_school
         current_school = record_school
