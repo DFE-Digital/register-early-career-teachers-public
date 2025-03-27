fe4adf1d8c59a1a232fb1e256fd473b4420b35aa
Leonidas Apostolidis <leoapost@gmail.com>
2025-03-27 10:11:07 +0000

    Add note about temporary attributes
    The appropriate_body_type, lead_provider_id and appropriate_body_id
    need to remain in Migration::SchoolPeriod until they are moved out
    of ECTAtSchoolPeriod.

--- app/models/migration/school_period.rb
+++ app/models/migration/school_period.rb
@@ -2,6 +2,10 @@ module Migration
   # NOTE: this is a PORO to help with collating school period data when processing InductionRecords
   #       this was originally a Struct but have made it a class so that we can reference it in
   #       multiple places and make it easier to test the code that uses it
+  #
+  #       The appropriate_body_type, lead_provider_id, and appropriate_body_id attributes are temporarily
+  #       on ECTAtSchoolPeriod and will be moved to other models. Until then, they need to remain here.
+
   class SchoolPeriod
     attr_accessor :urn, :start_date, :end_date, :start_source_id, :end_source_id,
                   :appropriate_body_id, :appropriate_body_type, :programme_type, :lead_provider_id
