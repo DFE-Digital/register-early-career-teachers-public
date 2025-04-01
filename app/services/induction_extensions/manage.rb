c3f5329225257b5030faf80d35e3c6f94e60247b
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-04-01 11:32:34 +0100

    - add events to create/update extension - combine into a single service - remove extensions from seeds for teachers who have no induction periods - add rake task to backfill events

---
+++ app/services/induction_extensions/manage.rb
@@ -0,0 +1,49 @@
+# Perform InductionExtension edits on behalf of an author and track change events:
+#
+class InductionExtensions::Manage
+  attr_reader :author,
+              :teacher,
+              :appropriate_body,
+              :induction_extension
+
+  def initialize(author:, teacher:, appropriate_body:)
+    @author = author
+    @teacher = teacher
+    @appropriate_body = appropriate_body
+  end
+
+  def create_or_update!(number_of_terms:, id: nil)
+    @induction_extension = teacher.induction_extensions.find_or_initialize_by(id:)
+    induction_extension.assign_attributes(number_of_terms:)
+
+    InductionExtension.transaction do
+      success = if induction_extension.new_record?
+                  [induction_extension.save!, record_create_event!].all?
+                else
+                  [induction_extension.save!, record_update_event!].all?
+                end
+
+      success or raise ActiveRecord::Rollback
+    end
+  end
+
+private
+
+  def record_create_event!
+    Events::Record.record_appropriate_body_adds_induction_extension_event(**event_params)
+  end
+
+  def record_update_event!
+    Events::Record.record_appropriate_body_updates_induction_extension_event(**event_params)
+  end
+
+  def event_params
+    {
+      author:,
+      appropriate_body:,
+      teacher:,
+      induction_extension:,
+      modifications: induction_extension.changes
+    }
+  end
+end
