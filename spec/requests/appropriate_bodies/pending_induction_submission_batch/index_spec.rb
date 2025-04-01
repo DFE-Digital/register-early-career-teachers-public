2da70405daf5a356e283e91be067b2feb699d27d
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-04-01 15:12:37 +0100

    - introduce claim journey with restful routes - type the batch for each journey - update CSV files with iso8601 dates - validate date format and date of birth

--- spec/requests/appropriate_bodies/pending_induction_submission_batch/index_spec.rb
+++ spec/requests/appropriate_bodies/pending_induction_submission_batch/index_spec.rb
@@ -2,10 +2,10 @@ RSpec.describe "Appropriate Body uploads index page", type: :request do
   include AuthHelper
   let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
 
-  describe 'GET /appropriate-body/imports' do
+  describe 'GET /appropriate-body/uploads' do
     context 'when not signed in' do
       it 'redirects to the root page' do
-        get("/appropriate-body/imports")
+        get("/appropriate-body/uploads")
         expect(response).to be_redirection
         expect(response.redirect_url).to eql(root_url)
       end
@@ -14,7 +14,7 @@ RSpec.describe "Appropriate Body uploads index page", type: :request do
     context 'when signed in as an appropriate body user' do
       it 'renders the page successfully' do
         sign_in_as(:appropriate_body_user, appropriate_body:)
-        get("/appropriate-body/imports")
+        get("/appropriate-body/uploads")
         expect(response).to be_successful
       end
     end
