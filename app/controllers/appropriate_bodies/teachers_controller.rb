662bf64f06119dde931107736514580579165b5c
Peter Yates <peter.yates@graphia.co.uk>
2025-04-02 11:45:00 +0100

    Super quick prototype of how tables of ECTs might look

--- app/controllers/appropriate_bodies/teachers_controller.rb
+++ app/controllers/appropriate_bodies/teachers_controller.rb
@@ -6,8 +6,9 @@ module AppropriateBodies
       @pagy, @teachers = pagy(
         ::Teachers::Search.new(
           query_string: params[:q],
-          appropriate_bodies: @appropriate_body
-        ).search
+          appropriate_bodies: @appropriate_body,
+        ).search,
+        size: 50
       )
       @claimed_inductions_count = AppropriateBodies::ECTs.new(@appropriate_body).current_or_completed_while_at_appropriate_body.count
     end
