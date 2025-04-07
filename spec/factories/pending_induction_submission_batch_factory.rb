83c2947da949ba9b0333c270ce402f4e105ecbb0
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-04-07 10:54:16 +0100

    - date format spec - mimetype validation and spec - batch factory traits - balanced and passing coverage

--- spec/factories/pending_induction_submission_batch_factory.rb
+++ spec/factories/pending_induction_submission_batch_factory.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory(:pending_induction_submission_batch) do
     association :appropriate_body
+    batch_status { 'pending' }
+    error_message { nil }
+
+    trait :action do
+      batch_type { 'action' }
+      csv_file { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/actions/valid_complete.csv'), 'text/csv') }
+    end
+
+    trait :claim do
+      batch_type { 'claim' }
+      csv_file { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/claims/valid_complete.csv'), 'text/csv') }
+    end
   end
 end
---
+++ spec/requests/appropriate_bodies/process_batch/claims/index_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe "Appropriate Body bulk claims index page", type: :request do
+  include AuthHelper
+  let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
+
+  describe 'GET /appropriate-body/uploads' do
+    context 'when not signed in' do
+      it 'redirects to the root page' do
+        get("/appropriate-body/bulk/claims")
+        expect(response).to be_redirection
+        expect(response.redirect_url).to eql(root_url)
+      end
+    end
+
+    context 'when signed in as an appropriate body user' do
+      it 'renders the page successfully' do
+        sign_in_as(:appropriate_body_user, appropriate_body:)
+        get("/appropriate-body/bulk/claims")
+        expect(response).to be_successful
+      end
+    end
+  end
+end
