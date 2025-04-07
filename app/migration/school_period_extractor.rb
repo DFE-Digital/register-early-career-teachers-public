119bd7638742a01e14cd42d706f5ffe2c6c93286
Leonidas Apostolidis <leoapost@gmail.com>
2025-04-07 17:03:37 +0100

    Extract the programme_type to a method

--- app/migration/school_period_extractor.rb
+++ app/migration/school_period_extractor.rb
@@ -32,8 +32,7 @@ private
 
     induction_records.each_with_object([]) do |induction_record, periods|
       record_school = induction_record.induction_programme.school_cohort.school
-      extracted_training_programme = induction_record.induction_programme.training_programme
-      programme_type = Mappers::TrainingProgrammeTypeMapper.new(extracted_training_programme).mapped_value
+      programme_type = discover_programme_type(induction_record)
 
       if current_school != record_school
         current_school = record_school
@@ -44,7 +43,7 @@ private
                                                      start_source_id: induction_record.id,
                                                      end_source_id: induction_record.id,
                                                      appropriate_body_type: induction_record.appropriate_body.body_type,
-                                                     programme_type:,
+                                                     programme_type: programme_type,
                                                      appropriate_body_id: induction_record.appropriate_body_id,
                                                      lead_provider_id: nil)
         periods << current_period
@@ -71,5 +70,10 @@ private
 
   def participant_profile
     @participant_profile ||= induction_records.first.participant_profile
+  end
+
+  def discover_programme_type(induction_record)
+    extracted_training_programme = induction_record.induction_programme.training_programme
+    Mappers::TrainingProgrammeTypeMapper.new(extracted_training_programme).mapped_value
   end
 end
