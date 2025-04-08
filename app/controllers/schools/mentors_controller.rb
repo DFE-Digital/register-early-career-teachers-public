4d0f0ef07bc0f5bc9b3b5c46d42158e38f6b2900
Avin Hurry <avinhurry@icloud.com>
2025-04-09 00:07:28 +0100

    [1301] Add route, controller action and view

--- app/controllers/schools/mentors_controller.rb
+++ app/controllers/schools/mentors_controller.rb
@@ -5,5 +5,11 @@ module Schools
     def index
       @pagy, @mentors = pagy(Schools::Home.new(school:).mentors_with_ects, limit: 20)
     end
+
+    def show
+      @mentor = MentorAtSchoolPeriod.find(params[:id])
+      @teacher = @mentor.teacher
+      @ects = Schools::Home.new(school:).ects_with_mentors
+    end
   end
 end
