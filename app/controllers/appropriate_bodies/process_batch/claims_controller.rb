c56fc46e2ed37ee8ea1fed2b5dc6ddf828f8f2b4
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-04-08 14:51:56 +0100

    - split the action journey in two to allow user to check answers - use create action for step 1 validation and update for step 2 persistence - add processed state to the batch_status - dry up debugging views with helpers - passing suite and linter

--- app/controllers/appropriate_bodies/process_batch/claims_controller.rb
+++ app/controllers/appropriate_bodies/process_batch/claims_controller.rb
@@ -2,10 +2,9 @@ module AppropriateBodies
   module ProcessBatch
     class ClaimsController < PendingInductionSubmissionBatchController
       def index
-        @pending_induction_submission_batches =
-          PendingInductionSubmissionBatch
+        @pending_induction_submission_batches = PendingInductionSubmissionBatch
+            .for_appropriate_body(@appropriate_body)
             .claim
-            .for_appropriate_body(@appropriate_body.id)
             .order(id: :desc)
             .select(:id, :batch_type, :batch_status, :error_message)
             .map { |b| b.attributes.values.map(&:to_s) }
@@ -13,16 +12,11 @@ module AppropriateBodies
 
       def create
         @pending_induction_submission_batch = PendingInductionSubmissionBatch.new_claim_for(appropriate_body: @appropriate_body, **import_params)
-        # PendingInductionSubmissionBatch.new(
-        #   appropriate_body: @appropriate_body,
-        #   batch_type: 'claim',
-        #   **import_params
-        # )
 
         if @pending_induction_submission_batch.save! && @pending_induction_submission_batch.save(context: :uploaded)
-          ProcessBatchClaimJob.perform_later(@pending_induction_submission_batch, current_user.email, current_user.name)
+          run_job
 
-          redirect_to ab_batch_claim_path(@pending_induction_submission_batch), alert: 'File uploaded'
+          redirect_to ab_batch_claim_path(@pending_induction_submission_batch), alert: 'File processing'
         else
           render :new
         end
@@ -34,6 +28,24 @@ module AppropriateBodies
         @pending_induction_submission_batch ||= PendingInductionSubmissionBatch.new
         @pending_induction_submission_batch.errors.add(:base, e.message)
         render :new
+      end
+
+      def update
+        @pending_induction_submission_batch = PendingInductionSubmissionBatch.find(params[:id])
+
+        run_job
+
+        redirect_to ab_batch_claim_path(@pending_induction_submission_batch), alert: 'Teachers claimed'
+      end
+
+    private
+
+      def run_job
+        ProcessBatchClaimJob.perform_later(
+          @pending_induction_submission_batch,
+          current_user.email,
+          current_user.name
+        )
       end
     end
   end
