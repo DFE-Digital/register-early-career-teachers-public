2060a9d3cd2e2423bd0664e33173d617d751b135
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-04-08 14:51:56 +0100

    - split the action journey in two to allow user to check answers - use create action for step 1 validation and update for step 2 persistence - add processed state to the batch_status - dry up debugging views with helpers - passing suite and linter

---
+++ app/helpers/batch_helper.rb
@@ -0,0 +1,62 @@
+module BatchHelper
+  def batch_progress_card(pending_induction_submission_batch)
+    govuk_summary_list(card: { title: 'Progress' }, rows: [
+      {
+        key: { text: 'Appropriate Body' },
+        value: { text: pending_induction_submission_batch.appropriate_body.name }
+      },
+      {
+        key: { text: 'Batch ID' },
+        value: { text: pending_induction_submission_batch.id }
+      },
+      {
+        key: { text: 'Batch status' },
+        value: { text: govuk_tag(text: pending_induction_submission_batch.batch_status) }
+      },
+      {
+        key: { text: 'Batch type' },
+        value: { text: govuk_tag(text: pending_induction_submission_batch.batch_type) }
+      },
+      {
+        key: { text: 'Batch error' },
+        value: { text: pending_induction_submission_batch.error_message }
+      },
+      {
+        key: { text: 'Number of CSV rows' },
+        value: { text: pending_induction_submission_batch.rows.count }
+      },
+      {
+        key: { text: 'Number of processed submission records' },
+        value: { text: pending_induction_submission_batch.processed_rows.count }
+      },
+      {
+        key: { text: 'Number of failures to download' },
+        value: { text: pending_induction_submission_batch.csv_download.count }
+      },
+    ])
+  end
+
+  def batch_raw_data_table(pending_induction_submission_batch)
+    govuk_table(
+      caption: "Uploaded CSV data (#{pending_induction_submission_batch.rows.count} rows)",
+      head: pending_induction_submission_batch.csv_headings.values,
+      rows: pending_induction_submission_batch.rows
+    )
+  end
+
+  def batch_processed_data_table(pending_induction_submission_batch)
+    govuk_table(
+      caption: "Processed (#{pending_induction_submission_batch.processed_rows.count} submissions)",
+      head: pending_induction_submission_batch.processed_headers,
+      rows: pending_induction_submission_batch.processed_rows
+    )
+  end
+
+  def batch_download_data_table(pending_induction_submission_batch)
+    govuk_table(
+      caption: "Downloadable bad CSV data (#{pending_induction_submission_batch.csv_download.count} rows)",
+      head: pending_induction_submission_batch.csv_headings.values,
+      rows: pending_induction_submission_batch.csv_download
+    )
+  end
+end
