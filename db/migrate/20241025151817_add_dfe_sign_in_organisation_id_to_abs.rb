6d0d50c09f0195871af87363f76018072b76ad49
Peter Yates <peter.yates@graphia.co.uk>
2025-04-08 11:22:03 +0100

    Remove references to AppropriateBody in migrations
    This has the potential to cause problems by loading the AppropriateBody
    model which makes certain expectations of the database before the
    remaining migrations have been run. This is problematic when some types
    are forced, like enums.

--- db/migrate/20241025151817_add_dfe_sign_in_organisation_id_to_abs.rb
+++ db/migrate/20241025151817_add_dfe_sign_in_organisation_id_to_abs.rb
@@ -1,9 +1,8 @@
 class AddDfESignInOrganisationIdToAbs < ActiveRecord::Migration[7.2]
   def change
-    add_column :appropriate_bodies, :dfe_sign_in_organisation_id, :uuid
-
-    change_column :appropriate_bodies, :dfe_sign_in_organisation_id, :uuid, null: false
-
+    # rubocop:disable Rails/NotNullColumn
+    add_column :appropriate_bodies, :dfe_sign_in_organisation_id, :uuid, null: false
+    # rubocop:enable Rails/NotNullColumn
     add_index :appropriate_bodies, :dfe_sign_in_organisation_id, unique: true
   end
 end
