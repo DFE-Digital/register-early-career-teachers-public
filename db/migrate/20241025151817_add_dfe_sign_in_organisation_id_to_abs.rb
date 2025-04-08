c95c340a86f97f276c0e70817e07adbf98c5a8d4
Peter Yates <peter.yates@graphia.co.uk>
2025-04-08 10:58:26 +0100

    Remove reference to AppropriateBody from old migrations
    This was included originally because there was a stipulation that
    AppropriateBody#dfe_sign_in_organisation_id was present and we had
    deployed non-production environments.
    
    It turns out that the loading of AppropriateBody at this stage is now
    causing issues when migrating and breaking the deployment of review
    apps.

--- db/migrate/20241025151817_add_dfe_sign_in_organisation_id_to_abs.rb
+++ db/migrate/20241025151817_add_dfe_sign_in_organisation_id_to_abs.rb
@@ -2,8 +2,6 @@ class AddDfESignInOrganisationIdToAbs < ActiveRecord::Migration[7.2]
   def change
     add_column :appropriate_bodies, :dfe_sign_in_organisation_id, :uuid
 
-    AppropriateBody.all.find_each { |ab| ab.update(dfe_sign_in_organisation_id: SecureRandom.uuid) }
-
     change_column :appropriate_bodies, :dfe_sign_in_organisation_id, :uuid, null: false
 
     add_index :appropriate_bodies, :dfe_sign_in_organisation_id, unique: true
