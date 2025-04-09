b1eab396967980c731f73e10ad27beb0518d6cf4
Peter Yates <peter.yates@graphia.co.uk>
2025-04-09 15:15:45 +0100

    Sort the env variable files alphabetically

--- config/global_config/domains.sh
+++ config/global_config/domains.sh
@@ -1,5 +1,5 @@
-AZURE_SUBSCRIPTION=s189-teacher-services-cloud-production
 AZURE_RESOURCE_PREFIX=s189p01
+AZURE_SUBSCRIPTION=s189-teacher-services-cloud-production
 CONFIG_SHORT=dom
 DISABLE_KEYVAULTS=true
 TERRAFORM_MODULES_TAG=stable
