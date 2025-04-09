d6630ab006e372422e23dd5ac065a583dd4af8ad
Lorenzo Tello <ltello8a@gmail.com>
2025-04-09 17:18:45 +0100

    only display the step about mentor training eligibility if the ect is provider-led

--- spec/wizards/schools/register_mentor_wizard/email_address_step_spec.rb
+++ spec/wizards/schools/register_mentor_wizard/email_address_step_spec.rb
@@ -1,10 +1,20 @@
 require_relative './shared_examples/email_step'
 
 describe Schools::RegisterMentorWizard::EmailAddressStep, type: :model do
-  context 'without funding exemption' do
+  context 'when email is in use' do
+    before do
+      allow(wizard.mentor).to receive(:cant_use_email?).and_return(true)
+    end
+
     it_behaves_like 'an email step', current_step: :email_address,
-                                     previous_step: :review_mentor_details,
-                                     next_step: :review_mentor_eligibility
+                    previous_step: :review_mentor_details,
+                    next_step: :cant_use_email
+  end
+
+  context 'with provider_led ect and without funding exemption' do
+    it_behaves_like 'an email step', current_step: :email_address,
+                    previous_step: :review_mentor_details,
+                    next_step: :review_mentor_eligibility
   end
 
   context 'with funding exemption' do
@@ -13,7 +23,15 @@ describe Schools::RegisterMentorWizard::EmailAddressStep, type: :model do
     end
 
     it_behaves_like 'an email step', current_step: :email_address,
-                                     previous_step: :review_mentor_details,
-                                     next_step: :check_answers
+                    previous_step: :review_mentor_details,
+                    next_step: :check_answers
+  end
+
+  context 'with school_led ect' do
+    it_behaves_like 'an email step', current_step: :email_address,
+                    previous_step: :review_mentor_details,
+                    next_step: :check_answers do
+      let(:ect) { FactoryBot.create(:ect_at_school_period, :active, :school_led) }
+    end
   end
 end
