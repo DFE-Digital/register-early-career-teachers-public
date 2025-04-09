869785bff21feb0422429f5d55f3cbcc11e5b420
Leonidas Apostolidis <leoapost@gmail.com>
2025-04-09 13:36:02 +0100

    Fix linter issues

--- app/migration/school_period_extractor.rb
+++ app/migration/school_period_extractor.rb
@@ -80,6 +80,7 @@ private
     return nil if programme_type == 'school_led'
 
     return nil if induction_record.induction_programme.partnership.nil?
+
     extracted_lead_provider_name = induction_record.induction_programme.partnership.lead_provider.name
     LeadProvider.find_by(name: extracted_lead_provider_name).id
   end
