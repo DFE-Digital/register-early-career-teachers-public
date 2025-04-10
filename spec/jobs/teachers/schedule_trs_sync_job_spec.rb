1664cf03e65774df8e2b2d47214ed2c7206f49af
Peter Yates <peter.yates@graphia.co.uk>
2025-04-10 11:23:49 +0100

    Update TRS sync scheduler to use nulls first scope
    This change means the query results will place rows with null sync
    timestamps before rows that have values, so we'll first refresh records
    that have never been refreshed and *then* refresh those that have,
    oldest first.

--- spec/jobs/teachers/schedule_trs_sync_job_spec.rb
+++ spec/jobs/teachers/schedule_trs_sync_job_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Teachers::ScheduleTRSSyncJob, type: :job do
   describe "#perform" do
+    let!(:teacher0) { FactoryBot.create(:teacher, trs_data_last_refreshed_at: nil) }
     let!(:teacher1) { FactoryBot.create(:teacher, trs_data_last_refreshed_at: 5.days.ago) }
     let!(:teacher2) { FactoryBot.create(:teacher, trs_data_last_refreshed_at: 4.days.ago) }
     let!(:teacher3) { FactoryBot.create(:teacher, trs_data_last_refreshed_at: 3.days.ago) }
@@ -8,11 +9,12 @@ RSpec.describe Teachers::ScheduleTRSSyncJob, type: :job do
 
     it "schedules sync jobs for teachers ordered by trs_data_last_refreshed_at" do
       [
-        [teacher1, 0],
-        [teacher2, 3],
-        [teacher3, 6],
-        [teacher4, 9],
-        [teacher5, 12],
+        [teacher0, 0],
+        [teacher1, 3],
+        [teacher2, 6],
+        [teacher3, 9],
+        [teacher4, 12],
+        [teacher5, 15],
       ].each do |teacher, wait_time|
         allow(Teachers::SyncTeacherWithTRSJob).to receive(:set).with(wait: wait_time.seconds).and_return(Teachers::SyncTeacherWithTRSJob)
 
@@ -26,11 +28,12 @@ RSpec.describe Teachers::ScheduleTRSSyncJob, type: :job do
       stub_const("Teachers::ScheduleTRSSyncJob::BATCH_SIZE", 3)
 
       # We expect the first 3 teachers to be processed (oldest refresh first)
+      expect(Teachers::SyncTeacherWithTRSJob).to receive(:perform_later).with(teacher: teacher0)
       expect(Teachers::SyncTeacherWithTRSJob).to receive(:perform_later).with(teacher: teacher1)
       expect(Teachers::SyncTeacherWithTRSJob).to receive(:perform_later).with(teacher: teacher2)
-      expect(Teachers::SyncTeacherWithTRSJob).to receive(:perform_later).with(teacher: teacher3)
 
       # We don't expect the newer teachers to be processed
+      expect(Teachers::SyncTeacherWithTRSJob).not_to receive(:perform_later).with(teacher: teacher3)
       expect(Teachers::SyncTeacherWithTRSJob).not_to receive(:perform_later).with(teacher: teacher4)
       expect(Teachers::SyncTeacherWithTRSJob).not_to receive(:perform_later).with(teacher: teacher5)
 
