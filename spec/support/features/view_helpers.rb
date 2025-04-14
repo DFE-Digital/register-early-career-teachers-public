b10f9dcb903e9a0b0f200f7943c4f2c249c08398
Avin Hurry <avinhurry@icloud.com>
2025-04-14 23:04:35 +0100

    [1301] Ensure back links work dynamically

--- spec/support/features/view_helpers.rb
+++ spec/support/features/view_helpers.rb
@@ -4,7 +4,7 @@ module Features
       page.locator('a.govuk-back-link').click
     end
 
-    alias_method :when_i_click_back_link, :given_i_click_the_back_link
-    alias_method :and_i_click_back_link, :given_i_click_the_back_link
+    alias_method :when_i_click_the_back_link, :given_i_click_the_back_link
+    alias_method :and_i_click_the_back_link, :given_i_click_the_back_link
   end
 end
