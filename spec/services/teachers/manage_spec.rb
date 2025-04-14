80b8ea3c0f20d160561598206dc7b3a8bed81881
Peter Yates <peter.yates@graphia.co.uk>
2025-04-14 14:58:22 +0100

    Fix the TRS state updating event
    There was a mismatch between the variable names
    'induction_status_before' and 'new_induction_status'. Because we were
    using instance variables they were interpolated as empty strings.

--- spec/services/teachers/manage_spec.rb
+++ spec/services/teachers/manage_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Teachers::Manage do
 
   let(:user) { FactoryBot.create(:user, name: 'Christopher Biggins', email: 'christopher.biggins@education.gov.uk') }
   let(:author) { Sessions::Users::DfEPersona.new(email: user.email) }
-  let(:teacher) { FactoryBot.create(:teacher, trs_first_name: 'Barry', trs_last_name: 'Allen') }
+  let(:teacher) { FactoryBot.create(:teacher, trs_first_name: 'Barry', trs_last_name: 'Allen', trs_induction_status: 'InProgress') }
   let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
 
   describe '#update_name!' do
@@ -65,6 +65,80 @@ RSpec.describe Teachers::Manage do
             "TRS data last refreshed at set to '#{Time.zone.now}'"
           ]
         )
+      end
+    end
+  end
+
+  describe '#update_trs_induction_status!' do
+    before { allow(RecordEventJob).to receive(:perform_later).and_return(true) }
+
+    context 'when the new induction status is different' do
+      it 'updates the teacher record' do
+        service.update_trs_induction_status!(trs_induction_status: 'Passed')
+
+        expect(teacher.trs_induction_status).to eql('Passed')
+      end
+
+      it 'records an event' do
+        freeze_time do
+          service.update_trs_induction_status!(trs_induction_status: 'Passed')
+
+          expect(RecordEventJob).to have_received(:perform_later).with(
+            author_email: 'christopher.biggins@education.gov.uk',
+            author_id: author.id,
+            author_name: 'Christopher Biggins',
+            author_type: :dfe_staff_user,
+            event_type: :teacher_induction_status_updated_by_trs,
+            appropriate_body:,
+            teacher:,
+            happened_at: Time.zone.now,
+            heading: %(Induction status changed from 'InProgress' to 'Passed')
+          )
+        end
+      end
+    end
+
+    context 'when the new induction status is the same' do
+      it 'does not records an event' do
+        service.update_trs_induction_status!(trs_induction_status: 'InProgress')
+
+        expect(RecordEventJob).not_to have_received(:perform_later)
+      end
+    end
+  end
+
+  describe '#update_qts_awarded_on!' do
+    before { allow(RecordEventJob).to receive(:perform_later).and_return(true) }
+
+    context 'when the new induction status is different' do
+      it 'updates the teacher record' do
+        service.update_qts_awarded_on!(trs_qts_awarded_on: 1.year.ago)
+
+        expect(teacher.trs_qts_awarded_on).to eql(1.year.ago.to_date)
+      end
+
+      it 'does not records an event' do
+        service.update_qts_awarded_on!(trs_qts_awarded_on: 1.year.ago)
+
+        expect(RecordEventJob).not_to have_received(:perform_later)
+      end
+    end
+  end
+
+  describe '#update_itt_provider_name!' do
+    before { allow(RecordEventJob).to receive(:perform_later).and_return(true) }
+
+    context 'when the new induction status is different' do
+      it 'updates the teacher record' do
+        service.update_itt_provider_name!(trs_initial_teacher_training_provider_name: 'Some other training provider')
+
+        expect(teacher.trs_initial_teacher_training_provider_name).to eql('Some other training provider')
+      end
+
+      it 'does not records an event' do
+        service.update_qts_awarded_on!(trs_qts_awarded_on: 1.year.ago)
+
+        expect(RecordEventJob).not_to have_received(:perform_later)
       end
     end
   end
