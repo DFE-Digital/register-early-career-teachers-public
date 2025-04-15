f801cd30bc0ca7806e7683b9fa2b467cab8814dc
Avin Hurry <avinhurry@icloud.com>
2025-04-15 12:47:54 +0100

    [1301] Ensure we always get the first lead provider
    the mentor was assigned to

--- app/components/schools/mentors/ect_mentor_training_details_component.rb
+++ app/components/schools/mentors/ect_mentor_training_details_component.rb
@@ -12,15 +12,19 @@ module Schools
       end
 
       def render?
-        ects.any? { |ect| ect.programme_type == "provider_led" }
+        ects.any?(&:provider_led?)
       end
 
       def eligible_for_training?
         teacher.mentor_completion_date.nil?
       end
 
-      def first_ect_lead_provider_name
-        ects.find { |ect| ect.lead_provider.present? }&.lead_provider&.name
+      def first_lead_provider_name
+        ects
+          .select { |ect| ect.lead_provider.present? }
+          .min_by(&:started_on)
+          &.lead_provider
+          &.name
       end
     end
   end
