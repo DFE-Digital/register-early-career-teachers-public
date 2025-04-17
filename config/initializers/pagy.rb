bcfe396a2a0e373e51603c805c76829aef0ed080
Peter Yates <peter.yates@graphia.co.uk>
2025-04-17 15:18:22 +0100

    Change default pagination limit from 10 to 20

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -4,5 +4,5 @@ require 'pagy/extras/size'
 
 Pagy::DEFAULT[:overflow] = :empty_page # default  (other options: :last_page and :exception)
 Pagy::DEFAULT[:size] = [1, 1, 1, 1].freeze
-Pagy::DEFAULT[:limit] = 10
+Pagy::DEFAULT[:limit] = 20
 Pagy::DEFAULT.freeze
