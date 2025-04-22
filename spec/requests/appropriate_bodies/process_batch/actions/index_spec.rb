47d6007a7892f20d48ec1853f6ca59e25bb70564
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-04-22 10:27:56 +0100

    - dedicated feature flag - address review comments

--- spec/requests/appropriate_bodies/process_batch/actions/index_spec.rb
+++ spec/requests/appropriate_bodies/process_batch/actions/index_spec.rb
@@ -2,7 +2,16 @@ RSpec.describe "Appropriate Body bulk actions index page", type: :request do
   include AuthHelper
   let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
 
-  describe 'GET /appropriate-body/uploads' do
+  describe 'GET /appropriate-body/bulk/actions' do
+    context 'when bulk upload is disabled (ENABLE_BULK_UPLOAD=false)' do
+      before { allow(Rails.application.config).to receive(:enable_bulk_upload).and_return(false) }
+
+      it 'is not found' do
+        get("/appropriate-body/bulk/actions")
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
     context 'when not signed in' do
       it 'redirects to the root page' do
         get("/appropriate-body/bulk/actions")
--- spec/requests/appropriate_bodies/process_batch/actions/show_spec.rb
+++ spec/requests/appropriate_bodies/process_batch/actions/show_spec.rb
@@ -8,19 +8,30 @@ RSpec.describe "Appropriate Body bulk actions show page", type: :request do
                       data: [])
   end
 
-  context 'when not signed in' do
-    it 'redirects to the root page' do
-      get("/appropriate-body/bulk/actions/#{batch.id}")
-      expect(response).to be_redirection
-      expect(response.redirect_url).to eql(root_url)
-    end
-  end
+  describe 'GET /appropriate-body/bulk/actions/:batch_id' do
+    context 'when bulk upload is disabled (ENABLE_BULK_UPLOAD=false)' do
+      before { allow(Rails.application.config).to receive(:enable_bulk_upload).and_return(false) }
 
-  context 'when signed in as an appropriate body user' do
-    it 'renders the page successfully' do
-      sign_in_as(:appropriate_body_user, appropriate_body:)
-      get("/appropriate-body/bulk/actions/#{batch.id}")
-      expect(response).to be_successful
+      it 'is not found' do
+        get("/appropriate-body/bulk/actions/#{batch.id}")
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context 'when not signed in' do
+      it 'redirects to the root page' do
+        get("/appropriate-body/bulk/actions/#{batch.id}")
+        expect(response).to be_redirection
+        expect(response.redirect_url).to eql(root_url)
+      end
+    end
+
+    context 'when signed in as an appropriate body user' do
+      it 'renders the page successfully' do
+        sign_in_as(:appropriate_body_user, appropriate_body:)
+        get("/appropriate-body/bulk/actions/#{batch.id}")
+        expect(response).to be_successful
+      end
     end
   end
 end
