c70322bb84a2412fe3092d97cc63928171b1c400
Leonidas Apostolidis <leoapost@gmail.com>
2025-04-24 11:14:27 +0100

    Improve programme details section on Register ECT CYA page
    This commit separates the programme details into their own summary list
    for better clarity.
    
    It also adds a new row to indicate whether the ECT will follow the same
    programme as the previously registered ECT.

--- spec/views/schools/register_ect_wizard/check_answers.html.erb_spec.rb
+++ spec/views/schools/register_ect_wizard/check_answers.html.erb_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe "schools/register_ect_wizard/check_answers.html.erb" do
-  let(:store) { FactoryBot.build(:session_repository, working_pattern: 'Full time') }
+  let(:use_previous_ect_choices) { nil }
+  let(:store) { FactoryBot.build(:session_repository, working_pattern: 'Full time', use_previous_ect_choices:) }
 
   let(:wizard) do
     FactoryBot.build(:register_ect_wizard, current_step: :check_answers, store:)
@@ -26,5 +27,48 @@ RSpec.describe "schools/register_ect_wizard/check_answers.html.erb" do
   it 'includes a continue button' do
     expect(rendered).to have_button('Confirm details')
     expect(rendered).to have_selector("form[action='#{schools_register_ect_wizard_check_answers_path}']")
+  end
+
+  it 'includes a Programme details heading' do
+    expect(rendered).to have_content("Programme details")
+  end
+
+  describe 'programme details section' do
+    before do
+      allow(wizard.school).to receive(:programme_choices?).and_return(programme_choices)
+      render
+    end
+
+    context 'when school has programme choices' do
+      let(:programme_choices) { true }
+
+      context 'when choosing the same choices' do
+        let(:use_previous_ect_choices) { true }
+
+        it 'displays the "Choices used by your school previously" row' do
+          expect(rendered).to have_content('Choices used by your school previously')
+          expect(rendered).to have_content('Yes, use the programme choices used by my school previously')
+          expect(rendered).to have_link('Change', href: schools_register_ect_wizard_change_use_previous_ect_choices_path)
+        end
+      end
+
+      context 'when choosing different choices' do
+        let(:use_previous_ect_choices) { false }
+
+        it 'displays the "Choices used by your school previously" row' do
+          expect(rendered).to have_content('Choices used by your school previously')
+          expect(rendered).to have_content("No, don't use the programme choices used by my school previously")
+          expect(rendered).to have_link('Change', href: schools_register_ect_wizard_change_use_previous_ect_choices_path)
+        end
+      end
+    end
+
+    context 'when school does not have programme choices' do
+      let(:programme_choices) { false }
+
+      it 'does not display the "Choices used by your school previously" row' do
+        expect(rendered).not_to have_content('Choices used by your school previously')
+      end
+    end
   end
 end
