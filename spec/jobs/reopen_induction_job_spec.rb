d6acaacd0e1d95fdb17a177f7dbea7fdf3b61a10
Tony Headford <tony@binarycircus.com>
2025-04-25 17:39:45 +0100

    Fix name change in spec

--- spec/jobs/reopen_induction_job_spec.rb
+++ spec/jobs/reopen_induction_job_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe ReopenInductionJob, type: :job do
 
     before do
       allow(TRS::APIClient).to receive(:new).and_return(trs_client)
-      allow(trs_client).to receive(:reopen_teacher_induction)
+      allow(trs_client).to receive(:reopen_teacher_induction!)
     end
 
     it "calls the TRS API to reopen the teacher's induction" do
-      expect(trs_client).to receive(:reopen_teacher_induction).with(trn:, start_date:)
+      expect(trs_client).to receive(:reopen_teacher_induction!).with(trn:, start_date:)
       described_class.new.perform(trn:, start_date:)
     end
   end
