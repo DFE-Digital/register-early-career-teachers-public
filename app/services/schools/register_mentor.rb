d4dbf6e403c7ba863588389a149dfb81aa5c8ef4
Peter Yates <peter.yates@graphia.co.uk>
2025-04-25 16:52:24 +0100

    Make mentor reg wizard record an event on completion
    The event will be visible on the timelines of the teacher, school and
    mentor_at_school_period

--- app/services/schools/register_mentor.rb
+++ app/services/schools/register_mentor.rb
@@ -1,6 +1,7 @@
 module Schools
   class RegisterMentor
-    attr_reader :trs_first_name,
+    attr_reader :author,
+                :trs_first_name,
                 :trs_last_name,
                 :corrected_name,
                 :school_urn,
@@ -9,7 +10,8 @@ module Schools
                 :email,
                 :started_on,
                 :mentor_completion_date,
-                :mentor_completion_reason
+                :mentor_completion_reason,
+                :mentor_at_school_period
 
     def initialize(trs_first_name:,
                    trs_last_name:,
@@ -19,7 +21,9 @@ module Schools
                    email:,
                    mentor_completion_date:,
                    mentor_completion_reason:,
+                   author:,
                    started_on: Date.current)
+      @author = author
       @trs_first_name = trs_first_name
       @trs_last_name = trs_last_name
       @corrected_name = corrected_name
@@ -35,7 +39,10 @@ module Schools
       ActiveRecord::Base.transaction do
         create_teacher!
         start_at_school!
+        record_event!
       end
+
+      mentor_at_school_period
     end
 
   private
@@ -62,7 +69,11 @@ module Schools
     end
 
     def start_at_school!
-      teacher.mentor_at_school_periods.create!(school:, started_on:, email:)
+      @mentor_at_school_period = teacher.mentor_at_school_periods.create!(school:, started_on:, email:)
+    end
+
+    def record_event!
+      Events::Record.record_teacher_registered_as_mentor_event!(author:, mentor_at_school_period:, teacher:, school:)
     end
   end
 end
