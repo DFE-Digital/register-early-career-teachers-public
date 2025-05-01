06693d8350690d9e178c6205376f1e3aa896b670
edujackedu <jack.scotti@education.gov.uk>
2025-05-01 14:49:30 +0100

    amend the InProgress status logic so it calls paused when the teacher is InProgress, has induction periods but no open induction period

--- spec/services/teachers/induction_status_spec.rb
+++ spec/services/teachers/induction_status_spec.rb
@@ -12,6 +12,49 @@ RSpec.describe Teachers::InductionStatus do
       it "returns the TRS induction status" do
         expect(service.induction_status).to eql('Exempt')
       end
+
+      it "returns the correct status color" do
+        expect(service.induction_status_colour).to eql('green')
+      end
+    end
+
+    context 'when the ECT has induction periods' do
+      let(:teacher) { FactoryBot.create(:teacher) }
+      let(:trs_induction_status) { 'InProgress' }
+
+      context 'with an ongoing induction period' do
+        let(:induction_periods) do
+          [
+            FactoryBot.create(:induction_period, :active, teacher:)
+          ]
+        end
+
+        it "has a status of 'Unknown'" do
+          expect(service.induction_status).to eql('Unknown')
+        end
+
+        it "has a grey status color" do
+          expect(service.induction_status_colour).to eql('grey')
+        end
+      end
+
+      context 'with a completed induction period' do
+        let(:induction_periods) do
+          [
+            FactoryBot.create(:induction_period, :pass, teacher:,
+                                                        started_on: 2.years.ago,
+                                                        finished_on: 1.year.ago)
+          ]
+        end
+
+        it "has a status of 'Unknown'" do
+          expect(service.induction_status).to eql('Unknown')
+        end
+
+        it "has a grey status color" do
+          expect(service.induction_status_colour).to eql('grey')
+        end
+      end
     end
   end
 
@@ -21,20 +64,60 @@ RSpec.describe Teachers::InductionStatus do
 
     context 'when there is no induction outcome' do
       {
-        "Exempt" => "Exempt",
-        "RequiredToComplete" => "Required to complete",
-        "InProgress" => "In progress",
-        "Failed" => "Failed",
-        "Passed" => "Passed",
-        "FailedInWales" => "Failed in Wales",
-        "None" => "None",
-      }.each do |trs_induction_status, our_description|
+        "Exempt" => { text: "Exempt", colour: "green" },
+        "RequiredToComplete" => { text: "Required to complete", colour: "yellow" },
+        "Failed" => { text: "Failed", colour: "red" },
+        "Passed" => { text: "Passed", colour: "green" },
+        "FailedInWales" => { text: "Failed in Wales", colour: "red" },
+        "None" => { text: "None", colour: "grey" },
+      }.each do |trs_induction_status, expected|
         context "when the trs_induction_status is #{trs_induction_status}" do
-          let(:our_description) { our_description }
           let(:trs_induction_status) { trs_induction_status }
 
-          it "has a status of '#{our_description}'" do
-            expect(service.induction_status).to eql(our_description)
+          it "has a status of '#{expected[:text]}'" do
+            expect(service.induction_status).to eql(expected[:text])
+          end
+
+          it "has a #{expected[:colour]} status color" do
+            expect(service.induction_status_colour).to eql(expected[:colour])
+          end
+        end
+      end
+
+      context "when the trs_induction_status is InProgress" do
+        let(:trs_induction_status) { "InProgress" }
+
+        context "when there is no open induction period" do
+          let(:induction_periods) do
+            [
+              FactoryBot.create(:induction_period,
+                                started_on: 2.years.ago,
+                                finished_on: 1.year.ago)
+            ]
+          end
+
+          it "has a status of 'Induction paused'" do
+            expect(service.induction_status).to eql("Induction paused")
+          end
+
+          it "has a pink status color" do
+            expect(service.induction_status_colour).to eql("pink")
+          end
+        end
+
+        context "when there is an open induction period" do
+          let(:induction_periods) do
+            [
+              FactoryBot.create(:induction_period, :active)
+            ]
+          end
+
+          it "has a status of 'In progress'" do
+            expect(service.induction_status).to eql("In progress")
+          end
+
+          it "has a blue status color" do
+            expect(service.induction_status_colour).to eql("blue")
           end
         end
       end
@@ -48,6 +131,10 @@ RSpec.describe Teachers::InductionStatus do
 
     it "has a status of 'Unknown'" do
       expect(service.induction_status).to eql('Unknown')
+    end
+
+    it "has a grey status color" do
+      expect(service.induction_status_colour).to eql('grey')
     end
   end
 
