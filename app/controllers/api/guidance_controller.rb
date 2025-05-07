75f573c48a04fd74f8eb50a6aa7014b21eb24254
Peter Yates <peter.yates@graphia.co.uk>
2025-05-07 15:49:33 +0100

    Add handler extra guidance pages
    This doesn't do anything fancy like extracting headings for TOC/nav etc
    just yet, but gives us a basic way to add pages without having to define
    custom controllers and routes for each one.

--- app/controllers/api/guidance_controller.rb
+++ app/controllers/api/guidance_controller.rb
@@ -2,9 +2,21 @@ module API
   class GuidanceController < ApplicationController
     skip_before_action :authenticate
 
-    layout 'api_guidance'
+    layout 'api_guidance', only: 'show'
 
     def show
+    end
+
+    def page
+      path = params[:page]
+
+      template = {
+        "page-1" => "page_1",
+        "page-2" => "page_2",
+        "page-3" => "page_3",
+      }.fetch(path)
+
+      render "api/guidance/" + template
     end
   end
 end
