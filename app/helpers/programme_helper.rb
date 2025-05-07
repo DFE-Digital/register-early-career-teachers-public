3f5226c6087fa62e02c7ecd0a28c55999c775c75
Leonidas Apostolidis <leoapost@gmail.com>
2025-05-07 09:56:56 +0100

    Rename the programme_choices helper to improve clarity

--- app/helpers/programme_helper.rb
+++ app/helpers/programme_helper.rb
@@ -3,7 +3,7 @@ module ProgrammeHelper
     ::PROGRAMME_TYPES.fetch(programme_type&.to_sym, 'Programme type is not recognised')
   end
 
-  def programme_choices(use_previous_ect_choices)
+  def previous_choice_message(use_previous_ect_choices)
     return "Yes, use the programme choices used by my school previously" if use_previous_ect_choices
 
     "No, don't use the programme choices used by my school previously"
