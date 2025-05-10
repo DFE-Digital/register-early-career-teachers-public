ddd6a219deeb0ec6f412a240cd2c9512eb56df4a
Peter Yates <peter.yates@graphia.co.uk>
2025-05-10 19:50:31 +0100

    Add Teachers::MentorFundingEligibility service
    This is intended to be the single place where we can evaluate a teacher
    to see whether or not they're eligible for mentor training.

--- app/wizards/schools/register_mentor_wizard/mentor.rb
+++ app/wizards/schools/register_mentor_wizard/mentor.rb
@@ -42,15 +42,7 @@ module Schools
       end
 
       def funding_available?
-        if (record = Teacher.find_by(trn:))
-          record.eligible_for_mentor_funding?
-        else
-          !check_ero_mentor.early_roll_out_mentor?
-        end
-      end
-
-      def check_ero_mentor
-        @check_ero_mentor ||= CheckEarlyRollOutMentor.new(trn)
+        Teachers::MentorFundingEligibility.new(trn:).eligible?
       end
 
       def register!(author:)
