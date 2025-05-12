c95b3be8e0d4a5dd0dd4c00ef2ee6b8002da1714
Peter Yates <peter.yates@graphia.co.uk>
2025-05-12 12:08:30 +0100

    Simplify eligibility check
    Now we're ensuring both mentor_became_ineligible_for_funding_reason and mentor_became_ineligible_for_funding_on
    we can just test for the presence of the reason.
    
    Co-authored-by: Lorenzo Tello <ltello8a@gmail.com>

--- app/services/teachers/mentor_funding_eligibility.rb
+++ app/services/teachers/mentor_funding_eligibility.rb
@@ -7,22 +7,9 @@ module Teachers
     end
 
     def eligible?
-      case
-      when teacher.nil?
-        # we don't know the teacher isn't eligible, so
-        # assume they are
-        true
-      when marked_as_ineligible?
-        false
-      else
-        true
-      end
-    end
+      return true if teacher.nil?
 
-  private
-
-    def marked_as_ineligible?
-      teacher.mentor_became_ineligible_for_funding_on.present? && teacher.mentor_became_ineligible_for_funding_reason.present?
+      teacher.mentor_became_ineligible_for_funding_reason.blank?
     end
   end
 end
