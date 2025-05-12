5cd64bfbd26c1da461ce27d63501d47351ba0eea
Avin Hurry <avinhurry@icloud.com>
2025-05-12 15:06:52 +0100

    [1324] Be explicit and use find_by
    This is less confusing and immediately obvious what is happening.

--- app/wizards/schools/register_ect_wizard/email_address_step.rb
+++ app/wizards/schools/register_ect_wizard/email_address_step.rb
@@ -18,7 +18,7 @@ module Schools
       end
 
       def previous_step
-        if Teacher.find_by_trn(@wizard.ect.trn)&.ect_at_school_periods&.exists?
+        if Teacher.find_by(trn: @wizard.ect.trn)&.ect_at_school_periods&.exists?
           :previous_ect_details
         else
           :review_ect_details
