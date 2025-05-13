9a3bab1d575c20a4edd074ae789c74b0335ada80
Avin Hurry <avinhurry@icloud.com>
2025-05-13 13:30:04 +0100

    [1324] Create helper method to determine `induction_start_date`
    This surfaces the induction start date from the earliest `InductionPeriod` for a teacher and formats its `started_on` date into GOV.UK format.
    
    This will return `nil` if non InductionPeriods exist.

--- spec/helpers/induction_helper_spec.rb
+++ spec/helpers/induction_helper_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe InductionHelper, type: :helper do
       expect(helper.claiming_body?(teacher, other_appropriate_body)).to be false
     end
   end
+
+  describe '#induction_start_date_for' do
+    let(:teacher) { FactoryBot.create(:teacher) }
+
+    context 'when the teacher has induction periods' do
+      before do
+        FactoryBot.create(:induction_period, teacher:, started_on: Date.new(2023, 6, 10), finished_on: Date.new(2023, 9, 30)) # earliest
+        FactoryBot.create(:induction_period, teacher:, started_on: Date.new(2023, 10, 1), finished_on: Date.new(2024, 4, 30))
+        FactoryBot.create(:induction_period, teacher:, started_on: Date.new(2024, 5, 1), finished_on: Date.new(2024, 6, 30))
+      end
+
+      it 'returns the earliest started_on date formatted as GOV.UK date' do
+        expect(helper.induction_start_date_for(teacher.trn)).to eq('10 June 2023')
+      end
+    end
+
+    context 'when the teacher has no induction periods' do
+      it 'returns nil' do
+        expect(helper.induction_start_date_for(teacher.trn)).to be_nil
+      end
+    end
+  end
 end
