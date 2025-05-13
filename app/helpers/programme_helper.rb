ce7c01481dd9f4254b2309955e91bfc08ee729e2
Avin Hurry <avinhurry@icloud.com>
2025-05-13 19:30:08 +0100

    [1324] Add `training_programme_for` method
    This helper looks up the latest `ECTAtSchoolPeriod` for a teacher (by `created_at`) and returns the training programme type.

--- app/helpers/programme_helper.rb
+++ app/helpers/programme_helper.rb
@@ -2,4 +2,14 @@ module ProgrammeHelper
   def programme_type_name(programme_type)
     ::PROGRAMME_TYPES.fetch(programme_type&.to_sym, 'Programme type is not recognised')
   end
+
+  def training_programme_for(trn)
+    teacher = Teacher.find_by(trn:)
+    return if teacher.blank?
+
+    programme_type = ECTAtSchoolPeriod.latest_for_teacher(teacher).pick(:programme_type)
+    return if programme_type.blank?
+
+    programme_type_name(programme_type)
+  end
 end
