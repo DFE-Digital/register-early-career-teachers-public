4622fab6ac2f107fef3bc1fde5c6a5290d2ad39b
Leandro Alemão <leandro.costantini@gmail.com>
2025-05-16 15:26:09 +0100

    Fix specs

--- spec/requests/api/v3/declarations_spec.rb
+++ spec/requests/api/v3/declarations_spec.rb
@@ -1,28 +1,28 @@
 RSpec.describe "Participant declarations API", type: :request do
   describe "#create" do
     it "returns method not allowed" do
-      post api_v3_declarations_path
+      api_post(api_v3_declarations_path)
       expect(response).to be_method_not_allowed
     end
   end
 
   describe "#index" do
     it "returns method not allowed" do
-      get api_v3_declarations_path
+      api_get(api_v3_declarations_path)
       expect(response).to be_method_not_allowed
     end
   end
 
   describe "#show" do
     it "returns method not allowed" do
-      get api_v3_declaration_path(123)
+      api_get(api_v3_declaration_path(123))
       expect(response).to be_method_not_allowed
     end
   end
 
   describe "#void" do
     it "returns method not allowed" do
-      put api_v3_declaration_void_path(123)
+      api_put(api_v3_declaration_void_path(123))
       expect(response).to be_method_not_allowed
     end
   end
