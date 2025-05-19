c38b700fced257aa2d1a51c31a03fdaa4f3142b2
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-05-19 09:00:47 +0100

    test reseed review GH workflow

--- lib/bulk_generate.rb
+++ lib/bulk_generate.rb
@@ -19,23 +19,39 @@ private
     @ects ||= trns.shuffle.take(sample_size)
   end
 
+  def claimable_ects
+    ects.select do |_trn, _dob, status|
+      %w[RequiredToComplete InProgress].include?(status)
+    end
+  end
+
+  def unclaimable_ects
+    ects.select do |_trn, _dob, status|
+      %w[Failed FailedInWales Exempt Passed None].include?(status)
+    end
+  end
+
   def columns_for(template)
     template.values.reject { |v| v.match?(/error/i) }
   end
 
   def claim_rows
-    ects.map do |trn, dob, _status|
-      [trn, dob, random_programme, random_date]
+    claimable_ects.map do |trn, dob, _status|
+      [trn, dob, random_programme, random_start_date]
     end
   end
 
   def action_rows
-    ects.map do |trn, dob, _status|
-      [trn, dob, random_date, random_term, random_outcome]
+    claimable_ects.map do |trn, dob, _status|
+      [trn, dob, random_end_date, random_term, random_outcome]
     end
   end
 
-  def random_date
+  def random_start_date
+    61.days.ago.to_date - rand(1..60)
+  end
+
+  def random_end_date
     Time.zone.today - rand(1..60)
   end
 
@@ -59,7 +75,8 @@ private
   end
 
   def sample_size
-    ::AppropriateBodies::ProcessBatchForm::MAX_ROW_SIZE
+    # ::AppropriateBodies::ProcessBatchForm::MAX_ROW_SIZE
+    500
   end
 
   def claim_template
