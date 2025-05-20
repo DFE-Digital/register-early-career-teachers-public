9305feb2df94a8517969fbae572e2a7e8beab496
Peter Yates <peter.yates@graphia.co.uk>
2025-05-20 13:59:26 +0100

    Add release notes page
    This is based on a YAML file which contains notes for a list of
    releases. Doing it with structured data means we can easily show the
    parts of individual notes if needed (i.e. we could show the most recent
    one on the main guidance page).

--- app/controllers/api/guidance_controller.rb
+++ app/controllers/api/guidance_controller.rb
@@ -7,6 +7,13 @@ module API
     def show
     end
 
+    def release_notes
+      @release_notes = YAML.load_file(
+        Rails.root.join('app/views/api/guidance/release_notes.yml'),
+        permitted_classes: [Date]
+      ).map { |note| API::ReleaseNote.new(**note.symbolize_keys) }
+    end
+
     def page
       path = params[:page]
 
---
+++ spec/features/api/guidance/viewing_the_release_notes_spec.rb
@@ -0,0 +1,33 @@
+RSpec.describe 'Viewing the release notes' do
+  let(:release_note_data) do
+    YAML.load_file(
+      Rails.root.join('app/views/api/guidance/release_notes.yml'),
+      permitted_classes: [Date]
+    ).map { |note| API::ReleaseNote.new(**note.symbolize_keys) }
+  end
+
+  scenario 'Displaying all the release notes' do
+    given_i_am_on_the_api_guidance_page
+    when_i_click_release_notes
+    then_i_should_see_an_entry_for_each_release_note
+  end
+
+private
+
+  def given_i_am_on_the_api_guidance_page
+    path = '/api/guidance'
+    page.goto(path)
+    expect(page.url).to end_with(path)
+  end
+
+  def when_i_click_release_notes
+    page.get_by_role('link', name: 'Release notes').click
+    expect(page.url).to end_with('/api/guidance/release-notes')
+  end
+
+  def then_i_should_see_an_entry_for_each_release_note
+    release_note_data.map(&:title).each do |title|
+      expect(page.get_by_role("heading", name: title)).to be_visible
+    end
+  end
+end
