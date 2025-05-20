bcfe5521031df4d0f169f5c9cfb300570b4b4add
Ross Oliver <ross.oliver36@gmail.com>
2025-05-20 15:28:23 +0100

    Add create for partnerships to test

--- app/controllers/api/v3/partnerships_controller.rb
+++ app/controllers/api/v3/partnerships_controller.rb
@@ -11,10 +11,33 @@ module API
         render json: to_json(partnerships_query.partnership(id: partnership_params[:id]))
       end
 
-      def create = head(:method_not_allowed)
+      def create
+        service = SchoolPartnerships::Create.new(
+          registration_year: declaration_params[:cohort],
+          school_ecf_id: declaration_params[:school_id],
+          delivery_partner_ecf_id: declaration_params[:delivery_partner_id],
+          lead_provider_ecf_id: current_lead_provider.ecf_id
+        )
+
+        if service.valid?
+          render json: to_json(service.create)
+        else
+          render json: API::Errors::Response.from(service), status: :unprocessable_entity
+        end
+      end
+
       def update = head(:method_not_allowed)
 
     private
+
+      def declaration_params
+        params
+          .require(:data)
+          .require(:attributes)
+          .permit(:cohort, :school_id, :delivery_partner_id)
+      rescue ActionController::ParameterMissing
+        raise ActionController::BadRequest, I18n.t(:invalid_data_structure)
+      end
 
       def partnerships_query
         conditions = { lead_provider: current_lead_provider }
