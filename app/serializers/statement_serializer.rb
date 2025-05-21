31923dee6aeccbd18799d6efe42c6e05f32db421
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-05-21 15:34:14 +0100

    [1648] Statement serializer with blueprinter

---
+++ app/serializers/statement_serializer.rb
@@ -0,0 +1,21 @@
+class StatementSerializer < Blueprinter::Base
+  class AttributesSerializer < Blueprinter::Base
+    exclude :id
+
+    field(:month) { |s, _| Date::MONTHNAMES[s.month] }
+    field(:year) { |s, _| s.year.to_s }
+    field(:cohort) { |s, _| s.active_lead_provider.registration_period.year.to_s }
+    field :deadline_date, name: :cut_off_date, datetime_format: "%Y-%m-%d"
+    field :payment_date, datetime_format: "%Y-%m-%d"
+    field(:paid?, name: :paid)
+    field :created_at
+    field :updated_at
+  end
+
+  identifier :api_id, name: :id
+  field(:type) { "statement" }
+
+  association :attributes, blueprint: AttributesSerializer do |statement|
+    statement
+  end
+end
