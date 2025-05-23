1da71d6cd2d13b22e24d6c181bbb5c22c9f65a62
Leandro Alemão <leandro.costantini@gmail.com>
2025-05-23 15:00:11 +0100

    [1652] Add API base and error handling

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,37 @@
 module API
-  class BaseController < ActionController::Base
+  class BaseController < ActionController::API
     include TokenAuthenticatable
+
+    before_action :set_cache_headers
+    before_action :remove_charset
+
+    include ActionController::MimeResponds
+
+    rescue_from ActionController::UnpermittedParameters, with: :unpermitted_parameter_response
+    rescue_from ActionController::BadRequest, with: :bad_request_response
+    rescue_from ArgumentError, with: :bad_request_response
+    rescue_from API::Errors::FilterValidationError, with: :filter_validation_error_response
+
+  private
+
+    def remove_charset
+      ActionDispatch::Response.default_charset = nil
+    end
+
+    def unpermitted_parameter_response(exception)
+      render json: { errors: API::Errors::Response.new(error: "Unpermitted parameters", params: exception.params).call }, status: :unprocessable_entity
+    end
+
+    def bad_request_response(exception)
+      render json: { errors: API::Errors::Response.new(error: "Bad request", params: exception.message).call }, status: :bad_request
+    end
+
+    def filter_validation_error_response(exception)
+      render json: { errors: API::Errors::Response.new(error: "Unpermitted parameters", params: exception.message).call }, status: :unprocessable_entity
+    end
+
+    def set_cache_headers
+      no_store
+    end
   end
 end
