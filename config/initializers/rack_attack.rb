6fb64d9d4d0792ac2274951d28116cf6d415f74b
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-05-27 12:52:43 +0100

    [1655] Making remote_ip changes from ecf1 PR #4085

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,19 @@
 class Rack::Attack
+  class Request < ::Rack::Request
+    def remote_ip
+      # This will always be present because the ActionDispatch::RemoteIp
+      # middleware runs long before this middleware. Use #fetch here
+      # so that we bail quickly if that middleware goes away or changes
+      # the field name. Have preferred this to instantiating a whole
+      # ActionDispatch::Request as that's a whole lot of work and this happens
+      # on every request.
+      #
+      # Favour the X-Real-IP header if set by Azure, if not fallback to
+      # ActionDispatch#remote_ip which is more reliable than Rack's ip method.
+      @remote_ip ||= env.fetch("x-real-ip", nil).presence || env.fetch("action_dispatch.remote_ip").to_s
+    end
+  end
+
   PROTECTED_ROUTES = %w[
     /otp-sign-in
     /otp-sign-in/code
@@ -21,17 +36,22 @@ class Rack::Attack
   end
 
   throttle("protected routes (OTP)", limit: 5, period: 20.seconds) do |request|
-    request.ip if protected_path?(request)
+    request.remote_ip if protected_path?(request)
   end
 
   # Throttle /api requests by auth token (1000 requests per 5 minutes)
   throttle("API requests by auth token", limit: 1000, period: 5.minutes) do |request|
-    auth_token(request) if api_request?(request)
+    if api_request?(request)
+      auth_token(request)
+    end
   end
+
+  # Fallback throttling for all other paths
+  throttle("All other requests by ip", limit: 1000, period: 5.minutes, &:remote_ip)
 end
 
 ActiveSupport::Notifications.subscribe("throttle.rack_attack") do |_name, _start, _finish, request_id, payload|
-  ip = payload.fetch(:request).ip
+  ip = payload.fetch(:request).remote_ip
   path = payload.fetch(:request).fullpath
 
   Rails.logger.warn("[rack-attack] Throttled request #{request_id} from #{ip} to '#{path}'")
