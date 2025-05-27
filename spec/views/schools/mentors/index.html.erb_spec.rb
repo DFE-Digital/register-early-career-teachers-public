4807065284651ad53f3f11c086c44bbf75dde6b2
Avin Hurry <avinhurry@icloud.com>
2025-05-27 15:13:17 +0100

    [1641] Add some missing view specs

--- spec/views/schools/mentors/index.html.erb_spec.rb
+++ spec/views/schools/mentors/index.html.erb_spec.rb
@@ -11,5 +11,34 @@ RSpec.describe 'schools/mentors/index.html.erb' do
     it 'shows a message that there are no mentors' do
       expect(rendered).to have_css('div.govuk-grid-column-two-thirds p.govuk-body', text: 'Your school currently has no registered mentors.')
     end
+
+    it 'shows the "assign" link' do
+      expect(rendered).to have_css('a.govuk-link', text: 'assign one to an ECT')
+    end
+
+    it 'does not render the summary component' do
+      expect(rendered).not_to have_css('.govuk-summary-card')
+    end
+  end
+
+  context 'when there are mentors' do
+    let(:teacher) { FactoryBot.create(:teacher, trs_first_name: 'Johnnie', trs_last_name: 'Walker') }
+    let(:mentor_period) { FactoryBot.create(:mentor_at_school_period, teacher:, school:) }
+
+    before do
+      assign(:school, school)
+      assign(:mentors, [teacher])
+      assign(:filtered_mentors, [teacher])
+      assign(:mentor_period_for_school, [mentor_period])
+      render
+    end
+
+    it 'shows the "assign" link' do
+      expect(rendered).to have_css('a.govuk-link', text: 'assign one to an ECT')
+    end
+
+    it 'renders the summary component' do
+      expect(rendered).to have_css('.govuk-summary-card__title', text: 'Johnnie Walker')
+    end
   end
 end
