d908f394e78977144a3c2bd033787ce1c242d267
Ross Oliver <ross.oliver36@gmail.com>
2025-05-29 10:54:07 +0100

    Implement `StatementsController` actions
    Hook up the `show` and `index` controller actions for statements to the
    existing query, serializer and filtering concerns.
    
    Update the route to accept the `api_id` as the `id` attribute.
    
    Add generic tests to cover these types of endpoints.

---
+++ spec/support/shared_contexts/api/index_endpoint.rb
@@ -0,0 +1,35 @@
+shared_examples "an index endpoint" do
+  context "when 2 resources exist for the lead provider" do
+    let!(:resources) do
+      [
+        create_resource(active_lead_provider:),
+        create_resource(active_lead_provider:)
+      ]
+    end
+
+    before do
+      # Resource for a different lead provider.
+      lead_provider = FactoryBot.create(:lead_provider, name: "Other Lead Provider")
+      registration_period = active_lead_provider.registration_period
+      create_resource(active_lead_provider: FactoryBot.create(:active_lead_provider, lead_provider:, registration_period:))
+    end
+
+    it "returns the correct resources in a serialized format" do
+      authenticated_api_get(path)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eql("application/json; charset=utf-8")
+      expect(response.body).to eq(serializer.render(apply_expected_order(resources), root: "data"))
+    end
+  end
+
+  context "when no resources exist for the lead provider" do
+    it "returns an empty result in serialized format" do
+      authenticated_api_get(path)
+
+      expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eql("application/json; charset=utf-8")
+      expect(response.body).to eq(serializer.render([], root: "data"))
+    end
+  end
+end
