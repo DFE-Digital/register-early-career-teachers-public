7c1793df94e4a8bb18c57ab0f578d7c5b3a915d4
Ross Oliver <ross.oliver36@gmail.com>
2025-05-29 12:42:27 +0100

    Only enable request spec timeouts in CI
    We want our request specs to fail fast in CI (rather than hang for long
    periods).
    
    When running the tests locally, however, this causes a timeout in the console
    when using breakpoints.
    
    Only timeout request specs in CI.

--- spec/support/testing_request_specs.rb
+++ spec/support/testing_request_specs.rb
@@ -1,5 +1,7 @@
 require 'timeout'
 
+return unless Rails.application.config.enable_request_specs_timeout
+
 RSpec.configure do |config|
   # Set a timeout for request specs
   config.add_setting :request_timeout, default: 3
