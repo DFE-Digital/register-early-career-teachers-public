10465f6a12d3620515ac94ae45b7e71dc149ac97
Leonidas Apostolidis <leoapost@gmail.com>
2025-05-30 15:41:53 +0100

    Display the correct year range for the registration date

--- app/helpers/register_ect_helper.rb
+++ app/helpers/register_ect_helper.rb
@@ -6,7 +6,14 @@ module RegisterECTHelper
     schools_register_ect_wizard_change_state_school_appropriate_body_path
   end
 
-  def formatted_academic_year_range(year)
+  def formatted_year_range_for_registration_date(date)
+    registration_period = RegistrationPeriod.ongoing_on(date).first
+    return "" if registration_period.blank?
+
+    academic_year_string(registration_period.year)
+  end
+
+  def academic_year_string(year)
     "#{year} to #{year + 1}"
   end
 end
