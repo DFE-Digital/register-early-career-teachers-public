2ba0d15011be4e7dc050519e750591996cc2eb8c
Peter Yates <peter.yates@graphia.co.uk>
2025-06-06 16:16:52 +0100

    Reconfigure scopes

--- app/models/school_partnership.rb
+++ app/models/school_partnership.rb
@@ -5,11 +5,6 @@ class SchoolPartnership < ApplicationRecord
   has_many :events
 
   # Validations
-
-  # Scopes
-  scope :for_registration_period, ->(year) { where(registration_period_id: year) }
-  scope :for_lead_provider, ->(lead_provider_id) { where(lead_provider_id:) }
-  scope :for_delivery_partner, ->(delivery_partner_id) { where(delivery_partner_id:) }
   validates :delivery_partner_id, presence: true
   validates :available_provider_pairing_id, presence: true
 end
