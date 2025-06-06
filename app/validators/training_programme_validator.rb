8672b862c24214c458b75878b2b879bbf7ed716b
Leonidas Apostolidis <leoapost@gmail.com>
2025-06-06 11:52:26 +0100

    Rename constants

--- app/validators/training_programme_validator.rb
+++ app/validators/training_programme_validator.rb
@@ -2,7 +2,7 @@ class TrainingProgrammeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if value.blank?
       record.errors.add(attribute, "Select either 'Provider-led' or 'School-led' training")
-    elsif !::PROGRAMME_TYPES.key?(value.to_sym)
+    elsif !::TRAINING_PROGRAMME.key?(value.to_sym)
       record.errors.add(attribute, "'#{value}' is not a valid programme type")
     end
   end
