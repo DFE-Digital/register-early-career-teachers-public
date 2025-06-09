3165167db301c1bd022d562a2570b6809a88394d
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-06-09 10:27:56 +0100

    Update failed batch action view content

---
+++ spec/views/appropriate_bodies/process_batch/actions/failed.html.erb_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe "appropriate_bodies/process_batch/actions/_failed.html.erb" do
+  let(:pending_induction_submission_batch) { FactoryBot.create(:pending_induction_submission_batch, :action, :failed) }
+
+  before do
+    assign(:pending_induction_submission_batch, pending_induction_submission_batch)
+
+    render
+  end
+
+  it 'links back to bulk actions overview' do
+    expect(rendered).to have_text("You'll need to try again")
+    expect(rendered).to have_link('Go back to your overview', href: ab_batch_actions_path)
+  end
+end
