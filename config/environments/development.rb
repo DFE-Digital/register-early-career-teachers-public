6ecffc71ec80f0c180f4865ade38c7df535393a8
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-06-09 16:58:15 +0100

    [1689] removed unneeded changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,5 +86,4 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :solid_queue
   # config.active_job.queue_name_prefix = "ecf2_development"
-  config.enable_api = true
 end
