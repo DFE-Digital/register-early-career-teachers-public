048db4709606e51295def775e636c632ca9f3314
Peter Yates <peter.yates@graphia.co.uk>
2025-06-10 23:31:15 +0100

    Reconfigure validations

--- app/models/school_partnership.rb
+++ app/models/school_partnership.rb
@@ -6,15 +6,5 @@ class SchoolPartnership < ApplicationRecord
   has_many :events
 
   # Validations
-  validates :registration_period_id,
-            presence: true,
-            uniqueness: { scope: %i[lead_provider_id delivery_partner_id],
-                          message: "has already been added" }
-
-  validates :lead_provider_id,
-            presence: true
-
-  validates :delivery_partner_id,
-            presence: true
-
+  validates :lead_provider_delivery_partnership_id, presence: true
 end
--- spec/models/school_partnership_spec.rb
+++ spec/models/school_partnership_spec.rb
@@ -9,30 +9,6 @@ describe SchoolPartnership do
   describe "validations" do
     subject { FactoryBot.create(:school_partnership) }
 
-    it { is_expected.to validate_presence_of(:registration_period_id) }
-    it { is_expected.to validate_presence_of(:lead_provider_id) }
-    it { is_expected.to validate_presence_of(:delivery_partner_id) }
-
-    context "uniqueness of registration_period scoped to lead_provider_id and delivery_partner_id" do
-      context "when the provider partnership matches the lead_provider_id, delivery_partner_id and registration_period values
-               of an existing provider partnership" do
-        subject { FactoryBot.build(:school_partnership, registration_period_id:, lead_provider_id:, delivery_partner_id:) }
-
-        let!(:existing_partnership) { FactoryBot.create(:school_partnership) }
-        let(:registration_period_id) { existing_partnership.registration_period_id }
-        let(:lead_provider_id) { existing_partnership.lead_provider_id }
-        let(:delivery_partner_id) { existing_partnership.delivery_partner_id }
-
-        before do
-          subject.valid?
-        end
-
-        it "add an error" do
-          expect(subject.errors.messages).to include(registration_period_id: ["has already been added"])
-        end
-      end
-    end
-  end
-
+    it { is_expected.to validate_presence_of(:lead_provider_delivery_partnership_id) }
   end
 end
