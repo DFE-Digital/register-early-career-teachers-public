17642144312a9a7c453de19a61e6a4e850bcbea5
Peter Yates <peter.yates@graphia.co.uk>
2025-06-10 23:37:40 +0100

    Update seeds to work with new schema

--- db/seeds/school_partnerships.rb
+++ db/seeds/school_partnerships.rb
@@ -1,38 +1,17 @@
-def describe_school_partnership(pp)
-  print_seed_info("#{pp.lead_provider.name} (lead_provider) 🤝 #{pp.delivery_partner.name} (delivery_partner) in #{pp.registration_period.year}", indent: 2)
+def describe_school_partnership(sp)
+  delivery_partner_name = sp.lead_provider_delivery_partnership.delivery_partner.name
+  lead_provider_name = sp.lead_provider_delivery_partnership.active_lead_provider.lead_provider.name
+  registration_period_year = sp.lead_provider_delivery_partnership.active_lead_provider.registration_period.year
+  school_name = "Some School" # sp.school.gias_school.name
+
+  print_seed_info("#{school_name} has partnered with:")
+  print_seed_info("🤝 lead provider: #{lead_provider_name}", indent: 4)
+  print_seed_info("🤝 delivery partner: #{delivery_partner_name}", indent: 4)
+  print_seed_info("🤝 registration period: #{registration_period_year}", indent: 4)
 end
 
-lead_providers = {
-  ambitious_institute: LeadProvider.find_by!(name: 'Ambitious Institute'),
-  teach_fast: LeadProvider.find_by!(name: 'Teach Fast'),
-  better_practice_network: LeadProvider.find_by!(name: 'Better Practice Network'),
-}
+lead_provider_delivery_partnerships = LeadProviderDeliveryPartnership.eager_load(:delivery_partner, active_lead_provider: %i[lead_provider registration_period])
 
-delivery_partners = {
-  artisan: DeliveryPartner.find_by(name: "Artisan Education Group"),
-  grain: DeliveryPartner.find_by(name: "Grain Teaching School Hub"),
-  rising_minds: DeliveryPartner.find_by(name: "Rising Minds Network")
-}
-
-registration_periods = {
-  2021 => RegistrationPeriod.find_by(year: 2021),
-  2022 => RegistrationPeriod.find_by(year: 2022),
-  2023 => RegistrationPeriod.find_by(year: 2023),
-  2024 => RegistrationPeriod.find_by(year: 2024)
-}
-
-[
-  { year: 2021, lead_provider: :ambitious_institute, delivery_partner: :artisan },
-  { year: 2022, lead_provider: :ambitious_institute, delivery_partner: :artisan },
-  { year: 2023, lead_provider: :ambitious_institute, delivery_partner: :artisan },
-  { year: 2022, lead_provider: :teach_fast, delivery_partner: :grain },
-  { year: 2023, lead_provider: :teach_fast, delivery_partner: :grain },
-  { year: 2023, lead_provider: :better_practice_network, delivery_partner: :rising_minds },
-  { year: 2024, lead_provider: :better_practice_network, delivery_partner: :rising_minds }
-].each do |data|
-  SchoolPartnership.create!(
-    registration_period: registration_periods[data[:year]],
-    lead_provider: lead_providers[data[:lead_provider]],
-    delivery_partner: delivery_partners[data[:delivery_partner]]
-  ).tap { |pp| describe_school_partnership(pp) }
+lead_provider_delivery_partnerships.each do |lead_provider_delivery_partnership|
+  SchoolPartnership.create!(lead_provider_delivery_partnership:).tap { describe_school_partnership(it) }
 end
