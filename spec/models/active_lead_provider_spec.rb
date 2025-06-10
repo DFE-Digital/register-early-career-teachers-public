1017ed0819654d7040697a708f7bad49f05ffe15
Peter Yates <peter.yates@graphia.co.uk>
2025-06-10 23:29:58 +0100

    Reconfigure scopes

--- spec/models/active_lead_provider_spec.rb
+++ spec/models/active_lead_provider_spec.rb
@@ -12,4 +12,27 @@ describe ActiveLeadProvider do
     it { is_expected.to validate_presence_of(:registration_period_id).with_message("Choose a registration period") }
     it { is_expected.to validate_uniqueness_of(:registration_period_id).scoped_to(:lead_provider_id).with_message("Registration period and lead provider must be unique") }
   end
+
+  describe "scopes" do
+    let!(:rp_1) { FactoryBot.create(:registration_period) }
+    let!(:rp_2) { FactoryBot.create(:registration_period) }
+    let!(:lp_1) { FactoryBot.create(:lead_provider) }
+    let!(:lp_2) { FactoryBot.create(:lead_provider) }
+    let!(:active_lead_provider_1) { FactoryBot.create(:active_lead_provider, registration_period: rp_1, lead_provider: lp_1) }
+    let!(:active_lead_provider_2) { FactoryBot.create(:active_lead_provider, registration_period: rp_1, lead_provider: lp_2) }
+    let!(:active_lead_provider_3) { FactoryBot.create(:active_lead_provider, registration_period: rp_2, lead_provider: lp_1) }
+    let!(:active_lead_provider_4) { FactoryBot.create(:active_lead_provider, registration_period: rp_2, lead_provider: lp_2) }
+
+    describe ".for_registration_period" do
+      it "returns provider partnerships only for the specified academic year" do
+        expect(described_class.for_registration_period(rp_1.id)).to contain_exactly(active_lead_provider_1, active_lead_provider_2)
+      end
+    end
+
+    describe ".for_lead_provider" do
+      it "returns provider partnerships only for the specified lead provider" do
+        expect(described_class.for_lead_provider(lp_2.id)).to contain_exactly(active_lead_provider_2, active_lead_provider_4)
+      end
+    end
+  end
 end
--- spec/models/school_partnership_spec.rb
+++ spec/models/school_partnership_spec.rb
@@ -34,34 +34,5 @@ describe SchoolPartnership do
     end
   end
 
-  describe "scopes" do
-    let!(:ay_1) { FactoryBot.create(:registration_period) }
-    let!(:ay_2) { FactoryBot.create(:registration_period) }
-    let!(:lp_1) { FactoryBot.create(:lead_provider) }
-    let!(:lp_2) { FactoryBot.create(:lead_provider) }
-    let!(:dp_1) { FactoryBot.create(:delivery_partner) }
-    let!(:dp_2) { FactoryBot.create(:delivery_partner) }
-    let!(:partnership_1) { FactoryBot.create(:school_partnership, registration_period: ay_1, lead_provider: lp_1, delivery_partner: dp_1) }
-    let!(:partnership_2) { FactoryBot.create(:school_partnership, registration_period: ay_1, lead_provider: lp_1, delivery_partner: dp_2) }
-    let!(:partnership_3) { FactoryBot.create(:school_partnership, registration_period: ay_1, lead_provider: lp_2, delivery_partner: dp_1) }
-    let!(:partnership_4) { FactoryBot.create(:school_partnership, registration_period: ay_2, lead_provider: lp_1, delivery_partner: dp_2) }
-
-    describe ".for_registration_period" do
-      it "returns provider partnerships only for the specified academic year" do
-        expect(described_class.for_registration_period(ay_1.id)).to contain_exactly(partnership_1, partnership_2, partnership_3)
-      end
-    end
-
-    describe ".for_lead_provider" do
-      it "returns provider partnerships only for the specified lead provider" do
-        expect(described_class.for_lead_provider(lp_2.id)).to contain_exactly(partnership_3)
-      end
-    end
-
-    describe ".for_delivery_partner" do
-      it "returns provider partnerships only for the specified delivery partner" do
-        expect(described_class.for_delivery_partner(dp_1.id)).to contain_exactly(partnership_1, partnership_3)
-      end
-    end
   end
 end
