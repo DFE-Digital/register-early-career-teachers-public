ba51deb80853cdc20747b9e453d23b93081f27b9
Leandro Alemão <leandro.costantini@gmail.com>
2025-06-11 11:24:08 +0100

    [1597] Tweak specs

--- spec/migration/migrators/statement_spec.rb
+++ spec/migration/migrators/statement_spec.rb
@@ -5,7 +5,7 @@ describe Migrators::Statement do
     end
 
     def create_resource(migration_resource)
-      lead_provider = FactoryBot.create(:lead_provider, name: migration_resource.lead_provider.name)
+      lead_provider = FactoryBot.create(:lead_provider, name: migration_resource.lead_provider.name, api_id: migration_resource.lead_provider.id)
       registration_period = FactoryBot.create(:registration_period, year: migration_resource.cohort.start_year)
       active_lead_provider = FactoryBot.create(:active_lead_provider, lead_provider:, registration_period:)
 
@@ -15,14 +15,15 @@ describe Migrators::Statement do
     def setup_failure_state
       # Statement to be migrated with no output_fee.
       migration_statement = FactoryBot.create(:migration_statement, output_fee: nil)
-      FactoryBot.create(:lead_provider, name: migration_statement.lead_provider.name)
+      FactoryBot.create(:lead_provider, name: migration_statement.lead_provider.name, api_id: migration_statement.lead_provider.id)
     end
 
     describe "#migrate!" do
       it "sets the created statement attributes correctly" do
         instance.migrate!
+
         statement = Statement.find_by(api_id: migration_resource1.id)
-        expect(statement).to have_attributes(migration_resource1.attributes.slice(:deadline_date, :payment_date, :output_fee, :marked_as_paid_at, :created_at, :updated_at))
+        expect(statement).to have_attributes(migration_resource1.attributes.slice("deadline_date", "payment_date", "output_fee", "marked_as_paid_at", "created_at", "updated_at"))
         expect(statement.month).to eq(2)
         expect(statement.year).to eq(2025)
         expect(statement.registration_period.year).to eq(migration_resource1.cohort.start_year)
