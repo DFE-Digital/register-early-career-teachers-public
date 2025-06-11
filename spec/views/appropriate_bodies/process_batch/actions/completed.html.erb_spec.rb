bae3e70695d26b1634b5ed05c9e72b00104ed7ed
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-06-11 10:44:31 +0100

    Add extra view specs

---
+++ spec/views/appropriate_bodies/process_batch/actions/completed.html.erb_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe "appropriate_bodies/process_batch/actions/_completed.html.erb" do
+  let(:pending_induction_submission_batch) { FactoryBot.create(:pending_induction_submission_batch, :action, :completed) }
+
+  before do
+    render locals: { batch: pending_induction_submission_batch }
+  end
+
+  it 'sets the page title' do
+    expect(view.content_for(:page_title)).to eql('Outcomes successfully recorded')
+  end
+
+  it 'links back to bulk actions overview' do
+    expect(rendered).to have_link('Go back to your overview', href: ab_batch_actions_path)
+  end
+end
