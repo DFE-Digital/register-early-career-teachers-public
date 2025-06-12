dbfba659653ef16a3bd0c276cf2d50bf59f78631
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-06-12 12:05:41 +0100

    [1749] Adjustments component

---
+++ app/components/admin/statements/adjustments.rb
@@ -0,0 +1,20 @@
+module Admin
+  module Statements
+    class Adjustments < ViewComponent::Base
+      delegate :number_to_pounds, to: :helpers
+
+      delegate :adjustments, :adjustment_editable?,
+               to: :statement
+
+      attr_accessor :statement
+
+      def initialize(statement:)
+        @statement = statement
+      end
+
+      def total_amount
+        adjustments.sum(&:amount)
+      end
+    end
+  end
+end
