c97f6bc1ddb504cfc3e557a9f6ec989ecff43358
Ross Oliver <ross.oliver36@gmail.com>
2025-06-12 15:32:41 +0100

    Add `ParityCheck::Client` and associated models/services
    We want to abstract the complexities involved with making parity check requests
    into a `Client`. The client will be responsible for taking an endpoint
    definition, constructing valid requests, making the requests and returning the
    results along with request duration information.
    
    Add `Endpoint` model to describe a callable endpoint.
    
    Add `RequestBuilder` service to take an `Endpoint` and construct the different
    aspects of a request (path, headers, etc).
    
    Add `Client` to perform the requests with `HTTParty`. To begin with we will
    only support basic GET endpoints.

---
+++ app/migration/parity_check/endpoint.rb
@@ -0,0 +1,18 @@
+module ParityCheck
+  class Endpoint
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+
+    attribute :method
+    attribute :path
+    attribute :options, default: {}
+
+    def options=(value)
+      super(value || {})
+    end
+
+    def method=(value)
+      super(value&.to_sym)
+    end
+  end
+end
