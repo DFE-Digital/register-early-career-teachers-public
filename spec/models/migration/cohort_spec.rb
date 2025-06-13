34fabf5b45cb6f32c42b4c8658d2798dcf32cb33
Leandro Alemão <leandro.costantini@gmail.com>
2025-06-13 09:55:31 +0100

    [1593] Add migration cohort model specs

---
+++ spec/models/migration/cohort_spec.rb
@@ -0,0 +1,11 @@
+describe Migration::Cohort, type: :model do
+  subject { FactoryBot.create(:migration_cohort) }
+
+  describe "#next" do
+    let!(:next_cohort) { FactoryBot.create(:migration_cohort, start_year: subject.start_year + 1) }
+
+    it "returns the next cohort of the given cohort" do
+      expect(subject.next).to eq(next_cohort)
+    end
+  end
+end
