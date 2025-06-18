995e56b190d8c9ec83b5acb91f3e305024962ace
Peter Yates <peter.yates@graphia.co.uk>
2025-06-18 14:18:52 +0100

    Rework ECTAtSchoolPeriods::Training
    This change updates the service class so it adheres to the new
    schema. It now eager loads the whole set of training data up front so
    we're not firing query after query while traversing.

--- app/services/ect_at_school_periods/training.rb
+++ app/services/ect_at_school_periods/training.rb
@@ -1,44 +1,74 @@
 module ECTAtSchoolPeriods
   class Training
-    attr_reader :ect_at_school_period
+    attr_reader :ect_at_school_period, :latest_training_period
 
     def initialize(ect_at_school_period)
       @ect_at_school_period = ect_at_school_period
+
+      @latest_training_period ||= ect_at_school_period.training_periods
+                                                      .eager_load(**eager_load_confirmed_partnership_tables)
+                                                      .started_before(Date.tomorrow)
+                                                      .latest_first
+                                                      .first
     end
 
     def current_training_period
       latest_training_period if latest_training_period&.ongoing?
     end
 
-    # current_delivery_partner
-    delegate :delivery_partner, to: :current_training_period, allow_nil: true, prefix: :current
+    def current_lead_provider
+      current_training_period
+        &.school_partnership
+        &.lead_provider_delivery_partnership
+        &.active_lead_provider
+        &.lead_provider
+    end
+
+    def current_delivery_partner
+      current_training_period
+        &.school_partnership
+        &.lead_provider_delivery_partnership
+        &.delivery_partner
+    end
 
     # current_delivery_partner_name
     delegate :name, to: :current_delivery_partner, allow_nil: true, prefix: true
 
-    # current_lead_provider
-    delegate :lead_provider, to: :current_training_period, allow_nil: true, prefix: :current
-
     # current_lead_provider_name
     delegate :name, to: :current_lead_provider, allow_nil: true, prefix: true
 
-    def latest_training_period
-      @latest_training_period ||= ect_at_school_period.training_periods
-                                                      .started_before(Date.tomorrow)
-                                                      .latest_first
-                                                      .first
+    def latest_lead_provider
+      latest_training_period
+        &.school_partnership
+        &.lead_provider_delivery_partnership
+        &.active_lead_provider
+        &.lead_provider
     end
 
-    # latest_delivery_partner
-    delegate :delivery_partner, to: :latest_training_period, allow_nil: true, prefix: :latest
+    def latest_delivery_partner
+      latest_training_period
+        &.school_partnership
+        &.lead_provider_delivery_partnership
+        &.delivery_partner
+    end
 
     # latest_delivery_partner_name
     delegate :name, to: :latest_delivery_partner, allow_nil: true, prefix: true
 
-    # latest_lead_provider
-    delegate :lead_provider, to: :latest_training_period, allow_nil: true, prefix: :latest
-
     # latest_lead_provider_name
     delegate :name, to: :latest_lead_provider, allow_nil: true, prefix: true
+
+  private
+
+    def eager_load_confirmed_partnership_tables
+      {
+        school_partnership: {
+          lead_provider_delivery_partnership: [
+            :delivery_partner,
+            { active_lead_provider: %i[lead_provider registration_period] }
+          ]
+        }
+      }
+    end
   end
 end
