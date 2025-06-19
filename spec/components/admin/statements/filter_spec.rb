b8e1663eb526236da74fd241aa29aca3531dc5fa
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-06-19 16:00:29 +0100

    [1776] Statements selector component

---
+++ spec/components/admin/statements/filter_spec.rb
@@ -0,0 +1,88 @@
+RSpec.describe Admin::Statements::Filter, type: :component do
+  let(:filter_params) { {} }
+  let(:component) { described_class.new filter_params: }
+
+  context ".lead_providers" do
+    let!(:lead_provider1) { FactoryBot.create(:lead_provider, name: "C") }
+    let!(:lead_provider2) { FactoryBot.create(:lead_provider, name: "A") }
+    let!(:lead_provider3) { FactoryBot.create(:lead_provider, name: "B") }
+
+    it "returns lead providers in alphabetical order" do
+      expect(component.lead_providers).to contain_exactly(lead_provider2, lead_provider3, lead_provider1)
+    end
+  end
+
+  context ".lead_provider_id" do
+    let(:filter_params) { { lead_provider_id: 123 } }
+
+    it "returns filter param lead_provider_id" do
+      expect(component.lead_provider_id).to eq(123)
+    end
+  end
+
+  context ".registration_periods" do
+    let!(:registration_period1) { FactoryBot.create(:registration_period, year: 2025) }
+    let!(:registration_period2) { FactoryBot.create(:registration_period, year: 2021) }
+    let!(:registration_period3) { FactoryBot.create(:registration_period, year: 2022) }
+
+    it "returns registration period in year order" do
+      expect(component.registration_periods).to contain_exactly(registration_period2, registration_period3, registration_period1)
+    end
+  end
+
+  context ".registration_period_id" do
+    let(:filter_params) { { registration_period_id: 2025 } }
+
+    it "returns filter param registration_period_id" do
+      expect(component.registration_period_id).to eq(2025)
+    end
+  end
+
+  context ".statement_dates" do
+    let!(:statement1) { FactoryBot.create(:statement, year: 2025, month: 5) }
+    let!(:statement2) { FactoryBot.create(:statement, year: 2024, month: 5) }
+    let!(:statement3) { FactoryBot.create(:statement, year: 2023, month: 5) }
+
+    it "returns statement dates in ascending order" do
+      dates = component.statement_dates
+      expect(dates[0].id).to eq("2023-5")
+      expect(dates[0].name).to eq("May 2023")
+
+      expect(dates[1].id).to eq("2024-5")
+      expect(dates[1].name).to eq("May 2024")
+
+      expect(dates[2].id).to eq("2025-5")
+      expect(dates[2].name).to eq("May 2025")
+    end
+  end
+
+  context ".statement_date" do
+    let(:filter_params) { { statement_date: "2025-01" } }
+
+    it "returns filter param statement_date" do
+      expect(component.statement_date).to eq("2025-01")
+    end
+  end
+
+  context ".statement_types" do
+    it "returns statement types" do
+      dates = component.statement_types
+      expect(dates[0].id).to eq("all")
+      expect(dates[0].name).to eq("All")
+
+      expect(dates[1].id).to eq("output_fee")
+      expect(dates[1].name).to eq("Output statements")
+
+      expect(dates[2].id).to eq("service_fee")
+      expect(dates[2].name).to eq("Service fee statements")
+    end
+  end
+
+  context ".statement_type" do
+    let(:filter_params) { { statement_type: "all" } }
+
+    it "returns filter param statement_type" do
+      expect(component.statement_type).to eq("all")
+    end
+  end
+end
---
+++ spec/features/admin/finance/statements/selector_spec.rb
@@ -0,0 +1,102 @@
+RSpec.describe "Admin finance statement selector" do
+  before do
+    sign_in_as_dfe_user(role: :admin)
+
+    given_statement_exist_with_dropdown_options
+    when_i_visit_the_statement_page
+    then_i_should_see_statement_with_selected_options
+  end
+
+  scenario "Select a statement that exists" do
+    when_i_select_a_different_statement
+    then_i_am_redirected_different_statement
+  end
+
+  scenario "Select a statement that does not exist" do
+    when_i_select_a_statement_that_does_not_exist
+    then_i_am_redirected_to_statement_not_found_page
+  end
+
+  def given_statement_exist_with_dropdown_options
+    @lead_provider1 = FactoryBot.create(:lead_provider)
+    @registration_period1 = FactoryBot.create(:registration_period)
+    @active_lead_provider1 = FactoryBot.create(:active_lead_provider, lead_provider: @lead_provider1, registration_period: @registration_period1)
+    @statement1 = FactoryBot.create(:statement, active_lead_provider: @active_lead_provider1, year: 2024, month: 7)
+
+    @lead_provider2 = FactoryBot.create(:lead_provider)
+    @registration_period2 = FactoryBot.create(:registration_period)
+    @active_lead_provider2 = FactoryBot.create(:active_lead_provider, lead_provider: @lead_provider2, registration_period: @registration_period2)
+    @statement2 = FactoryBot.create(:statement, active_lead_provider: @active_lead_provider2, year: 2025, month: 5)
+  end
+
+  def when_i_visit_the_statement_page
+    page.goto(admin_finance_statement_path(@statement1))
+  end
+
+  def then_i_should_see_statement_with_selected_options
+    data = selector_options
+
+    lead_provider_selected = data["Lead provider"].detect { |op| op[:selected] }[:value]
+    expect(lead_provider_selected.to_i).to eq(@lead_provider1.id)
+
+    registration_period_selected = data["Contract year"].detect { |op| op[:selected] }[:value]
+    expect(registration_period_selected.to_i).to eq(@registration_period1.id)
+
+    statement_date_selected = data["Statement date"].detect { |op| op[:selected] }[:value]
+    expect(statement_date_selected).to eq([@statement1.year, @statement1.month].join("-"))
+  end
+
+  def when_i_select_a_different_statement
+    selector = page.locator(".admin-statements-filter")
+
+    elem = selector.get_by_label("Lead provider", exact: true)
+    elem.select_option(label: @lead_provider2.name)
+
+    elem = selector.get_by_label("Contract year", exact: true)
+    elem.select_option(label: @registration_period2.year.to_s)
+
+    elem = selector.get_by_label("Statement date", exact: true)
+    elem.select_option(label: "May 2025")
+
+    selector.get_by_role('button', name: "View").click
+  end
+
+  def when_i_select_a_statement_that_does_not_exist
+    selector = page.locator(".admin-statements-filter")
+
+    elem = selector.get_by_label("Lead provider", exact: true)
+    elem.select_option(label: @lead_provider2.name)
+
+    elem = selector.get_by_label("Contract year", exact: true)
+    elem.select_option(label: @registration_period1.year.to_s)
+
+    elem = selector.get_by_label("Statement date", exact: true)
+    elem.select_option(label: "July 2024")
+
+    selector.get_by_role('button', name: "View").click
+  end
+
+  def then_i_am_redirected_different_statement
+    expect(page.url).to end_with(admin_finance_statement_path(@statement2))
+  end
+
+  def then_i_am_redirected_to_statement_not_found_page
+    expect(page.get_by_text("No financial statements found")).to be_visible
+    expect(page.url.split("?").first).to end_with(admin_finance_statements_path)
+  end
+
+  def selector_options
+    page.query_selector_all(".admin-statements-filter .filter-form-group").each_with_object({}) do |group, data|
+      label = group.query_selector("label").text_content
+      options = group.query_selector_all("option").map do |op|
+        {
+          value: op.get_attribute("value"),
+          text: op.text_content,
+          selected: (op.get_attribute("selected") == "selected"),
+        }
+      end
+
+      data[label] = options
+    end
+  end
+end
