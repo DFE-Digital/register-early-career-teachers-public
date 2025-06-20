d2b50ab38adf06d9adae118973a33828a93c2db3
Ross Oliver <ross.oliver36@gmail.com>
2025-06-20 21:25:36 +0100

    Extract dynamic request content from RequestBuilder
    To avoid the `RequestBuilder` bloating with all the dynamic request
    content we will be adding as we add new endpoints, it makes sense to
    extract them to their own class to keep things cleaner.

---
+++ app/migration/parity_check/dynamic_request_content.rb
@@ -0,0 +1,40 @@
+module ParityCheck
+  class DynamicRequestContent
+    class UnrecognizedIdentifierError < RuntimeError; end
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+
+    attribute :lead_provider
+
+    def fetch(identifier)
+      raise UnrecognizedIdentifierError, "Identifier not recognized: #{identifier}" unless respond_to?(identifier, true)
+
+      send(identifier)
+    end
+
+  private
+
+    # Path ID methods
+
+    def statement_id
+      Statement
+        .joins(:active_lead_provider)
+        .where(active_lead_provider: lead_provider.active_lead_providers)
+        .order("RANDOM()")
+        .pick(:api_id)
+    end
+
+    # Request body methods
+
+    def example_statement_body
+      {
+        data: {
+          type: "statements",
+          attributes: {
+            content: "This is an example request body.",
+          },
+        },
+      }
+    end
+  end
+end
