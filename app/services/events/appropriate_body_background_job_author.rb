e3c8896c6d27c322abbf4e5162ea8b0d132d20a2
Peter Yates <peter.yates@graphia.co.uk>
2025-06-24 11:54:01 +0100

    Add a new author type for AB job usage
    We ran into a problem in production because the Persona users guard
    against production use. Instead we've created a new user type that's
    permitted to record events.
    
    Co-authored-by: Peter Hamilton <pete@peterdavidhamilton.com>
    Co-authored-by: Jack Scotti <156658023+edujackedu@users.noreply.github.com>

---
+++ app/services/events/appropriate_body_background_job_author.rb
@@ -0,0 +1,18 @@
+class Events::AppropriateBodyBackgroundJobAuthor
+  attr_reader :email, :name, :appropriate_body_id
+
+  def initialize(email:, name:, appropriate_body_id:)
+    @email = email
+    @name = name
+    @appropriate_body_id = appropriate_body_id
+  end
+
+  def author_params
+    {
+      email:,
+      name:,
+      appropriate_body_id:,
+      author_type: 'appropriate_body_user',
+    }
+  end
+end
