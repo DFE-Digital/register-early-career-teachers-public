3a5ceaf735b45d9ee55e14e8f1428860743c2e8a
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-06-24 07:41:55 +0100

    Align bulk feature specs

--- spec/features/appropriate_bodies/process_batch_action_spec.rb
+++ spec/features/appropriate_bodies/process_batch_action_spec.rb
@@ -36,13 +36,11 @@ RSpec.describe 'Process bulk actions' do
 
     context 'with valid CSV file' do
       scenario 'creates a pending submission for each row' do
-        # This job does validation first, leaving the user to confirm with a CTA
         perform_enqueued_jobs
         page.reload
         expect(page.get_by_text('CSV file summary')).to be_visible
-
-        # NB: these will have failed because we have not factoried the ECTs and their inductions
         expect(page.get_by_text("Your CSV named 'valid_complete_action.csv' has 2 ECTs")).to be_visible
+        expect(page.get_by_role('link', name: 'Download CSV with error messages included')).to be_visible
       end
 
       scenario 'displays progress as batch is processing and submission records are created' do
@@ -62,7 +60,6 @@ RSpec.describe 'Process bulk actions' do
 
         expect(batch.progress).to eq(50.0)
         page.reload
-        # NB: not Hotwire capable
         expect(page.get_by_text('50%')).to be_visible
       end
     end
@@ -114,15 +111,14 @@ RSpec.describe 'Process bulk actions' do
         let(:corrected_file_path) { Rails.root.join("spec/fixtures/#{corrected_file_name}").to_s }
 
         scenario 'should allow uploading the corrected file' do
-          # First upload fails as expected
           then_i_should_see_the_error('The selected file must follow the template')
-          # Now try to upload a corrected file
           when_i_upload_a_file(corrected_file_path)
 
           perform_enqueued_jobs
           page.reload
           expect(page.get_by_text('CSV file summary')).to be_visible
           expect(page.get_by_text("Your CSV named 'valid_complete_action.csv' has 2 ECTs")).to be_visible
+          expect(page.get_by_role('link', name: 'Download CSV with error messages included')).to be_visible
         end
       end
     end
