cc85413c60fc53278b8a1f0c08b693c278983b72
Ross Oliver <ross.oliver36@gmail.com>
2025-06-25 14:37:12 +0100

    Add completed parity checks page
    Add a new route/page to show completed parity check runs.
    
    Link to the new page from the start page when there are completed runs present.
    
    Display key/high-level information about each run.
    
    Paginate the results in case we end up with a lot of retained runs.
    
    Add breadcrumbs to make navigation easier (eventually the parity check views
    will become quite nested when we drill down a run -> requests -> responses ->
    diffs).

---
+++ spec/features/migration/completed_parity_checks_spec.rb
@@ -0,0 +1,73 @@
+RSpec.describe "Completed parity checks" do
+  include ActionView::Helpers::DateHelper
+
+  before do
+    FactoryBot.create(:lead_provider)
+    sign_in_as_dfe_user(role: :admin)
+    allow(Rails.application.config).to receive(:parity_check).and_return({ enabled: true })
+  end
+
+  scenario "Viewing completed parity checks" do
+    completed_run = FactoryBot.create(:parity_check_run, :completed)
+
+    statements_endpoint = FactoryBot.create(:parity_check_endpoint, path: "/api/v1/statements")
+    user_endpoint = FactoryBot.create(:parity_check_endpoint, path: "/api/v1/users/:id")
+
+    FactoryBot.create(:parity_check_request, :completed, response_types: %i[matching different], run: completed_run, endpoint: statements_endpoint)
+    FactoryBot.create(:parity_check_request, :completed, response_types: %i[matching matching], run: completed_run, endpoint: user_endpoint)
+
+    page.goto(new_migration_parity_check_path)
+    page.get_by_role("link", name: "View completed runs").click
+
+    expect(page.get_by_role("heading", name: "Completed parity checks")).to be_visible
+
+    expect(page.get_by_text(/In concurrent mode/)).not_to be_visible
+    page.get_by_text("How the run mode affects performance").click
+    expect(page.get_by_text(/In concurrent mode/)).to be_visible
+
+    page.screenshot(path: "completed_parity_checks.png")
+
+    completed_run_details = page.locator("table tbody")
+    expect(completed_run_details.get_by_text(completed_run.id.to_s)).to be_visible
+    expect(completed_run_details.get_by_text("Statements")).to be_visible
+    expect(completed_run_details.get_by_text("Users")).to be_visible
+    expect(completed_run_details.get_by_text("3 minutes")).to be_visible
+    expect(completed_run_details.get_by_text("Concurrent")).to be_visible
+    expect(completed_run_details.get_by_text("50%")).to be_visible
+    expect(completed_run_details.get_by_text(/faster|slower|equal/)).to be_visible
+
+    expect(page.locator(".govuk-pagination")).not_to be_visible
+  end
+
+  scenario "Paginating the completed parity checks when there are many" do
+    FactoryBot.create_list(:parity_check_run, Pagy::DEFAULT[:limit] + 1, :completed)
+
+    page.goto(completed_migration_parity_checks_path)
+
+    expect(page.locator("tbody tr")).to have_count(Pagy::DEFAULT[:limit])
+
+    pagination = page.locator(".govuk-pagination")
+    expect(pagination).to be_visible
+
+    expect(pagination.get_by_role("link", name: "1")).to be_visible
+    expect(pagination.get_by_role("link", name: "2")).to be_visible
+
+    pagination.get_by_role("link", name: "Next").click
+    expect(page.locator("tbody tr")).to have_count(1)
+  end
+
+  scenario "Viewing completed parity checks when there are none" do
+    page.goto(new_migration_parity_check_path)
+
+    expect(page.get_by_role("link", name: "View completed runs")).not_to be_visible
+
+    page.goto(completed_migration_parity_checks_path)
+
+    expect(page.get_by_role("heading", name: "Completed parity checks")).to be_visible
+    expect(page.get_by_text("There are no completed parity checks.")).to be_visible
+
+    breadcrumbs = page.locator(".govuk-breadcrumbs")
+    breadcrumbs.get_by_role("link", name: "Run a parity check").click
+    expect(page.url).to end_with(new_migration_parity_check_path)
+  end
+end
--- spec/helpers/parity_check_helper_spec.rb
+++ spec/helpers/parity_check_helper_spec.rb
@@ -1,22 +1,60 @@
 RSpec.describe ParityCheckHelper, type: :helper do
   describe "#grouped_endpoints" do
-    subject(:grouping) { grouped_endpoints(endpoints) }
+    subject(:grouping) { helper.grouped_endpoints(endpoints) }
 
     let(:endpoints) do
       [
         FactoryBot.build(:parity_check_endpoint, path: "/api/v1/users"),
         FactoryBot.build(:parity_check_endpoint, path: "/api/v3/users/create"),
-        FactoryBot.build(:parity_check_endpoint, path: "/api/v2/statements"),
+        FactoryBot.build(:parity_check_endpoint, path: "/api/v2/participant-declarations"),
         FactoryBot.build(:parity_check_endpoint, path: "/login"),
       ]
     end
 
     it "groups endpoints by their group name" do
-      expect(grouping).to eq({
+      expect(grouping).to eq(
         users: endpoints[0..1],
-        statements: [endpoints[2]],
+        "participant-declarations": [endpoints[2]],
         miscellaneous: [endpoints[3]]
-      })
+      )
+    end
+  end
+
+  describe "#formatted_endpoint_group_name" do
+    subject { helper.formatted_endpoint_group_name(:"participant-declarations") }
+
+    it { is_expected.to eq("Participant declarations") }
+  end
+
+  describe "#formatted_endpoint_group_names" do
+    subject { helper.formatted_endpoint_group_names(run) }
+
+    let(:run) { FactoryBot.build(:parity_check_run) }
+
+    before { allow(run).to receive(:request_group_names).and_return(%i[participant-declarations users]) }
+
+    it { is_expected.to eq(%(<ul class=\"govuk-list\"><li>Participant declarations</li><li>Users</li></ul>)) }
+  end
+
+  describe "#match_rate_tag" do
+    subject { helper.match_rate_tag(run) }
+
+    let(:run) { FactoryBot.build(:parity_check_run, :completed) }
+
+    {
+      0 => "red",
+      49 => "red",
+      50 => "orange",
+      74 => "orange",
+      75 => "yellow",
+      99 => "yellow",
+      100 => "green"
+    }.each do |match_rate, colour|
+      context "when match rate is #{match_rate}%" do
+        before { allow(run).to receive(:match_rate).and_return(match_rate) }
+
+        it { is_expected.to eq(%(<strong class=\"govuk-tag govuk-tag--#{colour}\">#{match_rate}%</strong>)) }
+      end
     end
   end
 
