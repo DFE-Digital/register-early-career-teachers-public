32f6b3e3bf17c17fd5f3442e636f8ed7b4abb697
Lorenzo Tello <ltello8a@gmail.com>
2025-06-27 15:58:51 +0100

    add factory for the new models

--- spec/factories/gias/school_factory.rb
+++ spec/factories/gias/school_factory.rb
@@ -10,6 +10,7 @@ FactoryBot.define do
     establishment_number { Faker::Number.unique.within(range: 1..9_999) }
     phase_name { "Phase one" }
     urn { Faker::Number.unique.within(range: 10_000..9_999_999) }
+    ukprn { Faker::Number.unique.number(digits: 5) }
 
     # eligibility to be registered in the service
     trait(:eligible_for_registration) do
