477da096998c2080d8b49b9c9cc237f6d43ac1ec
Tony Headford <tony@binarycircus.com>
2025-07-09 12:59:42 +0100

    Remove unused method

--- app/controllers/migration/model_failures_controller.rb
+++ app/controllers/migration/model_failures_controller.rb
@@ -32,22 +32,11 @@ private
   end
 
   def model_grouped_failures(model)
-    failures = migration_failures_for_model(model:)
+    failures = MigrationFailure.joins(:data_migration).where(data_migration: { model: })
     failure_group.new(model, failures.count, failures.group(:failure_message).count.sort.to_h)
-  end
-
-  def generic_grouped_failures
-    migrator_models.map do |model|
-      failures = migration_failures_for_model(model:)
-      failure_group.new(model, failures.count, failures.group(:failure_message).count.sort.to_h)
-    end
   end
 
   def failure_group
     @failure_group ||= Data.define(:model, :failure_count, :failures)
-  end
-
-  def migration_failures_for_model(model:)
-    MigrationFailure.joins(:data_migration).where(data_migration: { model: })
   end
 end
