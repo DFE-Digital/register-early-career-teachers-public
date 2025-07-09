249e89ed9bf4e3af55940d351975170b4bf94ff8
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-09 09:39:56 +0100

    [1784] Add GIAS helper

---
+++ app/models/concerns/gias_helpers.rb
@@ -0,0 +1,17 @@
+module GIASHelpers
+  extend ActiveSupport::Concern
+
+  included do
+    scope :in_gias_schools, -> { includes(:gias_school).references(:gias_schools) }
+
+    scope :currently_open, -> { in_gias_schools.where(gias_school: { status: GIAS::Types::OPEN_STATUSES }) }
+    scope :eligible_establishment_type, -> { in_gias_schools.where(gias_school: { type_name: GIAS::Types::ELIGIBLE_TYPES }) }
+    scope :in_england, -> { in_gias_schools.where(gias_school: { in_england: true }) }
+    scope :section_41, -> { in_gias_schools.where(gias_school: { section_41_approved: true }) }
+    scope :eligible, -> { currently_open.eligible_establishment_type.in_england.or(currently_open.in_england.section_41) }
+  end
+
+  def independent? = GIAS::Types::INDEPENDENT_SCHOOLS_TYPES.include?(type_name)
+
+  def state_funded? = GIAS::Types::STATE_SCHOOL_TYPES.include?(type_name)
+end
