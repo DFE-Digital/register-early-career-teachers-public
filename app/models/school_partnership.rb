25302c7e1e6f94d8652382231bc00b93cdcfc92e
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-09 16:06:09 +0100

    [1783] Add schools training programme service class

--- app/models/school_partnership.rb
+++ app/models/school_partnership.rb
@@ -3,6 +3,8 @@ class SchoolPartnership < ApplicationRecord
   belongs_to :lead_provider_delivery_partnership, inverse_of: :school_partnerships
   belongs_to :school
   has_many :events
+  has_one :active_lead_provider, through: :lead_provider_delivery_partnership
+  has_one :contract_period, through: :active_lead_provider
 
   # delegates
   delegate :lead_provider, :delivery_partner, :contract_period, to: :lead_provider_delivery_partnership
@@ -16,5 +18,7 @@ class SchoolPartnership < ApplicationRecord
               message: 'School and lead provider delivery partnership combination must be unique'
             }
 
+  # Scopes
   scope :earliest_first, -> { order(created_at: 'asc') }
+  scope :for_contract_period, ->(year) { joins(:contract_period).where(contract_periods: { year: }) }
 end
