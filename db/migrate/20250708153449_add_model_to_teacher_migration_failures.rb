d673db1e332f456fc1cb9f60fa0c352570797d4b
Tony Headford <tony@binarycircus.com>
2025-07-09 13:50:07 +0100

    Rubocop fix

--- db/migrate/20250708153449_add_model_to_teacher_migration_failures.rb
+++ db/migrate/20250708153449_add_model_to_teacher_migration_failures.rb
@@ -1,6 +1,6 @@
 class AddModelToTeacherMigrationFailures < ActiveRecord::Migration[8.0]
   def change
-    add_column :teacher_migration_failures, :model, :string, null: false
+    add_column :teacher_migration_failures, :model, :string, null: false, default: "teacher"
     add_index :teacher_migration_failures, :model
   end
 end
