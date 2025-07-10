6f6ac85b152d01624866e4b4b667fa9552466e29
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-07-10 10:52:05 +0100

    [1249] mentoring at new school only and started on steps

---
+++ app/wizards/schools/register_mentor_wizard/mentoring_at_new_school_only_step.rb
@@ -0,0 +1,34 @@
+module Schools
+  module RegisterMentorWizard
+    class MentoringAtNewSchoolOnlyStep < Step
+      attr_accessor :mentoring_at_new_school_only
+
+      validates :mentoring_at_new_school_only,
+                inclusion: { in: %w[yes no],
+                             message: "Select 'Yes' or 'No' to confirm whether they will be mentoring at our school only" }
+
+      def self.permitted_params
+        %i[mentoring_at_new_school_only]
+      end
+
+      def next_step
+        # Ask school: are they mentoring at new school only?
+        if mentoring_at_new_school_only == "yes"
+          :started_on
+        else
+          :check_answers
+        end
+      end
+
+      def previous_step
+        :email_address
+      end
+
+    private
+
+      def persist
+        mentor.update(mentoring_at_new_school_only:)
+      end
+    end
+  end
+end
--- app/wizards/schools/register_mentor_wizard/review_mentor_eligibility_step.rb
+++ app/wizards/schools/register_mentor_wizard/review_mentor_eligibility_step.rb
@@ -2,11 +2,36 @@ module Schools
   module RegisterMentorWizard
     class ReviewMentorEligibilityStep < Step
       def next_step
-        :check_answers
+        return :check_answers unless has_mentor_at_school_periods?
+
+        if has_open_mentor_at_school_period_at_another_school?
+          # Ask school: are they mentoring at new school only?
+          :mentoring_at_new_school_only
+        else
+          # Ask school: when will mentor start?
+          :started_on
+        end
       end
 
       def previous_step
         :email_address
+      end
+
+    private
+
+      def mentor_at_school_periods
+        ::MentorAtSchoolPeriod.includes(:teacher).where(teacher: { trn: mentor.trn })
+      end
+
+      # Does mentor have any previous mentor_at_school_periods (open or closed)?
+      def has_mentor_at_school_periods?
+        mentor_at_school_periods.exists?
+      end
+
+      # Does mentor have an open mentor_at_school_period at another school?
+      def has_open_mentor_at_school_period_at_another_school?
+        finishes_in_the_future_scope = ::MentorAtSchoolPeriod.finished_on_or_after(Date.tomorrow)
+        mentor_at_school_periods.where.not(school: mentor.school).ongoing.or(finishes_in_the_future_scope).exists?
       end
     end
   end
