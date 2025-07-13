b740fd340b759946c1775bcb1583488235c49774
Peter Yates <peter.yates@graphia.co.uk>
2025-07-13 11:18:06 +0100

    Fix batch processing tests
    These tests relied on the 'randomising' behaviours of the fake client
    before it was split. Now shared contexts creating teachers in the right
    state are used to ensure required data is present instead.
    
    Co-authored-by: peterdavidhamilton <pete@peterdavidhamilton.com>

--- spec/requests/appropriate_bodies/process_batch/claims/update_spec.rb
+++ spec/requests/appropriate_bodies/process_batch/claims/update_spec.rb
@@ -11,43 +11,64 @@ RSpec.describe "Appropriate Body bulk claims confirmation", type: :request do
                       file_name: 'test-file.csv')
   end
 
-  include_context 'fake trs api client'
-  include_context '2 valid claims'
+  include_context 'test trs api client'
 
   describe 'PATCH /appropriate-body/bulk/claims/:batch_id' do
-    it "enqueues a job" do
-      expect {
+    context 'when both claims are valid' do
+      include_context '2 valid claims'
+
+      it "enqueues a job" do
+        expect {
+          put ab_batch_claim_path(batch)
+        }.to have_enqueued_job(ProcessBatchClaimJob).with(batch, user.email, user.name)
+      end
+
+      it "records an upload completed event" do
+        allow(Events::Record).to receive(:record_bulk_upload_completed_event!).and_call_original
+
         put ab_batch_claim_path(batch)
-      }.to have_enqueued_job(ProcessBatchClaimJob).with(batch, user.email, user.name)
+
+        expect(Events::Record).to have_received(:record_bulk_upload_completed_event!).with(
+          batch:,
+          author: an_instance_of(Sessions::Users::AppropriateBodyPersona)
+        )
+
+        perform_enqueued_jobs
+
+        expect(Event.last.event_type).to eq("bulk_upload_completed")
+        expect(Event.last.pending_induction_submission_batch.id).to eq(batch.id)
+      end
+
+      it "redirects" do
+        put ab_batch_claim_path(batch)
+
+        expect(response).to redirect_to(ab_batch_claim_path(batch))
+        follow_redirect!
+        expect(response.body).to include("We're processing your CSV file, it could take up to 5 minutes.")
+
+        perform_enqueued_jobs
+        get ab_batch_claim_path(batch)
+
+        expect(response.body).to include("Your CSV named 'test-file.csv' has 2 ECT records that you can claim")
+      end
     end
 
-    it "records an upload completed event" do
-      allow(Events::Record).to receive(:record_bulk_upload_completed_event!).and_call_original
+    context 'when 1 valid and one invalid claim' do
+      include_context '1 valid and 1 invalid claim'
 
-      put ab_batch_claim_path(batch)
+      it "redirects" do
+        put ab_batch_claim_path(batch)
 
-      expect(Events::Record).to have_received(:record_bulk_upload_completed_event!).with(
-        batch:,
-        author: an_instance_of(Sessions::Users::AppropriateBodyPersona)
-      )
+        expect(response).to redirect_to(ab_batch_claim_path(batch))
+        follow_redirect!
+        expect(response.body).to include("We're processing your CSV file, it could take up to 5 minutes.")
 
-      perform_enqueued_jobs
+        perform_enqueued_jobs
+        get ab_batch_claim_path(batch)
 
-      expect(Event.last.event_type).to eq("bulk_upload_completed")
-      expect(Event.last.pending_induction_submission_batch.id).to eq(batch.id)
-    end
-
-    it "redirects" do
-      put ab_batch_claim_path(batch)
-
-      expect(response).to redirect_to(ab_batch_claim_path(batch))
-      follow_redirect!
-      expect(response.body).to include("We're processing your CSV file, it could take up to 5 minutes.")
-
-      perform_enqueued_jobs
-      get ab_batch_claim_path(batch)
-      # NB: these will have failed because we have not factoried the ECTs and their inductions
-      expect(response.body).to include("Your CSV named 'test-file.csv' has 2 ECTs with errors")
+        expect(response.body).to include("Your CSV named 'test-file.csv' has 1 ECT records that you can claim")
+        expect(response.body).to include("You have 1 ECTs with errors")
+      end
     end
   end
 end
