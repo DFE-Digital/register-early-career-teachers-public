f04a0fc01537fbb669fa955a962325063fc7e924
Peter Yates <peter.yates@graphia.co.uk>
2025-07-13 11:18:06 +0100

    Fix batch processing tests
    These tests relied on the 'randomising' behaviours of the fake client
    before it was split. Now shared contexts creating teachers in the right
    state are used to ensure required data is present instead.
    
    Co-authored-by: peterdavidhamilton <pete@peterdavidhamilton.com>

--- spec/support/shared_contexts/csv_file.rb
+++ spec/support/shared_contexts/csv_file.rb
@@ -11,6 +11,24 @@ RSpec.shared_context '2 valid claims' do
   end
 end
 
+RSpec.shared_context '1 valid and 1 invalid claim' do
+  let(:file_name) do
+    '1 valid 1 invalid claim.csv'
+  end
+
+  let(:data) do
+    [
+      { trn: '1234567', date_of_birth: '1981-06-30', induction_programme: 'fip', started_on: '2025-01-30', error: '' },
+      { trn: '7654321', date_of_birth: '1981-06-30', induction_programme: 'CIP', started_on: '2025-01-30', error: '' }
+    ]
+  end
+
+  before do
+    already_claimed_teacher = FactoryBot.create(:teacher, trn: '1234567')
+    FactoryBot.create(:induction_period, :active, teacher: already_claimed_teacher)
+  end
+end
+
 RSpec.shared_context '3 valid actions' do
   let(:file_name) do
     '3 valid actions.csv'
