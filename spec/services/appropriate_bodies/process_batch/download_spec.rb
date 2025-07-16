f05d6e906d5d622764209986ef71e95e179ef60c
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-07-16 17:03:25 +0100

    move all download file responsibility to the service

--- spec/services/appropriate_bodies/process_batch/download_spec.rb
+++ spec/services/appropriate_bodies/process_batch/download_spec.rb
@@ -1,16 +1,46 @@
 RSpec.describe AppropriateBodies::ProcessBatch::Download do
-  subject(:service) do
-    described_class.new(pending_induction_submission_batch:)
-  end
+  subject(:service) { described_class.new(pending_induction_submission_batch:) }
 
-  # let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
   let(:appropriate_body) { FactoryBot.build(:appropriate_body) }
 
   let(:pending_induction_submission_batch) do
-    # FactoryBot.create(:pending_induction_submission_batch, :action,
     FactoryBot.build(:pending_induction_submission_batch, :action,
                      appropriate_body:,
+                     file_name: 'foo.csv',
                      data:)
+  end
+
+  let(:data) do
+    [
+      {
+        trn: '1234567',
+        date_of_birth: '',
+        finished_on: '2023-12-31',
+        number_of_terms: '2.0',
+        outcome: 'pass',
+        error: ''
+      },
+      {
+        trn: '7654321',
+        date_of_birth: '1980-01-01',
+        finished_on: '2023-12-31',
+        number_of_terms: '2.0',
+        outcome: 'pass',
+        error: ''
+      }
+    ]
+  end
+
+  describe '#type' do
+    it 'prefixes the original uploaded file' do
+      expect(service.type).to eq('text/csv')
+    end
+  end
+
+  describe '#filename' do
+    it 'adds a prefix to the original uploaded filename' do
+      expect(service.filename).to eq('Errors for foo.csv')
+    end
   end
 
   describe '#to_csv' do
@@ -30,69 +60,42 @@ RSpec.describe AppropriateBodies::ProcessBatch::Download do
       end
     end
 
-    context 'when there is persisted CSV data' do
-      let(:data) do
-        [
-          {
-            trn: '1234567',
-            date_of_birth: '',
-            finished_on: '2023-12-31',
-            number_of_terms: '2.0',
-            outcome: 'pass',
-            error: ''
-          },
-          {
-            trn: '7654321',
-            date_of_birth: '1980-01-01',
-            finished_on: '2023-12-31',
-            number_of_terms: '2.0',
-            outcome: 'pass',
-            error: ''
-          }
-        ]
+    context 'without failed submissions' do
+      it 'returns CSV content with the correct headers, quoted cells, and no rows' do
+        expect(service.to_csv).to eq(
+          <<~CSV_DATA
+            "TRN","Date of birth","Induction period end date","Number of terms","Outcome","Error message"
+          CSV_DATA
+        )
+      end
+    end
+
+    context 'with failed submissions' do
+      before do
+        FactoryBot.create(:pending_induction_submission,
+                          appropriate_body:,
+                          pending_induction_submission_batch:,
+                          trn: '1234567',
+                          error_messages: [
+                            'error one',
+                            'error two',
+                            'error three',
+                            'error four'
+                          ])
+
+        FactoryBot.create(:pending_induction_submission,
+                          appropriate_body:,
+                          pending_induction_submission_batch:,
+                          trn: '7654321')
       end
 
-      it 'returns a CSV string' do
-        expect(service.to_csv).to be_a(String)
-      end
-
-      context 'without failed submissions' do
-        it 'returns CSV content with the correct headers, quoted cells, and no rows' do
-          expect(service.to_csv).to eq(
-            <<~CSV_DATA
-              "TRN","Date of birth","Induction period end date","Number of terms","Outcome","Error message"
-            CSV_DATA
-          )
-        end
-      end
-
-      context 'with failed submissions' do
-        before do
-          FactoryBot.create(:pending_induction_submission,
-                            appropriate_body:,
-                            pending_induction_submission_batch:,
-                            trn: '1234567',
-                            error_messages: [
-                              'error one',
-                              'error two',
-                              'error three',
-                              'error four'
-                            ])
-
-          FactoryBot.create(:pending_induction_submission,
-                            appropriate_body:,
-                            pending_induction_submission_batch:,
-                            trn: '7654321')
-        end
-
-        it 'returns only failed submissions with their errors as a sentence' do
-          expect(service.to_csv).to eq(
-            <<~CSV_DATA
-              "TRN","Date of birth","Induction period end date","Number of terms","Outcome","Error message"
-              "1234567","","2023-12-31","2.0","pass","error one, error two, error three, and error four"
-            CSV_DATA
-          )
-        end
+      it 'returns CSV content with the correct headers, quoted cells, and failed submissions with their errors' do
+        expect(service.to_csv).to eq(
+          <<~CSV_DATA
+            "TRN","Date of birth","Induction period end date","Number of terms","Outcome","Error message"
+            "1234567","","2023-12-31","2.0","pass","error one, error two, error three, and error four"
+          CSV_DATA
+        )
       end
     end
   end
