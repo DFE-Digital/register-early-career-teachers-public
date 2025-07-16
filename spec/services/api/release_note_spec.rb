e2671d0ee52e936fa7949869947d9367843d3fbc
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-16 08:44:54 +0100

    [1767] Add release note tags functionality

--- spec/services/api/release_note_spec.rb
+++ spec/services/api/release_note_spec.rb
@@ -1,21 +1,27 @@
 describe API::ReleaseNote do
-  describe 'initialization' do
-    subject(:release_note) { API::ReleaseNote.new(title:, date:, body:) }
+  describe "initialization" do
+    subject(:release_note) { API::ReleaseNote.new(title:, date:, body:, tags:) }
 
-    let(:title) { 'A title' }
+    let(:title) { "A title" }
     let(:date) { Date.new(2024, 1, 1) }
-    let(:body) { 'Some body text' }
+    let(:body) { "Some body text" }
+    let(:tags) { ["#bug-fix", "#new-feature"] }
 
-    it 'is initialized with title, date and body' do
+    it "is initialized with title, date, body and tags" do
       expect(release_note.title).to eql(title)
       expect(release_note.date).to eql(date.to_formatted_s(:govuk))
       expect(release_note.body).to eql(%(<p class="govuk-body-m">#{body}</p>))
+      expect(release_note.tags).to eql(
+        %(<div class=\"tag-group\">) +
+        %(<strong class=\"govuk-tag govuk-tag--green govuk-!-font-weight-bold\">NEW FEATURE</strong>) +
+        %(<strong class=\"govuk-tag govuk-tag--yellow govuk-!-font-weight-bold\">BUG FIX</strong></div>)
+      )
     end
 
-    context 'when the body contains markdown' do
-      let(:body) { 'Some **body** text' }
+    context "when the body contains markdown" do
+      let(:body) { "Some **body** text" }
 
-      it 'the body contains rendered markdown' do
+      it "the body contains rendered markdown" do
         expected_body_with_markdown = %(<p class="govuk-body-m">Some <strong>body</strong> text</p>)
         expect(release_note.body).to eql(expected_body_with_markdown)
       end
