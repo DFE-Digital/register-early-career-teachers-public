da04be5b6330e5a4b06fd6872079ce2f28ed2983
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-07-16 18:13:32 +0100

    [1249] MentorAtSchoolPeriods::LatestRegistrationChoices service

---
+++ spec/services/mentor_at_school_periods/latest_registration_choices_spec.rb
@@ -0,0 +1,20 @@
+describe MentorAtSchoolPeriods::LatestRegistrationChoices do
+  subject { MentorAtSchoolPeriods::LatestRegistrationChoices.new(trn: teacher.trn) }
+
+  let(:teacher) { FactoryBot.create(:teacher) }
+  let(:school_partnership) { FactoryBot.create(:school_partnership) }
+  let(:mentor_at_school_period) { FactoryBot.create(:mentor_at_school_period, teacher:) }
+  let!(:training_period) { FactoryBot.create(:training_period, :for_mentor, school_partnership:, mentor_at_school_period:) }
+
+  describe '#school' do
+    it { expect(subject.school).to eq(school_partnership.school) }
+  end
+
+  describe '#lead_provider' do
+    it { expect(subject.lead_provider).to eq(school_partnership.lead_provider) }
+  end
+
+  describe '#delivery_partner' do
+    it { expect(subject.delivery_partner).to eq(school_partnership.delivery_partner) }
+  end
+end
