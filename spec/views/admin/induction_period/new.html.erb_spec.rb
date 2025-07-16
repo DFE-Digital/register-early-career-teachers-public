c71174ab53f6f1c36f0fc6f076690c7bfaab7641
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-07-16 13:43:08 +0100

    Add flag and spec on admin new induction form

--- spec/views/admin/induction_period/new.html.erb_spec.rb
+++ spec/views/admin/induction_period/new.html.erb_spec.rb
@@ -1,10 +1,11 @@
-RSpec.describe 'admin/induction_periods/edit.html.erb' do
+RSpec.describe 'admin/induction_periods/new.html.erb' do
   let(:ect) { FactoryBot.create(:ect_at_school_period, :active) }
   let(:back_path) { admin_teacher_path(ect.teacher) }
-  let(:induction_period) { FactoryBot.create(:induction_period, teacher: ect.teacher) }
+  let(:induction_period) { FactoryBot.build(:induction_period, teacher: ect.teacher) }
 
   before do
     assign(:induction_period, induction_period)
+    assign(:teacher, ect.teacher)
   end
 
   it 'includes a back button that links to the ECT admin page' do
