9510bbea900fb1a06adac44d8a9b57981ed1086d
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-16 08:44:54 +0100

    [1767] Add release note tags functionality

--- app/services/api/release_note.rb
+++ app/services/api/release_note.rb
@@ -1,17 +1,22 @@
 module API
   class ReleaseNote
-    attr_accessor :title, :date, :body
+    attr_accessor :title, :date, :body, :tags
 
-    def initialize(title:, date:, body:)
+    def initialize(title:, date:, body:, tags:)
       @title = title
       @date = date.to_formatted_s(:govuk)
       @body = render(body)
+      @tags = render_tags(tags)
     end
 
   private
 
     def render(markdown)
       GovukMarkdown.render(markdown.to_str, { strip_front_matter: false, headings_start_with: 'l' })
+    end
+
+    def render_tags(tags)
+      API::TagsRenderer.render(tags)
     end
   end
 end
