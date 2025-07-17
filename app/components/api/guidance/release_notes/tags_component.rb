a3d604057e1a39d1a5b54d82e78546be664981a7
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-17 11:32:09 +0100

    [1767] Replace new-course tag by removed-field tag

--- app/components/api/guidance/release_notes/tags_component.rb
+++ app/components/api/guidance/release_notes/tags_component.rb
@@ -11,7 +11,7 @@ module API
           "new-field" => "turquoise",
           "data-update" => "blue",
           "contract-closure" => "orange",
-          "new-course" => "pink",
+          "removed-field" => "pink",
           "production-release" => "purple",
           "sandbox-release" => "grey",
         }.freeze
