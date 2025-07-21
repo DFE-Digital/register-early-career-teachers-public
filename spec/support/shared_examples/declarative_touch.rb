2250ec6e93fd530edfa10a8f1da85f44a3a68185
Ross Oliver <ross.oliver36@gmail.com>
2025-07-21 14:08:51 +0100

    Update partnership logic for touching `School#api_updated_at`
    We want to touch the school's `api_updated_at` when any partnership is
    created/destroyed instead of just the first/last.
    
    This is due to the contract period scoping on the endpoint; we'd rather
    over-touch the timestamp than under-touch it (which can happen if a partnership
    is added for a new contract period and there are already partnerships against
    the school for other periods).
    
    Update `DeclarativeTouch` concern to support touching on specific events
    (create/update/destroy).

--- spec/support/shared_examples/declarative_touch.rb
+++ spec/support/shared_examples/declarative_touch.rb
@@ -1,46 +1,78 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "a declarative touch model" do |when_changing:, timestamp_attribute:|
-  when_changing.each do |attribute_to_change|
-    context "when the #{attribute_to_change} attribute changes" do
-      let(:new_value) do
-        column = instance.class.columns_hash[attribute_to_change.to_s]
-        Faker::Types.send("rb_#{column.type}")
-      end
+RSpec.shared_examples "a declarative touch model" do |when_changing: [], on_event: %i[update], timestamp_attribute: :updated_at|
+  if :update.in?(on_event)
+    before { instance } # Ensure its created first.
 
-      before do
-        will_change_attribute(attribute_to_change:, new_value:) if defined?(will_change_attribute)
-      end
+    when_changing.each do |attribute_to_change|
+      context "when the #{attribute_to_change} attribute changes" do
+        let(:new_value) do
+          column = instance.class.columns_hash[attribute_to_change.to_s]
+          Faker::Types.send("rb_#{column.type}")
+        end
 
-      it "touches the #{timestamp_attribute} of the associated model(s)" do
-        expect {
-          instance.update_attribute(attribute_to_change, new_value)
-        }.to(change { Array.wrap(target).map { |t| t.reload.send(timestamp_attribute) } }.to(all(be_within(5.seconds).of(Time.current))))
-      end
+        before do
+          will_change_attribute(attribute_to_change:, new_value:) if defined?(will_change_attribute)
+        end
 
-      it "does not touch the updated_at of the associated model(s)" do
-        # If the target is the same as the instance the updated_at will always be updated.
-        unless instance == target
+        it "touches the #{timestamp_attribute} of the associated model(s)" do
           expect {
             instance.update_attribute(attribute_to_change, new_value)
-          }.not_to(change { Array.wrap(target).map { |t| t.reload.updated_at } })
+          }.to(change { Array.wrap(target).map { |t| t.reload.send(timestamp_attribute) } }.to(all(be_within(5.seconds).of(Time.current))))
+        end
+
+        it "does not touch the updated_at of the associated model(s)" do
+          # If the target is the same as the instance the updated_at will always be updated.
+          unless instance == target
+            expect {
+              instance.update_attribute(attribute_to_change, new_value)
+            }.not_to(change { Array.wrap(target).map { |t| t.reload.updated_at } })
+          end
+        end
+
+        context "when the target is nil" do
+          let(:target) { nil }
+
+          it "does not raise an error" do
+            expect { instance.update_attribute(attribute_to_change, new_value) }.not_to raise_error
+          end
         end
       end
 
-      context "when the target is nil" do
-        let(:target) { nil }
-
-        it "does not raise an error" do
-          expect { instance.update_attribute(attribute_to_change, new_value) }.not_to raise_error
-        end
+      it "does not touch the associated model(s) when the #{attribute_to_change} attribute has not changed" do
+        existing_value = instance.send(attribute_to_change)
+        expect {
+          instance.update!(attribute_to_change => existing_value)
+        }.not_to(change { Array.wrap(target).map { |t| t.reload.send(timestamp_attribute) } })
       end
     end
+  end
 
-    it "does not touch the associated model(s) when the #{attribute_to_change} attribute has not changed" do
-      existing_value = instance.send(attribute_to_change)
+  if :create.in?(on_event)
+    it "touches the #{timestamp_attribute} of the associated model(s) on create" do
       expect {
-        instance.update!(attribute_to_change => existing_value)
-      }.not_to(change { Array.wrap(target).map { |t| t.reload.send(timestamp_attribute) } })
+        instance.save!
+      }.to(change { Array.wrap(target).map { |t| t.reload.send(timestamp_attribute) } }.to(all(be_within(5.seconds).of(Time.current))))
+    end
+
+    it "does not touch the updated_at of the associated model(s) on create" do
+      expect {
+        instance.save!
+      }.not_to(change { Array.wrap(target).map { |t| t.reload.updated_at } })
+    end
+  end
+
+  if :destroy.in?(on_event)
+    it "touches the #{timestamp_attribute} of the associated model(s) on destroy" do
+      expect {
+        instance.destroy!
+      }.to(change { Array.wrap(target).map { |t| t.reload.send(timestamp_attribute) } }.to(all(be_within(5.seconds).of(Time.current))))
+    end
+
+    it "does not touch the updated_at of the associated model(s) on create" do
+      expect {
+        instance.destroy!
+      }.not_to(change { Array.wrap(target).map { |t| t.reload.updated_at } })
     end
   end
 end
