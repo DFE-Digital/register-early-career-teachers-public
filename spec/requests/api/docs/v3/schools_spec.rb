e127c0e502c7161a9c700ca70e683d3f322b79ea
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-24 23:37:20 +0100

    [1785] Make more clear that cohort filter is a required param in school endpoints swagger docs

--- spec/requests/api/docs/v3/schools_spec.rb
+++ spec/requests/api/docs/v3/schools_spec.rb
@@ -7,28 +7,20 @@ RSpec.describe "Schools endpoint", openapi_spec: "v3/swagger.yaml", type: :reque
   let(:school_partnership) { FactoryBot.create(:school_partnership, school:) }
   let(:"filter[cohort]") { school_partnership.contract_period.year }
 
-  before do |example|
-    example.metadata[:example_group][:operation][:parameters] += [{
-      name: "filter[cohort]",
-      in: :query,
-      type: :string,
-      required: true
-    }]
-  end
-
   it_behaves_like "an API index endpoint documentation",
                   "/api/v3/schools",
                   "Schools",
                   "ECF schools scoped to cohort",
-                  "#/components/schemas/SchoolsFilter",
                   "#/components/schemas/SchoolsResponse",
+                  "#/components/schemas/SchoolsFilter",
                   true
 
   it_behaves_like "an API show endpoint documentation",
                   "/api/v3/schools/{id}",
                   "Schools",
                   "ECF school scoped to cohort",
-                  "#/components/schemas/SchoolResponse" do
+                  "#/components/schemas/SchoolResponse",
+                  "#/components/schemas/SchoolsFilter" do
     let(:resource) { school }
   end
 end
