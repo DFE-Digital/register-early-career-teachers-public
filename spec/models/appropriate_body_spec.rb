3c0964697bfa596f302f325fdf54c28f898cdd54
Connor McQuillan <me@cpjmcquillan.com>
2025-07-24 16:05:01 +0100

    Remove redundant columns
    We're not using this data anywhere (we just imported it from DQT as a starting
    point).
    
    This removes it in the interests of maintenance!

--- spec/models/appropriate_body_spec.rb
+++ spec/models/appropriate_body_spec.rb
@@ -21,20 +21,5 @@ describe AppropriateBody do
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
-
-    it { is_expected.to validate_presence_of(:local_authority_code).with_message('Enter a local authority code').allow_blank }
-    it { is_expected.to validate_inclusion_of(:local_authority_code).in_range(50..999).allow_blank.with_message('Must be a number between 50 and 999') }
-
-    it { is_expected.to validate_presence_of(:establishment_number).with_message('Enter a establishment number').allow_blank }
-    it { is_expected.to validate_inclusion_of(:establishment_number).in_range(1000..9999).allow_blank.with_message('Must be a number between 1000 and 9999') }
-
-    it "ensures local_authority_code and establishment_number are unique in combination" do
-      ab_args = { local_authority_code: 123, establishment_number: 4567 }
-      FactoryBot.create(:appropriate_body, name: "AB1", **ab_args)
-      FactoryBot.build(:appropriate_body, name: "AB2", **ab_args).tap do |duplicate_ab|
-        expect(duplicate_ab).to be_invalid
-        expect(duplicate_ab.errors.messages.fetch(:local_authority_code)).to include(/local authority code and establishment number already exists/)
-      end
-    end
   end
 end
