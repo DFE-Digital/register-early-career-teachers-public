948b0832269af9f1c53384d0e8e6689d61b7b697
Leandro Alemão <leandro.costantini@gmail.com>
2025-07-25 09:55:25 +0100

    [1786] Make school serialiser accepts contract period and lead provider in the params/options

--- app/serializers/school_serializer.rb
+++ app/serializers/school_serializer.rb
@@ -4,39 +4,47 @@ class SchoolSerializer < Blueprinter::Base
 
     field :name
     field(:urn) { |school, _| school.urn.to_s }
-    field(:cohort) do |school, _|
-      school.transient_contract_period_id
+    field(:cohort) do |school, options|
+      school_contrat_period_id(school, options)
     end
-    field(:in_partnership) do |school, _|
-      in_partnership?(school)
+    field(:in_partnership) do |school, options|
+      in_partnership?(school, options)
     end
-    field(:induction_programme_choice) do |school, _|
-      school.training_programme_for(school.transient_contract_period_id)
+    field(:induction_programme_choice) do |school, options|
+      school.training_programme_for(school_contrat_period_id(school, options))
     end
-    field(:expression_of_interest) do |school, _|
-      expressions_of_interest?(school)
+    field(:expression_of_interest) do |school, options|
+      expressions_of_interest?(school, options)
     end
     field :created_at
     field(:api_updated_at, name: :updated_at)
 
     class << self
-      def in_partnership?(school)
-        if school.respond_to?(:transient_in_partnership)
-          school.transient_in_partnership
+      def school_contrat_period_id(school, options)
+        if school.respond_to?(:transient_contract_period_id)
+          school.transient_contract_period_id
         else
-          school.school_partnerships.for_contract_period(contract_period_id).exists?
+          options[:contract_period_id].to_s
         end
       end
 
-      def expressions_of_interest?(school)
+      def in_partnership?(school, options)
+        if school.respond_to?(:transient_in_partnership)
+          school.transient_in_partnership
+        else
+          school.school_partnerships.for_contract_period(options[:contract_period_id]).exists?
+        end
+      end
+
+      def expressions_of_interest?(school, options)
         if school.respond_to?(:transient_expression_of_interest_ects) &&
             school.respond_to?(:transient_expression_of_interest_mentors)
           return school.transient_expression_of_interest_ects ||
               school.transient_expression_of_interest_mentors
         end
 
-        school.ect_at_school_periods.with_expressions_of_interest_for_lead_provider_and_contract_period(school.transient_contract_period_id, school.transient_lead_provider_id).exists? ||
-          school.mentor_at_school_periods.with_expressions_of_interest_for_lead_provider_and_contract_period(school.transient_contract_period_id, school.transient_lead_provider_id).exists?
+        school.ect_at_school_periods.with_expressions_of_interest_for_lead_provider_and_contract_period(options[:contract_period_id], options[:lead_provider_id]).exists? ||
+          school.mentor_at_school_periods.with_expressions_of_interest_for_lead_provider_and_contract_period(options[:contract_period_id], options[:lead_provider_id]).exists?
       end
     end
   end
