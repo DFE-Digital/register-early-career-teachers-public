e596ad55d87f239b1c450ab68f7f3eb9ccc91e8b
Ross Oliver <ross.oliver36@gmail.com>
2025-07-28 13:39:10 +0100

    Touch `DeliveryPartner#api_updated_at` on relevant changes
    We want to update the `api_updated_at` of a `DeliveryPartner` when a relevant
    attribute changes that would result in the serialized response being different.
    
    Looking at ECF, for a delivery partner this would be when the `name` of the
    delivery partner changes, or its relevant `cohort(s)`.
    
    Touch `api_updated_at` when the delivery partner `name` changes or when a new
    `LeadProviderDeliveryPartnership` is added/removed.
    
    Assumes lead provider delivery partnerships are not expected to change their
    `active_lead_provider` or `delivery_partner` and `ActiveLeadProvider` would
    never change its `contract_period`.

--- app/models/delivery_partner.rb
+++ app/models/delivery_partner.rb
@@ -1,4 +1,6 @@
 class DeliveryPartner < ApplicationRecord
+  include DeclarativeTouch
+
   # Associations
   has_many :lead_provider_delivery_partnerships, inverse_of: :delivery_partner
   has_many :school_partnerships, through: :lead_provider_delivery_partnerships
@@ -9,4 +11,6 @@ class DeliveryPartner < ApplicationRecord
             presence: true,
             uniqueness: true
   validates :api_id, uniqueness: { case_sensitive: false, message: "API id already exists for another delivery partner" }
+
+  touch -> { self }, when_changing: %i[name], timestamp_attribute: :api_updated_at
 end
