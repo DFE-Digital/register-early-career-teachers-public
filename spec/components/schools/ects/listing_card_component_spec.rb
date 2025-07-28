e115477f28f1812980c2c16b61dade096cceef54
Peter Yates <peter.yates@graphia.co.uk>
2025-07-28 10:40:48 +0100

    Rename ect variable to ect_at_school_period
    Here we're referring to the period not the teacher, so adjust the
    variable name accordingly. We'll soon be passing training_period into
    this component too and it'd be confusing, as the 't' in ect stands for
    teacher but we're referring to the period.

--- spec/components/schools/ects/listing_card_component_spec.rb
+++ spec/components/schools/ects/listing_card_component_spec.rb
@@ -2,13 +2,13 @@ RSpec.describe Schools::ECTs::ListingCardComponent, type: :component do
   let(:school) { FactoryBot.create(:school) }
   let(:started_on) { Date.new(2023, 9, 1) }
   let(:teacher) { FactoryBot.create(:teacher, trs_first_name: 'Naruto', trs_last_name: 'Uzumaki') }
-  let(:ect) { FactoryBot.create(:ect_at_school_period, teacher:, school:, started_on:, finished_on: nil) }
+  let(:ect_at_school_period) { FactoryBot.create(:ect_at_school_period, teacher:, school:, started_on:, finished_on: nil) }
   let(:mentor) { FactoryBot.create(:mentor_at_school_period, school:, started_on:, finished_on: nil) }
 
   context 'when the ECT has a mentor assigned' do
     before do
-      FactoryBot.create(:mentorship_period, :ongoing, started_on: ect.started_on, mentee: ect, mentor:)
-      render_inline(described_class.new(ect:))
+      FactoryBot.create(:mentorship_period, :ongoing, started_on: ect_at_school_period.started_on, mentee: ect_at_school_period, mentor:)
+      render_inline(described_class.new(ect_at_school_period:))
     end
 
     it "renders 'Registered' status" do
@@ -18,7 +18,7 @@ RSpec.describe Schools::ECTs::ListingCardComponent, type: :component do
   end
 
   context 'when the ECT has no mentor assigned' do
-    before { render_inline(described_class.new(ect:)) }
+    before { render_inline(described_class.new(ect_at_school_period:)) }
 
     it "renders 'Mentor required' status" do
       expect(rendered_content).to have_selector('.govuk-summary-list__row', text: 'Status')
@@ -27,31 +27,31 @@ RSpec.describe Schools::ECTs::ListingCardComponent, type: :component do
   end
 
   it "renders the TRN" do
-    render_inline(described_class.new(ect:))
+    render_inline(described_class.new(ect_at_school_period:))
 
     expect(rendered_content).to have_selector('.govuk-summary-list__row', text: 'TRN')
-    expect(rendered_content).to have_text(ect.trn)
+    expect(rendered_content).to have_text(ect_at_school_period.trn)
   end
 
   it "renders the school start date" do
-    render_inline(described_class.new(ect:))
+    render_inline(described_class.new(ect_at_school_period:))
 
     expect(rendered_content).to have_selector(".govuk-summary-list__row", text: "School start date")
     expect(rendered_content).to have_text('1 September 2023')
   end
 
   it "renders the school reported appropriate body name" do
-    render_inline(described_class.new(ect:))
+    render_inline(described_class.new(ect_at_school_period:))
 
     expect(rendered_content).to have_selector('.govuk-summary-list__row', text: 'Appropriate body')
-    expect(rendered_content).to have_text(ect.school_reported_appropriate_body_name)
+    expect(rendered_content).to have_text(ect_at_school_period.school_reported_appropriate_body_name)
   end
 
   context 'when provider led chosen' do
-    let!(:training_period) { FactoryBot.create(:training_period, ect_at_school_period: ect, started_on:) }
+    let!(:training_period) { FactoryBot.create(:training_period, ect_at_school_period: ect_at_school_period, started_on:) }
 
     it "renders their latest providers" do
-      render_inline(described_class.new(ect:))
+      render_inline(described_class.new(ect_at_school_period:))
 
       expect(rendered_content).to have_selector('.govuk-summary-list__row', text: 'Delivery partner')
       expect(rendered_content).to have_text(training_period.delivery_partner_name)
@@ -62,10 +62,10 @@ RSpec.describe Schools::ECTs::ListingCardComponent, type: :component do
   end
 
   context 'when school led chosen' do
-    let(:ect) { FactoryBot.create(:ect_at_school_period, :school_led, teacher:, school:, started_on:, finished_on: nil) }
+    let(:ect_at_school_period) { FactoryBot.create(:ect_at_school_period, :school_led, teacher:, school:, started_on:, finished_on: nil) }
 
     it "don't render providers" do
-      render_inline(described_class.new(ect:))
+      render_inline(described_class.new(ect_at_school_period:))
 
       expect(rendered_content).not_to have_selector('.govuk-summary-list__row', text: 'Delivery partner')
       expect(rendered_content).not_to have_selector('.govuk-summary-list__row', text: 'Lead provider')
@@ -74,7 +74,7 @@ RSpec.describe Schools::ECTs::ListingCardComponent, type: :component do
 
   context 'when latest training period is an expression of interest only' do
     let(:lead_provider) { FactoryBot.create(:lead_provider, name: 'Jimmy Provider') }
-    let(:ect) do
+    let(:ect_at_school_period) do
       FactoryBot.create(
         :ect_at_school_period,
         :ongoing,
@@ -86,14 +86,14 @@ RSpec.describe Schools::ECTs::ListingCardComponent, type: :component do
     end
 
     it 'renders lead provider name on the EOI' do
-      render_inline(described_class.new(ect:))
+      render_inline(described_class.new(ect_at_school_period:))
 
       expect(rendered_content).to have_selector('.govuk-summary-list__row', text: 'Lead provider')
       expect(rendered_content).to have_text('Jimmy Provider')
     end
 
     it 'renders the delivery partner fallback text' do
-      render_inline(described_class.new(ect:))
+      render_inline(described_class.new(ect_at_school_period:))
       expect(rendered_content).to have_selector('.govuk-summary-list__row', text: 'Delivery partner')
       expect(rendered_content).to have_text('Their lead provider will confirm this')
     end
