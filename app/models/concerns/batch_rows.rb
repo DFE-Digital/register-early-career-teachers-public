3b7f62644c3e330c2c66404f666471b37af471c5
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-07-29 16:35:40 +0100

    Ensure programme type validation can cope with a blank cell
    Change specs to assert missing values using nil which is what the validation
    receives

--- app/models/concerns/batch_rows.rb
+++ app/models/concerns/batch_rows.rb
@@ -26,7 +26,7 @@ module BatchRows
 
       # @return [Boolean] 7 digits only
       def invalid_trn?
-        trn.strip !~ /\A\d{7}\z/
+        trn.to_s.strip !~ /\A\d{7}\z/
       end
 
       # @return [Boolean] only "error" cell can be blank
