7ea18bf47fd38f52bff33bdc409a2d500c3a24e6
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-07-29 17:14:21 +0100

    [1249] some refactoring

--- spec/factories/active_lead_provider_factory.rb
+++ spec/factories/active_lead_provider_factory.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory(:active_lead_provider) do
     association :lead_provider
     association :contract_period
+
+    initialize_with do
+      ActiveLeadProvider.find_or_create_by(lead_provider:, contract_period:)
+    end
   end
 end
