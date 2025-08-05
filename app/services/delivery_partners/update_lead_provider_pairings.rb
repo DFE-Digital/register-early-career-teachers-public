7543c8cb7e55d46e26fef1ed42d80a274f37680c
edujackedu <jack.scotti@education.gov.uk>
2025-08-05 15:04:40 +0100

    Refactor service

--- app/services/delivery_partners/update_lead_provider_pairings.rb
+++ app/services/delivery_partners/update_lead_provider_pairings.rb
@@ -11,39 +11,52 @@ module DeliveryPartners
 
     def update!
       ActiveRecord::Base.transaction do
-        # Get current partnerships for this delivery partner and contract period
-        current_partnerships = delivery_partner
-          .lead_provider_delivery_partnerships
-          .joins(:active_lead_provider)
-          .where(active_lead_providers: { contract_period_id: contract_period.id })
-
-        current_active_lead_provider_ids = current_partnerships.map(&:active_lead_provider_id)
-
-        # Find active lead provider IDs to add (those not currently associated)
-        active_lead_provider_ids_to_add = active_lead_provider_ids - current_active_lead_provider_ids
-
-        # Add new partnerships only
-        active_lead_provider_ids_to_add.each do |active_lead_provider_id|
-          active_lead_provider = ActiveLeadProvider.find(active_lead_provider_id)
-          new_partnership = LeadProviderDeliveryPartnership.create!(
-            delivery_partner:,
-            active_lead_provider:
-          )
-
-          Events::Record.record_lead_provider_delivery_partnership_added_event!(
-            delivery_partner:,
-            lead_provider: active_lead_provider.lead_provider,
-            contract_period:,
-            author:,
-            lead_provider_delivery_partnership: new_partnership
-          )
-        end
-
+        ids_to_add = active_lead_provider_ids_to_add
+        add_partnerships(ids_to_add)
         true
       end
     rescue ActiveRecord::RecordInvalid => e
       Rails.logger.error "Failed to update lead provider pairings: #{e.message}"
       false
+    end
+
+  private
+
+    def current_partnerships
+      @current_partnerships ||= delivery_partner
+        .lead_provider_delivery_partnerships
+        .joins(:active_lead_provider)
+        .where(active_lead_providers: { contract_period_id: contract_period.id })
+    end
+
+    def current_active_lead_provider_ids
+      @current_active_lead_provider_ids ||= current_partnerships.map(&:active_lead_provider_id)
+    end
+
+    def active_lead_provider_ids_to_add
+      active_lead_provider_ids - current_active_lead_provider_ids
+    end
+
+    def add_partnerships(ids_to_add)
+      ids_to_add.each do |active_lead_provider_id|
+        active_lead_provider = ActiveLeadProvider.find(active_lead_provider_id)
+        new_partnership = LeadProviderDeliveryPartnership.create!(
+          delivery_partner:,
+          active_lead_provider:
+        )
+
+        record_partnership_added_event(active_lead_provider, new_partnership)
+      end
+    end
+
+    def record_partnership_added_event(active_lead_provider, new_partnership)
+      Events::Record.record_lead_provider_delivery_partnership_added_event!(
+        delivery_partner:,
+        lead_provider: active_lead_provider.lead_provider,
+        contract_period:,
+        author:,
+        lead_provider_delivery_partnership: new_partnership
+      )
     end
   end
 end
