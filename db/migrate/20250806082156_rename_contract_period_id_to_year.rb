9bde5aff11a041ed7bb8715a41dedc356b947abd
Peter Yates <peter.yates@graphia.co.uk>
2025-08-06 14:50:52 +0100

    Fix incorrect migration

--- db/migrate/20250806082156_rename_contract_period_id_to_year.rb
+++ db/migrate/20250806082156_rename_contract_period_id_to_year.rb
@@ -1,5 +1,5 @@
 class RenameContractPeriodIdToYear < ActiveRecord::Migration[8.0]
   def change
-    rename_column :active_lead_providers, :contract_period_year, :contract_period_year
+    rename_column :active_lead_providers, :contract_period_id, :contract_period_year
   end
 end
