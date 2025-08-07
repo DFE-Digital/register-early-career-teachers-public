6660f6fa2989fb09a90ba37bd2b87379607136de
Connor McQuillan <me@cpjmcquillan.com>
2025-08-07 12:26:05 +0100

    Admins can only "Reopen induction" when there is an outcome
    In #522 we introduced functionality that allowed Admin users to reopen a
    completed induction.
    
    This works well, but it also allows Admin users to "reopen" a completed
    induction without an outcome (i.e the induction is "paused"). This was not the
    original intention.
    
    Even though this works without issue, it adds a level of complexity for Admin
    users that isn't necessary. Since they can achieve the desired outcome already
    by "Edit"ing a paused induction, we can safely remove this.
    
    Now, Admin users can only "reopen induction" when the latest induction period is
    completed **and** has an outcome (i.e "pass" or "fail").

--- app/controllers/admin/teachers/reopen_induction_controller.rb
+++ app/controllers/admin/teachers/reopen_induction_controller.rb
@@ -8,8 +8,10 @@ module Admin
 
         last_induction_period = @teacher.last_induction_period
 
-        if last_induction_period.blank? || last_induction_period.ongoing?
-          redirect_to admin_teacher_path(@teacher), notice: "No completed induction period found"
+        if last_induction_period.blank? ||
+            last_induction_period.ongoing? ||
+            last_induction_period.outcome.blank?
+          return redirect_to admin_teacher_path(@teacher), notice: "No completed induction period found"
         end
 
         Admin::ReopenInductionPeriod.new(author: current_user, induction_period: last_induction_period).reopen_induction_period!
