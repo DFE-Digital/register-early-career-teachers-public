d36b8dda429774d87afe19d96163cfe083dfcd4a
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-08-11 11:17:59 +0100

    [2030] some refactoring

--- app/components/api/guidance/sidebar_component.rb
+++ app/components/api/guidance/sidebar_component.rb
@@ -1,10 +1,11 @@
 module API
   module Guidance
     class SidebarComponent < ViewComponent::Base
+      GUIDANCE_PREFIX = "guidance-for-lead-providers".freeze
       GUIDANCE_PAGES = [
-        { title: "API IDs explained", path: "guidance-for-lead-providers/api-ids-explained" },
-        { title: "API data states", path: "guidance-for-lead-providers/api-data-states" },
-        { title: "Syncing data best practice", path: "guidance-for-lead-providers/data-syncing" },
+        { title: "API IDs explained", path: "api-ids-explained" },
+        { title: "API data states", path: "api-data-states" },
+        { title: "Syncing data best practice", path: "data-syncing" },
       ].freeze
 
       attr_reader :current_path, :page
@@ -15,13 +16,15 @@ module API
       end
 
       def render?
-        page.to_s.starts_with?("guidance-for-lead-providers")
+        page.to_s.starts_with?(GUIDANCE_PREFIX)
       end
 
       def structure
+        node = Struct.new(:name, :href, :prefix, :nodes)
+
         GUIDANCE_PAGES.map do |p|
-          path = api_guidance_page_path(p[:path])
-          Struct.new(:name, :href, :prefix, :nodes).new(p[:title], path, path, [])
+          path = api_guidance_page_path("#{GUIDANCE_PREFIX}/#{p[:path]}")
+          node.new(p[:title], path, path, [])
         end
       end
     end
