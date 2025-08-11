abd4b4ab6b2b8ff9a74cb6bc8598808ec77904b0
edujackedu <jack.scotti@education.gov.uk>
2025-08-11 20:19:52 +0100

    Update ECT details page
    This work is to update the ECT individual details page for schools, to help group and clarify the different fields shown and the state they are in (e.g. awaiting confirmation vs confirmed).

---
+++ app/components/schools/ect_induction_details_component.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Schools
+  class ECTInductionDetailsComponent < ViewComponent::Base
+    include TeacherHelper
+
+    def initialize(ect)
+      @ect = ect
+    end
+
+    def call
+      safe_join([
+        tag.h2('Induction details', class: 'govuk-heading-m'),
+        govuk_summary_list(rows:)
+      ])
+    end
+
+  private
+
+    def rows
+      [
+        appropriate_body_row,
+        induction_start_date_row
+      ]
+    end
+
+    def appropriate_body_row
+      { key: { text: 'Appropriate body' }, value: { text: @ect.school_reported_appropriate_body_name } }
+    end
+
+    def induction_start_date_row
+      date = induction_start_date
+      if date.present?
+        {
+          key: { text: 'Induction start date' },
+          value: { text: induction_start_date_with_suffix(date) }
+        }
+      else
+        {
+          key: { text: 'Induction start date' },
+          value: { text: 'Yet to be reported by the appropriate body' }
+        }
+      end
+    end
+
+    def induction_start_date
+      @ect.teacher.first_induction_period&.started_on&.to_fs(:govuk)
+    end
+
+    def induction_start_date_with_suffix(date)
+      safe_join([
+        date,
+        tag.br,
+        tag.span("This has been reported by an appropriate body", class: 'govuk-hint')
+      ])
+    end
+  end
+end
--- app/components/schools/teacher_profile_summary_list_component.rb
+++ app/components/schools/teacher_profile_summary_list_component.rb
@@ -7,40 +7,29 @@ module Schools
       @ect = ect
     end
 
-    def rows
-      [
-        name_row,
-        email_row,
-        mentor_row,
-        school_start_date_row,
-        working_pattern_row
-      ]
-    end
-
-    def call
-      safe_join([tag.h2('ECT details', class: 'govuk-heading-m'), govuk_summary_list(rows:)])
-    end
-
   private
 
-    def name_row
-      { key: { text: 'Name' }, value: { text: teacher_full_name(@ect.teacher) } }
+    def ect_status_tag
+      induction_status = @ect.teacher.trs_induction_status
+
+      case induction_status
+      when "Passed"
+        govuk_tag(text: "Completed induction", colour: "blue")
+      when "Failed"
+        govuk_tag(text: "Failed induction", colour: "pink")
+      when "Exempt"
+        govuk_tag(text: "Exempt", colour: "grey")
+      else
+        if current_mentor_name(@ect)
+          govuk_tag(text: "Registered", colour: "green")
+        else
+          govuk_tag(text: "Mentor required", colour: "red")
+        end
+      end
     end
 
-    def email_row
-      { key: { text: 'Email address' }, value: { text: @ect.email } }
-    end
-
-    def mentor_row
-      { key: { text: 'Mentor' }, value: { text: ect_mentor_details(@ect) } }
-    end
-
-    def school_start_date_row
-      { key: { text: 'School start date' }, value: { text: ect_start_date(@ect) } }
-    end
-
-    def working_pattern_row
-      { key: { text: 'Working pattern' }, value: { text: @ect.working_pattern&.humanize } }
+    def current_mentor_name(ect)
+      ECTAtSchoolPeriods::Mentorship.new(ect).current_mentor_name
     end
   end
 end
