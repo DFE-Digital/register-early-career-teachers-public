c79666c7a7fde762d24fc2a968dd6a111ab9fc4e
Tony Headford <tony@binarycircus.com>
2025-08-13 16:15:45 +0100

    Fix training period builder specs

--- spec/factories/school_partnership_factory.rb
+++ spec/factories/school_partnership_factory.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory(:school_partnership) do
-    association :lead_provider_delivery_partnership
+    transient do
+      contract_year { Random.rand(2021..2120) }
+    end
+
+    lead_provider_delivery_partnership { FactoryBot.create(:lead_provider_delivery_partnership, contract_year:) }
     association :school
   end
 end
