dbb4c253b281dec0ccab2fa82e4f73087d696bef
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-08-14 15:22:33 +0100

    Just rename the existing role

--- app/services/organisation/access.rb
+++ app/services/organisation/access.rb
@@ -9,7 +9,7 @@ module Organisation
 
     def can_access?
       Rails.logger.info(access_level)
-      access_level.roles.any?
+      access_level.has_register_ect_access_role?
     end
 
   private
