8b30fb48a62b2a267509f1e1ba35fa5a1953be8e
Tony Headford <tony@binarycircus.com>
2025-08-14 13:32:23 +0100

    Simpler approach

--- spec/factories/lead_provider_delivery_partnership_factory.rb
+++ spec/factories/lead_provider_delivery_partnership_factory.rb
@@ -1,13 +1,6 @@
 FactoryBot.define do
   factory(:lead_provider_delivery_partnership) do
-    transient do
-      contract_year { Random.rand(2021..2120) }
-    end
-
-    active_lead_provider do
-      FactoryBot.create(:active_lead_provider, contract_period: (ContractPeriod.find_by(year: contract_year) || FactoryBot.create(:contract_period, year: contract_year)))
-    end
-
+    association :active_lead_provider
     association :delivery_partner
   end
 end
--- spec/factories/school_partnership_factory.rb
+++ spec/factories/school_partnership_factory.rb
@@ -1,10 +1,6 @@
 FactoryBot.define do
   factory(:school_partnership) do
-    transient do
-      contract_year { Random.rand(2021..2120) }
-    end
-
-    lead_provider_delivery_partnership { FactoryBot.create(:lead_provider_delivery_partnership, contract_year:) }
+    association :lead_provider_delivery_partnership
     association :school
   end
 end
