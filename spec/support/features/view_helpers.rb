6e3528a8cdc05b72b170fe0618cd032531a95e71
Avin Hurry <avinhurry@icloud.com>
2025-08-14 21:37:33 +0100

    [1834] Add continue button test helper

--- spec/support/features/view_helpers.rb
+++ spec/support/features/view_helpers.rb
@@ -6,5 +6,12 @@ module Features
 
     alias_method :when_i_click_the_back_link, :given_i_click_the_back_link
     alias_method :and_i_click_the_back_link, :given_i_click_the_back_link
+
+    def given_i_click_continue
+      page.get_by_role('button', name: "Continue").click
+    end
+
+    alias_method :when_i_click_continue, :given_i_click_continue
+    alias_method :and_i_click_continue, :given_i_click_continue
   end
 end
