b8be9fdb151101f9a97802539a17abc88a8a55aa
Ross Oliver <ross.oliver36@gmail.com>
2025-08-14 13:40:04 +0100

    Switch to using real lead provider names
    Originally we used similar but different lead provider names in development and
    review. This was in an effort to reduce the risk of changing something in an
    environment accidently, however its caused more issues and confusion than we
    expected so we are going to use the normal/real lead provider names everywhere.

--- db/seeds/school_partnerships.rb
+++ db/seeds/school_partnerships.rb
@@ -28,23 +28,23 @@ rp2022 = ContractPeriod.find_by(year: 2022)
 rp2023 = ContractPeriod.find_by(year: 2023)
 rp2025 = ContractPeriod.find_by(year: 2025)
 
-ambitious_institute = LeadProvider.find_by!(name: 'Ambitious Institute')
-teach_fast = LeadProvider.find_by!(name: 'Teach Fast')
+ambition_institute = LeadProvider.find_by!(name: 'Ambition Institute')
+teach_first = LeadProvider.find_by!(name: 'Teach First')
 
 artisan = DeliveryPartner.find_by!(name: "Artisan Education Group")
 grain = DeliveryPartner.find_by!(name: "Grain Teaching School Hub")
 
-ambitious_institute__artisan__2022 = find_lead_provider_delivery_partnership(delivery_partner: artisan, lead_provider: ambitious_institute, contract_period: rp2022)
-ambitious_institute__artisan__2023 = find_lead_provider_delivery_partnership(delivery_partner: artisan, lead_provider: ambitious_institute, contract_period: rp2023)
-teach_fast__grain__2022 = find_lead_provider_delivery_partnership(delivery_partner: grain, lead_provider: teach_fast, contract_period: rp2022)
-teach_fast__grain__2023 = find_lead_provider_delivery_partnership(delivery_partner: grain, lead_provider: teach_fast, contract_period: rp2023)
-teach_fast__grain__2025 = find_lead_provider_delivery_partnership(delivery_partner: grain, lead_provider: teach_fast, contract_period: rp2025)
+ambition_institute__artisan__2022 = find_lead_provider_delivery_partnership(delivery_partner: artisan, lead_provider: ambition_institute, contract_period: rp2022)
+ambition_institute__artisan__2023 = find_lead_provider_delivery_partnership(delivery_partner: artisan, lead_provider: ambition_institute, contract_period: rp2023)
+teach_first__grain__2022 = find_lead_provider_delivery_partnership(delivery_partner: grain, lead_provider: teach_first, contract_period: rp2022)
+teach_first__grain__2023 = find_lead_provider_delivery_partnership(delivery_partner: grain, lead_provider: teach_first, contract_period: rp2023)
+teach_first__grain__2025 = find_lead_provider_delivery_partnership(delivery_partner: grain, lead_provider: teach_first, contract_period: rp2025)
 [
-  { school: abbey_grove_school, lead_provider_delivery_partnership: ambitious_institute__artisan__2022 },
-  { school: abbey_grove_school, lead_provider_delivery_partnership: ambitious_institute__artisan__2023 },
-  { school: abbey_grove_school, lead_provider_delivery_partnership: teach_fast__grain__2023 },
-  { school: abbey_grove_school, lead_provider_delivery_partnership: teach_fast__grain__2025 },
-  { school: ackley_bridge, lead_provider_delivery_partnership: ambitious_institute__artisan__2023 },
-  { school: mallory_towers, lead_provider_delivery_partnership: teach_fast__grain__2022 },
-  { school: brookfield_school, lead_provider_delivery_partnership: teach_fast__grain__2022 },
+  { school: abbey_grove_school, lead_provider_delivery_partnership: ambition_institute__artisan__2022 },
+  { school: abbey_grove_school, lead_provider_delivery_partnership: ambition_institute__artisan__2023 },
+  { school: abbey_grove_school, lead_provider_delivery_partnership: teach_first__grain__2023 },
+  { school: abbey_grove_school, lead_provider_delivery_partnership: teach_first__grain__2025 },
+  { school: ackley_bridge, lead_provider_delivery_partnership: ambition_institute__artisan__2023 },
+  { school: mallory_towers, lead_provider_delivery_partnership: teach_first__grain__2022 },
+  { school: brookfield_school, lead_provider_delivery_partnership: teach_first__grain__2022 },
 ].each { |kwargs| SchoolPartnership.create!(**kwargs).tap { |sp| describe_school_partnership(sp) } }
