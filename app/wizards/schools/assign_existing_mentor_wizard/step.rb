4968d0242b66b7f2798c0955ca25d5bbc5c22a74
Avin Hurry <avinhurry@icloud.com>
2025-08-18 18:08:07 +0100

    [1834] Add AssignExistingMentorWizard steps
    Includes:
    - Step base class
    - `ReviewMentorEligibilityStep`
    - `LeadProviderStep`
    - `ConfirmationStep`
    - Specs for each step

---
+++ app/wizards/schools/assign_existing_mentor_wizard/step.rb
@@ -0,0 +1,27 @@
+module Schools
+  module AssignExistingMentorWizard
+    class Step < ApplicationWizardStep
+      def self.permitted_params = []
+
+      def save!
+        persist if wizard.valid_step?
+      end
+
+    private
+
+      def pre_populate_attributes
+        # Pre-populate form fields from the MentorAssignmentContext object
+        # Currently not used but implemented, once the wizard grows it should "just work"
+        return unless wizard.context
+
+        self.class.permitted_params.each do |key|
+          public_send("#{key}=", wizard.context.send(key)) if wizard.context.respond_to?(key)
+        end
+      end
+
+      def step_params
+        @step_params ||= wizard.step_params.to_h.slice(*self.class.permitted_params.map(&:to_s))
+      end
+    end
+  end
+end
