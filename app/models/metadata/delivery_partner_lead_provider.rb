e4277534739f08f9fde3e5b4e8e69bb579dea960
Ross Oliver <ross.oliver36@gmail.com>
2025-08-18 14:14:44 +0100

    Fix validation on `contract_period_years`
    Allow empty arrays as we will create metadata for all lead providers even
    if they don't have a delivery partnership.
    
    The `inclusion` validation rule was also just checking that it is always
    an array that contains 2021 to the current year, but we want to allow any
    combination thereof.

--- app/models/metadata/delivery_partner_lead_provider.rb
+++ app/models/metadata/delivery_partner_lead_provider.rb
@@ -7,9 +7,17 @@ module Metadata
 
     validates :delivery_partner, presence: true
     validates :lead_provider, presence: true
-    validates :contract_period_years,
-              presence: true,
-              inclusion: { in: (2021..Date.current.year).to_a }
-    validates :contract_period_years, uniqueness: { scope: %i[delivery_partner_id lead_provider_id] }
+    validates :delivery_partner_id, uniqueness: { scope: :lead_provider_id }
+    validate :contract_period_years_is_an_array_of_valid_years
+
+  private
+
+    def contract_period_years_is_an_array_of_valid_years
+      if contract_period_years.nil? || !contract_period_years.is_a?(Array)
+        errors.add(:contract_period_years, "must be an array")
+      elsif contract_period_years.present? && contract_period_years.any? { |year| !year.between?(2021, Date.current.year) }
+        errors.add(:contract_period_years, "must contain valid years between 2021 and the current year")
+      end
+    end
   end
 end
--- spec/models/metadata/delivery_partner_lead_provider_spec.rb
+++ spec/models/metadata/delivery_partner_lead_provider_spec.rb
@@ -11,8 +11,9 @@ describe Metadata::DeliveryPartnerLeadProvider do
 
     it { is_expected.to validate_presence_of(:delivery_partner) }
     it { is_expected.to validate_presence_of(:lead_provider) }
-    it { is_expected.to validate_presence_of(:contract_period_years) }
     it { is_expected.to allow_values((2021..Date.current.year).to_a).for(:contract_period_years) }
-    it { is_expected.to validate_uniqueness_of(:contract_period_years).scoped_to(:delivery_partner_id, :lead_provider_id) }
+    it { is_expected.not_to allow_value(nil, false, 2022).for(:contract_period_years).with_message("must be an array") }
+    it { is_expected.not_to allow_value([2020], [2019, 2022]).for(:contract_period_years).with_message("must contain valid years between 2021 and the current year") }
+    it { is_expected.to validate_uniqueness_of(:delivery_partner_id).scoped_to(:lead_provider_id) }
   end
 end
