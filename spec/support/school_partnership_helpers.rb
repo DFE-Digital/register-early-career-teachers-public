c40b3097b1148cc48a15cf571910b8fe69242cd3
Avin Hurry <avinhurry@icloud.com>
2025-08-19 21:59:04 +0100

    [1185] Utilise school partnership setup helper
    There was duplicated setup logic in several specs and we expect to have to do this setup more. This reduces repetition and makes it easier to reuse the full partnership setup.

--- spec/support/school_partnership_helpers.rb
+++ spec/support/school_partnership_helpers.rb
@@ -1,6 +1,7 @@
 module SchoolPartnershipHelpers
-  def make_partnership_for(school, contract_period)
-    active_lead_provider = FactoryBot.create(:active_lead_provider, contract_period:)
+  def make_partnership_for(school, contract_period, lead_provider_name: "Xavier's School for Gifted Youngsters")
+    lead_provider = FactoryBot.create(:lead_provider, name: lead_provider_name)
+    active_lead_provider = FactoryBot.create(:active_lead_provider, contract_period:, lead_provider:)
     lead_provider_delivery_partnership = FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:)
 
     FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:, school:)
