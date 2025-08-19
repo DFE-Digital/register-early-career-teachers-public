fbfa2e192944f75c1ffd9b2771dd0e98ede7dd33
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-08-19 12:36:20 +0100

    [2057] Create school partnership with same school and different delivery partners

--- app/services/sandbox_seed_data/school_partnerships.rb
+++ app/services/sandbox_seed_data/school_partnerships.rb
@@ -15,14 +15,30 @@ module SandboxSeedData
   private
 
     def create_school_partnerships
-      LeadProviderDeliveryPartnership.find_each do |lead_provider_delivery_partnership|
-        school = School.order("RANDOM()").first
-
-        next if school.school_partnerships.map(&:lead_provider_delivery_partnership).map(&:id).include?(lead_provider_delivery_partnership.id)
+      ids = LeadProviderDeliveryPartnership.pluck(:id)
+      LeadProviderDeliveryPartnership.where(id: ids).find_each do |lead_provider_delivery_partnership|
+        existing_school_ids = lead_provider_delivery_partnership.school_partnerships.pluck(:school_id)
+        school = School.where.not(id: existing_school_ids).order("RANDOM()").first
+        next unless school
 
         school_partnership = FactoryBot.create(:school_partnership, school:, lead_provider_delivery_partnership:)
 
         log_seed_info("#{school_partnership.school.name} -> #{school_partnership.lead_provider.name}", colour: Colourize::COLOURS.keys.sample)
+
+        # Create another school partnership with the same school but different delivery_partner
+        if rand < 0.1
+          active_lead_provider = lead_provider_delivery_partnership.active_lead_provider
+
+          lead_provider_delivery_partnership2 = LeadProviderDeliveryPartnership
+            .where(active_lead_provider:)
+            .where.not(delivery_partner: lead_provider_delivery_partnership.delivery_partner)
+            .order("RANDOM()")
+            .first
+          next if SchoolPartnership.where(school:, lead_provider_delivery_partnership: lead_provider_delivery_partnership2).exists?
+
+          school_partnership2 = FactoryBot.create(:school_partnership, school:, lead_provider_delivery_partnership: lead_provider_delivery_partnership2)
+          log_seed_info("#{school_partnership2.school.name} -> #{school_partnership2.lead_provider.name}", colour: Colourize::COLOURS.keys.sample)
+        end
       end
     end
   end
