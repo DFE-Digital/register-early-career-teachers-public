e88583e89ba0d85be30b36e95a9474b76169275b
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-08-19 12:24:07 +0100

    Fix back link whilst bulk summary is linked to from 2 index pages

--- app/components/admin/appropriate_bodies/bulk_upload_component.rb
+++ app/components/admin/appropriate_bodies/bulk_upload_component.rb
@@ -3,8 +3,8 @@ module Admin
     class BulkUploadComponent < ViewComponent::Base
       renders_one :batch_cards,       -> { Batches::BatchCardsComponent.new(batch:) }         # coloured cards
       renders_one :batch_details,     -> { Batches::BatchDetailsComponent.new(batch:) }       # summary list
-      renders_one :error_details,     -> { Batches::ErrorDetailsComponent.new(batch:) }       # table (paginated)
-      renders_one :induction_details, -> { Batches::InductionDetailsComponent.new(batch:) }   # table (paginated)
+      renders_one :error_details,     -> { Batches::ErrorDetailsComponent.new(batch:) }       # table (needs pagination?)
+      renders_one :induction_details, -> { Batches::InductionDetailsComponent.new(batch:) }   # table (needs pagination?)
 
       attr_reader :batch
 
