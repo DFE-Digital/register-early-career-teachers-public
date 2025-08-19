6ee9f47a1c05af3a1830b32509bad4b847dd7e1a
Ross Oliver <ross.oliver36@gmail.com>
2025-08-19 10:45:22 +0100

    Integrate metadata into delivery partners endpoint
    Update the `DeliveryPartners::Query` to preload the metadata and remove the
    transient cohort subquery.
    
    Update the `DeliveryPartnerSerializer` to serialize using the metadata.
    
    Ensure we pass the `lead_provider_id` into the `DeliveryPartnerSerializer` from
    the `DeliveryPartnersController`.

--- spec/factories/lead_provider_delivery_partnership_factory.rb
+++ spec/factories/lead_provider_delivery_partnership_factory.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory(:lead_provider_delivery_partnership) do
     association :active_lead_provider
     association :delivery_partner
+
+    trait :with_delivery_partner_metadata do
+      delivery_partner do
+        lead_provider = active_lead_provider.lead_provider
+
+        association :delivery_partner, :with_metadata, lead_provider:
+      end
+    end
   end
 end
