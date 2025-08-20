7050bd3d4ede29f64c40eae16a45d441f74153d3
Avin Hurry <avinhurry@icloud.com>
2025-08-20 23:24:22 +0100

    [1249] Add unit tests

--- spec/services/mentor_at_school_periods/latest_registration_choices_spec.rb
+++ spec/services/mentor_at_school_periods/latest_registration_choices_spec.rb
@@ -17,4 +17,48 @@ describe MentorAtSchoolPeriods::LatestRegistrationChoices do
   describe '#delivery_partner' do
     it { expect(subject.delivery_partner).to eq(school_partnership.delivery_partner) }
   end
+
+  context 'when the latest training period has an EOI (no partnership)' do
+    let(:school) { FactoryBot.create(:school) }
+    let!(:mentor_at_school_period) do
+      FactoryBot.create(
+        :mentor_at_school_period,
+        teacher:,
+        school:
+      )
+    end
+
+    let(:lp_from_eoi) { FactoryBot.create(:lead_provider, name: "EOI LP") }
+    let(:expression_of_interest) { FactoryBot.create(:active_lead_provider, lead_provider: lp_from_eoi) }
+
+    let!(:training_period) do
+      FactoryBot.create(
+        :training_period,
+        :for_mentor,
+        :ongoing,
+        mentor_at_school_period:,
+        school_partnership: nil,
+        expression_of_interest:,
+        training_programme: 'provider_led'
+      )
+    end
+
+    describe '#school' do
+      it 'returns the school from the mentor_at_school_period when only an EOI exists' do
+        expect(subject.school).to eq(school)
+      end
+    end
+
+    describe '#lead_provider' do
+      it "returns the lead provider from the expression of interest (EOI)" do
+        expect(subject.lead_provider).to eq(lp_from_eoi)
+      end
+    end
+
+    describe '#delivery_partner' do
+      it 'returns nil' do
+        expect(subject.delivery_partner).to be_nil
+      end
+    end
+  end
 end
