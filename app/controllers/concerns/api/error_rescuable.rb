c27fe4d3fffdb2101f4c63a3b4748e07f9df6f3e
Ross Oliver <ross.oliver36@gmail.com>
2025-08-21 10:04:52 +0100

    Add POST /partnerships endpoint
    Call the `SchoolPartnerships::Create` service from the `create` action of the
    `PartnershipsController`.
    
    Add shared context for testing `create` actions.

--- app/controllers/concerns/api/error_rescuable.rb
+++ app/controllers/concerns/api/error_rescuable.rb
@@ -6,6 +6,7 @@ module API
       rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
       rescue_from ActionController::UnpermittedParameters, with: :unpermitted_parameter_response
       rescue_from ActionController::BadRequest, with: :bad_request_response
+      rescue_from ActionController::ParameterMissing, with: :parameters_missing_response
       rescue_from ArgumentError, with: :bad_request_response
     end
 
@@ -21,6 +22,11 @@ module API
 
     def not_found_response
       render json: { errors: API::Errors::Response.new(title: "Resource not found", messages: "Nothing could be found for the provided details").call }, status: :not_found
+    end
+
+    def parameters_missing_response
+      bad_request_error = ActionController::BadRequest.new("Correct json data structure required. See API docs for reference.")
+      bad_request_response(bad_request_error)
     end
   end
 end
