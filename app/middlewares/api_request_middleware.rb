36db0b02013918dc854559a56cf7f5ca46309d51
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-08-21 18:16:52 +0100

    [1934] DfE::Analytics events should now match ecf1 - We have a `APIRequestMiddleware` which records API requests with   custom data values as a entity update - We also have `include DfE::Analytics::Requests` in   `API::BaseController` which will record web requests

---
+++ app/middlewares/api_request_middleware.rb
@@ -0,0 +1,74 @@
+class APIRequestMiddleware
+  REQUEST_HEADER_KEYS = %w[
+    HTTP_VERSION
+    HTTP_HOST
+    HTTP_USER_AGENT
+    HTTP_ACCEPT
+    HTTP_ACCEPT_ENCODING
+    HTTP_AUTHORIZATION
+    HTTP_CONNECTION
+    HTTP_CACHE_CONTROL
+    QUERY_STRING
+  ].freeze
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    dup._call(env)
+  end
+
+  def _call(env)
+    @request = Rack::Request.new(env)
+    status, @headers, @response = @app.call(env)
+
+    begin
+      if trace_request?
+        # dfe_analytics_request_id is explicitly being sent to APIRequestJob as sidekiq does not share the same locals of the main app
+        # and dfe_analytics_request_id is used to set request_uuid value for analytics events
+        request_uuid = RequestLocals.fetch(:dfe_analytics_request_id) { nil } # rubocop:disable Style/RedundantFetchBlock
+        APIRequestJob.perform_later(request_data.stringify_keys, response_data.stringify_keys, status, Time.zone.now.to_s, request_uuid)
+      end
+    rescue StandardError => e
+      Rails.logger.warn e.message
+      Sentry.capture_exception(e)
+    end
+
+    [status, @headers, @response]
+  end
+
+private
+
+  def response_data
+    body = @response.respond_to?(:body) ? @response.body : @response.join
+    body = body.join if body.is_a?(Array)
+
+    {
+      headers: @headers,
+      body:,
+    }
+  end
+
+  def request_data
+    {
+      path: @request.path,
+      params: @request.params,
+      body: @request.body&.read&.dup&.force_encoding("utf-8"),
+      headers: request_headers,
+      method: @request.request_method,
+    }
+  end
+
+  def request_headers
+    @request.env.slice(*REQUEST_HEADER_KEYS)
+  end
+
+  def trace_request?
+    DfE::Analytics.enabled? && vendor_api_path?
+  end
+
+  def vendor_api_path?
+    @request.path.starts_with?("/api/v")
+  end
+end
