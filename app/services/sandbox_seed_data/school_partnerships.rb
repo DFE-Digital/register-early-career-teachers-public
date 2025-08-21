7b5a6a7dfd3d231faeb2926c981b7df9b1653fe7
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-08-21 14:04:04 +0100

    [2057] refactored and fixed some issues

--- app/services/sandbox_seed_data/school_partnerships.rb
+++ app/services/sandbox_seed_data/school_partnerships.rb
@@ -9,21 +9,25 @@ module SandboxSeedData
       log_plant_info("school partnerships")
 
       ActiveLeadProvider.find_each do |active_lead_provider|
-        SCHOOL_PARTNERSHIPS_PER_ACTIVE_LEAD_PROVIDER.times do |n|
-          school = create_school_partnerships(active_lead_provider)
+        SCHOOL_PARTNERSHIPS_PER_ACTIVE_LEAD_PROVIDER.times do
+          create_school_partnership(active_lead_provider)
+        end
 
-          if n < SAME_SCHOOL_DIFFERENT_DELIVERY_PARTNER_PER_ACTIVE_LEAD_PROVIDER
-            create_school_partnerships(active_lead_provider, school:)
-          end
+        schools = select_existing_schools(active_lead_provider)
+        schools.each do |school|
+          create_school_partnership(active_lead_provider, school:)
         end
       end
     end
 
   private
 
-    def create_school_partnerships(active_lead_provider, school: nil)
+    def create_school_partnership(active_lead_provider, school: nil)
       school ||= find_available_school(active_lead_provider)
+      return unless school
+
       delivery_partner = find_available_delivery_partner(active_lead_provider)
+      return unless delivery_partner
 
       lead_provider_delivery_partnership = find_or_create_lead_provider_delivery_partnership(active_lead_provider:, delivery_partner:)
       school_partnership = FactoryBot.create(:school_partnership, school:, lead_provider_delivery_partnership:)
@@ -38,14 +42,29 @@ module SandboxSeedData
         FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:, delivery_partner:)
     end
 
+    def school_partnerships(active_lead_provider)
+      SchoolPartnership
+        .includes(:lead_provider_delivery_partnership)
+        .where(lead_provider_delivery_partnership: { active_lead_provider: })
+    end
+
     def find_available_school(active_lead_provider)
-      existing_school_ids = LeadProviderDeliveryPartnership.find_by(active_lead_provider:).school_partnerships.pluck(:school_id)
-      School.where.not(id: existing_school_ids).order("RANDOM()").first!
+      existing_school_ids = school_partnerships(active_lead_provider).pluck(:school_id)
+
+      School.where.not(id: existing_school_ids).order("RANDOM()").first
+    end
+
+    def select_existing_schools(active_lead_provider)
+      school_partnerships(active_lead_provider)
+        .order("RANDOM()")
+        .first(SAME_SCHOOL_DIFFERENT_DELIVERY_PARTNER_PER_ACTIVE_LEAD_PROVIDER)
+        .map(&:school)
     end
 
     def find_available_delivery_partner(active_lead_provider)
       existing_delivery_partner_ids = active_lead_provider.lead_provider_delivery_partnerships.pluck(:delivery_partner_id)
-      DeliveryPartner.where.not(id: existing_delivery_partner_ids).order("RANDOM()").first!
+
+      DeliveryPartner.where.not(id: existing_delivery_partner_ids).order("RANDOM()").first
     end
   end
 end
