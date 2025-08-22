0cc64adf00cccb3b798d4fe9e3f573836eef6e2e
edujackedu <jack.scotti@education.gov.uk>
2025-08-22 16:20:00 +0100

    Add feasible steps functionality to register ECT wizard

--- app/controllers/schools/register_ect_wizard_controller.rb
+++ app/controllers/schools/register_ect_wizard_controller.rb
@@ -2,6 +2,7 @@ module Schools
   class RegisterECTWizardController < SchoolsController
     before_action :initialize_wizard, only: %i[new create]
     before_action :reset_wizard, only: :new
+    before_action :check_allowed_step, except: %i[start]
 
     FORM_KEY = :register_ect_wizard
     WIZARD_CLASS = Schools::RegisterECTWizard::Wizard.freeze
@@ -38,6 +39,17 @@ module Schools
       request.path.split("/").last.underscore.to_sym.tap do |step_from_path|
         return :not_found unless WIZARD_CLASS.step?(step_from_path)
       end
+    end
+
+    def check_allowed_step
+      return if @wizard.allowed_step?
+
+      # Don't redirect change steps or error steps - let them through
+      step_name = current_step.to_s
+      return if step_name.start_with?('change_', 'no_previous_ect_choices_change_', 'training_programme_change_')
+      return if step_name.in?(%w[not_found trn_not_found induction_completed induction_exempt induction_failed cannot_register_ect already_active_at_school])
+
+      redirect_to @wizard.allowed_step_path
     end
 
     def reset_wizard
