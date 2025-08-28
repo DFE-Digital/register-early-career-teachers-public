c3aebd15af64781ccf49d9530207b677868d3806
edujackedu <jack.scotti@education.gov.uk>
2025-08-28 07:52:23 +0100

    Refactor school details view and update index action
    Enhanced the `show` view to include breadcrumbs and a tabbed layout for better organization of school information, replacing the previous summary list with dedicated components for overview and teachers.

--- app/controllers/admin/schools_controller.rb
+++ app/controllers/admin/schools_controller.rb
@@ -5,11 +5,16 @@ module Admin
     layout "full"
 
     def index
-      @pagy, @schools = pagy(Schools::Search.new(params[:q]).call)
+      schools = params[:q].present? ? School.search(params[:q]) : School.includes(:gias_school)
+      @pagy, @schools = pagy(schools)
     end
 
     def show
       @school = School.includes(:gias_school).find_by!(urn: params[:urn])
+      @breadcrumbs = {
+        "Schools" => admin_schools_path(page: params[:page], q: params[:q]),
+        @school.name => nil
+      }
     end
   end
 end
