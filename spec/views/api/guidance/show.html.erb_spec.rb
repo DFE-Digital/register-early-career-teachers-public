d51a98ec9b3626a5e91afbdd913d3afd5602be57
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-08-29 16:16:45 +0100

    [2240] release notes with index page

--- spec/views/api/guidance/show.html.erb_spec.rb
+++ spec/views/api/guidance/show.html.erb_spec.rb
@@ -12,25 +12,4 @@ RSpec.describe "api/guidance/show.html.erb" do
     header_text = "Lead provider guidance: early career training programme"
     expect(view.content_for(:page_header)).to have_text(header_text)
   end
-
-  context "when a release note is present" do
-    let(:release_note_data) do
-      YAML.load_file(
-        Rails.root.join("app/views/api/guidance/release_notes.yml"),
-        permitted_classes: [Date]
-      ).map { |note| API::ReleaseNote.new(**note.symbolize_keys) }
-    end
-    let!(:release_note) { release_note_data.first }
-
-    it "displays the latest release note summary" do
-      assign(:latest_release_note, release_note)
-
-      render
-
-      expect(rendered).to have_text(release_note.date.to_s)
-      expect(rendered).to have_text(release_note.title)
-      expect(rendered).to have_text(release_note.tags.first.titleize.humanize)
-      expect(rendered).to have_text(release_note.tags.last.titleize.humanize)
-    end
-  end
 end
---
+++ spec/views/api/release_notes/show.html.erb_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe "api/release_notes/show.html.erb" do
+  let(:release_note) { { title: 'Note 1', date: 1.month.ago, body: 'Note 1 body', tags: %w[bug-fix new-feature] } }
+
+  before do
+    assign(:release_note, API::ReleaseNote.new(**release_note))
+    render
+  end
+
+  it 'shows the correct title' do
+    expect(view.content_for(:page_title)).to have_text(release_note[:title])
+  end
+
+  it 'shows the correct heading' do
+    expect(view.content_for(:page_header)).to have_text(release_note[:title])
+  end
+
+  it 'shows the correct caption' do
+    expect(view.content_for(:page_caption)).to have_text(release_note[:date].to_formatted_s(:govuk))
+  end
+
+  it 'displays the release note body' do
+    expect(rendered).to have_css('p', text: release_note[:body])
+
+    expect(rendered).to have_css('.tag-group .govuk-tag--green', text: "New feature")
+    expect(rendered).to have_css('.tag-group .govuk-tag--yellow', text: "Bug fix")
+  end
+end
