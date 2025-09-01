ac5e88a99db4ac1a8c2a9bec39c9720ce37fbff4
Abeer Salameh <abeer.salameh@gmail.com>
2025-09-01 18:47:50 +0100

    Add new search statement service
    This service will be used in the admin interface, as query services
    will be going forward API specific only

---
+++ app/services/statements/search.rb
@@ -0,0 +1,65 @@
+module Statements
+  class Search
+    class InvalidFeeTypeError < StandardError; end
+
+    include Queries::FilterIgnorable
+    include Queries::ConditionFormats
+
+    attr_reader :scope
+
+    def initialize(lead_provider_id: :ignore, contract_period_years: :ignore, fee_type: 'output', statement_date: :ignore, order_by: :payment_date)
+      @scope = Statement.distinct.includes(active_lead_provider: %i[lead_provider contract_period])
+
+      where_lead_provider_is(lead_provider_id)
+      where_contract_period_year_in(contract_period_years)
+      where_fee_type_is(fee_type)
+      where_statement_date(statement_date)
+      set_order_by(order_by)
+    end
+
+    def statements
+      scope
+    end
+
+  private
+
+    def where_lead_provider_is(lead_provider_id)
+      return if ignore?(filter: lead_provider_id)
+
+      scope.merge!(Statement.joins(:lead_provider).where(lead_providers: { id: lead_provider_id }))
+    end
+
+    def where_contract_period_year_in(contract_period_years)
+      return if ignore?(filter: contract_period_years)
+
+      scope.merge!(Statement.joins(:contract_period).where(contract_periods: { year: extract_conditions(contract_period_years) }))
+    end
+
+    def where_fee_type_is(fee_type)
+      return if ignore?(filter: fee_type)
+
+      fail InvalidFeeTypeError unless fee_type.in?(Statement::VALID_FEE_TYPES)
+
+      scope.merge!(Statement.with_fee_type(fee_type))
+    end
+
+    def where_statement_date(statement_date)
+      return if ignore?(filter: statement_date)
+      return if statement_date.blank?
+
+      year, month = statement_date.split("-").map(&:to_i) # 2025-01 -> 2025 & 1
+      scope.merge!(Statement.with_statement_date(year:, month:))
+    end
+
+    def set_order_by(order_by)
+      return if ignore?(filter: order_by)
+
+      case order_by
+      when :statement_date
+        scope.merge!(Statement.order(year: :asc, month: :asc))
+      when :payment_date
+        scope.merge!(Statement.order(payment_date: :asc))
+      end
+    end
+  end
+end
