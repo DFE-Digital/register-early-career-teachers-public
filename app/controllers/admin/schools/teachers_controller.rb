4705e9cd46fe72dbed95c05d7e163ee2519b86ac
edujackedu <jack.scotti@education.gov.uk>
2025-09-01 09:15:43 +0100

    Remove before_action blocks from controllers with just one method

--- app/controllers/admin/schools/teachers_controller.rb
+++ app/controllers/admin/schools/teachers_controller.rb
@@ -3,23 +3,12 @@ module Admin
     class TeachersController < AdminController
       layout "full"
 
-      before_action :setup_school_page
-      before_action :setup_navigation_items
-
       def show
-      end
-
-    private
-
-      def setup_school_page
         @school = School.includes(:gias_school).find_by!(urn: params[:school_urn])
         @breadcrumbs = {
           "Schools" => admin_schools_path(page: params[:page], q: params[:q]),
           @school.name => nil
         }
-      end
-
-      def setup_navigation_items
         @navigation_items = helpers.admin_school_navigation_items(params[:school_urn], request.fullpath)
       end
     end
