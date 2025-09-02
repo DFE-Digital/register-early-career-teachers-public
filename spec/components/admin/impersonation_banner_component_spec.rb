c548e18ca5f468b227ca2d9ebb3c1c0b05dcad58
Peter Yates <peter.yates@graphia.co.uk>
2025-09-02 13:02:20 +0100

    Add impersonation banner
    This shows at the top of the page when an admin is impersonating a
    school user.

---
+++ spec/components/admin/impersonation_banner_component_spec.rb
@@ -0,0 +1,32 @@
+describe 'Admin::ImpersonationBannerComponent', type: :component do
+  let(:component) { Admin::ImpersonationBannerComponent.new(user:, school:) }
+
+  let(:school) { FactoryBot.create(:school) }
+  let(:dfe_staff_member) { FactoryBot.create(:user) }
+
+  context 'when the user is not impersonating' do
+    let(:user) { FactoryBot.create(:school_user, school_urn: school.urn) }
+
+    before { render_inline(component) }
+
+    it 'does not render' do
+      expect(rendered_content).to be_empty
+    end
+  end
+
+  context 'when the user is impersonating' do
+    let(:user) do
+      FactoryBot.build(:dfe_user_impersonating_school_user, email: dfe_staff_member.email, school_urn: school.urn)
+    end
+
+    before { render_inline(component) }
+
+    it 'renders a sentence explaining that impersonation is happening' do
+      expect(rendered_content).to have_content("You are signed in as #{school.name}")
+    end
+
+    it 'renders a button to finish impersonating' do
+      expect(rendered_content).to have_button("Sign out from #{school.name}")
+    end
+  end
+end
---
+++ spec/factories/sessions/dfe_user_impersonating_school_user_factory.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory(:dfe_user_impersonating_school_user, class: 'Sessions::Users::DfEUserImpersonatingSchoolUser') do
+    skip_create
+    initialize_with { new(email:, school_urn:, original_type: 'Sessions::DfEUser') }
+
+    school_urn { nil }
+    trait(:at_random_school) { school_urn { FactoryBot.create(:school).urn } }
+  end
+end
