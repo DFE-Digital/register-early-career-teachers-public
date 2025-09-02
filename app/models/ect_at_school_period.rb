f442d1f81e119ff18ce4bc3a5ba0890a95b23004
Peter Yates <peter.yates@graphia.co.uk>
2025-09-02 16:47:57 +0100

    Move earliest_first call from scope to relationships
    Putting it in the scope was a mistake on my part, it wasn't obvious it
    would affect the ECT index page.

--- app/models/ect_at_school_period.rb
+++ app/models/ect_at_school_period.rb
@@ -12,8 +12,8 @@ class ECTAtSchoolPeriod < ApplicationRecord
   has_many :training_periods, inverse_of: :ect_at_school_period
   has_many :mentor_at_school_periods, through: :teacher
   has_many :events
-  has_one :current_training_period, -> { current_or_future }, class_name: 'TrainingPeriod'
-  has_one :current_mentorship_period, -> { current_or_future }, class_name: 'MentorshipPeriod'
+  has_one :current_training_period, -> { current_or_future.earliest_first }, class_name: 'TrainingPeriod'
+  has_one :current_mentorship_period, -> { current_or_future.earliest_first }, class_name: 'MentorshipPeriod'
 
   refresh_metadata -> { school }, on_event: %i[create destroy update]
 
