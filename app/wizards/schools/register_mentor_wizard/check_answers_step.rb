f1acb88fafe229f36bdf8f116f5f20e47489b586
Avin Hurry <avinhurry@icloud.com>
2025-09-02 20:07:05 +0100

    [1249] Fix back links for previously registered mentors in check answers step
    - Return to `:mentoring_at_new_school_only` if user selected 'no' to "Mentoring only at your school"
    - Return to `:programme_choices` if user selected 'yes' to both "Mentoring only at your school" and "Use same programme choices"
    - Return to `:lead_provider` if not using previous programme choices

--- app/wizards/schools/register_mentor_wizard/check_answers_step.rb
+++ app/wizards/schools/register_mentor_wizard/check_answers_step.rb
@@ -6,18 +6,38 @@ module Schools
       end
 
       def previous_step
-        if wizard.store.back_to == 'eligibility_lead_provider'
-          wizard.store.back_to = nil
-          :eligibility_lead_provider
-        else
+        return :eligibility_lead_provider if pop_back_to!(:eligibility_lead_provider)
 
-          return :review_mentor_eligibility if ect.provider_led_training_programme? && mentor.funding_available?
+        return previous_for_previously_registered if mentor.previously_registered_as_mentor?
 
-          :email_address
-        end
+        return :review_mentor_eligibility if ect.provider_led_training_programme? && mentor.funding_available?
+
+        :email_address
       end
 
     private
+
+      def pop_back_to!(key)
+        return false unless store.back_to.to_s == key.to_s
+
+        store.back_to = nil
+        true
+      end
+
+      def previous_for_previously_registered
+        return :mentoring_at_new_school_only if user_chose_no_to_mentoring_at_new_school_only?
+        return :programme_choices if user_chose_yes_to_use_same_programme_choices?
+
+        :lead_provider
+      end
+
+      def user_chose_no_to_mentoring_at_new_school_only?
+        store.mentoring_at_new_school_only == 'no'
+      end
+
+      def user_chose_yes_to_use_same_programme_choices?
+        store.use_same_programme_choices == 'yes'
+      end
 
       def persist
         ActiveRecord::Base.transaction do
