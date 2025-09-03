1d88e564bc07dc14c66a81cb0089bcd69e81ad5f
Peter Yates <peter.yates@graphia.co.uk>
2025-09-03 15:58:00 +0100

    Move test setup into the the story
    Co-authored-by: avinhurry <avinhurry@icloud.com>

--- spec/features/admin/schools/impersonating_a_school_spec.rb
+++ spec/features/admin/schools/impersonating_a_school_spec.rb
@@ -1,11 +1,12 @@
 RSpec.describe 'Impersonating a school user' do
-  let(:admin_user) { FactoryBot.create(:user, :admin) }
-  let(:school) { FactoryBot.create(:school) }
+  before do
+    allow(Rails.application.config).to receive(:enable_schools_interface).and_return(true)
+  end
 
-  before { sign_in_as_dfe_user(role: :admin, user: admin_user) }
+  scenario 'successfully impersonating a school user' do
+    given_i_am_signed_in_as_an_admin_user
+    and_i_am_on_the_admin_show_page_for_a_school
 
-  scenario 'impersonating' do
-    given_i_am_on_the_admin_show_page_for_a_school
     when_i_click_sign_in_as_school
     then_i_should_be_on_the_schools_home_page
     and_i_should_be_impersonating_the_school
@@ -16,15 +17,21 @@ RSpec.describe 'Impersonating a school user' do
 
 private
 
-  def given_i_am_on_the_admin_show_page_for_a_school
-    path = "/admin/schools/#{school.urn}/overview"
+  def given_i_am_signed_in_as_an_admin_user
+    admin_user = FactoryBot.create(:user, :admin)
+    sign_in_as_dfe_user(role: :admin, user: admin_user)
+  end
+
+  def and_i_am_on_the_admin_show_page_for_a_school
+    @school = FactoryBot.create(:school)
+    path = "/admin/schools/#{@school.urn}/overview"
     page.goto(path)
 
     expect(page.url).to end_with(path)
   end
 
   def when_i_click_sign_in_as_school
-    page.get_by_role('button', name: "Sign in as #{school.name}").click
+    page.get_by_role('button', name: "Sign in as #{@school.name}").click
   end
 
   def then_i_should_be_on_the_schools_home_page
@@ -34,15 +41,15 @@ private
   def and_i_should_be_impersonating_the_school
     body = page.locator('body')
 
-    expect(body).to have_text("You are signed in as #{school.name}")
+    expect(body).to have_text("You are signed in as #{@school.name}")
   end
 
   def when_i_click_sign_out
-    page.get_by_role('button', name: "Sign out from #{school.name}").click
+    page.get_by_role('button', name: "Sign out from #{@school.name}").click
   end
 
   def then_i_should_be_on_the_admin_school_show_page
-    path = "/admin/schools/#{school.urn}/overview"
+    path = "/admin/schools/#{@school.urn}/overview"
 
     expect(page.url).to end_with(path)
   end
