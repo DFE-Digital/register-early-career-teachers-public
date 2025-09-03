500332e0d08b8993811ed5026839e658f9f2dfb5
Leandro Alemão <leandro.costantini@gmail.com>
2025-09-03 17:08:17 +0100

    [2302] Use new api controller concerns instead

--- app/services/api/statements/query.rb
+++ app/services/api/statements/query.rb
@@ -2,13 +2,11 @@ module API::Statements
   class Query
     class InvalidFeeTypeError < StandardError; end
 
-    include Queries::ConditionFormats
     include Queries::FilterIgnorable
-    include Queries::Orderable
 
     attr_reader :scope
 
-    def initialize(lead_provider_id: :ignore, contract_period_years: :ignore, updated_since: :ignore, fee_type: 'output', sort: nil)
+    def initialize(lead_provider_id: :ignore, contract_period_years: :ignore, updated_since: :ignore, fee_type: 'output', sort: { payment_date: :asc })
       @scope = Statement.distinct.includes(active_lead_provider: %i[lead_provider contract_period])
 
       where_lead_provider_is(lead_provider_id)
@@ -45,7 +43,7 @@ module API::Statements
     def where_contract_period_year_in(contract_period_years)
       return if ignore?(filter: contract_period_years)
 
-      scope.merge!(Statement.joins(:contract_period).where(contract_periods: { year: extract_conditions(contract_period_years) }))
+      scope.merge!(Statement.joins(:contract_period).where(contract_periods: { year: contract_period_years }))
     end
 
     def where_updated_since(updated_since)
@@ -63,7 +61,7 @@ module API::Statements
     end
 
     def set_sort_by(sort)
-      @scope = scope.order(sort_order(sort:, model: Statement, default: { payment_date: :asc }))
+      @scope = scope.order(sort)
     end
   end
 end
