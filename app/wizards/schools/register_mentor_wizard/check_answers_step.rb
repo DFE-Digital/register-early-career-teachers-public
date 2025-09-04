488a703b5e0f42c62237d14149c85476f4381a47
Avin Hurry <avinhurry@icloud.com>
2025-09-04 16:13:04 +0100

    [1249] Refactor `previous_step` to use case statement for clearer flow

--- app/wizards/schools/register_mentor_wizard/check_answers_step.rb
+++ app/wizards/schools/register_mentor_wizard/check_answers_step.rb
@@ -6,13 +6,22 @@ module Schools
       end
 
       def previous_step
-        return :eligibility_lead_provider if pop_back_to!(:eligibility_lead_provider)
-
-        return previous_for_previously_registered if mentor.previously_registered_as_mentor?
-
-        return :review_mentor_eligibility if ect.provider_led_training_programme? && mentor.funding_available?
-
-        :email_address
+        case
+        when pop_back_to!(:eligibility_lead_provider)
+          :eligibility_lead_provider
+        when mentor.previously_registered_as_mentor?
+          if user_chose_no_to_mentoring_at_new_school_only?
+            :mentoring_at_new_school_only
+          elsif user_chose_yes_to_use_same_programme_choices?
+            :programme_choices
+          else
+            :lead_provider
+          end
+        when ect.provider_led_training_programme? && mentor.funding_available?
+          :review_mentor_eligibility
+        else
+          :email_address
+        end
       end
 
     private
@@ -22,13 +31,6 @@ module Schools
 
         store.back_to = nil
         true
-      end
-
-      def previous_for_previously_registered
-        return :mentoring_at_new_school_only if user_chose_no_to_mentoring_at_new_school_only?
-        return :programme_choices if user_chose_yes_to_use_same_programme_choices?
-
-        :lead_provider
       end
 
       def user_chose_no_to_mentoring_at_new_school_only?
