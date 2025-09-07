75ce4d4533d90638ca1d3c988db2966fa9b3725c
edujackedu <jack.scotti@education.gov.uk>
2025-09-07 19:37:07 +0100

    Add event logging for closing mentor at school periods
    - Add teacher_finishes_mentor_at_school_period event type
    - Record events when MentorAtSchoolPeriods::Finish closes periods

--- app/services/mentor_at_school_periods/finish.rb
+++ app/services/mentor_at_school_periods/finish.rb
@@ -1,9 +1,10 @@
 class MentorAtSchoolPeriods::Finish
-  attr_reader :teacher, :finished_on
+  attr_reader :teacher, :finished_on, :author
 
-  def initialize(teacher:, finished_on:)
+  def initialize(teacher:, finished_on:, author:)
     @teacher = teacher
     @finished_on = finished_on
+    @author = author
   end
 
   def finish_existing_at_school_periods!
@@ -12,6 +13,14 @@ class MentorAtSchoolPeriods::Finish
         period.update!(finished_on:)
         period.mentorship_periods.ongoing_on(finished_on).update!(finished_on:)
         period.training_periods.ongoing_on(finished_on).update!(finished_on:)
+
+        Events::Record.record_teacher_left_school_as_mentor!(
+          author:,
+          mentor_at_school_period: period,
+          teacher:,
+          school: period.school,
+          happened_at: finished_on
+        )
       end
     end
   end
