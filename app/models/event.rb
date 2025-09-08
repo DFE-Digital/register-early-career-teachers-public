bab9b6b3a1806ea38e7d18bfd10b93d68ac57bb5
Erica Porter <ericaporter@users.noreply.github.com>
2025-09-08 08:52:50 +0100

    Allow admins to change delivery partner name

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   EVENT_TYPES = %w[
     bulk_upload_completed
     bulk_upload_started
+    delivery_partner_name_changed
     import_from_dqt
     induction_extension_created
     induction_extension_updated
