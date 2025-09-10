3b5b5ed86f91ce3e2e44b3a35c4a2096f127a19b
Avin Hurry <avinhurry@icloud.com>
2025-09-10 21:21:54 +0100

    [2172] Add `.at_school` scope to TrainingPeriod
    Provides a unified way to fetch training periods for both ECTs and mentors at a given school.

--- app/models/training_period.rb
+++ app/models/training_period.rb
@@ -45,6 +45,10 @@ class TrainingPeriod < ApplicationRecord
   scope :for_ect, ->(ect_at_school_period_id) { where(ect_at_school_period_id:) }
   scope :for_mentor, ->(mentor_at_school_period_id) { where(mentor_at_school_period_id:) }
   scope :for_school_partnership, ->(school_partnership_id) { where(school_partnership_id:) }
+  scope :at_school, ->(school_id) {
+    left_joins(:ect_at_school_period, :mentor_at_school_period)
+      .where('ect_at_school_periods.school_id = :id OR mentor_at_school_periods.school_id = :id', id: school_id)
+  }
 
   # Delegations
   delegate :name, to: :delivery_partner, prefix: true, allow_nil: true
