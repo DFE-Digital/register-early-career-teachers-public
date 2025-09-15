4ad3b4ead1fe80f91a2ffe94588898972020e35c
Avin Hurry <avinhurry@icloud.com>
2025-09-15 13:23:34 +0100

    [2172] Add TrainingPeriods::Search#linkable_to_school_partnership
    Adds query method to find training periods that:
    - belong to the given school (via ECT or mentor periods)
    - are not yet linked to a school partnership
    - are associated with an expression of interest
    - match the given lead provider and contract period

--- app/services/training_periods/search.rb
+++ app/services/training_periods/search.rb
@@ -12,6 +12,17 @@ module TrainingPeriods
       scope
     end
 
+    def linkable_to_school_partnership(school:, lead_provider:, contract_period:)
+      @scope = scope
+        .where(school_partnership_id: nil)
+        .at_school(school.id)
+        .joins(:expression_of_interest)
+        .where(active_lead_providers: {
+          lead_provider_id: lead_provider.id,
+          contract_period_year: contract_period.year
+        }).distinct
+    end
+
   private
 
     def filter_by_ect_id(ect_id)
