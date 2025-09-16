68de06fe0ac854780095039cf552a68959037694
Erica Porter <ericaporter@users.noreply.github.com>
2025-09-16 17:33:05 +0100

    Update mentor details page

--- app/components/schools/mentors/ect_mentor_training_details_component.rb
+++ app/components/schools/mentors/ect_mentor_training_details_component.rb
@@ -3,6 +3,11 @@ module Schools
     class ECTMentorTrainingDetailsComponent < ApplicationComponent
       include TeacherHelper
 
+      COMPLETED_REASONS = %w[
+        completed_during-early_roll_out
+        completed_declaration_received
+      ].freeze
+
       attr_reader :teacher, :mentor
 
       def initialize(teacher:, mentor:)
@@ -11,24 +16,61 @@ module Schools
       end
 
       def render?
-        assigned_ects.any?(&:provider_led_training_programme?)
+        ineligible_for_training? || mentor_training_period.present?
       end
 
       def eligible_for_training?
         teacher.mentor_became_ineligible_for_funding_on.nil?
       end
 
-      def first_lead_provider_name
-        assigned_ects
-          .order(:started_on)
-          .detect do |ect|
-            name = ECTAtSchoolPeriods::CurrentTraining.new(ect).lead_provider_name
-            return name if name
-          end
+      def ineligible_for_training?
+        teacher.mentor_became_ineligible_for_funding_on.present?
       end
 
-      def assigned_ects
-        @assigned_ects ||= @mentor.currently_assigned_ects
+      def completed_reason?
+        COMPLETED_REASONS.include?(teacher.mentor_became_ineligible_for_funding_reason)
+      end
+
+      def started_not_completed_reason?
+        teacher.mentor_became_ineligible_for_funding_reason == "started_not_completed"
+      end
+
+      def ineligible_date_govuk
+        teacher.mentor_became_ineligible_for_funding_on&.to_fs(:govuk)
+      end
+
+      def mentor_training_period
+        @mentor_training_period ||= TrainingPeriod
+                                      .current_or_future
+                                      .provider_led_training_programme
+                                      .for_mentor(@mentor.id)
+                                      .order(:started_on)
+                                      .first
+      end
+
+      def partnership_confirmed?
+        mentor_training_period&.school_partnership.present?
+      end
+
+      def lead_provider_name
+        if partnership_confirmed?
+          mentor_training_period&.lead_provider_name
+        else
+          mentor_training_period&.expression_of_interest_lead_provider&.name
+        end
+      end
+
+      def delivery_partner_name
+        mentor_training_period&.delivery_partner_name
+      end
+
+    private
+
+      def provider_led_registered?
+        TrainingPeriod.current_or_future
+                      .provider_led_training_programme
+                      .for_mentor(@mentor.id)
+                      .exists?
       end
     end
   end
