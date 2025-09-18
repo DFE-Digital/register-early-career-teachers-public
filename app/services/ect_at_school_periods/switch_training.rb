c5e72d9bf4d0675c5a4f1edaffddc04ce073951a
Connor McQuillan <me@cpjmcquillan.com>
2025-09-18 13:31:31 +0100

    Introduce `ECTAtSchoolPeriods::SwitchTraining` service object
    This was pulled out as part of the work to facilitate schools changing an ECT's
    training programme.
    
    There is some relatively complex business logic to consider when changing an
    ECT's training programme from school-led to provider-led (and vice versa), so it
    seemed sensible to abstract this into a service object we can test rigorously in
    isolation.
    
    This has been designed and implemented based on point 6. of the acceptance
    criteria outlined [here]. Lots of this is new ground for me, so it's possible I
    might have missed something!
    
    The intention is to call this service object as part of the "Check answers" step
    in the new "Change training programme" wizard, which will follow in a subsequent
    PR.
    
    [here]: https://github.com/DFE-Digital/register-ects-project-board/issues/2100

---
+++ app/services/ect_at_school_periods/switch_training.rb
@@ -0,0 +1,85 @@
+module ECTAtSchoolPeriods
+  class IncorrectTrainingProgrammeError < StandardError; end
+  class NoTrainingPeriodError < StandardError; end
+
+  class SwitchTraining
+    include TrainingPeriodSources
+
+    def self.to_school_led(...) = new(...).to_school_led
+    def self.to_provider_led(...) = new(...).to_provider_led
+
+    def initialize(ect_at_school_period, author:, lead_provider: nil)
+      raise ArgumentError, "an `ECTAtSchoolPeriod` must be provided" unless
+        ect_at_school_period.is_a?(ECTAtSchoolPeriod)
+
+      @ect_at_school_period = ect_at_school_period
+      @training_period = ect_at_school_period.current_or_next_training_period
+
+      raise NoTrainingPeriodError unless @training_period
+
+      @school = @ect_at_school_period.school
+      @lead_provider = lead_provider.presence || @training_period.lead_provider
+      @author = author
+    end
+
+    def to_school_led
+      raise IncorrectTrainingProgrammeError if
+        @ect_at_school_period.school_led_training_programme?
+
+      ActiveRecord::Base.transaction do
+        finish_or_destroy_existing_training_period!
+        create_school_led_training_period!
+      end
+    end
+
+    def to_provider_led
+      raise IncorrectTrainingProgrammeError if
+        @ect_at_school_period.provider_led_training_programme?
+
+      ActiveRecord::Base.transaction do
+        finish_training_period!
+        create_provider_led_training_period!
+      end
+    end
+
+  private
+
+    attr_reader :school, :lead_provider
+
+    def finish_or_destroy_existing_training_period!
+      if @training_period.school_partnership.present?
+        finish_training_period!
+      else
+        @training_period.destroy!
+      end
+    end
+
+    def finish_training_period!
+      TrainingPeriods::Finish.ect_training(
+        training_period: @training_period,
+        ect_at_school_period: @ect_at_school_period,
+        finished_on:,
+        author: @author
+      ).finish!
+    end
+
+    def create_school_led_training_period!
+      TrainingPeriods::Create.school_led(
+        period: @ect_at_school_period,
+        started_on:
+      ).call
+    end
+
+    def create_provider_led_training_period!
+      TrainingPeriods::Create.provider_led(
+        period: @ect_at_school_period,
+        started_on:,
+        school_partnership: earliest_matching_school_partnership,
+        expression_of_interest:
+      ).call
+    end
+
+    def started_on = Date.current
+    def finished_on = Date.current
+  end
+end
