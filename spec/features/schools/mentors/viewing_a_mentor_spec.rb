4fe046b22fe31849870c4a7b9c8a9b2b4247d51c
Erica Porter <ericaporter@users.noreply.github.com>
2025-09-18 08:49:57 +0100

    Update specs, add feature tests, remove unused methods

--- spec/features/schools/mentors/viewing_a_mentor_spec.rb
+++ spec/features/schools/mentors/viewing_a_mentor_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "Viewing a mentor" do
     allow(Rails.application.config).to receive(:enable_schools_interface).and_return(true)
   end
 
-  scenario 'Happy path' do
+  scenario "Happy path" do
     given_that_i_have_an_active_mentor_with_an_ect
     and_i_sign_in_as_a_school
     when_i_visit_the_index_page
@@ -19,30 +19,86 @@ RSpec.describe "Viewing a mentor" do
     then_i_am_on_the_mentors_index_page
   end
 
-private
+  scenario "Mentor training – EOI (awaiting confirmation)" do
+    given_an_eligible_mentor_with_eoi_training
+    and_i_sign_in_as_a_school
+    when_i_visit_the_index_page
+    and_i_click_on_the_current_mentor
+
+    then_i_see_ecte_mentor_training_details
+    and_i_see_lead_provider("Hidden leaf village")
+    and_i_see_hint("Awaiting confirmation by Hidden leaf village")
+    and_i_see_delivery_partner("Yet to be reported by the lead provider")
+  end
+
+  scenario "Mentor training – confirmed partnership (LP+DP shown)" do
+    given_an_eligible_mentor_with_confirmed_training
+    and_i_sign_in_as_a_school
+    when_i_visit_the_index_page
+    and_i_click_on_the_current_mentor
+
+    then_i_see_ecte_mentor_training_details
+    and_i_see_lead_provider("Hidden leaf village")
+    and_i_see_hint("Confirmed by Hidden leaf village")
+    and_i_see_delivery_partner("Artisan Education Group")
+    and_i_see_change_dp_hint
+  end
+
+  scenario "Mentor training – ineligible (completed)" do
+    given_an_ineligible_mentor_completed_on(Date.new(2024, 1, 1))
+    and_i_sign_in_as_a_school
+    when_i_visit_the_index_page
+    and_i_click_on_the_current_mentor
+
+    then_i_see_text("Naruto Uzumaki completed mentor training on 1 January 2024.")
+    and_i_do_not_see_summary_rows
+  end
+
+  scenario "Mentor training – ineligible (started_not_completed)" do
+    given_an_ineligible_mentor_started_not_completed
+    and_i_sign_in_as_a_school
+    when_i_visit_the_index_page
+    and_i_click_on_the_current_mentor
+
+    then_i_see_text(/cannot do further mentor training/i)
+    and_i_do_not_see_summary_rows
+  end
+
+  scenario "Mentor training – nothing to show (no current_or_future period)" do
+    given_an_eligible_mentor_with_only_past_training
+    and_i_sign_in_as_a_school
+    when_i_visit_the_index_page
+    and_i_click_on_the_current_mentor
+
+    then_i_do_not_see_ecte_mentor_training_details
+    and_i_do_not_see_summary_rows
+  end
 
   def given_that_i_have_an_active_mentor_with_an_ect
     start_date = Date.new(2023, 9, 1)
 
-    @school = FactoryBot.create(:school, urn: '1234567')
-    @mentor_teacher = FactoryBot.create(:teacher, trs_first_name: 'Naruto', trs_last_name: 'Uzumaki')
-    @mentor = FactoryBot.create(:mentor_at_school_period, teacher: @mentor_teacher, school: @school, started_on: start_date, finished_on: nil, id: 1)
+    @school = FactoryBot.create(:school, urn: "1234567")
+    @mentor_teacher = FactoryBot.create(:teacher, trs_first_name: "Naruto", trs_last_name: "Uzumaki")
+    @mentor = FactoryBot.create(:mentor_at_school_period,
+                                teacher: @mentor_teacher,
+                                school: @school,
+                                started_on: start_date,
+                                finished_on: nil,
+                                id: 1)
 
-    @ect_teacher = FactoryBot.create(:teacher, trs_first_name: 'Boruto', trs_last_name: 'Uzumaki')
-    @ect = FactoryBot.create(
-      :ect_at_school_period,
-      teacher: @ect_teacher,
-      school: @school,
-      started_on: start_date,
-      finished_on: nil
-    )
+    @ect_teacher = FactoryBot.create(:teacher, trs_first_name: "Boruto", trs_last_name: "Uzumaki")
+    @ect = FactoryBot.create(:ect_at_school_period,
+                             teacher: @ect_teacher,
+                             school: @school,
+                             started_on: start_date,
+                             finished_on: nil)
 
-    FactoryBot.create(:training_period, :ongoing, :provider_led, ect_at_school_period: @ect)
+    FactoryBot.create(:training_period, :ongoing, :provider_led, :for_ect, ect_at_school_period: @ect)
     FactoryBot.create(:mentorship_period, mentor: @mentor, mentee: @ect, started_on: start_date, finished_on: nil)
   end
 
   def given_i_click_on_an_assigned_ect
-    page.get_by_role('link', name: 'Boruto Uzumaki').click
+    page.get_by_role("link", name: "Boruto Uzumaki").click
   end
 
   def and_i_am_on_the_ect_details_page
@@ -62,14 +118,143 @@ private
   end
 
   def and_i_click_on_a_mentor
-    page.get_by_role('link', name: 'Naruto Uzumaki').click
+    page.get_by_role("link", name: "Naruto Uzumaki").click
   end
 
   def then_i_am_on_the_mentor_details_page
     expect(page).to have_path("/schools/mentors/#{@mentor.id}")
   end
 
+  def given_i_click_the_back_link
+    page.locator('a.govuk-back-link').first.click
+  end
+
+  def when_i_click_the_back_link
+    page.locator('a.govuk-back-link').first.click
+  end
+
   def then_i_am_on_the_mentors_index_page
-    expect(page).to have_path('/schools/home/mentors')
+    expect(page).to have_path("/schools/home/mentors")
+  end
+
+  def build_school_and_mentor
+    @school = FactoryBot.create(:school, urn: "1234567")
+    @mentor_teacher = FactoryBot.create(:teacher, trs_first_name: "Naruto", trs_last_name: "Uzumaki")
+    @mentor = FactoryBot.create(:mentor_at_school_period,
+                                teacher: @mentor_teacher,
+                                school: @school,
+                                started_on: Date.new(2023, 9, 1),
+                                finished_on: nil,
+                                id: 1)
+    @ects = []
+  end
+
+  def lp_dp_and_partnership
+    @lead_provider = FactoryBot.create(:lead_provider, name: "Hidden leaf village")
+    @active_lp     = FactoryBot.create(:active_lead_provider, lead_provider: @lead_provider)
+    @dp            = FactoryBot.create(:delivery_partner, name: "Artisan Education Group")
+    @lpdp          = FactoryBot.create(:lead_provider_delivery_partnership,
+                                       active_lead_provider: @active_lp,
+                                       delivery_partner: @dp)
+    @school_partnership = FactoryBot.create(:school_partnership,
+                                            lead_provider_delivery_partnership: @lpdp,
+                                            school: @school)
+  end
+
+  def given_an_eligible_mentor_with_eoi_training
+    build_school_and_mentor
+    lp_dp_and_partnership
+    FactoryBot.create(:training_period, :provider_led, :for_mentor,
+                      mentor_at_school_period: @mentor,
+                      started_on: Time.zone.today.beginning_of_month,
+                      finished_on: nil,
+                      school_partnership: nil,
+                      expression_of_interest: @active_lp)
+  end
+
+  def given_an_eligible_mentor_with_confirmed_training
+    build_school_and_mentor
+    lp_dp_and_partnership
+    FactoryBot.create(:training_period, :provider_led, :for_mentor,
+                      mentor_at_school_period: @mentor,
+                      started_on: Time.zone.today.beginning_of_month,
+                      finished_on: nil,
+                      school_partnership: @school_partnership)
+  end
+
+  def given_an_ineligible_mentor_completed_on(date)
+    build_school_and_mentor
+    @mentor_teacher.update!(
+      mentor_became_ineligible_for_funding_on: date,
+      mentor_became_ineligible_for_funding_reason: "completed_declaration_received"
+    )
+  end
+
+  def given_an_ineligible_mentor_started_not_completed
+    build_school_and_mentor
+    @mentor_teacher.update!(
+      mentor_became_ineligible_for_funding_on: Date.new(2024, 1, 1),
+      mentor_became_ineligible_for_funding_reason: "started_not_completed"
+    )
+  end
+
+  def given_an_eligible_mentor_with_only_past_training
+    build_school_and_mentor
+    lp_dp_and_partnership
+    started  = Date.new(2023, 10, 1)
+    finished = Date.new(2023, 12, 1)
+    FactoryBot.create(:training_period, :provider_led, :for_mentor,
+                      mentor_at_school_period: @mentor,
+                      started_on: started, finished_on: finished,
+                      school_partnership: @school_partnership)
+  end
+
+  def and_i_click_on_the_current_mentor
+    page.get_by_role("link", name: "Naruto Uzumaki").click
+  end
+
+  def then_i_see_ecte_mentor_training_details
+    expect(page.locator('h2.govuk-heading-m:has-text("ECTE mentor training details")')).to be_visible
+  end
+
+  def then_i_do_not_see_ecte_mentor_training_details
+    expect(page.locator('h2.govuk-heading-m:has-text("ECTE mentor training details")')).to have_count(0)
+  end
+
+  def and_i_see_lead_provider(name)
+    row = page.locator('.govuk-summary-list__row:has(dt.govuk-summary-list__key:has-text("Lead provider"))')
+    expect(row.locator('dd.govuk-summary-list__value')).to be_visible
+    expect(row.locator(%(dd.govuk-summary-list__value:has-text("#{name}")))).to be_visible
+  end
+
+  def and_i_see_delivery_partner(text_or_name)
+    row = page.locator('.govuk-summary-list__row:has(dt.govuk-summary-list__key:has-text("Delivery partner"))')
+    expect(row.locator('dd.govuk-summary-list__value')).to be_visible
+    expect(row.locator(%(dd.govuk-summary-list__value:has-text("#{text_or_name}")))).to be_visible
+  end
+
+  def and_i_see_change_dp_hint
+    expect(page.locator(%(.govuk-hint:has-text("To change the delivery partner, you must contact the lead provider")))).to be_visible
+  end
+
+  def and_i_see_hint(str_or_regex)
+    if str_or_regex.is_a?(String)
+      expect(page.locator(%(.govuk-hint:has-text("#{str_or_regex}")))).to be_visible
+    else
+      expect(page.locator("text=/#{str_or_regex.source}/i")).to be_visible
+    end
+  end
+
+  def and_i_do_not_see_summary_rows
+    expect(page.locator('.govuk-summary-list__row:has(dt.govuk-summary-list__key:has-text("Lead provider"))')).to have_count(0)
+    expect(page.locator('.govuk-summary-list__row:has(dt.govuk-summary-list__key:has-text("Delivery partner"))')).to have_count(0)
+  end
+
+  def then_i_see_text(text_or_regex)
+    if text_or_regex.is_a?(String)
+      expect(page.locator("text=#{text_or_regex}")).to be_visible
+    else
+      expect(page.locator("text=/#{text_or_regex.source}/i")).to be_visible
+    end
   end
 end
--- spec/views/schools/mentors/show.html.erb_spec.rb
+++ spec/views/schools/mentors/show.html.erb_spec.rb
@@ -1,11 +1,16 @@
-RSpec.describe 'schools/mentors/show.html.erb' do
-  let(:school) { FactoryBot.create(:school) }
-  let(:start_date) { Date.new(2023, 9, 1) }
+RSpec.describe "schools/mentors/show.html.erb" do
+  let(:school)      { FactoryBot.create(:school) }
+  let(:start_date)  { Date.new(2023, 9, 1) }
 
   let(:mentor_teacher) do
-    FactoryBot.create(:teacher, trs_first_name: 'Naruto', trs_last_name: 'Uzumaki', mentor_became_ineligible_for_funding_on:, mentor_became_ineligible_for_funding_reason:)
+    FactoryBot.create(
+      :teacher,
+      trs_first_name: "Naruto",
+      trs_last_name: "Uzumaki",
+      mentor_became_ineligible_for_funding_on:,
+      mentor_became_ineligible_for_funding_reason:
+    )
   end
-
   let(:mentor_became_ineligible_for_funding_on) { nil }
   let(:mentor_became_ineligible_for_funding_reason) { nil }
 
@@ -13,119 +18,90 @@ RSpec.describe 'schools/mentors/show.html.erb' do
     FactoryBot.create(:mentor_at_school_period, teacher: mentor_teacher, school:, started_on: start_date, finished_on: nil)
   end
 
-  let(:lead_provider) { FactoryBot.create(:lead_provider, name: 'Hidden leaf village') }
+  let(:ects) { [] }
 
-  let(:ect_teacher) do
-    FactoryBot.create(:teacher, trs_first_name: 'Konohamaru', trs_last_name: 'Sarutobi')
+  let(:lead_provider)                  { FactoryBot.create(:lead_provider, name: "Hidden leaf village") }
+  let(:active_lead_provider)           { FactoryBot.create(:active_lead_provider, lead_provider:) }
+  let(:lead_provider_delivery_partner) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:) }
+  let(:school_partnership)             { FactoryBot.create(:school_partnership, lead_provider_delivery_partnership: lead_provider_delivery_partner, school:) }
+
+  def render_view
+    assign(:mentor, mentor_period)
+    assign(:teacher, mentor_teacher)
+    assign(:ects, ects)
+    render
   end
 
-  let(:ect_period) do
-    FactoryBot.create(
-      :ect_at_school_period,
-      teacher: ect_teacher,
-      school:,
-      started_on: start_date,
-      finished_on: nil
-    )
-  end
-
-  let!(:mentorship_period) do
-    FactoryBot.create(
-      :mentorship_period,
-      mentor: mentor_period,
-      mentee: ect_period,
-      started_on: start_date,
-      finished_on: nil
-    )
-  end
-
-  let(:active_lead_provider) { FactoryBot.create(:active_lead_provider, lead_provider:) }
-  let(:lead_provider_delivery_partnership) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:) }
-  let(:school_partnership) { FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:, school:) }
-
-  context 'when mentor is eligible (no completion date)' do
-    let!(:training_period) do
+  context "when mentor is eligible via EOI (awaiting confirmation)" do
+    before do
       FactoryBot.create(
-        :training_period,
-        :provider_led,
-        :for_mentor,
+        :training_period, :provider_led, :for_mentor,
         mentor_at_school_period: mentor_period,
-        school_partnership:,
-        started_on: start_date,
-        finished_on: nil
+        started_on: start_date, finished_on: nil,
+        school_partnership: nil,
+        expression_of_interest: active_lead_provider
       )
+      render_view
     end
 
-    before do
-      assign(:mentor, mentor_period)
-      assign(:teacher, mentor_teacher)
-      assign(:ects, mentor_period.currently_assigned_ects)
-      render
+    it "shows the ECTE mentor training details heading" do
+      expect(rendered).to have_css("h2.govuk-heading-m", text: "ECTE mentor training details")
     end
 
-    it 'renders the ECTE mentor training details H2' do
-      expect(rendered).to have_css('h2.govuk-heading-m', text: 'ECTE mentor training details')
+    it "shows the lead provider and 'Awaiting confirmation' hint" do
+      expect(rendered).to have_css("dt.govuk-summary-list__key", text: "Lead provider")
+      expect(rendered).to have_text("Hidden leaf village")
+      expect(rendered).to have_css(".govuk-hint", text: /Awaiting confirmation by Hidden leaf village/)
     end
 
-    it 'renders the lead provider row with the correct label' do
-      expect(rendered).to have_css('dt.govuk-summary-list__key', text: 'Lead provider')
-    end
-
-    it 'renders the lead provider row with a value' do
-      expect(rendered).to have_css('dd.govuk-summary-list__value', text: 'Hidden leaf village')
+    it "shows the delivery partner as 'Yet to be reported by the lead provider'" do
+      expect(rendered).to have_css("dt.govuk-summary-list__key", text: "Delivery partner")
+      expect(rendered).to have_text("Yet to be reported by the lead provider")
     end
   end
 
-  context 'when mentor is not eligible (i.e has a `mentor_became_ineligible_for_funding_on` date)' do
+  context "when mentor has a confirmed partnership" do
+    before do
+      FactoryBot.create(
+        :training_period, :provider_led, :for_mentor,
+        mentor_at_school_period: mentor_period,
+        started_on: start_date, finished_on: nil,
+        school_partnership:
+      )
+      render_view
+    end
+
+    it "shows the lead provider with a 'Confirmed by' hint" do
+      expect(rendered).to have_css(".govuk-hint", text: /Confirmed by Hidden leaf village/)
+    end
+
+    it "shows the delivery partner and the change-DP hint" do
+      dp_name = lead_provider_delivery_partner.delivery_partner.name
+      expect(rendered).to have_css("dt.govuk-summary-list__key", text: "Delivery partner")
+      expect(rendered).to have_text(dp_name)
+      expect(rendered).to have_css(".govuk-hint", text: /To change the delivery partner, you must contact the lead provider/)
+    end
+  end
+
+  context "when mentor is not eligible (completed)" do
     let(:mentor_became_ineligible_for_funding_on) { Date.new(2024, 1, 1) }
-    let(:mentor_became_ineligible_for_funding_reason) { 'completed_declaration_received' }
+    let(:mentor_became_ineligible_for_funding_reason) { "completed_declaration_received" }
 
-    before do
-      assign(:mentor, mentor_period)
-      assign(:teacher, mentor_teacher)
-      assign(:ects, mentor_period.currently_assigned_ects)
-      render
-    end
+    before { render_view }
 
-    it 'renders the ineligible message' do
-      expect(rendered).to have_css('.govuk-body', text: /Naruto Uzumaki completed mentor training on 1 January 2024/)
-    end
-
-    it 'does not render the lead provider row with the correct label' do
-      expect(rendered).not_to have_css('dt.govuk-summary-list__key', text: 'Lead provider')
-    end
-
-    it 'does not render the lead provider row with a value' do
-      expect(rendered).not_to have_css('dd.govuk-summary-list__value', text: 'Hidden leaf village')
-    end
-
-    it 'does not show the no info text' do
-      expect(rendered).not_to have_text('Your lead provider has not reported any information to us yet')
+    it "shows the completed message with GOV.UK date and hides the summary list" do
+      expect(rendered).to have_css(".govuk-body", text: "Naruto Uzumaki completed mentor training on 1 January 2024.")
+      expect(rendered).not_to have_css("dt.govuk-summary-list__key", text: "Lead provider")
+      expect(rendered).not_to have_css("dt.govuk-summary-list__key", text: "Delivery partner")
     end
   end
 
-  context 'when all ECTs are school-led' do
-    let(:ect_period) do
-      FactoryBot.create(:ect_at_school_period, teacher: ect_teacher, school:, started_on: start_date, finished_on: nil)
-    end
+  context "when nothing should render (no mentor training and not ineligible)" do
+    before { render_view }
 
-    let!(:training_period) do
-      FactoryBot.create(:training_period, :school_led, ect_at_school_period: ect_period, started_on: ect_period.started_on)
-    end
-
-    before do
-      assign(:mentor, mentor_period)
-      assign(:teacher, mentor_teacher)
-      assign(:ects, mentor_period.currently_assigned_ects)
-      render
-    end
-
-    it 'does not render the ECTE mentor training details H2' do
-      expect(rendered).not_to have_css('h2.govuk-heading-m', text: 'ECTE mentor training details')
-    end
-
-    it 'does not render the lead provider/delivery partner row' do
-      expect(rendered).not_to have_css('dt.govuk-summary-list__key', text: 'Lead provider')
+    it "does not render the section" do
+      expect(rendered).not_to have_css("h2.govuk-heading-m", text: "ECTE mentor training details")
+      expect(rendered).not_to have_css("dt.govuk-summary-list__key", text: "Lead provider")
       expect(rendered).not_to have_css("dt.govuk-summary-list__key", text: "Delivery partner")
     end
   end
