36cb523269eb1c99d778d5d3103e2f9321645aad
Ross Oliver <ross.oliver36@gmail.com>
2025-09-25 10:43:03 +0100

    Add ability to skip metadata/touch updates
    Add a `skip_updates` method to `DeclarativeTouch` and `DeclarativeMetadata` so
    that we can easily skip updates for certain actions.
    
    We already have a `skip_metadata_updates` method on `Metadata::Manager` used
    for migration; we'll move away from this in a follow up commit.

--- app/models/concerns/declarative_metadata.rb
+++ app/models/concerns/declarative_metadata.rb
@@ -1,9 +1,13 @@
 module DeclarativeMetadata
   extend ActiveSupport::Concern
 
+  SKIP_UPDATES_THREAD_KEY = :skip_declarative_metadata_updates
+
   class_methods do
     def refresh_metadata(target, when_changing: [], on_event: %i[update])
       after_commit(on: on_event) do
+        next if DeclarativeMetadata.skip_updates?
+
         should_touch = when_changing.blank? || when_changing.any? do |attr|
           saved_change_to_attribute?(attr)
         end
@@ -11,5 +15,17 @@ module DeclarativeMetadata
         Metadata::Manager.new.refresh_metadata!(instance_exec(&target)) if should_touch
       end
     end
+  end
+
+  def self.skip_updates
+    prev = Thread.current[SKIP_UPDATES_THREAD_KEY]
+    Thread.current[SKIP_UPDATES_THREAD_KEY] = true
+    yield
+  ensure
+    Thread.current[SKIP_UPDATES_THREAD_KEY] = prev
+  end
+
+  def self.skip_updates?
+    Thread.current[SKIP_UPDATES_THREAD_KEY]
   end
 end
