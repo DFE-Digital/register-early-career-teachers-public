458833fd18674aec93b46660eb66ff087f76a3f7
Peter Yates <peter.yates@graphia.co.uk>
2025-09-25 08:05:38 +0100

    Add roles
    Roles are now backed by a single role column and a user can have one
    role.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,22 @@
 class User < ApplicationRecord
+  Role = Data.define(:identifier, :name)
+
+  ROLES = [
+    Role.new(identifier: :admin, name: 'Admin'),
+    Role.new(identifier: :super_admin, name: 'Super admin'),
+    Role.new(identifier: :finance, name: 'Finance'),
+  ].freeze
+
+  enum :role, ROLES.to_h { |r| [r.identifier, r.identifier.to_s] }, validate: { message: 'Must be admin, finance or super_admin' }
+
   encrypts :otp_secret
 
   # Validations
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, notify_email: true
+  validates :role, presence: true
 
   # Associations
-  has_many :dfe_roles
   has_many :events
   has_many :authored_events, class_name: 'Event', inverse_of: :author
-
-  # Instance Methods
-  def dfe_user?
-    dfe_roles.any?
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,24 @@ describe User do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_inclusion_of(:role).in_array(%i[admin finance super_admin]).with_message("Must be admin, finance or super_admin") }
+    it { is_expected.to validate_presence_of(:role) }
   end
 
   describe 'associations' do
     it { is_expected.to have_many(:events) }
     it { is_expected.to have_many(:authored_events).inverse_of(:author).class_name('Event') }
+  end
+
+  describe 'enums' do
+    it 'has a roles enum with admin, finance and superuser' do
+      expect(subject).to(
+        define_enum_for(:role)
+          .with_values({ admin: "admin",
+                         super_admin: "super_admin",
+                         finance: "finance" })
+          .backed_by_column_of_type(:enum)
+      )
+    end
   end
 end
