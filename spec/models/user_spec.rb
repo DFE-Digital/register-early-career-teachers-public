1f3524f0cd0035924556b88d75b39c758849bce6
Peter Yates <peter.yates@graphia.co.uk>
2025-09-25 06:20:23 +0100

    Drop table dfe_roles
    Instead users will have a single role, admin < finance < super admin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,6 @@ describe User do
   end
 
   describe 'associations' do
-    it { is_expected.to have_many(:dfe_roles) }
     it { is_expected.to have_many(:events) }
     it { is_expected.to have_many(:authored_events).inverse_of(:author).class_name('Event') }
   end
