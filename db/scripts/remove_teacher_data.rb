5889dae350b78bc36c14575a34a1a6708ad9786c
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-09-30 22:43:53 +0100

    Update one-off database script to remove many teachers

--- db/scripts/remove_teacher_data.rb
+++ db/scripts/remove_teacher_data.rb
@@ -3,5 +3,20 @@
 # $ kubectl exec -it <pod-name> -- bin/rails runner db/scripts/remove_teacher_data.rb
 # $ kubectl exec -it <pod-name> -- bundle exec rails runner db/scripts/remove_teacher_data.rb
 
-teacher = Teacher.find(79_588)
-RemoveTeacher.new(teacher.id).call
+[
+  9000,
+  38_625,
+  16_390,
+  29_640,
+  26_939,
+  56_966,
+  66_740,
+  25_882,
+  61_669,
+  34_433,
+  34_472,
+  76_279,
+  76_278,
+].map do |teacher_id|
+  RemoveTeacher.new(teacher_id).call
+end
