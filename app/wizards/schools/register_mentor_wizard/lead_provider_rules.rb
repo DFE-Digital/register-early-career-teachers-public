2101865622ac70acb6101be40f3d5f4421d522f5
Avin Hurry <avinhurry@icloud.com>
2025-09-30 12:18:47 +0100

    [2377] Refactor LeadProviderRules to use delegation
    - Delegate subject methods for readability
    - Extract mentoring/funding condition into helper
    - Simplify previous_step_from_lead_provider logic
    
    Co-authored-by: Peter Yates <peter.yates@graphia.co.uk>

--- app/wizards/schools/register_mentor_wizard/lead_provider_rules.rb
+++ app/wizards/schools/register_mentor_wizard/lead_provider_rules.rb
@@ -1,22 +1,30 @@
 module Schools
   module RegisterMentorWizard
     class LeadProviderRules < ::Rules::Base
+      delegate :provider_led_ect?,
+               :mentoring_at_new_school_only?,
+               :funding_available?,
+               :ect_lead_provider_invalid?,
+               :previously_registered_as_mentor?,
+               to: :subject
+
       def show_row_in_check_your_answers?
-        subject.provider_led_ect? &&
-          ((subject.mentoring_at_new_school_only? && subject.funding_available?) ||
-           subject.ect_lead_provider_invalid?)
+        provider_led_ect? && (mentoring_at_new_school_with_funding? || ect_lead_provider_invalid?)
       end
 
       def needs_selection_for_new_registration?
-        !subject.previously_registered_as_mentor? && subject.ect_lead_provider_invalid?
+        !previously_registered_as_mentor? && ect_lead_provider_invalid?
       end
 
       def previous_step_from_lead_provider
-        return :email_address if subject.ect_lead_provider_invalid? && !subject.previously_registered_as_mentor?
-        return :previous_training_period_details if subject.ect_lead_provider_invalid? && subject.previously_registered_as_mentor?
+        return :programme_choices unless ect_lead_provider_invalid?
 
-        :programme_choices
+        previously_registered_as_mentor? ? :previous_training_period_details : :email_address
       end
+
+    private
+
+      def mentoring_at_new_school_with_funding? = mentoring_at_new_school_only? && funding_available?
     end
   end
 end
