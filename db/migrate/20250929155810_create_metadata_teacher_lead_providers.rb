c93cea6adef8d88079d9e1ae2b37c244dc890468
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-10-01 15:53:02 +0100

    [2477] removed refresh from ECT/Mentor at school periods

--- db/migrate/20250929155810_create_metadata_teacher_lead_providers.rb
+++ db/migrate/20250929155810_create_metadata_teacher_lead_providers.rb
@@ -4,8 +4,8 @@ class CreateMetadataTeacherLeadProviders < ActiveRecord::Migration[8.0]
       t.references :teacher, foreign_key: true
       t.references :lead_provider, foreign_key: true
 
-      t.references :latest_ect_training_period, foreign_key: { to_table: :training_periods }, null: true
-      t.references :latest_mentor_training_period, foreign_key: { to_table: :training_periods }, null: true
+      t.references :latest_ect_training_period, foreign_key: { to_table: :training_periods, on_delete: :nullify }, null: true
+      t.references :latest_mentor_training_period, foreign_key: { to_table: :training_periods, on_delete: :nullify }, null: true
 
       t.timestamps
     end
