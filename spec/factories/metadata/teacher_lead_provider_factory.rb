5c21841ec8644d602bfe5b0ef9e859bcf4454e19
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-10-02 12:22:21 +0100

    [2477] dry up the test traits

--- spec/factories/metadata/teacher_lead_provider_factory.rb
+++ spec/factories/metadata/teacher_lead_provider_factory.rb
@@ -3,54 +3,51 @@ FactoryBot.define do
     association :teacher
     association :lead_provider
 
-    trait :with_latest_ect_training_period do
-      latest_ect_training_period do
-        contract_period = FactoryBot.create(:contract_period, :current)
-        active_lead_provider = FactoryBot.create(:active_lead_provider, contract_period:, lead_provider:)
-        lead_provider_delivery_partnership = FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:)
-        school_partnership = FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:)
-        school = school_partnership.school
-        ect_at_school_period = FactoryBot.create(
-          :ect_at_school_period,
+    transient do
+      ect_teacher_type { false }
+      mentor_teacher_type { false }
+    end
+
+    after(:create) do |metadata, evaluator|
+      teacher = metadata.teacher
+      lead_provider = metadata.lead_provider
+
+      contract_period = FactoryBot.create(:contract_period, :current)
+      active_lead_provider = FactoryBot.create(:active_lead_provider, contract_period:, lead_provider:)
+      lead_provider_delivery_partnership = FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:)
+      school_partnership = FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:)
+      school = school_partnership.school
+
+      [
+        (:ect if evaluator.ect_teacher_type),
+        (:mentor if evaluator.mentor_teacher_type),
+      ].compact.each do |teacher_type|
+        at_school_period = FactoryBot.create(
+          :"#{teacher_type}_at_school_period",
           :ongoing,
           school:,
           teacher:
         )
 
-        FactoryBot.create(
+        training_period = FactoryBot.create(
           :training_period,
-          :for_ect,
-          started_on: ect_at_school_period.started_on,
-          finished_on: ect_at_school_period.finished_on,
-          ect_at_school_period:,
+          :"for_#{teacher_type}",
+          started_on: at_school_period.started_on,
+          finished_on: at_school_period.finished_on,
+          "#{teacher_type}_at_school_period": at_school_period,
           school_partnership:
         )
+
+        metadata.public_send("latest_#{teacher_type}_training_period=", training_period)
       end
     end
 
-    trait :with_latest_mentor_training_period do
-      latest_mentor_training_period do
-        contract_period = FactoryBot.create(:contract_period, :current)
-        active_lead_provider = FactoryBot.create(:active_lead_provider, contract_period:, lead_provider:)
-        lead_provider_delivery_partnership = FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:)
-        school_partnership = FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:)
-        school = school_partnership.school
-        mentor_at_school_period = FactoryBot.create(
-          :mentor_at_school_period,
-          :ongoing,
-          school:,
-          teacher:
-        )
+    trait :with_latest_ect_training_period do
+      ect_teacher_type { true }
+    end
 
-        FactoryBot.create(
-          :training_period,
-          :for_mentor,
-          started_on: mentor_at_school_period.started_on,
-          finished_on: mentor_at_school_period.finished_on,
-          mentor_at_school_period:,
-          school_partnership:
-        )
-      end
+    trait :with_latest_mentor_training_period do
+      mentor_teacher_type { true }
     end
   end
 end
