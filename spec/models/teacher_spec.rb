3e189e223e9493dae07bb3cd4dfa387145739185
Leandro Alemão <leandro.costantini@gmail.com>
2025-10-06 21:49:34 +0100

    [2518] Add validations for teacher eligibility for funding

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -259,6 +259,27 @@ describe Teacher do
         end
       end
     end
+
+    describe ".first_became_eligible_for_ect_training_at, .first_became_eligible_for_mentor_training_at" do
+      context "when not yet set" do
+        subject { FactoryBot.create(:teacher, first_became_eligible_for_ect_training_at: nil, first_became_eligible_for_mentor_training_at: nil) }
+
+        it { is_expected.to allow_values("", " ", nil, "test", Date.new).for(:first_became_eligible_for_ect_training_at) }
+        it { is_expected.to allow_values("", " ", nil, "test", Date.new).for(:first_became_eligible_for_mentor_training_at) }
+      end
+
+      context "when already set" do
+        subject { FactoryBot.create(:teacher, first_became_eligible_for_ect_training_at: time, first_became_eligible_for_mentor_training_at: time) }
+
+        let(:time) { Time.zone.now }
+
+        it { is_expected.not_to allow_values("", " ", nil, "test", Date.new).for(:first_became_eligible_for_ect_training_at) }
+        it { is_expected.to allow_value(time).for(:first_became_eligible_for_ect_training_at) }
+
+        it { is_expected.not_to allow_values("", " ", nil, "test", Date.new).for(:first_became_eligible_for_mentor_training_at) }
+        it { is_expected.to allow_value(time).for(:first_became_eligible_for_mentor_training_at) }
+      end
+    end
   end
 
   describe 'scopes' do
