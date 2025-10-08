efe6462ab66c10ae7c5da62047e848a6b5488716
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-10-08 19:14:41 +0100

    Reorganise school routes

--- config/routes/school.rb
+++ config/routes/school.rb
@@ -1,7 +1,14 @@
 constraints -> { Rails.application.config.enable_schools_interface } do
   namespace :schools, path: :school do
     get "/home/ects", to: "ects#index", as: :ects_home
+    resources :ects, only: %i[index show] do
+      resource :mentorship, only: %i[new create] do
+        get :confirmation, on: :collection
+      end
+    end
+
     get "/home/mentors", to: "mentors#index", as: :mentors_home
+    resources :mentors, only: %i[index show]
 
     scope module: :ects, path: "/ects/:ect_id", as: :ects do
       namespace :change_name_wizard, path: "change-name" do
@@ -21,12 +28,6 @@ constraints -> { Rails.application.config.enable_schools_interface } do
       end
     end
 
-    resources :ects, only: %i[index show] do
-      resource :mentorship, only: %i[new create] do
-        get :confirmation, on: :collection
-      end
-    end
-
     scope module: :mentors, path: "/mentors/:mentor_id", as: :mentors do
       namespace :change_name_wizard, path: "change-name" do
         concerns :wizardable, wizard: Schools::Mentors::ChangeNameWizard
@@ -36,8 +37,6 @@ constraints -> { Rails.application.config.enable_schools_interface } do
         concerns :wizardable, wizard: Schools::Mentors::ChangeEmailAddressWizard
       end
     end
-
-    resources :mentors, only: %i[index show]
 
     namespace :register_ect_wizard, path: "register-ect" do
       get "what-you-will-need", as: :start, action: :start
@@ -51,7 +50,7 @@ constraints -> { Rails.application.config.enable_schools_interface } do
       concerns :wizardable, wizard: Schools::RegisterMentorWizard
     end
 
-    namespace :assign_existing_mentor_wizard, path: 'assign-existing-mentor' do
+    namespace :assign_existing_mentor_wizard, path: "assign-existing-mentor" do
       concerns :wizardable, wizard: Schools::AssignExistingMentorWizard
     end
   end
