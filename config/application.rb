8775629e1f5609d31fd7315a927cf02fc1fa11c0
Connor McQuillan <me@cpjmcquillan.com>
2025-10-09 10:46:11 +0100

    Incident!
    Put a banner up!

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,7 @@ module RegisterEarlyCareerTeachers
     config.enable_persona_avatars = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_PERSONA_AVATARS', true))
     config.enable_migration_testing = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_MIGRATION_TESTING', false))
     config.enable_schools_interface = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_SCHOOLS_INTERFACE', false))
+    config.enable_incident_banner = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_INCIDENT_BANNER', false))
     config.enable_sentry = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_SENTRY', false))
     config.enable_blazer = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_BLAZER', false))
     config.enable_api = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_API', false))
