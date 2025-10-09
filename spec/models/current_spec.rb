5de878a7038e91005758f35742752cc603321f11
Peter David Hamilton <pete@peterdavidhamilton.com>
2025-10-10 00:01:22 +0100

    Current request attributes
    - Adopt CurrentAttributes model
    - Move controller authentication to a concern
    - Render Administrator name and role when debugging

---
+++ spec/models/current_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe Current, type: :model do
+  let(:current_user) { FactoryBot.create(:dfe_user, name: 'Admin User', role: 'super_admin') }
+
+  it 'has a version number' do
+    expect(described_class).to respond_to(:user)
+    expect(Current.user).to be_nil
+    expect(described_class).to respond_to(:administrator)
+    expect(Current.administrator).to be_nil
+    expect(described_class).to respond_to(:role)
+    expect(Current.role).to be_nil
+    expect(described_class).to respond_to(:session)
+    expect(Current.session).to eq({})
+  end
+
+  describe 'user' do
+    it 'can be assigned and retrieved' do
+      Current.user = current_user
+      expect(Current.user).to eq(current_user)
+    end
+  end
+
+  describe 'administrator' do
+    it 'is derived from user and can be retrieved' do
+      Current.user = current_user
+      expect(Current.administrator).to eq(current_user.user)
+      expect(Current.administrator.name).to eq('Admin User')
+    end
+  end
+
+  describe 'role' do
+    it 'is derived from user and can be retrieved' do
+      Current.user = current_user
+      expect(Current.role).to eq('Super admin')
+    end
+  end
+
+  describe 'session' do
+    it 'can be assigned and retrieved' do
+      current_session = { token: 'abc123' }
+      Current.session = current_session
+      expect(Current.session).to eq(current_session)
+    end
+  end
+end
