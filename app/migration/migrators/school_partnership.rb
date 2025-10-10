9e8831f9b5c73c8387ffa873839e804246427ae7
Lorenzo Tello <ltello8a@gmail.com>
2025-10-10 10:15:48 +0100

    add school as a dependency before migrating school partnerships

--- app/migration/migrators/school_partnership.rb
+++ app/migration/migrators/school_partnership.rb
@@ -14,7 +14,7 @@ module Migrators
 
     def self.dependencies
       # FIXME: add school in here as it might be needed but not currently migrated?
-      %i[lead_provider_delivery_partnership]
+      %i[school lead_provider_delivery_partnership]
     end
 
     def self.reset!
