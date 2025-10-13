b4f6dc65b9ff412ca3e5308e200cc7987a21182d
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-10-13 17:33:16 +0100

    [2565] Update Teacher serializer to return eligible_for_funding

--- spec/serializers/api/teacher_serializer_spec.rb
+++ spec/serializers/api/teacher_serializer_spec.rb
@@ -194,8 +194,20 @@ describe API::TeacherSerializer, :with_metadata, type: :serializer do
             expect(ect_enrolment["participant_status"]).to eq("active")
           end
 
-          it "serializes `eligible_for_funding`" do
-            expect(ect_enrolment["eligible_for_funding"]).to be(true)
+          context "when `eligible_for_funding` is true" do
+            let(:teacher) { FactoryBot.create(:teacher, ect_first_became_eligible_for_training_at: Time.zone.now) }
+
+            it "serializes `eligible_for_funding`" do
+              expect(ect_enrolment["eligible_for_funding"]).to be(true)
+            end
+          end
+
+          context "when `eligible_for_funding` is false" do
+            let(:teacher) { FactoryBot.create(:teacher, ect_first_became_eligible_for_training_at: nil) }
+
+            it "serializes `eligible_for_funding`" do
+              expect(ect_enrolment["eligible_for_funding"]).to be(false)
+            end
           end
 
           it "serializes `pupil_premium_uplift`" do
@@ -327,8 +339,20 @@ describe API::TeacherSerializer, :with_metadata, type: :serializer do
             expect(mentor_enrolment["participant_status"]).to eq("active")
           end
 
-          it "serializes `eligible_for_funding`" do
-            expect(mentor_enrolment["eligible_for_funding"]).to be(true)
+          context "when `eligible_for_funding` is true" do
+            let(:teacher) { FactoryBot.create(:teacher, mentor_first_became_eligible_for_training_at: Time.zone.now) }
+
+            it "serializes `eligible_for_funding`" do
+              expect(mentor_enrolment["eligible_for_funding"]).to be(true)
+            end
+          end
+
+          context "when `eligible_for_funding` is false" do
+            let(:teacher) { FactoryBot.create(:teacher, mentor_first_became_eligible_for_training_at: nil) }
+
+            it "serializes `eligible_for_funding`" do
+              expect(mentor_enrolment["eligible_for_funding"]).to be(false)
+            end
           end
 
           it "serializes `pupil_premium_uplift`" do
