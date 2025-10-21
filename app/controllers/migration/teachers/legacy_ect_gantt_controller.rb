2c1e8398a028df71ece3d19674169f2fd224b0c0
Peter Yates <peter.yates@graphia.co.uk>
2025-10-21 12:16:33 +0100

    Add declarations to the Gantt chart

--- app/controllers/migration/teachers/legacy_ect_gantt_controller.rb
+++ app/controllers/migration/teachers/legacy_ect_gantt_controller.rb
@@ -3,8 +3,9 @@ module Migration::Teachers
     def show
       teacher = Teacher.find(params[:teacher_id])
       legacy_ect_induction_records = legacy_ect_induction_records(teacher.api_ect_training_record_id)
+      legacy_ect_declarations = legacy_ect_declarations(teacher.api_ect_training_record_id)
 
-      send_data(Migration::Gantt.new(legacy_ect_induction_records).to_png, type: "image/png")
+      send_data(Migration::Gantt.new(legacy_ect_induction_records, legacy_ect_declarations).to_png, type: "image/png")
     end
 
   private
@@ -13,6 +14,10 @@ module Migration::Teachers
       return unless id
 
       Migration::InductionRecordExporter.new.where_ect_participant_profile_id_is(id).rows
+    end
+
+    def legacy_ect_declarations(participant_profile_id)
+      Migration::ParticipantDeclaration.where(participant_profile_id:).order(created_at: :asc)
     end
   end
 end
