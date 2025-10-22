0bd506a56019ffd84793264ef3f77d25398ee775
Leandro Alemão <leandro.costantini@gmail.com>
2025-10-22 12:11:05 +0100

    [2525] Add shared context specs to withdraw service specs as well

--- spec/services/api/teachers/withdraw_spec.rb
+++ spec/services/api/teachers/withdraw_spec.rb
@@ -1,5 +1,5 @@
-RSpec.describe API::Teachers::Withdraw, :with_metadata, type: :model do
-  subject do
+RSpec.describe API::Teachers::Withdraw, type: :model do
+  subject(:instance) do
     described_class.new(
       lead_provider_id:,
       teacher_api_id:,
@@ -8,190 +8,153 @@ RSpec.describe API::Teachers::Withdraw, :with_metadata, type: :model do
     )
   end
 
-  let(:lead_provider_id) { training_period.lead_provider.id }
-  let(:teacher_api_id) { training_period.trainee.teacher.api_id }
-  let(:reason) { API::Teachers::Withdraw::WITHDRAWAL_REASONS.sample }
+  let(:reason) { described_class::WITHDRAWAL_REASONS.sample }
 
-  describe "validations" do
-    %i[ect mentor].each do |trainee_type|
-      context "for #{trainee_type}" do
-        let(:at_school_period) { FactoryBot.create(:"#{trainee_type}_at_school_period", started_on: 2.months.ago) }
-        let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
-        let(:course_identifier) { trainee_type == :ect ? "ecf-induction" : "ecf-mentor" }
+  it_behaves_like "an API teacher shared action" do
+    describe "validations" do
+      %i[ect mentor].each do |trainee_type|
+        context "for #{trainee_type}" do
+          let(:at_school_period) { FactoryBot.create(:"#{trainee_type}_at_school_period", started_on: 2.months.ago) }
+          let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
+          let(:course_identifier) { trainee_type == :ect ? "ecf-induction" : "ecf-mentor" }
 
-        it { is_expected.to be_valid }
+          context "when reason is invalid" do
+            let(:reason) { "does-not-exist" }
 
-        it { is_expected.to validate_presence_of(:lead_provider_id).with_message("Enter a '#/lead_provider_id'.") }
-        it { is_expected.to validate_presence_of(:teacher_api_id).with_message("Enter a '#/teacher_api_id'.") }
-        it { is_expected.to validate_presence_of(:reason).with_message("Enter a '#/reason'.") }
-        it { is_expected.to validate_presence_of(:course_identifier).with_message("Enter a '#/course_identifier'.") }
-
-        context "when the lead_provider does not exist" do
-          let(:lead_provider_id) { 9999 }
-
-          it "is invalid" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:lead_provider_id]).to eq(["The '#/lead_provider_id' you have entered is invalid."])
+            it { is_expected.to have_one_error_per_attribute }
+            it { is_expected.to have_error(:reason, "The entered '#/reason' is not recognised for the given participant. Check details and try again.") }
           end
-        end
 
-        context "when the teacher does not exist" do
-          let(:teacher_api_id) { SecureRandom.uuid }
+          context "when reason values are dashed" do
+            described_class::WITHDRAWAL_REASONS.each do |reason_val|
+              let(:reason) { reason_val }
 
-          it "is invalid" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:teacher_api_id]).to eq(["Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again."])
-          end
-        end
-
-        context "when teacher exists but training_period does not exist" do
-          let(:teacher_api_id) { FactoryBot.create(:teacher).api_id }
-
-          it "is invalid" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:teacher_api_id]).to eq(["Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again."])
-          end
-        end
-
-        context "when course_identifier is invalid" do
-          let(:course_identifier) { "does-not-exist" }
-
-          it "is invalid" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:course_identifier]).to eq(["The entered '#/course_identifier' is not recognised for the given participant. Check details and try again."])
-          end
-        end
-
-        context "when reason is invalid" do
-          let(:reason) { "does-not-exist" }
-
-          it "is invalid" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:reason]).to eq(["The entered '#/reason' is not recognised for the given participant. Check details and try again."])
-          end
-        end
-
-        context "when reason values are dashed" do
-          API::Teachers::Withdraw::WITHDRAWAL_REASONS.each do |reason_val|
-            let(:reason) { reason_val }
-
-            it "is valid when reason is '#{reason_val}'" do
-              expect(subject).to be_valid
+              it "is valid when reason is '#{reason_val}'" do
+                expect(instance).to be_valid
+              end
             end
           end
-        end
 
-        context "when reason is underscored" do
-          let(:reason) { "left_teaching_profession" }
+          context "when reason is underscored" do
+            let(:reason) { "long_term_sickness" }
 
-          it "is invalid when reason is 'left_teaching_profession'" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:reason]).to eq(["The entered '#/reason' is not recognised for the given participant. Check details and try again."])
+            it { is_expected.to have_one_error_per_attribute }
+            it { is_expected.to have_error(:reason, "The entered '#/reason' is not recognised for the given participant. Check details and try again.") }
           end
-        end
 
-        context "when teacher already withdrawn" do
-          let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :withdrawn, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
+          context "when teacher already withdrawn" do
+            let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :withdrawn, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
 
-          it "is invalid" do
-            expect(subject).to be_invalid
-            expect(subject.errors[:teacher_api_id]).to eq(["The '#/teacher_api_id' is already withdrawn."])
+            it { is_expected.to have_one_error_per_attribute }
+            it { is_expected.to have_error(:teacher_api_id, "The '#/teacher_api_id' is already withdrawn.") }
           end
-        end
 
-        context "guarded error messages" do
-          subject { described_class.new }
+          context "guarded error messages" do
+            subject(:instance) { described_class.new }
 
-          it { is_expected.to have_one_error_per_attribute }
+            it { is_expected.to have_one_error_per_attribute }
+          end
         end
       end
     end
-  end
 
-  describe "#withdraw" do
-    %i[ect mentor].each do |trainee_type|
-      context "for #{trainee_type}" do
-        let(:at_school_period) { FactoryBot.create(:"#{trainee_type}_at_school_period", started_on: 6.months.ago, finished_on: nil) }
-        let(:course_identifier) { trainee_type == :ect ? "ecf-induction" : "ecf-mentor" }
+    describe "#withdraw" do
+      %i[ect mentor].each do |trainee_type|
+        context "for #{trainee_type}" do
+          let(:at_school_period) { FactoryBot.create(:"#{trainee_type}_at_school_period", started_on: 6.months.ago, finished_on: nil) }
+          let(:course_identifier) { trainee_type == :ect ? "ecf-induction" : "ecf-mentor" }
 
-        before do
-          allow(Events::Record).to receive(:record_teacher_withdraws_training_period_event!).once
-        end
+          context "when invalid" do
+            let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing) }
+            let(:teacher_api_id) { SecureRandom.uuid }
 
-        context "when invalid" do
-          let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing) }
-          let(:teacher_api_id) { SecureRandom.uuid }
-
-          it { expect(subject.withdraw).to be(false) }
-          it { expect { subject.withdraw }.not_to(change { training_period.reload.attributes }) }
-        end
-
-        context "when training period ongoing" do
-          let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
-
-          it "withdraws training period" do
-            freeze_time
-
-            expect(subject.withdraw).not_to be(false)
-
-            training_period.reload
-            expect(training_period.withdrawn_at).to eq(Time.zone.now)
-            expect(training_period.withdrawal_reason.dasherize).to eq(reason)
-            expect(training_period.finished_on).to eq(training_period.withdrawn_at.to_date)
-
-            expect(Events::Record).to have_received(:record_teacher_withdraws_training_period_event!)
-          end
-        end
-
-        context "when training period already finished in the past" do
-          let!(:training_period) do
-            FactoryBot.create(
-              :training_period,
-              :"for_#{trainee_type}",
-              :ongoing,
-              "#{trainee_type}_at_school_period": at_school_period,
-              started_on: at_school_period.started_on,
-              finished_on: 1.month.ago
-            )
+            it { expect(instance.withdraw).to be(false) }
+            it { expect { instance.withdraw }.not_to(change { training_period.reload.attributes }) }
           end
 
-          it "sets withdrawn_at to the current date and doesn't change finished_on" do
-            freeze_time
+          context "when training period ongoing" do
+            let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
 
-            expect(subject.withdraw).not_to be(false)
+            it "withdraws training period" do
+              freeze_time
 
-            training_period.reload
-            expect(training_period.withdrawn_at).to eq(Time.zone.now)
-            expect(training_period.withdrawal_reason.dasherize).to eq(reason)
-            expect(training_period.finished_on).to eq(1.month.ago.to_date)
+              expect(instance.withdraw).not_to be(false)
 
-            expect(Events::Record).to have_received(:record_teacher_withdraws_training_period_event!)
-          end
-        end
-
-        context "when training period will finished in the future" do
-          let!(:training_period) do
-            FactoryBot.create(
-              :training_period,
-              :"for_#{trainee_type}",
-              :ongoing,
-              "#{trainee_type}_at_school_period": at_school_period,
-              started_on: at_school_period.started_on,
-              finished_on: 3.months.from_now
-            )
+              training_period.reload
+              expect(training_period.withdrawn_at).to eq(Time.zone.now)
+              expect(training_period.withdrawal_reason.dasherize).to eq(reason)
+              expect(training_period.finished_on).to eq(training_period.withdrawn_at.to_date)
+            end
           end
 
-          it "sets withdrawn_at and finished_on to the current date" do
-            freeze_time
+          context "when training period already finished in the past" do
+            let!(:training_period) do
+              FactoryBot.create(
+                :training_period,
+                :"for_#{trainee_type}",
+                :ongoing,
+                "#{trainee_type}_at_school_period": at_school_period,
+                started_on: at_school_period.started_on,
+                finished_on: 1.month.ago
+              )
+            end
 
-            expect(subject.withdraw).not_to be(false)
+            it "sets withdrawn_at to the current date and doesn't change finished_on" do
+              freeze_time
 
-            training_period.reload
-            expect(training_period.withdrawn_at).to eq(Time.zone.now)
-            expect(training_period.withdrawal_reason.dasherize).to eq(reason)
-            expect(training_period.finished_on).to eq(training_period.withdrawn_at.to_date)
+              expect(instance.withdraw).not_to be(false)
 
-            expect(Events::Record).to have_received(:record_teacher_withdraws_training_period_event!)
+              training_period.reload
+              expect(training_period.withdrawn_at).to eq(Time.zone.now)
+              expect(training_period.withdrawal_reason.dasherize).to eq(reason)
+              expect(training_period.finished_on).to eq(1.month.ago.to_date)
+            end
+          end
+
+          context "when training period will finished in the future" do
+            let!(:training_period) do
+              FactoryBot.create(
+                :training_period,
+                :"for_#{trainee_type}",
+                :ongoing,
+                "#{trainee_type}_at_school_period": at_school_period,
+                started_on: at_school_period.started_on,
+                finished_on: 3.months.from_now
+              )
+            end
+
+            it "sets withdrawn_at and finished_on to the current date" do
+              freeze_time
+
+              expect(instance.withdraw).not_to be(false)
+
+              training_period.reload
+              expect(training_period.withdrawn_at).to eq(Time.zone.now)
+              expect(training_period.withdrawal_reason.dasherize).to eq(reason)
+              expect(training_period.finished_on).to eq(training_period.withdrawn_at.to_date)
+            end
+          end
+
+          context "event recording" do
+            let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
+
+            it "records a teacher withdraws training period event" do
+              freeze_time do
+                expect(Events::Record).to receive(:record_teacher_withdraws_training_period_event!)
+                  .with(author: an_instance_of(Events::LeadProviderAPIAuthor),
+                        teacher:,
+                        lead_provider:,
+                        training_period:,
+                        modifications: {
+                          finished_on: [nil, Time.zone.today],
+                          updated_at: [training_period.updated_at, Time.zone.now],
+                          withdrawal_reason: [nil, reason.underscore],
+                          withdrawn_at: [nil, Time.zone.now]
+                        })
+
+                instance.withdraw
+              end
+            end
           end
         end
       end
--- spec/support/shared_examples/api_teacher_shared_action_support.rb
+++ spec/support/shared_examples/api_teacher_shared_action_support.rb
@@ -1,86 +1,70 @@
 RSpec.shared_examples "an API teacher shared action", :with_metadata do
-  let(:active_lead_provider) { FactoryBot.create(:active_lead_provider) }
-  let(:lead_provider) { active_lead_provider.lead_provider }
-  let(:lead_provider_delivery_partnership) do
-    FactoryBot.create(
-      :lead_provider_delivery_partnership,
-      active_lead_provider:
-    )
-  end
-  let(:school_partnership) do
-    FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:)
-  end
-  let(:ect_at_school_period) do
-    FactoryBot.create(
-      :ect_at_school_period,
-      school: school_partnership.school,
-      started_on: 1.year.ago,
-      finished_on: nil
-    )
-  end
-  let!(:training_period) do
-    FactoryBot.create(
-      :training_period,
-      :for_ect,
-      :with_school_partnership,
-      school_partnership:,
-      ect_at_school_period:,
-      started_on: ect_at_school_period.started_on + 1.week,
-      finished_on: nil
-    )
-  end
-  let(:teacher) { ect_at_school_period.teacher }
+  let(:lead_provider) { training_period.lead_provider }
   let(:lead_provider_id) { lead_provider.id }
+  let(:teacher) { training_period.trainee.teacher }
   let(:teacher_api_id) { teacher.api_id }
-  let(:course_identifier) { "ecf-induction" }
-
-  it { expect(instance).to be_valid }
 
   describe "validations" do
     subject { instance }
 
-    it { is_expected.to validate_presence_of(:lead_provider_id).with_message("Enter a '#/lead_provider_id'.") }
-    it { is_expected.to validate_presence_of(:teacher_api_id).with_message("Enter a '#/teacher_api_id'.") }
-    it { is_expected.to validate_inclusion_of(:course_identifier).in_array(%w[ecf-induction ecf-mentor]).with_message("The entered '#/course_identifier' is not recognised for the given participant. Check details and try again.") }
+    %i[ect mentor].each do |trainee_type|
+      context "for #{trainee_type}" do
+        let(:at_school_period) { FactoryBot.create(:"#{trainee_type}_at_school_period", started_on: 2.months.ago) }
+        let!(:training_period) { FactoryBot.create(:training_period, :"for_#{trainee_type}", :ongoing, "#{trainee_type}_at_school_period": at_school_period, started_on: at_school_period.started_on) }
+        let(:course_identifier) { trainee_type == :ect ? "ecf-induction" : "ecf-mentor" }
 
-    context "when a matching training period does not exist (different course identifier)" do
-      let(:course_identifier) { "ecf-mentor" }
+        it { is_expected.to be_valid }
+        it { is_expected.to validate_presence_of(:lead_provider_id).with_message("Enter a '#/lead_provider_id'.") }
+        it { is_expected.to validate_presence_of(:teacher_api_id).with_message("Enter a '#/teacher_api_id'.") }
+        it { is_expected.to validate_inclusion_of(:course_identifier).in_array(%w[ecf-induction ecf-mentor]).with_message("The entered '#/course_identifier' is not recognised for the given participant. Check details and try again.") }
 
-      it { is_expected.to have_one_error_per_attribute }
-      it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
-    end
+        context "when the `lead_provider` does not exist" do
+          let(:lead_provider_id) { 9999 }
 
-    context "when a matching training period does not exist (different lead provider)" do
-      let(:lead_provider_id) { FactoryBot.create(:lead_provider, name: "Different to #{lead_provider.name}").id }
+          it { is_expected.to have_one_error_per_attribute }
+          it { is_expected.to have_error(:lead_provider_id, "The '#/lead_provider_id' you have entered is invalid.") }
+        end
 
-      it { is_expected.to have_one_error_per_attribute }
-      it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
-    end
+        context "when a matching training period does not exist (different course identifier)" do
+          let(:course_identifier) { trainee_type == :ect ? "ecf-mentor" : "ecf-induction" }
 
-    context "when the teacher does not exist" do
-      let(:teacher_api_id) { "non-existent-participant-id" }
+          it { is_expected.to have_one_error_per_attribute }
+          it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
+        end
 
-      it { is_expected.to have_one_error_per_attribute }
-      it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
-    end
+        context "when a matching training period does not exist (different lead provider)" do
+          let(:lead_provider_id) { FactoryBot.create(:lead_provider, name: "Different to #{lead_provider.name}").id }
 
-    context "when a non-existent course identifier is provided" do
-      let(:course_identifier) { "non-existent-course-identifier" }
+          it { is_expected.to have_one_error_per_attribute }
+          it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
+        end
 
-      it { is_expected.to have_one_error_per_attribute }
-      it { is_expected.to have_error(:course_identifier, "The entered '#/course_identifier' is not recognised for the given participant. Check details and try again.") }
-    end
+        context "when the teacher does not exist" do
+          let(:teacher_api_id) { "non-existent-participant-id" }
 
-    context "when an empty course identifier is provided" do
-      let(:course_identifier) { "" }
+          it { is_expected.to have_one_error_per_attribute }
+          it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
+        end
 
-      it { is_expected.to have_error(:course_identifier, "Enter a '#/course_identifier'.") }
-    end
+        context "when a non-existent course identifier is provided" do
+          let(:course_identifier) { "non-existent-course-identifier" }
 
-    context "when a nil course identifier is provided" do
-      let(:course_identifier) { nil }
+          it { is_expected.to have_one_error_per_attribute }
+          it { is_expected.to have_error(:course_identifier, "The entered '#/course_identifier' is not recognised for the given participant. Check details and try again.") }
+        end
 
-      it { is_expected.to have_error(:course_identifier, "Enter a '#/course_identifier'.") }
+        context "when an empty course identifier is provided" do
+          let(:course_identifier) { "" }
+
+          it { is_expected.to have_error(:course_identifier, "Enter a '#/course_identifier'.") }
+        end
+
+        context "when a nil course identifier is provided" do
+          let(:course_identifier) { nil }
+
+          it { is_expected.to have_error(:course_identifier, "Enter a '#/course_identifier'.") }
+        end
+      end
     end
   end
 end
