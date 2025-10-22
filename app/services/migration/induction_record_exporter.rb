4b832b2aa8a7e1de024b23319efda460f58b7db9
Tony Headford <tony@binarycircus.com>
2025-10-22 13:50:17 +0100

    Add a spec

--- app/services/migration/induction_record_exporter.rb
+++ app/services/migration/induction_record_exporter.rb
@@ -2,10 +2,6 @@ module Migration
   class InductionRecordExporter
     CACHE_KEY = "induction-record-data"
 
-    def run
-      Migration::Base.connection.execute(query)
-    end
-
     def generate_and_cache_csv
       Rails.cache.fetch(CACHE_KEY, expires_in: 24.hours) do
         generate_csv
@@ -15,26 +11,15 @@ module Migration
     def generate_csv
       CSV.generate(headers: true) do |csv|
         csv << csv_headers
-        ar_query.find_each(batch_size: 2_000) do |induction_record|
+        query.find_each(batch_size: 2_000) do |induction_record|
           csv << csv_row(induction_record)
         end
-      end
-    end
-
-    def stream_csv_to(output_stream)
-      output_stream.write CSV.generate_line(csv_headers)
-      ar_query.find_each(batch_size: 2_000) do |induction_record|
-        output_stream.write CSV.generate_line(csv_row(induction_record))
       end
     end
 
   private
 
     def csv_headers
-      attributes
-    end
-
-    def attributes
       %w[induction_record_id start_date end_date right_way_round duration training_status school_transfer induction_record_created ect_participant_profile_id mentor_participant_profile_id urn challenged lead_provider_name].freeze
     end
 
@@ -82,70 +67,10 @@ module Migration
       (induction_record.end_date.to_date - induction_record.start_date.to_date).to_i
     end
 
-    def ar_query
-      InductionRecord.eager_load(:participant_profile, :preferred_identity, induction_programme: { school_cohort: :school, partnership: :lead_provider }).order(:participant_profile_id)
-    end
-
     def query
-      <<~SQL
-        with
-          ect_info as (
-            select u.id as user_id,
-                  pp.id as participant_profile_id,
-                  tp.trn
-            from users u
-            inner join participant_identities pi on u.id = pi.user_id
-            inner join participant_profiles pp on pi.id = pp.participant_identity_id
-            inner join teacher_profiles tp on u.id = tp.user_id
-            where pp.type = 'ParticipantProfile::ECT'
-          ),
-          mentor_info as (
-            select u.id as user_id,
-                  pp.id as participant_profile_id,
-                  tp.trn
-            from users u
-            inner join participant_identities pi on u.id = pi.user_id
-            inner join participant_profiles pp on pi.id = pp.participant_identity_id
-            inner join teacher_profiles tp on u.id = tp.user_id
-            where pp.type = 'ParticipantProfile::Mentor'
-          ),
-          school_and_induction_programme_info as (
-            select s.urn,
-                  ip.id as induction_programme_id,
-                  p.challenged_at,
-                  case when (p.challenged_at is null) then false
-                  else true
-                  end as challenged,
-                  lp.name as lead_provider_name
-            from schools s
-            inner join school_cohorts sc on s.id = sc.school_id
-            inner join induction_programmes ip on ip.school_cohort_id = sc.id
-            inner join partnerships p on ip.partnership_id = p.id
-            inner join lead_providers lp on p.lead_provider_id = lp.id
-          )
-
-        select ir.id as induction_record_id,
-              ir.start_date::date,
-              ir.end_date::date,
-              case
-              when ir.end_date::date is null then true
-              when ir.start_date::date < ir.end_date::date then true
-              else false end as right_way_round,
-              ir.end_date::date - ir.start_date::date as duration,
-              ir.training_status,
-              ir.school_transfer,
-              ir.created_at::date as induction_record_created,
-              ei.participant_profile_id as ect_particiapnt_profile_id,
-              mi.participant_profile_id as mentor_particiapnt_profile_id,
-              sipi.urn,
-              sipi.challenged,
-              sipi.lead_provider_name
-        from induction_records ir
-        left outer join ect_info ei on ir.participant_profile_id = ei.participant_profile_id
-        left outer join mentor_info mi on ir.mentor_profile_id = mi.participant_profile_id
-        left outer join school_and_induction_programme_info sipi on ir.induction_programme_id = sipi.induction_programme_id
-        ;
-      SQL
+      InductionRecord.eager_load(:participant_profile,
+                                 :preferred_identity,
+                                 induction_programme: { school_cohort: :school, partnership: :lead_provider })
     end
   end
 end
