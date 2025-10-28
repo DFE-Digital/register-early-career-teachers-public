569e156eead510f5f6fc023130a2fe833dc4b353
thenapking <tom.giles@gmail.com>
2025-10-28 14:41:43 +0000

    Confirmation Step

--- app/wizards/schools/mentors/change_lead_provider_wizard/confirmation_step.rb
+++ app/wizards/schools/mentors/change_lead_provider_wizard/confirmation_step.rb
@@ -5,13 +5,7 @@ module Schools
         def previous_step = :check_answers
 
         def new_lead_provider
-          latest_registration_choice.lead_provider
-        end
-
-      private
-
-        def latest_registration_choice
-          @latest_registration_choice ||= MentorAtSchoolPeriods::LatestRegistrationChoices.new(trn: mentor_trn)
+          wizard.latest_registration_choice.lead_provider
         end
       end
     end
--- app/wizards/schools/mentors/change_lead_provider_wizard/wizard.rb
+++ app/wizards/schools/mentors/change_lead_provider_wizard/wizard.rb
@@ -9,6 +9,16 @@ module Schools
             confirmation: ConfirmationStep,
           }]
         end
+
+        def latest_registration_choice
+          @latest_registration_choice ||= MentorAtSchoolPeriods::LatestRegistrationChoices.new(trn: mentor_trn)
+        end
+
+      private
+
+        def mentor_trn
+          mentor_at_school_period.teacher.trn
+        end
       end
     end
   end
