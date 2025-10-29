26c6cec4ac73dffa7234a59277ead392703b865d
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-10-29 20:09:06 +0000

    [2568] move full_name to presenter and delegate from Context

--- app/wizards/schools/register_ect_wizard/context/presenter.rb
+++ app/wizards/schools/register_ect_wizard/context/presenter.rb
@@ -5,6 +5,10 @@ module Schools
         def initialize(context:)
           @context = context
         end
+
+        def full_name
+          (@context.corrected_name.presence || @context.trs_full_name)&.strip
+        end
       end
     end
   end
