fd5dd3b99651e700b8c4da3fd460e646fabbcbf5
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-10-29 19:57:26 +0000

    [2568] add Context helpers (Presenter/Status/Queries)
    Wire them in and add fetch helper to context.

---
+++ app/wizards/schools/register_ect_wizard/context/presenter.rb
@@ -0,0 +1,11 @@
+module Schools
+  module RegisterECTWizard
+    class Context
+      class Presenter
+        def initialize(context:)
+          @context = context
+        end
+      end
+    end
+  end
+end
