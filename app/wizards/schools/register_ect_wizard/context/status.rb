18fa947f8916a63397cbfb73be912dc6225dcce5
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-10-30 23:28:13 +0000

    [2568] Extract status logic
    Into status.rb

--- app/wizards/schools/register_ect_wizard/context/status.rb
+++ app/wizards/schools/register_ect_wizard/context/status.rb
@@ -6,6 +6,60 @@ module Schools
           @context = context
           @queries = queries
         end
+
+        def cant_use_email?
+          Schools::TeacherEmail.new(email: context.email, trn: context.trn).is_currently_used?
+        end
+
+        def in_trs?
+          context.trs_first_name.present?
+        end
+
+        def induction_completed?
+          context.trs_induction_status == 'Passed'
+        end
+
+        def induction_exempt?
+          context.trs_induction_status == 'Exempt'
+        end
+
+        def induction_failed?
+          context.trs_induction_status == 'Failed'
+        end
+
+        def prohibited_from_teaching?
+          context.trs_prohibited_from_teaching == true
+        end
+
+        def registered?
+          context.ect_at_school_period_id.present?
+        end
+
+        def was_school_led?
+          context.previous_training_programme == 'school_led'
+        end
+
+        def matches_trs_dob?
+          return false if [context.date_of_birth, context.trs_date_of_birth].any?(&:blank?)
+
+          context.trs_date_of_birth.to_date == context.date_of_birth.to_date
+        end
+
+        def previous_provider_led?
+          queries.previous_training_period&.provider_led_training_programme?
+        end
+
+        def provider_led?
+          context.training_programme == 'provider_led'
+        end
+
+        def school_led?
+          context.training_programme == 'school_led'
+        end
+
+      private
+
+        attr_reader :context, :queries
       end
     end
   end
