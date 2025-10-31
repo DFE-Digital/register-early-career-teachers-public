1388c1212c4f65cd92a904e0945c4ba8317d24af
Leandro Alemão <leandro.costantini@gmail.com>
2025-10-31 11:32:46 +0000

    [2558] Rename some parity check path id methods

--- spec/migration/parity_check/dynamic_request_content_spec.rb
+++ spec/migration/parity_check/dynamic_request_content_spec.rb
@@ -170,8 +170,8 @@ RSpec.describe ParityCheck::DynamicRequestContent, :with_metadata do
       end
     end
 
-    context "when fetching `teacher_api_id_for_withdraw`" do
-      let(:identifier) { :teacher_api_id_for_withdraw }
+    context "when fetching `active_teacher_api_id_for_participant_action`" do
+      let(:identifier) { :active_teacher_api_id_for_participant_action }
       let(:lead_provider_delivery_partnership) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:) }
       let(:school_partnership) { FactoryBot.create(:school_partnership, active_lead_provider:) }
       let!(:training_period) { FactoryBot.create(:training_period, :for_ect, :ongoing, school_partnership:) }
@@ -189,8 +189,8 @@ RSpec.describe ParityCheck::DynamicRequestContent, :with_metadata do
       it { is_expected.to eq(teacher.api_id) }
     end
 
-    context "when fetching `withdrawn_teacher_api_id_for_withdraw`" do
-      let(:identifier) { :withdrawn_teacher_api_id_for_withdraw }
+    context "when fetching `withdrawn_teacher_api_id_for_participant_action`" do
+      let(:identifier) { :withdrawn_teacher_api_id_for_participant_action }
       let(:lead_provider_delivery_partnership) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:) }
       let(:school_partnership) { FactoryBot.create(:school_partnership, active_lead_provider:) }
       let!(:training_period) { FactoryBot.create(:training_period, :for_ect, :ongoing, :withdrawn, school_partnership:) }
@@ -208,8 +208,8 @@ RSpec.describe ParityCheck::DynamicRequestContent, :with_metadata do
       it { is_expected.to eq(teacher.api_id) }
     end
 
-    context "when fetching `participant_withdraw_body`" do
-      let(:identifier) { :participant_withdraw_body }
+    context "when fetching `active_participant_withdraw_body`" do
+      let(:identifier) { :active_participant_withdraw_body }
       let(:lead_provider_delivery_partnership) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:) }
       let!(:school_partnership) { FactoryBot.create(:school_partnership, lead_provider_delivery_partnership:) }
       let!(:training_period) { FactoryBot.create(:training_period, :for_ect, :ongoing, :withdrawn, school_partnership:) }
@@ -220,6 +220,8 @@ RSpec.describe ParityCheck::DynamicRequestContent, :with_metadata do
         FactoryBot.create(:training_period, :for_ect, :ongoing, school_partnership:)
         # Deferred participant for current lead provider
         FactoryBot.create(:training_period, :for_ect, :ongoing, :deferred, school_partnership:)
+        # Mentor participant for current lead provider
+        FactoryBot.create(:training_period, :for_mentor, :ongoing, :deferred, school_partnership:)
         # Participant for different lead providers should not be used.
         FactoryBot.create(:training_period, :for_ect, :ongoing, :withdrawn)
         # Stub withdrawal reasons so we have a predictable value
@@ -251,6 +253,8 @@ RSpec.describe ParityCheck::DynamicRequestContent, :with_metadata do
         FactoryBot.create(:training_period, :for_ect, :ongoing, school_partnership:)
         # Deferred participant for current lead provider
         FactoryBot.create(:training_period, :for_ect, :ongoing, :deferred, school_partnership:)
+        # Mentor participant for current lead provider
+        FactoryBot.create(:training_period, :for_mentor, :ongoing, :deferred, school_partnership:)
         # Participant for different lead providers should not be used.
         FactoryBot.create(:training_period, :for_ect, :ongoing, :withdrawn)
         # Stub withdrawal reasons so we have a predictable value
