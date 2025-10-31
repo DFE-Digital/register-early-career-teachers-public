b6257ac4424413b70c66a065548e156bd5e45cdb
Tony Headford <tony@binarycircus.com>
2025-10-31 11:01:16 +0000

    refactor to allow mentor inductions in gantt chart

---
+++ app/controllers/migration/legacy_profile_gantt_controller.rb
@@ -0,0 +1,23 @@
+module Migration
+  class LegacyProfileGanttController < AdminController
+    def show
+      profile_id = params[:profile_id]
+      legacy_induction_records = legacy_induction_records(profile_id)
+      legacy_declarations = legacy_declarations(profile_id)
+
+      send_data(Migration::Gantt.new(legacy_induction_records, legacy_declarations).to_png, type: "image/png")
+    end
+
+  private
+
+    def legacy_induction_records(participant_profile_id)
+      return unless participant_profile_id
+
+      Migration::InductionRecordExporter.new.where_participant_profile_id_is(participant_profile_id).rows
+    end
+
+    def legacy_declarations(participant_profile_id)
+      Migration::ParticipantDeclaration.where(participant_profile_id:).order(created_at: :asc)
+    end
+  end
+end
