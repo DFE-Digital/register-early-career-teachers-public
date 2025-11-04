9840229b3ad788de4d884ffb1b581b64c1036996
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-11-04 17:42:38 +0000

    [2568] Consolidate context delegations
    and utilize attr_reader

--- app/wizards/schools/register_ect_wizard/context.rb
+++ app/wizards/schools/register_ect_wizard/context.rb
@@ -6,25 +6,28 @@ module Schools
         super(store)
         @queries   = Context::Queries.new(context: self)
         @presenter = Context::Presenter.new(context: self)
-        @previous_registration = Context::PreviousRegistration.new(context: self, queries: @queries)
-        @status = Context::Status.new(context: self, queries: @queries)
+        @previous_registration = Context::PreviousRegistration.new(context: self, queries:)
+        @status = Context::Status.new(context: self, queries:)
       end
+
+      delegate :ect_at_school_period,
+               :active_record_at_school, :appropriate_body, :lead_providers_within_contract_period, :contract_start_date, :lead_provider, :previous_ect_at_school_period, to: :queries
 
       delegate :full_name,
                :formatted_working_pattern,
                :govuk_date_of_birth,
-               to: :@presenter
+               to: :presenter
 
-      delegate :ect_at_school_period, to: :queries
-
-      def active_at_school?(urn)
-        active_record_at_school(urn).present?
-      end
-
-      delegate :active_record_at_school, :appropriate_body, to: :queries
-
-      # appropriate_body_name
-      delegate :name, to: :appropriate_body, prefix: true, allow_nil: true
+      delegate :previous_school,
+               :previous_school_name,
+               :previous_lead_provider,
+               :previous_lead_provider_name,
+               :previous_delivery_partner_name,
+               :previous_appropriate_body_name,
+               :previous_training_programme,
+               :previous_provider_led?,
+               :previous_eoi_lead_provider_name,
+               to: :previous_registration
 
       delegate :cant_use_email?,
                :in_trs?,
@@ -37,33 +40,26 @@ module Schools
                :matches_trs_dob?,
                :provider_led?,
                :school_led?,
-               to: :@status
+               :lead_provider_has_confirmed_partnership_for_contract_period?,
+               to: :status
+
+      def active_at_school?(urn)
+        active_record_at_school(urn).present?
+      end
+
+      # appropriate_body_name
+      delegate :name, to: :appropriate_body, prefix: true, allow_nil: true
 
       def induction_start_date
         queries.first_induction_period&.started_on
       end
 
-      delegate :lead_provider, to: :queries
-
       # lead_provider_name
       delegate :name, to: :lead_provider, prefix: true, allow_nil: true
-
-      delegate :lead_providers_within_contract_period, :contract_start_date, to: :queries
 
       def previously_registered?
         previous_registration.present?
       end
-
-      delegate :previous_school,
-               :previous_school_name,
-               :previous_lead_provider,
-               :previous_lead_provider_name,
-               :previous_delivery_partner_name,
-               :previous_appropriate_body_name,
-               :previous_training_programme,
-               :previous_provider_led?,
-               :previous_eoi_lead_provider_name,
-               to: :previous_registration
 
       def register!(school, author:)
         Schools::RegisterECT.new(school_reported_appropriate_body: appropriate_body,
@@ -85,12 +81,9 @@ module Schools
         Teachers::Name.new(self).full_name_in_trs
       end
 
-      delegate :previous_ect_at_school_period, to: :queries
-      delegate :lead_provider_has_confirmed_partnership_for_contract_period?, to: :@status
-
     private
 
-      attr_reader :queries, :previous_registration
+      attr_reader :queries, :previous_registration, :presenter, :status
     end
   end
 end
