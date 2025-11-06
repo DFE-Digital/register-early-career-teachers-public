26dd9e9c832eeae596f4925a67c570a12284e044
edujackedu <jack.scotti@education.gov.uk>
2025-11-06 09:34:56 +0000

    Add a migrator for schedules
    new migrator that migrates Finance::Schedule::ECF records from ECF1 to Schedule records in RECT.
        These should be basically be a 1-to-1 copy and will need to map ECF Cohort to the RECT ContractPeriod.

--- spec/factories/migration/schedule_factory.rb
+++ spec/factories/migration/schedule_factory.rb
@@ -1,7 +1,22 @@
 FactoryBot.define do
   factory :migration_schedule, class: "Migration::Schedule" do
     cohort { FactoryBot.create(:migration_cohort) }
-    schedule_identifier { "#{Faker::Lorem.word} #{Faker::Alphanumeric.alpha(number: 5).upcase}" }
+    schedule_identifier do
+      %w[
+        ecf-standard-september
+        ecf-standard-january
+        ecf-standard-april
+        ecf-extended-september
+        ecf-extended-january
+        ecf-extended-april
+        ecf-reduced-september
+        ecf-reduced-january
+        ecf-reduced-april
+        ecf-replacement-september
+        ecf-replacement-january
+        ecf-replacement-april
+      ].sample
+    end
     name { Faker::Lorem.words(number: 2) }
   end
 end
