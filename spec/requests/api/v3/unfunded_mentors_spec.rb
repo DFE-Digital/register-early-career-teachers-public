09e6b38d1c7b482b8022c488e8875af9ba2dc8ac
Leandro Alemão <leandro.costantini@gmail.com>
2025-11-14 10:09:20 +0000

    [2736] Move unfunded serializer to teachers module

--- spec/requests/api/v3/unfunded_mentors_spec.rb
+++ spec/requests/api/v3/unfunded_mentors_spec.rb
@@ -1,26 +1,11 @@
 RSpec.describe "Unfunded mentors API", :with_metadata, type: :request do
-  let(:serializer) { API::UnfundedMentorSerializer }
+  include MentorshipPeriodHelpers
+
+  let(:serializer) { API::Teachers::UnfundedMentorSerializer }
   let(:serializer_options) { { lead_provider_id: lead_provider.id } }
   let(:query) { API::Teachers::UnfundedMentors::Query }
   let(:active_lead_provider) { FactoryBot.create(:active_lead_provider) }
   let(:lead_provider) { active_lead_provider.lead_provider }
-
-  def create_mentorship_period(mentor_school_partnership:, mentee_school_partnership:)
-    mentee = FactoryBot.create(:teacher)
-    mentee_school_period = FactoryBot.create(:ect_at_school_period, :ongoing, teacher: mentee, started_on: 2.months.ago)
-    FactoryBot.create(:training_period, :for_ect, started_on: 1.month.ago, ect_at_school_period: mentee_school_period, school_partnership: mentee_school_partnership)
-
-    unfunded_mentor = FactoryBot.create(:teacher)
-    unfunded_mentor_school_period = FactoryBot.create(:mentor_at_school_period, :ongoing, teacher: unfunded_mentor, started_on: 2.months.ago)
-    FactoryBot.create(:training_period, :for_mentor, started_on: 1.month.ago, mentor_at_school_period: unfunded_mentor_school_period, school_partnership: mentor_school_partnership)
-
-    FactoryBot.create(
-      :mentorship_period,
-      :ongoing,
-      mentee: mentee_school_period,
-      mentor: unfunded_mentor_school_period
-    )
-  end
 
   def create_resource(active_lead_provider:)
     lead_provider_delivery_partnership = FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:)
@@ -28,7 +13,7 @@ RSpec.describe "Unfunded mentors API", :with_metadata, type: :request do
     other_school_partnership = FactoryBot.create(:school_partnership)
 
     # Unfunded mentor associated with the lead provider (should be returned)
-    create_mentorship_period(mentor_school_partnership: other_school_partnership, mentee_school_partnership: school_partnership).mentor.teacher
+    create_mentorship_period_for(mentor_school_partnership: other_school_partnership, mentee_school_partnership: school_partnership).mentor.teacher
   end
 
   describe "#index" do
