ca6dcfd6514afffd8b5b61f1e712ec3c57be8c81
Leandro Alemão <leandro.costantini@gmail.com>
2025-11-14 15:52:56 +0000

    [2737] Add unfunded mentors endpoints to swagger docs

---
+++ spec/swagger_schemas/filters/unfunded_mentors.rb
@@ -0,0 +1,11 @@
+UNFUNDED_MENTORS_FILTER = {
+  description: "Filter unfunded mentors to return more specific results",
+  type: "object",
+  properties: {
+    updated_since: {
+      description: "Return only records that have been updated since this date and time (ISO 8601 date format)",
+      type: "string",
+      example: "2021-05-13T11:21:55Z",
+    },
+  },
+}.freeze
