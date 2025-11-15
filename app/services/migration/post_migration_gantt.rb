460be00ac1ce643ef94aad3b8f2e3a5e1e744260
Peter Yates <peter.yates@graphia.co.uk>
2025-11-15 19:54:51 +0000

    Add Gantt chart for migrated data
    This chart is built using ECF2 data only and will be useful for
    contrasting pre and post migration data

---
+++ app/services/migration/post_migration_gantt.rb
@@ -0,0 +1,97 @@
+module Migration
+  class PostMigrationGantt
+    include Gantt
+
+    TEMP_LP_NAME = "Ambition Institute"
+
+    attr_reader :ect_at_school_periods, :mentor_at_school_periods
+
+    def initialize(ect_at_school_periods:, mentor_at_school_periods: [])
+      @ect_at_school_periods = ect_at_school_periods
+      @mentor_at_school_periods = mentor_at_school_periods
+    end
+
+    def build
+      <<~PLANTUML
+        @startgantt
+
+        hide footbox
+        printscale monthly
+        project starts on #{earliest_start}
+
+        #{academic_year_boundaries.join("\n")}
+
+        #{ect_at_school_period_bars.join("\n")}
+
+        #{legend(present_lead_provider_names, extras: { 'School led' => 'yellow' })}
+
+        @endgantt
+      PLANTUML
+    end
+
+    def earliest_start
+      [ect_at_school_periods, mentor_at_school_periods].flatten.map(&:started_on).min
+    end
+
+    def present_lead_provider_names
+      # TODO: get a list of all the lead providers associated with this teacher, either
+      #       as an ECT or mentor
+      at_school_periods.flat_map(&:training_periods).compact.map { it.lead_provider&.name }
+    end
+
+    def at_school_periods
+      [*ect_at_school_periods, *mentor_at_school_periods]
+    end
+
+    def at_school_periods_grouped_by_school
+      at_school_periods.group_by(&:school)
+    end
+
+    def ect_at_school_period_bars
+      urn = nil
+
+      at_school_periods_grouped_by_school.map do |school, at_school_periods|
+        chunk = []
+
+        at_school_periods.each do |at_school_period|
+          chunk << %(-- #{school.urn} --) if school.urn != urn
+
+          identifier = %(ECT:#{at_school_period.id})
+
+          chunk << %([#{identifier}] starts on #{at_school_period.started_on} and ends on #{at_school_period.finished_on || Time.zone.today})
+
+          at_school_period.mentorship_periods.each do |mp|
+            mp_identifier = "Mentored by #{mp.mentor.teacher.trn}"
+
+            chunk << <<~BAR
+              [#{mp_identifier}] starts on #{mp.started_on} and ends on #{mp.finished_on || Time.zone.today}
+              [#{mp_identifier}] is colored in red
+            BAR
+          end
+
+          at_school_period.training_periods.each do |tp|
+            tp_identifier = %(Training period:#{tp.id})
+
+            chunk << %([#{tp_identifier}] starts on #{tp.started_on} and ends on #{tp.finished_on || Time.zone.today})
+
+            case tp.training_programme
+            when 'provider_led'
+              chunk << if (lead_provider = tp.lead_provider)
+                         %([#{tp_identifier}] is colored in #{colour(lead_provider.name)})
+                       else
+                         %([#{tp_identifier}] is colored in bisque)
+                       end
+            when 'school_led'
+              chunk << %([#{tp_identifier}] starts on #{tp.started_on} and ends on #{tp.finished_on || Time.zone.today})
+              chunk << %([#{tp_identifier}] is colored in yellow)
+            end
+          end
+
+          urn = school.urn
+        end
+
+        chunk.join("\n")
+      end
+    end
+  end
+end
