203ad7aa50bf6ffe142b5260b2ca115e5690b9f4
Lorenzo Tello <ltello8a@gmail.com>
2025-11-18 11:35:01 +0000

    correct end_dates of mentorship periods from induction records with flipped dates

--- app/migration/mentorship_period_extractor.rb
+++ app/migration/mentorship_period_extractor.rb
@@ -1,8 +1,10 @@
 class MentorshipPeriodExtractor
   include Enumerable
+  include DataFixes
 
   def initialize(induction_records:)
     @induction_records = induction_records
+    @last_created_induction_record = induction_records.max_by(&:created_at)
   end
 
   def each(&block)
@@ -11,11 +13,13 @@ class MentorshipPeriodExtractor
     mentorship_periods.each(&block)
   end
 
-private
-
   def mentorship_periods
     @mentorship_periods ||= build_mentorship_periods
   end
+
+private
+
+  attr_reader :last_created_induction_record
 
   def build_mentorship_periods
     current_period = nil
@@ -26,6 +30,8 @@ private
 
       next if current_mentor_id.nil? && mentor_id.nil?
 
+      end_date = corrected_end_date(induction_record:, last_created: last_created_induction_record?(induction_record))
+
       if current_mentor_id.present? && mentor_id.nil?
         current_mentor_id = nil
       elsif current_mentor_id != mentor_id
@@ -35,14 +41,16 @@ private
 
         current_period = Migration::MentorshipPeriodData.new(mentor_teacher:,
                                                              start_date: induction_record.start_date,
-                                                             end_date: induction_record.end_date,
+                                                             end_date:,
                                                              start_source_id: induction_record.id,
                                                              end_source_id: induction_record.id)
         periods << current_period
       else
-        current_period.end_date = induction_record.end_date
+        current_period.end_date = end_date
         current_period.end_source_id = induction_record.id
       end
     end
   end
+
+  def last_created_induction_record?(induction_record) = induction_record.id == last_created_induction_record.id
 end
