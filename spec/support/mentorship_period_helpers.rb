0e3b60294b368cbf62afe2cb22ebbebe4aa6fc52
Leandro Alemão <leandro.costantini@gmail.com>
2025-11-19 17:16:49 +0000

    [2738] Add unfunded mentors to seed data for sandbox

--- spec/support/mentorship_period_helpers.rb
+++ spec/support/mentorship_period_helpers.rb
@@ -1,18 +1,19 @@
 module MentorshipPeriodHelpers
-  def create_mentorship_period_for(mentee_school_partnership:, mentor_school_partnership: FactoryBot.create(:school_partnership))
-    mentee = FactoryBot.create(:teacher, :with_realistic_name)
-    mentee_school_period = FactoryBot.create(:ect_at_school_period, :ongoing, teacher: mentee, started_on: 2.months.ago, school: mentee_school_partnership.school)
+  def create_mentorship_period_for(mentee_school_partnership:,
+                                   mentee: FactoryBot.create(:teacher, :with_realistic_name),
+                                   mentor_school_partnership: FactoryBot.create(:school_partnership),
+                                   mentor: FactoryBot.create(:teacher, :with_realistic_name))
+    mentee_school_period = FactoryBot.create(:ect_at_school_period, :ongoing, teacher: mentee, school: mentee_school_partnership.school, started_on: 2.months.ago)
     FactoryBot.create(:training_period, :for_ect, started_on: 1.month.ago, ect_at_school_period: mentee_school_period, school_partnership: mentee_school_partnership)
 
-    unfunded_mentor = FactoryBot.create(:teacher, :with_realistic_name)
-    unfunded_mentor_school_period = FactoryBot.create(:mentor_at_school_period, :ongoing, teacher: unfunded_mentor, started_on: 2.months.ago, school: mentor_school_partnership.school)
-    FactoryBot.create(:training_period, :for_mentor, started_on: 1.month.ago, mentor_at_school_period: unfunded_mentor_school_period, school_partnership: mentor_school_partnership)
+    mentor_school_period = FactoryBot.create(:mentor_at_school_period, :ongoing, teacher: mentor, school: mentor_school_partnership.school, started_on: 2.months.ago)
+    FactoryBot.create(:training_period, :for_mentor, started_on: 1.month.ago, mentor_at_school_period: mentor_school_period, school_partnership: mentor_school_partnership)
 
     FactoryBot.create(
       :mentorship_period,
       :ongoing,
       mentee: mentee_school_period,
-      mentor: unfunded_mentor_school_period
+      mentor: mentor_school_period
     )
   end
 end
