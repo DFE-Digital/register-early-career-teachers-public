abc43df94e3177bcd515ab556b35cebafd5951b3
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-11-20 22:26:31 +0000

    [2376] Uncomment breadcrumb

--- app/controllers/admin/teachers/trainings_controller.rb
+++ app/controllers/admin/teachers/trainings_controller.rb
@@ -15,7 +15,7 @@ module Admin
       def teacher_breadcrumbs
         {
           "Teachers" => admin_teachers_path(page: params[:page], q: params[:q]),
-          # @teacher.full_name => admin_teacher_path(@teacher, page: params[:page], q: params[:q]), #todo: uncomment once https://github.com/DFE-Digital/register-early-career-teachers-public/pull/1741 is merged
+          @teacher.full_name => admin_teacher_path(@teacher, page: params[:page], q: params[:q]),
           "Training" => nil
         }
       end
