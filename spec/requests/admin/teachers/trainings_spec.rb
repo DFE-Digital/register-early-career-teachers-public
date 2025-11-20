74d5c2b2a6bc0ee6a96cc6f19a1b02af7b05a313
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-11-20 22:47:52 +0000

    [2376] Add request specs

---
+++ spec/requests/admin/teachers/trainings_spec.rb
@@ -0,0 +1,103 @@
+RSpec.describe "Admin::Teachers::Training", type: :request do
+  let(:teacher) { FactoryBot.create(:teacher) }
+
+  describe "GET /admin/teachers/:teacher_id/training" do
+    it "redirects to sign in path" do
+      get admin_teacher_training_path(teacher)
+      expect(response).to redirect_to(sign_in_path)
+    end
+
+    context "with an authenticated non-DfE user" do
+      include_context "sign in as non-DfE user"
+
+      it "requires authorisation" do
+        get admin_teacher_training_path(teacher)
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context "with an authenticated DfE user" do
+      include_context "sign in as DfE user"
+
+      it "returns http success" do
+        get admin_teacher_training_path(teacher)
+        expect(response).to have_http_status(:success)
+      end
+
+      context "when the schools interface flag is enabled" do
+        before do
+          allow(Rails.application.config).to receive(:enable_schools_interface).and_return(true)
+        end
+
+        it "renders the teacher navigation" do
+          get admin_teacher_training_path(teacher)
+          expect(response.body).to include("x-govuk-secondary-navigation")
+        end
+      end
+
+      context "when the schools interface flag is disabled" do
+        before do
+          allow(Rails.application.config).to receive(:enable_schools_interface).and_return(false)
+        end
+
+        it "does not render the teacher navigation" do
+          get admin_teacher_training_path(teacher)
+          expect(response.body).not_to include("x-govuk-secondary-navigation")
+        end
+      end
+
+      context "when the teacher has an ECT training period" do
+        let(:ect_training_period) { FactoryBot.create(:training_period) }
+        let(:teacher) { ect_training_period.ect_at_school_period.teacher }
+
+        it "shows the ECT history section" do
+          get admin_teacher_training_path(teacher)
+          expect(response.body).to include("ECT history")
+          expect(response.body).to include(ect_training_period.lead_provider_name)
+        end
+      end
+
+      context "when the teacher has a mentor training period" do
+        let(:mentor_training_period) { FactoryBot.create(:training_period, :for_mentor) }
+        let(:teacher) { mentor_training_period.mentor_at_school_period.teacher }
+
+        it "shows the mentor history section" do
+          get admin_teacher_training_path(teacher)
+          expect(response.body).to include("Mentor history")
+          expect(response.body).to include(mentor_training_period.school_partnership.school.name)
+        end
+      end
+
+      context "when the teacher has no training periods" do
+        let(:teacher) { FactoryBot.create(:teacher) }
+
+        it "shows the empty state message" do
+          get admin_teacher_training_path(teacher)
+          expect(response.body).to include("There is no training history recorded for this teacher")
+        end
+      end
+
+      context "with multiple ECT training periods" do
+        let(:teacher) { FactoryBot.create(:teacher) }
+        let(:older_started_on) { 2.years.ago.to_date }
+        let(:newer_started_on) { 1.year.ago.to_date }
+        let!(:older_training_period) do
+          ect_period = FactoryBot.create(:ect_at_school_period, teacher:, started_on: older_started_on, finished_on: older_started_on + 1.year)
+          FactoryBot.create(:training_period, ect_at_school_period: ect_period, started_on: older_started_on, finished_on: older_started_on + 1.year)
+        end
+        let!(:newer_training_period) do
+          ect_period = FactoryBot.create(:ect_at_school_period, teacher:, started_on: newer_started_on, finished_on: newer_started_on + 1.year)
+          FactoryBot.create(:training_period, ect_at_school_period: ect_period, started_on: newer_started_on, finished_on: newer_started_on + 1.year)
+        end
+
+        it "orders the periods with the most recent first" do
+          get admin_teacher_training_path(teacher)
+
+          expect(response.body).to include(newer_started_on.to_fs(:govuk))
+          expect(response.body).to include(older_started_on.to_fs(:govuk))
+          expect(response.body.index(newer_started_on.to_fs(:govuk))).to be < body.index(older_started_on.to_fs(:govuk))
+        end
+      end
+    end
+  end
+end
