d58ba828c41205d9f0197ed7b4d9d3ff1c5263c4
Lorenzo Tello <ltello8a@gmail.com>
2025-11-23 21:39:11 +0000

    refactor migration period extractors

--- app/migration/mentorship_period_extractor.rb
+++ app/migration/mentorship_period_extractor.rb
@@ -4,7 +4,6 @@ class MentorshipPeriodExtractor
 
   def initialize(induction_records:)
     @induction_records = induction_records
-    @last_created_induction_record = induction_records.max_by(&:created_at)
   end
 
   def each(&block)
@@ -19,18 +18,18 @@ class MentorshipPeriodExtractor
 
 private
 
-  attr_reader :last_created_induction_record
+  attr_reader :induction_records
 
   def build_mentorship_periods
     current_period = nil
     current_mentor_id = nil
 
-    @induction_records.each_with_object([]) do |induction_record, periods|
+    induction_records.each_with_object([]) do |induction_record, periods|
       mentor_id = induction_record.mentor_profile_id
 
       next if current_mentor_id.nil? && mentor_id.nil?
 
-      end_date = corrected_end_date(induction_record:, last_created: last_created_induction_record?(induction_record))
+      end_date = corrected_end_date(induction_record, induction_records)
 
       if current_mentor_id.present? && mentor_id.nil?
         current_mentor_id = nil
@@ -51,6 +50,4 @@ private
       end
     end
   end
-
-  def last_created_induction_record?(induction_record) = induction_record.id == last_created_induction_record.id
 end
