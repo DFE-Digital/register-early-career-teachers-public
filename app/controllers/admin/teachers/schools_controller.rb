6926833ea465fee50f99378928eac08d7595be78
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-11-24 17:12:56 +0000

    [2289] Flesh out controller action

--- app/controllers/admin/teachers/schools_controller.rb
+++ app/controllers/admin/teachers/schools_controller.rb
@@ -5,6 +5,14 @@ module Admin
 
       def show
         @teacher = TeacherPresenter.new(Teacher.find_by(id: params[:teacher_id]))
+        @ect_at_school_periods = @teacher.school_periods_as_an_ect.includes(
+          school: :gias_school,
+          mentorship_periods: { mentor: :teacher }
+        )
+        @mentor_at_school_periods = @teacher.school_periods_as_a_mentor.includes(
+          school: :gias_school,
+          mentorship_periods: { mentee: :teacher }
+        )
         @navigation_items = helpers.admin_teacher_navigation_items(@teacher, :school)
         @breadcrumbs = teacher_breadcrumbs
       end
