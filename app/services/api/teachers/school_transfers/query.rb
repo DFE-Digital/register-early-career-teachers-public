1af3a2d20167f5240f9902e2c5abc85a8cf648d7
Connor McQuillan <me@cpjmcquillan.com>
2025-11-24 18:02:45 +0000

    Introduce `API::Teachers::SchoolTransfers::Query`
    This query object returns all the participants with school transfers for a given
    lead provider.
    
    It leans on established patterns used in other query objects in the API.
    
    Since the query to retrieve relevant Teachers proved to be extremely complex
    (see #1760), we decided to use a flag on the `TeacherLeadProvider` metadata
    instead.
    
    This makes the query much more straightforward, and means all the logic around
    what is/isn't a "school transfer" is contained in the
    `Teachers::SchoolTransfers::History` class.

---
+++ app/services/api/teachers/school_transfers/query.rb
@@ -0,0 +1,87 @@
+module API::Teachers::SchoolTransfers
+  class Query
+    include Queries::FilterIgnorable
+
+    attr_reader :scope
+
+    def initialize(
+      lead_provider_id:,
+      updated_since: :ignore,
+      sort: { created_at: :asc }
+    )
+      @scope = Teacher.distinct
+
+      where_lead_provider_is(lead_provider_id)
+      where_updated_since(updated_since)
+      set_sort_by(sort)
+    end
+
+    def school_transfers
+      preload_associations(block_given? ? yield(scope) : scope)
+    end
+
+    def school_transfers_by_api_id(api_id)
+      return preload_associations(scope).find_by!(api_id:) if api_id.present?
+
+      fail(ArgumentError, "api_id needed")
+    end
+
+    def school_transfers_by_id(id)
+      return preload_associations(scope).find(id) if id.present?
+
+      fail(ArgumentError, "id needed")
+    end
+
+  private
+
+    def preload_associations(results)
+      results
+        .strict_loading
+        .includes(
+          lead_provider_metadata: [],
+          finished_induction_period: [],
+          ect_at_school_periods: {
+            earliest_training_period: {
+              school_partnership: :school,
+              active_lead_provider: :lead_provider
+            },
+            latest_training_period: {
+              school_partnership: :school,
+              active_lead_provider: :lead_provider
+            }
+          },
+          mentor_at_school_periods: {
+            earliest_training_period: {
+              school_partnership: :school,
+              active_lead_provider: :lead_provider
+            },
+            latest_training_period: {
+              school_partnership: :school,
+              active_lead_provider: :lead_provider
+            }
+          }
+        )
+    end
+
+    def where_lead_provider_is(lead_provider_id)
+      @scope = scope
+        .joins(:lead_provider_metadata)
+        .where(
+          lead_provider_metadata: {
+            lead_provider_id:,
+            involved_in_school_transfer: true
+          }
+        )
+    end
+
+    def where_updated_since(updated_since)
+      return if ignore?(filter: updated_since)
+
+      @scope = scope.where(api_updated_at: updated_since..)
+    end
+
+    def set_sort_by(sort)
+      @scope = scope.order(sort)
+    end
+  end
+end
