997fcee05d02296e295811f38ada4443d8adb2bb
Connor McQuillan <me@cpjmcquillan.com>
2025-11-27 13:27:45 +0000

    Add School Transfers endpoints to API
    Before, the API participant `/transfers` endpoints responded with
    `:method_not_allowed`.
    
    Now that the query and serializer for school transfers have been built, we can
    use them to build out these endpoints.
    
    This updates the GET show and index controller actions to return a specific
    participant's school transfers or all the school transfers for a given lead
    provider.

--- spec/requests/api/v3/participant_transfers_spec.rb
+++ spec/requests/api/v3/participant_transfers_spec.rb
@@ -1,23 +1,42 @@
-RSpec.describe "Participant transfers API", type: :request do
+RSpec.describe "Participant transfers API", :with_metadata, type: :request do
+  include SchoolTransferHelpers
+
+  let(:serializer) { API::Teachers::SchoolTransferSerializer }
+  let(:serializer_options) { { lead_provider_id: lead_provider.id } }
+  let(:query) { API::Teachers::SchoolTransfers::Query }
+  let(:active_lead_provider) { FactoryBot.create(:active_lead_provider) }
+  let(:lead_provider) { active_lead_provider.lead_provider }
+
+  def create_resource(active_lead_provider:)
+    teacher = FactoryBot.create(:teacher)
+    build_new_school_transfer(
+      teacher:,
+      lead_provider: active_lead_provider.lead_provider
+    )
+    teacher
+  end
+
   describe "#index" do
     let(:path) { api_v3_transfers_path }
 
-    it_behaves_like "a token authenticated endpoint", :get
-
-    it "returns method not allowed" do
-      authenticated_api_get api_v3_transfers_path
-      expect(response).to be_method_not_allowed
+    def apply_expected_order(resources)
+      resources.sort_by(&:created_at)
     end
+
+    it_behaves_like "a token authenticated endpoint", :get
+    it_behaves_like "an index endpoint"
+    it_behaves_like "a paginated endpoint"
+    it_behaves_like "a filter by updated_since endpoint", updated_at_column: :api_updated_at
+    it_behaves_like "a sortable endpoint", updated_at_column: :api_updated_at
   end
 
   describe "#show" do
-    let(:path) { transfers_api_v3_participant_path(123) }
+    let(:resource) { create_resource(active_lead_provider:) }
+    let(:path_id) { resource.api_id }
+    let(:path) { transfers_api_v3_participant_path(path_id) }
 
     it_behaves_like "a token authenticated endpoint", :get
-
-    it "returns method not allowed" do
-      authenticated_api_get path
-      expect(response).to be_method_not_allowed
-    end
+    it_behaves_like "a show endpoint"
+    it_behaves_like "a does not filter by updated_since endpoint"
   end
 end
