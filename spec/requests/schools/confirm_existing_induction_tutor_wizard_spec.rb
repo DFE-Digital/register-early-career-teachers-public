8e9e26cef385fe2500f5f2c22f699205c058154a
thenapking <tom.giles@gmail.com>
2025-11-28 13:36:51 +0000

    Request specs

--- spec/requests/schools/confirm_existing_induction_tutor_wizard_spec.rb
+++ spec/requests/schools/confirm_existing_induction_tutor_wizard_spec.rb
@@ -1,5 +1,8 @@
-describe "Schools::Induction::ConfirmExistingInductionTutorWizardController", :enable_schools_interface do
+describe "Schools::ConfirmExistingInductionTutorWizardController", :enable_schools_interface do
   let(:school) { FactoryBot.create(:school) }
+  let(:params) { {} }
+  let(:induction_tutor_name) { "New Induction Tutor Name" }
+  let(:induction_tutor_email) { "new.name@gmail.com"}
 
   describe "GET #new" do
     context "when not signed in" do
@@ -39,12 +42,16 @@ describe "Schools::Induction::ConfirmExistingInductionTutorWizardController", :e
         end
       end
     end
+
+    context "when visiting an invalid step" do
+      it "renders a 404 page" do
+        get path_for_step("fake-step")
+        expect(response).to have_http_status(:not_found)
+      end
+    end
   end
 
   describe "POST #create" do
-    let(:new_email) { "mentor@example.com" }
-    let(:params) { { edit: { email: new_email } } }
-
     context "when not signed in" do
       it "redirects to the root path" do
         post(path_for_step("edit"), params:)
@@ -75,6 +82,8 @@ describe "Schools::Induction::ConfirmExistingInductionTutorWizardController", :e
       end
 
       context "when the current details are confirmed" do
+        let(:params) { { edit: { are_these_details_correct: "true" } } }
+
         it "does not update anything" do
           expect { post(path_for_step("edit"), params:) }
             .not_to change(school, :induction_tutor_email)
@@ -82,12 +91,51 @@ describe "Schools::Induction::ConfirmExistingInductionTutorWizardController", :e
           expect(response).to redirect_to(path_for_step("confirmation"))
         end
       end
+
+      context "when the current details changed" do
+        let(:params) { { edit: { are_these_details_correct: "false", induction_tutor_name:, induction_tutor_email: } } }
+
+        it "does not update anything" do
+          expect { post(path_for_step("edit"), params:) }
+            .not_to change(school, :induction_tutor_email)
+
+          expect(response).to redirect_to(path_for_step("check-answers"))
+
+          follow_redirect!
+
+          expect { post path_for_step("check-answers") }
+            .to change { school.reload.induction_tutor_email }
+            .to(induction_tutor_email)
+            .and change { school.reload.induction_tutor_name }
+            .to(induction_tutor_name)
+
+          expect(response).to redirect_to(path_for_step("confirmation"))
+        end
+      end
+
+      context "when the current details are not changed" do
+        let(:params) { { edit: { are_these_details_correct: "false", induction_tutor_name: school.induction_tutor_name, induction_tutor_email: school.induction_tutor_email } } }
+
+        it "is invalid" do
+          post(path_for_step("edit"), params:)
+
+          expect(response).to have_http_status(:ok)
+          expect(response.body).to include("You must change the induction tutor details or confirm they are correct")
+        end
+      end
+    end
+
+    context "when visiting an invalid step" do
+      it "renders a 404 page" do
+        post path_for_step("fake-step")
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 
 private
 
   def path_for_step(step)
-    "/school/induction/confirm-existing-induction-tutor/#{step}"
+    "/school/confirm-existing-induction-tutor/#{step}"
   end
 end
