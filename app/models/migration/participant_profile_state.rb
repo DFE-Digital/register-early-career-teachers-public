b228c0030fde115355a12d35662eb05c173c5dc0
Tony Headford <tony@binarycircus.com>
2025-12-01 11:29:46 +0000

    pulling data from ECF and profile states model

--- app/models/migration/participant_profile_state.rb
+++ app/models/migration/participant_profile_state.rb
@@ -1,11 +1,17 @@
 module Migration
   class ParticipantProfileState < Migration::Base
-    belongs_to :cpd_lead_provider
+    belongs_to :cpd_lead_provider, optional: true
     belongs_to :participant_profile
     has_one :lead_provider, through: :cpd_lead_provider
 
     def deferred? = state == "deferred"
 
     def withdrawn? = state == "withdrawn"
+
+    enum state: {
+      active: "active",
+      deferred: "deferred",
+      withdrawn: "withdrawn",
+    }
   end
 end
