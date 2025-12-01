0ceb5f02cde8d6255c9944a099a6872f94f61cc3
Leandro Alemão <leandro.costantini@gmail.com>
2025-12-01 18:18:30 +0000

    [2857] Add `detailed_evidence_types_enabled` to `ContractPeriod`

--- app/services/sandbox_seed_data/contract_periods.rb
+++ app/services/sandbox_seed_data/contract_periods.rb
@@ -2,11 +2,28 @@ module SandboxSeedData
   class ContractPeriods < Base
     # Seed contract periods and attributes same as in production
     DATA = [
-      { year: 2021, enabled: false, payments_frozen_at: Time.zone.local(2024, 6, 18), mentor_funding_enabled: false },
-      { year: 2022, enabled: false, payments_frozen_at: Time.zone.local(2025, 6, 16), mentor_funding_enabled: false },
-      { year: 2023, enabled: true, mentor_funding_enabled: false },
-      { year: 2024, enabled: true, mentor_funding_enabled: false },
-      { year: 2025, enabled: true, mentor_funding_enabled: true },
+      { year: 2021,
+        enabled: false,
+        payments_frozen_at: Time.zone.local(2024, 6, 18),
+        mentor_funding_enabled: false,
+        detailed_evidence_types_enabled: false },
+      { year: 2022,
+        enabled: false,
+        payments_frozen_at: Time.zone.local(2025, 6, 16),
+        mentor_funding_enabled: false,
+        detailed_evidence_types_enabled: false },
+      { year: 2023,
+        enabled: true,
+        mentor_funding_enabled: false,
+        detailed_evidence_types_enabled: false },
+      { year: 2024,
+        enabled: true,
+        mentor_funding_enabled: false,
+        detailed_evidence_types_enabled: false },
+      { year: 2025,
+        enabled: true,
+        mentor_funding_enabled: true,
+        detailed_evidence_types_enabled: true },
     ].freeze
 
     def plant
@@ -19,7 +36,8 @@ module SandboxSeedData
                           year: data[:year],
                           enabled: data[:enabled],
                           payments_frozen_at: data[:payments_frozen_at],
-                          mentor_funding_enabled: data[:mentor_funding_enabled]).tap do |contract_period|
+                          mentor_funding_enabled: data[:mentor_funding_enabled],
+                          detailed_evidence_types_enabled: data[:detailed_evidence_types_enabled]).tap do |contract_period|
           log_seed_info("#{contract_period.year} (running from #{contract_period.started_on} until #{contract_period.finished_on})")
         end
       end
--- db/seeds/contract_periods.rb
+++ db/seeds/contract_periods.rb
@@ -4,16 +4,37 @@ end
 
 # Seed contract periods and attributes same as in production
 [
-  { year: 2021, enabled: false, payments_frozen_at: Time.zone.local(2024, 6, 18), mentor_funding_enabled: false },
-  { year: 2022, enabled: false, payments_frozen_at: Time.zone.local(2025, 6, 16), mentor_funding_enabled: false },
-  { year: 2023, enabled: true, mentor_funding_enabled: false },
-  { year: 2024, enabled: true, mentor_funding_enabled: false },
-  { year: 2025, enabled: true, mentor_funding_enabled: true },
-  { year: 2026, enabled: false, mentor_funding_enabled: true }
+  { year: 2021,
+    enabled: false,
+    payments_frozen_at: Time.zone.local(2024, 6, 18),
+    mentor_funding_enabled: false,
+    detailed_evidence_types_enabled: false },
+  { year: 2022,
+    enabled: false,
+    payments_frozen_at: Time.zone.local(2025, 6, 16),
+    mentor_funding_enabled: false,
+    detailed_evidence_types_enabled: false },
+  { year: 2023,
+    enabled: true,
+    mentor_funding_enabled: false,
+    detailed_evidence_types_enabled: false },
+  { year: 2024,
+    enabled: true,
+    mentor_funding_enabled: false,
+    detailed_evidence_types_enabled: false },
+  { year: 2025,
+    enabled: true,
+    mentor_funding_enabled: true,
+    detailed_evidence_types_enabled: true },
+  { year: 2026,
+    enabled: false,
+    mentor_funding_enabled: true,
+    detailed_evidence_types_enabled: true }
 ].each do |data|
   FactoryBot.create(:contract_period,
                     year: data[:year],
                     enabled: data[:enabled],
                     payments_frozen_at: data[:payments_frozen_at],
-                    mentor_funding_enabled: data[:mentor_funding_enabled]).tap { |cp| describe_contract_period(cp) }
+                    mentor_funding_enabled: data[:mentor_funding_enabled],
+                    detailed_evidence_types_enabled: data[:detailed_evidence_types_enabled]).tap { |cp| describe_contract_period(cp) }
 end
