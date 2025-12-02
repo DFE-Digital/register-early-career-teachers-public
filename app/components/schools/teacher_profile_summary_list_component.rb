9a083bb23228db8bd508c1fabe362a8e7026a8da
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2025-12-02 21:19:27 +0000

    [2690] Add leaving status

--- app/components/schools/teacher_profile_summary_list_component.rb
+++ app/components/schools/teacher_profile_summary_list_component.rb
@@ -9,27 +9,7 @@ module Schools
 
   private
 
-    def ect_status_tag
-      induction_status = @ect.teacher.trs_induction_status
-
-      case induction_status
-      when "Passed"
-        govuk_tag(text: "Completed induction", colour: "blue")
-      when "Failed"
-        govuk_tag(text: "Failed induction", colour: "pink")
-      when "Exempt"
-        govuk_tag(text: "Exempt", colour: "grey")
-      else
-        if current_mentor_name
-          govuk_tag(text: "Registered", colour: "green")
-        else
-          govuk_tag(text: "Mentor required", colour: "red")
-        end
-      end
-    end
-
     def current_mentor = mentorship.current_mentor
-    def current_mentor_name = mentorship.current_mentor_name
 
     def mentorship = @mentorship ||= ECTAtSchoolPeriods::Mentorship.new(@ect)
   end
