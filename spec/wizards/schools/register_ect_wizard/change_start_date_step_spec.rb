4da35a3f23a4cb272cf6d8281caa549fbede5b2d
Erica Porter <ericaporter@users.noreply.github.com>
2025-12-02 12:47:55 +0000

    Ensure reuse behaviour is consistent whenm editing answers

--- spec/wizards/schools/register_ect_wizard/change_start_date_step_spec.rb
+++ spec/wizards/schools/register_ect_wizard/change_start_date_step_spec.rb
@@ -1,21 +1,47 @@
 RSpec.describe Schools::RegisterECTWizard::ChangeStartDateStep, type: :model do
-  subject { described_class.new(start_date: { 1 => "2025", 2 => "01" }) }
+  subject(:step) { described_class.new(wizard:, start_date: new_start_date) }
 
-  describe "inheritance" do
-    it "inherits from StartDateStep" do
-      expect(subject).to be_a(Schools::RegisterECTWizard::StartDateStep)
-    end
+  let(:new_start_date) { "1 July 2024" }
+
+  let(:school) { FactoryBot.create(:school, :state_funded) }
+
+  let(:store) do
+    FactoryBot.build(
+      :session_repository,
+      start_date: "3 September 2024",
+      use_previous_ect_choices: true
+    )
   end
 
-  describe "#next_step" do
-    it "returns :check_answers" do
-      expect(subject.next_step).to eq(:check_answers)
-    end
+  let(:wizard) do
+    FactoryBot.build(
+      :register_ect_wizard,
+      current_step: :change_start_date,
+      store:,
+      school:
+    )
   end
 
-  describe "#previous_step" do
-    it "returns :check_answers" do
-      expect(subject.next_step).to eq(:check_answers)
+  before do
+    store[:school_partnership_to_reuse_id] = 123
+  end
+
+  describe "#save!" do
+    it "updates the wizard ect start date" do
+      expect { step.save! }
+        .to change(step.ect, :start_date)
+        .to("1 July 2024")
+    end
+
+    it "resets use_previous_ect_choices on the ect" do
+      expect { step.save! }
+        .to change(step.ect, :use_previous_ect_choices)
+        .to(nil)
+    end
+
+    it "clears stored school_partnership_to_reuse_id" do
+      step.save!
+      expect(store[:school_partnership_to_reuse_id]).to be_nil
     end
   end
 end
