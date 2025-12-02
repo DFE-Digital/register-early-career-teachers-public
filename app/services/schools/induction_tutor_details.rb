877635398be09214e4622fb7f2e9bdf60245acde
thenapking <tom.giles@gmail.com>
2025-12-02 14:41:53 +0000

    Service and Primary Navigation

---
+++ app/services/schools/induction_tutor_details.rb
@@ -0,0 +1,39 @@
+module Schools
+  class InductionTutorDetails
+    include Rails.application.routes.url_helpers
+    attr_reader :school, :user
+
+    def initialize(user)
+      @user = user
+    end
+
+    def needs_update_by_user?
+      return unless user.school_user?
+      return if user.dfe_user_impersonating_school_user?
+      return if user.has_multiple_roles?
+      return unless school
+
+      last_updated_year.blank? || last_updated_year < current_contract_year
+    end
+
+    # TODO: A subsequent PR will check whether the induction tutor details are present
+    # If they are not we'll probably redirect to a different wizard
+    def wizard_path
+      schools_confirm_existing_induction_tutor_wizard_edit_path
+    end
+
+  private
+
+    def current_contract_year
+      ContractPeriod.containing_date(Time.zone.today).year
+    end
+
+    def school
+      @school ||= @user&.school
+    end
+
+    def last_updated_year
+      @user.school.induction_tutor_last_nominated_in_year&.year
+    end
+  end
+end
---
+++ spec/services/schools/induction_tutor_details_spec.rb
@@ -0,0 +1,80 @@
+RSpec.describe Schools::InductionTutorDetails do
+  subject(:service) do
+    described_class.new(user)
+  end
+
+  let(:school) { FactoryBot.create(:school) }
+  let(:user) { FactoryBot.create(:school_user, school_urn: school.urn) }
+
+  describe "#needs_update_by_user?" do
+    context "when the user is not a school user" do
+      let(:user) { FactoryBot.create(:dfe_user) }
+
+      it "returns false" do
+        expect(service.needs_update_by_user?).to be_falsey
+      end
+    end
+
+    context "when the user is a school user impersonated by a DfE user" do
+      let(:school_user) { FactoryBot.create(:user) }
+
+      let(:user) do
+        FactoryBot.build(:dfe_user_impersonating_school_user, email: school_user.email, school_urn: school.urn)
+      end
+
+      it "returns false" do
+        expect(service.needs_update_by_user?).to be_falsey
+      end
+    end
+
+    xcontext "when the user has multiple roles" do
+      let(:user) { FactoryBot.create(:school_user, :with_multiple_roles) }
+
+      it "returns false" do
+        expect(service.needs_update_by_user?).to be_falsey
+      end
+    end
+
+    xcontext "when the user is not linked to a school" do
+      let(:user) { FactoryBot.create(:school_user) }
+
+      it "returns false" do
+        expect(service.needs_update_by_user?).to be_falsey
+      end
+    end
+
+    context "when the induction tutor details have never been confirmed" do
+      it "returns true" do
+        expect(service.needs_update_by_user?).to be_truthy
+      end
+    end
+
+    context "when the induction tutor details were last confirmed before the current contract year" do
+      before do
+        FactoryBot.create(:contract_period, :current)
+        previous_contract_period = FactoryBot.create(:contract_period, :previous)
+
+        school.update(induction_tutor_last_nominated_in_year: previous_contract_period,
+                      induction_tutor_name: "Alastair Sim",
+                      induction_tutor_email: "alastair.sim@st-trinians.org.uk")
+      end
+
+      it "returns true" do
+        expect(service.needs_update_by_user?).to be_truthy
+      end
+    end
+
+    context "when the induction tutor details were last confirmed in the current contract year" do
+      before do
+        current_contract_period = FactoryBot.create(:contract_period, :current)
+        school.update(induction_tutor_last_nominated_in_year: current_contract_period,
+                      induction_tutor_name: "Alastair Sim",
+                      induction_tutor_email: "alastair.sim@st-trinians.org.uk")
+      end
+
+      it "returns false" do
+        expect(service.needs_update_by_user?).to be_falsey
+      end
+    end
+  end
+end
