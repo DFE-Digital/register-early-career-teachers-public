1568b100b6d62b3f43d93dd9e6e344a82075c9e9
Peter Yates <peter.yates@graphia.co.uk>
2025-12-03 12:14:08 +0000

    Add very basic induction record migrator
    This is a first attempt at building the new style of induction record
    migrator
    
    Co-authored-by: tonyheadford <tony@binarycircus.com>
    Co-authored-by: Lorenzo Tello <ltello8a@gmail.com>
    Co-authored-by: Jack Scotti <156658023+edujackedu@users.noreply.github.com>

--- app/migration/teacher_history_converter.rb
+++ app/migration/teacher_history_converter.rb
@@ -4,16 +4,37 @@ class TeacherHistoryConverter
   end
 
   def convert_to_ecf2!
-    ECF2TeacherHistory.new.tap do |th|
-      # set the name, TRN, etc
-      #
-      # ecf1_events.each do |event|
-      #   add the periods
-      # end
-    end
+    ECF2TeacherHistory.new(teacher_row:, ect_at_school_period_rows:)
   end
 
 private
+
+  def teacher_row
+    ECF2TeacherHistory::TeacherRow.new(
+      trn: @ecf1_teacher_history.user.trn,
+      trs_first_name: parsed_name.first_name,
+      trs_last_name: parsed_name.last_name,
+      api_id: @ecf1_teacher_history.user.user_id
+    )
+  end
+
+  def ect_at_school_period_rows
+    @ecf1_teacher_history.ect.induction_records.map do |ir|
+      ECF2TeacherHistory::ECTAtSchoolPeriodRow.new(
+        started_on: ir.start_date,
+        finished_on: ir.end_date,
+        school: ECF2TeacherHistory::SchoolData.new(urn: ir.school_urn, name: "Thing"),
+        email: ir.preferred_identity_email,
+        mentorship_period_rows: [],
+        training_period_rows: [],
+        appropriate_body: nil
+      )
+    end
+  end
+
+  def parsed_name
+    @parsed_name ||= Teachers::FullNameParser.new(full_name: @ecf1_teacher_history.user.full_name)
+  end
 
   def ecf1_events
     @ecf1_teacher_history.induction_records.map do |ir|
