b41db06751201bb6511d67beaef33e4568786fd9
Peter Yates <peter.yates@graphia.co.uk>
2025-12-03 15:39:44 +0000

    Add some useful traits to the ECF1 factories

--- spec/migration/teacher_history_converter/one_induction_record_spec.rb
+++ spec/migration/teacher_history_converter/one_induction_record_spec.rb
@@ -1,14 +1,6 @@
 describe "One induction record" do
   subject { TeacherHistoryConverter.new(ecf1_teacher_history:).convert_to_ecf2! }
 
-  let(:ecf1_user) do
-    FactoryBot.build(
-      :ecf1_teacher_history_user,
-      trn: "1234567",
-      full_name: "Bob Carolgees"
-    )
-  end
-
   let(:cohort_year) { 2024 }
   let(:schedule) { FactoryBot.build(:ecf1_teacher_history_schedule_info, cohort_year:) }
 
@@ -17,59 +9,42 @@ describe "One induction record" do
   let(:delivery_partner_id) { SecureRandom.uuid }
   let(:delivery_partner_name) { "Delivery Partner A" }
 
-  let(:ecf1_ect) do
-    FactoryBot.build(
-      :ecf1_teacher_history_ect,
-      induction_records: [
-        FactoryBot.build(
-          :ecf1_teacher_history_induction_record_row,
-          start_date: Date.new(2024, 1, 1),
-          end_date: nil,
-          induction_status: "active",
-          training_status: "active",
-          mentor_profile_id: nil,
-          cohort_year:,
-          schedule:,
-          school_urn: 123_456,
-          training_provider_info: FactoryBot.build(
-            :ecf1_teacher_history_training_provider_info,
-            cohort_year:,
-            lead_provider_id:,
-            lead_provider_name:,
-            delivery_partner_id:,
-            delivery_partner_name:
-          )
-        )
-      ]
-    )
-  end
+  let(:ecf1_ect) { FactoryBot.build(:ecf1_teacher_history_ect) }
 
   let(:ecf1_teacher_history) do
-    ECF1TeacherHistory.new(user: ecf1_user, ect: ecf1_ect)
+    FactoryBot.build(:ecf1_teacher_history, :ect_with_one_induction_record)
   end
 
   describe "teacher attributes" do
     it "sets the right TRN" do
-      expect(subject.teacher_row.trn).to eql(ecf1_user.trn)
+      expect(subject.teacher_row.trn).to eql(ecf1_teacher_history.user.trn)
     end
 
-    it "sets the right first name" do
-      expect(subject.teacher_row.trs_first_name).to eql("Bob")
-    end
+    it "sets the right first and last name" do
+      first_name, last_name = *ecf1_teacher_history.user.full_name.split
 
-    it "sets the right last name" do
-      expect(subject.teacher_row.trs_last_name).to eql("Carolgees")
+      aggregate_failures do
+        expect(subject.teacher_row.trs_first_name).to eql(first_name)
+        expect(subject.teacher_row.trs_last_name).to eql(last_name)
+      end
     end
 
     it "set the right api_id" do
-      expect(subject.teacher_row.api_id).to eql(ecf1_user.user_id)
+      expect(subject.teacher_row.api_id).to eql(ecf1_teacher_history.user.user_id)
     end
 
-    pending "set the right timestamps" # will be teacher.updated_at/created_at
+    it "set the right timestamps" do
+      skip "not implemented yet"
+
+      aggregate_failures do
+        expect(subject.teacher_row.created_at).to eql(ecf1_teacher_history.user.created_at)
+        expect(subject.teacher_row.updated_at).to eql(ecf1_teacher_history.user.updated_at)
+      end
+    end
   end
 
   describe "ECT at school period attributes" do
-    let(:ecf1_induction_record_row) { ecf1_ect.induction_records.first }
+    let(:ecf1_induction_record_row) { ecf1_teacher_history.ect.induction_records.first }
     let(:ecf2_ect_at_school_period_row) { subject.ect_at_school_period_rows.first }
 
     it "sets the right URN" do
