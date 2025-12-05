3c6175cc02f74090ee994a04d990a2fcc9436cf1
Peter Yates <peter.yates@graphia.co.uk>
2025-12-05 11:26:38 +0000

    Add the start date vs created date context
    Co-authored-by: tonyheadford <tony@binarycircus.com>

--- spec/migration/teacher_history_converter/one_induction_record_spec.rb
+++ spec/migration/teacher_history_converter/one_induction_record_spec.rb
@@ -2,44 +2,27 @@ describe "One induction record" do
   subject { TeacherHistoryConverter.new(ecf1_teacher_history:).convert_to_ecf2! }
 
   let(:cohort_year) { 2024 }
-  let(:schedule) { FactoryBot.build(:ecf1_teacher_history_schedule_info, cohort_year:) }
+  # let(:lead_provider) { Types::LeadProvider.new(id: SecureRandom.uuid, name: "Lead Provider A") }
+  # let(:delivery_partner) { Types::DeliveryPartner.new(id: SecureRandom.uuid, name: "Delivery Partner A") }
 
-  let(:lead_provider) do
-    Types::LeadProvider.new(id: SecureRandom.uuid,
-                            name: "Lead Provider A")
+  let(:induction_record) do
+    FactoryBot.build(:ecf1_teacher_history_induction_record_row, cohort_year:)
   end
-
-  let(:delivery_partner) do
-    Types::DeliveryPartner.new(id: SecureRandom.uuid,
-                               name: "Delivery Partner A")
-  end
-
-  let(:ecf1_ect) { FactoryBot.build(:ecf1_teacher_history_ect) }
 
   let(:ecf1_teacher_history) do
     FactoryBot.build(:ecf1_teacher_history) do |history|
       history.ect = FactoryBot.build(:ecf1_teacher_history_ect) do |ect|
-        ect.induction_records = [
-          FactoryBot.build(
-            :ecf1_teacher_history_induction_record_row,
-            cohort_year:,
-            training_provider_info: FactoryBot.build(
-              :ecf1_teacher_history_training_provider_info,
-              lead_provider:,
-              delivery_partner:
-            )
-          )
-        ]
+        ect.induction_records = [induction_record]
       end
     end
   end
 
   describe "teacher attributes" do
-    it "sets the right TRN" do
+    it "sets the TRN from the teacher profile" do
       expect(subject.teacher_row.trn).to eql(ecf1_teacher_history.user.trn)
     end
 
-    it "sets the right first and last name" do
+    it "sets the first and last name from the user" do
       first_name, last_name = *ecf1_teacher_history.user.full_name.split
 
       aggregate_failures do
@@ -48,11 +31,11 @@ describe "One induction record" do
       end
     end
 
-    it "set the right api_id" do
+    it "sets the api_id to the user_id" do
       expect(subject.teacher_row.api_id).to eql(ecf1_teacher_history.user.user_id)
     end
 
-    it "set the right timestamps" do
+    it "set the created and updated timestamps from the user" do
       skip "not implemented yet"
 
       aggregate_failures do
@@ -63,27 +46,64 @@ describe "One induction record" do
   end
 
   describe "ECT at school period attributes" do
-    let(:ecf1_induction_record_row) { ecf1_teacher_history.ect.induction_records.first }
+    let(:ecf1_induction_record_row) {ecf1_teacher_history.ect.induction_records.first}
     let(:ecf2_ect_at_school_period_row) { subject.ect_at_school_period_rows.first }
 
-    it "sets the right URN" do
+    it "sets the URN from the induction record's induction programme" do
       expect(ecf2_ect_at_school_period_row.school.urn).to eql(ecf1_induction_record_row.school_urn)
     end
 
-    it "sets the right email address" do
+    it "sets the email address to the email from the induction record's preferred identity" do
       expect(ecf2_ect_at_school_period_row.email).to eql(ecf1_induction_record_row.preferred_identity_email)
     end
 
-    it "leaves appropriate body as nil" do
-      expect(ecf2_ect_at_school_period_row.appropriate_body).to be_nil
+    describe "appropriate body" do
+      context "when there is no appropriate body" do
+        it "leaves appropriate body as nil" do
+          expect(ecf2_ect_at_school_period_row.appropriate_body).to be_nil
+        end
+      end
+
+      context "when there is an appropriate body" do
+        it "sets the appropriate body to the one on the induction record" do
+          skip "Not implemented"
+        end
+      end
     end
 
-    it "sets the right start date" do
+    it "sets the start date to the induction record start date" do
       expect(ecf2_ect_at_school_period_row.started_on).to eql(ecf1_induction_record_row.start_date)
     end
 
-    it "sets the right end date" do
+    it "sets the end date to the induction record end date" do
       expect(ecf2_ect_at_school_period_row.finished_on).to eql(ecf1_induction_record_row.end_date)
     end
+
+    describe "choosing the right started_on date" do
+      context "when the induction record created_at is later than the start_date" do
+        let(:induction_record) do
+          FactoryBot.build(:ecf1_teacher_history_induction_record_row, :created_at_later_than_start_date)
+        end
+
+        it "sets the start date to the induction record created date" do
+          expect(ecf2_ect_at_school_period_row.started_on).to eql(ecf1_induction_record_row.start_date.to_date)
+        end
+      end
+
+      context "when the induction record start date is later than the created_at" do
+        let(:induction_record) do
+          FactoryBot.build(:ecf1_teacher_history_induction_record_row, :start_date_later_than_created_at)
+        end
+
+        it "sets the start date to the induction record created date" do
+          expect(ecf2_ect_at_school_period_row.started_on).to eql(ecf1_induction_record_row.created_at.to_date)
+        end
+      end
+    end
   end
+
+  # Extra contexts:
+  # * IR created_at date is later than the start_date
+  # * with an school-reported AB
+  # * deferred / withdrawn?
 end
