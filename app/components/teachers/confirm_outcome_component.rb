11ff6cdcb7ea80ea4e007ca9cb121428375bffe2
abdullaaden77 <abdullah.aden@education.gov.uk>
2025-12-09 17:46:52 +0000

    fix tests

--- app/components/teachers/confirm_outcome_component.rb
+++ app/components/teachers/confirm_outcome_component.rb
@@ -1,6 +1,5 @@
 module Teachers
   class ConfirmOutcomeComponent < ApplicationComponent
-
     attr_reader :service, :teacher_full_name
 
     delegate :appropriate_body,
@@ -9,12 +8,12 @@ module Teachers
              :outcome,
              to: :service
 
-    def initialize(mode:, service:)
+    def initialize(service:)
       @service = service
       @teacher_full_name = ::Teachers::Name.new(teacher).full_name
     end
 
-    private
+  private
 
     def appeal_notice
       "#{teacher_full_name} can appeal this outcome. You must tell them about their right to appeal and the appeal process."
