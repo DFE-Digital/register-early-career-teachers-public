df0e6624dc02e35afd879710c4e35819d6ad2cb1
Erica Porter <ericaporter@users.noreply.github.com>
2025-12-09 09:34:56 +0000

    Add functionality to show/hide finance section in admin

--- app/controllers/admin/finance_controller.rb
+++ app/controllers/admin/finance_controller.rb
@@ -2,5 +2,18 @@ module Admin
   class FinanceController < AdminController
     def show
     end
+
+  private
+
+    def authorised?
+      current_user&.dfe_user? && current_user.finance_access?
+    end
+
+    def authorise
+      return if authorised?
+
+      @unauthorised_context = :finance
+      render "errors/unauthorised", status: :unauthorized
+    end
   end
 end
