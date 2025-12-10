88ff1a7360cc80ed819413e50fa1c24b34ec8f1e
thenapking <tom.giles@gmail.com>
2025-12-10 15:24:49 +0000

    Refactor helper

--- spec/support/shared_examples/induction_redirect.rb
+++ spec/support/shared_examples/induction_redirect.rb
@@ -10,46 +10,94 @@ RSpec.shared_examples "an induction redirectable route" do
       before { sign_in_as(:school_user, school:) }
 
       context "when the feature is disabled" do
-        it "does not redirect" do
-          subject
-
-          expect(response).not_to redirect_to(schools_confirm_existing_induction_tutor_wizard_edit_path)
-        end
+        it_behaves_like "does not redirect to wizard"
       end
 
       context "when the feature is enabled", :enable_induction_tutor_prompt do
         context "when the school's induction tutor has never been confirmed" do
           let(:induction_tutor_last_nominated_in) { nil }
 
-          it "redirects to the wizard" do
-            subject
-
-            expect(response).to redirect_to(schools_confirm_existing_induction_tutor_wizard_edit_path)
-          end
+          it_behaves_like "redirects to wizard"
         end
 
         context "when the school's induction tutor needs to update information" do
           let(:year) { 2024 }
 
-          it "redirects to the wizard" do
-            FactoryBot.create(:contract_period, year: Time.zone.now.year)
+          before { FactoryBot.create(:contract_period, year: Time.zone.now.year) }
 
-            subject
-            expect(response).to redirect_to(schools_confirm_existing_induction_tutor_wizard_edit_path)
-          end
+          it_behaves_like "redirects to wizard"
         end
 
         context "when the school's induction tutor does not need to update information" do
-          it "does not redirect" do
-            subject
-
-            expect(response).not_to redirect_to(schools_confirm_existing_induction_tutor_wizard_edit_path)
-          end
+          it_behaves_like "does not redirect to wizard"
         end
       end
     end
 
     context "when signed in as a dfe user" do
+      let(:user) { FactoryBot.create(:user, :admin) }
+
+      before { sign_in_as(:dfe_user, user:) }
+
+      context "when the feature is disabled" do
+        it_behaves_like "does not redirect to wizard"
+      end
+
+      context "when the feature is enabled", :enable_induction_tutor_prompt do
+        context "when the school's induction tutor has never been confirmed" do
+          let(:induction_tutor_last_nominated_in) { nil }
+
+          it_behaves_like "does not redirect to wizard"
+        end
+
+        context "when the school's induction tutor needs to update information" do
+          let(:year) { 2024 }
+
+          before { FactoryBot.create(:contract_period, year: Time.zone.now.year) }
+
+          it_behaves_like "does not redirect to wizard"
+        end
+      end
     end
+
+    context "when signed in as an appropriate body" do
+      let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
+
+      before { sign_in_as(:appropriate_body_user, appropriate_body:) }
+
+      context "when the feature is disabled" do
+        it_behaves_like "does not redirect to wizard"
+      end
+
+      context "when the feature is enabled", :enable_induction_tutor_prompt do
+        context "when the school's induction tutor has never been confirmed" do
+          let(:induction_tutor_last_nominated_in) { nil }
+
+          it_behaves_like "does not redirect to wizard"
+        end
+
+        context "when the school's induction tutor needs to update information" do
+          let(:year) { 2024 }
+
+          before { FactoryBot.create(:contract_period, year: Time.zone.now.year) }
+
+          it_behaves_like "does not redirect to wizard"
+        end
+      end
+    end
+  end
+end
+
+RSpec.shared_examples "redirects to wizard" do
+  it "redirects to the wizard" do
+    subject
+    expect(response).to redirect_to(schools_confirm_existing_induction_tutor_wizard_edit_path)
+  end
+end
+
+RSpec.shared_examples "does not redirect to wizard" do
+  it "does not redirect to the wizard" do
+    subject
+    expect(response).not_to redirect_to(schools_confirm_existing_induction_tutor_wizard_edit_path)
   end
 end
