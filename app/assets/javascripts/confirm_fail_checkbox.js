099bb8915db2ebc493a40ea05fbbd0e72ad56f66
abdullaaden77 <abdullah.aden@education.gov.uk>
2025-12-10 09:43:08 +0000

    fix js lint

--- app/assets/javascripts/confirm_fail_checkbox.js
+++ app/assets/javascripts/confirm_fail_checkbox.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const failCheckbox = document.querySelector('#confirm_fail_checkbox');
-  const continueButton = document.querySelector('.govuk-button');
+  const failCheckbox = document.querySelector('#confirm_fail_checkbox')
+  const continueButton = document.querySelector('.govuk-button')
 
   failCheckbox.addEventListener('click', () => {
     if (failCheckbox.checked) {
-      continueButton.removeAttribute('disabled');
-      continueButton.setAttribute('aria-disabled', 'false');
+      continueButton.removeAttribute('disabled')
+      continueButton.setAttribute('aria-disabled', 'false')
     } else {
-      continueButton.setAttribute('disabled', 'disabled');
-      continueButton.setAttribute('aria-disabled', 'true');
+      continueButton.setAttribute('disabled', 'disabled')
+      continueButton.setAttribute('aria-disabled', 'true')
     }
   })
 })
