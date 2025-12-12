36e2cbf364ae17a8806bea272db71b3f98e0fef6
Ross Oliver <ross.oliver36@gmail.com>
2025-12-12 15:47:11 +0000

    Close/open training periods on mentor completion
    When a we get a completed, billable declaration submitted for a mentor we
    wantto also close off their latest/ongoing training period for that lead
    provider.
    
    Where possible we will use the `declaration_date` as the `finished_on`.
    If,however, that date is before the training period `started_on` we will close
    it the day after it started.
    
    When a billable, completed declaration is voided we 'uncomplete' the mentor
    andwe also want to resume  their training period with the lead provider (unless
    ithas been resumed through another avenue). To do this, we will create a new
    training period that starts the day their previous period finished and it will
    mirror the `finished_on` of the school period.

--- app/services/declarations/mentor_completion.rb
+++ app/services/declarations/mentor_completion.rb
@@ -13,8 +13,10 @@ module Declarations
       ActiveRecord::Base.transaction do
         if latest_completed_declaration.billable_or_changeable?
           mentor_completed_training!
+          finish_training_period!
         else
           mentor_not_completed_training!
+          create_training_period! unless latest_training_period.ongoing_today?
         end
 
         record_completion_change_event!
@@ -29,6 +31,45 @@ module Declarations
 
     def mentor_completion_event?
       training_period.for_mentor? && declaration.declaration_type_completed?
+    end
+
+    def create_training_period!
+      period = latest_training_period.mentor_at_school_period
+
+      TrainingPeriods::Create.provider_led(
+        period:,
+        started_on: latest_training_period.finished_on,
+        finished_on: period.finished_on,
+        school_partnership: latest_training_period.school_partnership,
+        expression_of_interest: nil,
+        schedule: latest_training_period.schedule,
+        author:
+      ).call
+    end
+
+    def finish_training_period!
+      TrainingPeriods::Finish.mentor_training(
+        training_period: latest_training_period,
+        mentor_at_school_period: latest_training_period.mentor_at_school_period,
+        finished_on:,
+        author:
+      ).finish!
+    end
+
+    def finished_on
+      if declaration.declaration_date > latest_training_period.started_on
+        declaration.declaration_date
+      else
+        latest_training_period.started_on + 1.day
+      end
+    end
+
+    def latest_training_period
+      @latest_training_period ||= metadata.latest_mentor_training_period
+    end
+
+    def metadata
+      @metadata ||= teacher.lead_provider_metadata.find_by(lead_provider_id: training_period.lead_provider.id)
     end
 
     def mentor_completed_training!
