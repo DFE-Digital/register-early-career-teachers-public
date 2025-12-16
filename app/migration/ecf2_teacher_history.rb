0cd1cfec521653ab5591869663dcf904532c8038
edujackedu <jack.scotti@education.gov.uk>
2025-12-16 17:23:37 +0000

    Extract record_failure! method in ECF2TeacherHistory
    Reduce duplication by extracting failure recording into a helper method.
    Add MIGRATION_ITEM_TYPE constant for the source record type.

--- app/migration/ecf2_teacher_history.rb
+++ app/migration/ecf2_teacher_history.rb
@@ -1,6 +1,8 @@
 class ECF2TeacherHistory
   class InvalidPeriodType < StandardError; end
 
+  MIGRATION_ITEM_TYPE = "Migration::InductionRecord"
+
   SchoolData = Data.define(:urn, :name)
   AppropriateBodyData = Data.define(:id, :name)
   MentorData = Data.define(:trn, :urn, :started_on, :finished_on)
@@ -50,6 +52,17 @@ private
     teacher
   end
 
+  def record_failure!(teacher:, model:, message:, migration_item_id:)
+    @success = false
+    TeacherMigrationFailure.create!(
+      teacher:,
+      model:,
+      message:,
+      migration_item_id:,
+      migration_item_type: MIGRATION_ITEM_TYPE
+    )
+  end
+
   def save_ect_periods!(teacher)
     ect_at_school_period_rows.each do |ect_at_school_period_row|
       ect_at_school_period = ECTAtSchoolPeriod.create!(teacher:, **ect_at_school_period_row)
@@ -61,14 +74,7 @@ private
           **training_period_row
         )
       rescue ActiveRecord::ActiveRecordError => e
-        @success = false
-        TeacherMigrationFailure.create!(
-          teacher:,
-          model: :training_period,
-          message: e.message,
-          migration_item_id: training_period_row.ecf_start_induction_record_id,
-          migration_item_type: "Migration::InductionRecord"
-        )
+        record_failure!(teacher:, model: :training_period, message: e.message, migration_item_id: training_period_row.ecf_start_induction_record_id)
       end
 
       ect_at_school_period_row.mentorship_period_rows.each do |mentorship_period_row|
@@ -78,24 +84,10 @@ private
           **mentorship_period_row
         )
       rescue ActiveRecord::ActiveRecordError => e
-        @success = false
-        TeacherMigrationFailure.create!(
-          teacher:,
-          model: :mentorship_period,
-          message: e.message,
-          migration_item_id: mentorship_period_row.ecf_start_induction_record_id,
-          migration_item_type: "Migration::InductionRecord"
-        )
+        record_failure!(teacher:, model: :mentorship_period, message: e.message, migration_item_id: mentorship_period_row.ecf_start_induction_record_id)
       end
     rescue ActiveRecord::ActiveRecordError => e
-      @success = false
-      TeacherMigrationFailure.create!(
-        teacher:,
-        model: :ect_at_school_period,
-        message: e.message,
-        migration_item_id: ect_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id,
-        migration_item_type: "Migration::InductionRecord"
-      )
+      record_failure!(teacher:, model: :ect_at_school_period, message: e.message, migration_item_id: ect_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id)
     end
   end
 
@@ -110,24 +102,10 @@ private
           **training_period_row
         )
       rescue ActiveRecord::ActiveRecordError => e
-        @success = false
-        TeacherMigrationFailure.create!(
-          teacher:,
-          model: :training_period,
-          message: e.message,
-          migration_item_id: training_period_row.ecf_start_induction_record_id,
-          migration_item_type: "Migration::InductionRecord"
-        )
+        record_failure!(teacher:, model: :training_period, message: e.message, migration_item_id: training_period_row.ecf_start_induction_record_id)
       end
     rescue ActiveRecord::ActiveRecordError => e
-      @success = false
-      TeacherMigrationFailure.create!(
-        teacher:,
-        model: :mentor_at_school_period,
-        message: e.message,
-        migration_item_id: mentor_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id,
-        migration_item_type: "Migration::InductionRecord"
-      )
+      record_failure!(teacher:, model: :mentor_at_school_period, message: e.message, migration_item_id: mentor_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id)
     end
   end
 end
