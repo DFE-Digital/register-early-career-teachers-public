65fde520ef15fb7a2dac50ebd93bfda5346f5941
edujackedu <jack.scotti@education.gov.uk>
2025-12-16 17:33:49 +0000

    Refactor error handling with with_failure_recording block
    Replace repetitive rescue blocks with a declarative block-based helper
    that wraps create operations and records failures consistently.

--- app/migration/ecf2_teacher_history.rb
+++ app/migration/ecf2_teacher_history.rb
@@ -52,12 +52,14 @@ private
     teacher
   end
 
-  def record_failure!(teacher:, model:, message:, migration_item_id:)
+  def with_failure_recording(teacher:, model:, migration_item_id:)
+    yield
+  rescue ActiveRecord::ActiveRecordError => e
     @success = false
     TeacherMigrationFailure.create!(
       teacher:,
       model:,
-      message:,
+      message: e.message,
       migration_item_id:,
       migration_item_type: MIGRATION_ITEM_TYPE
     )
@@ -65,47 +67,47 @@ private
 
   def save_ect_periods!(teacher)
     ect_at_school_period_rows.each do |ect_at_school_period_row|
-      ect_at_school_period = ECTAtSchoolPeriod.create!(teacher:, **ect_at_school_period_row)
+      with_failure_recording(teacher:, model: :ect_at_school_period, migration_item_id: ect_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id) do
+        ect_at_school_period = ECTAtSchoolPeriod.create!(teacher:, **ect_at_school_period_row)
 
-      ect_at_school_period_row.training_period_rows.each do |training_period_row|
-        TrainingPeriod.create!(
-          ect_at_school_period:,
-          **training_period_row.school_partnership(school: ect_at_school_period_row.real_school),
-          **training_period_row
-        )
-      rescue ActiveRecord::ActiveRecordError => e
-        record_failure!(teacher:, model: :training_period, message: e.message, migration_item_id: training_period_row.ecf_start_induction_record_id)
-      end
+        ect_at_school_period_row.training_period_rows.each do |training_period_row|
+          with_failure_recording(teacher:, model: :training_period, migration_item_id: training_period_row.ecf_start_induction_record_id) do
+            TrainingPeriod.create!(
+              ect_at_school_period:,
+              **training_period_row.school_partnership(school: ect_at_school_period_row.real_school),
+              **training_period_row
+            )
+          end
+        end
 
-      ect_at_school_period_row.mentorship_period_rows.each do |mentorship_period_row|
-        MentorshipPeriod.create!(
-          mentee: ect_at_school_period,
-          **mentorship_period_row.mentor_at_school_period,
-          **mentorship_period_row
-        )
-      rescue ActiveRecord::ActiveRecordError => e
-        record_failure!(teacher:, model: :mentorship_period, message: e.message, migration_item_id: mentorship_period_row.ecf_start_induction_record_id)
+        ect_at_school_period_row.mentorship_period_rows.each do |mentorship_period_row|
+          with_failure_recording(teacher:, model: :mentorship_period, migration_item_id: mentorship_period_row.ecf_start_induction_record_id) do
+            MentorshipPeriod.create!(
+              mentee: ect_at_school_period,
+              **mentorship_period_row.mentor_at_school_period,
+              **mentorship_period_row
+            )
+          end
+        end
       end
-    rescue ActiveRecord::ActiveRecordError => e
-      record_failure!(teacher:, model: :ect_at_school_period, message: e.message, migration_item_id: ect_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id)
     end
   end
 
   def save_mentor_periods!(teacher)
     mentor_at_school_period_rows.each do |mentor_at_school_period_row|
-      mentor_at_school_period = MentorAtSchoolPeriod.create!(teacher:, **mentor_at_school_period_row)
+      with_failure_recording(teacher:, model: :mentor_at_school_period, migration_item_id: mentor_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id) do
+        mentor_at_school_period = MentorAtSchoolPeriod.create!(teacher:, **mentor_at_school_period_row)
 
-      mentor_at_school_period_row.training_period_rows.each do |training_period_row|
-        TrainingPeriod.create!(
-          mentor_at_school_period:,
-          **training_period_row.school_partnership(school: mentor_at_school_period_row.real_school),
-          **training_period_row
-        )
-      rescue ActiveRecord::ActiveRecordError => e
-        record_failure!(teacher:, model: :training_period, message: e.message, migration_item_id: training_period_row.ecf_start_induction_record_id)
+        mentor_at_school_period_row.training_period_rows.each do |training_period_row|
+          with_failure_recording(teacher:, model: :training_period, migration_item_id: training_period_row.ecf_start_induction_record_id) do
+            TrainingPeriod.create!(
+              mentor_at_school_period:,
+              **training_period_row.school_partnership(school: mentor_at_school_period_row.real_school),
+              **training_period_row
+            )
+          end
+        end
       end
-    rescue ActiveRecord::ActiveRecordError => e
-      record_failure!(teacher:, model: :mentor_at_school_period, message: e.message, migration_item_id: mentor_at_school_period_row.training_period_rows.first&.ecf_start_induction_record_id)
     end
   end
 end
