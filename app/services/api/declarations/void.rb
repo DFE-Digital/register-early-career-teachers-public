0459d79fa8949a3587e608e43141dbfc2e682878
Connor McQuillan <me@cpjmcquillan.com>
2025-12-17 08:03:48 +0000

    Refactor `declaration_id` to `declaration_api_id` for void services
    We want to be consistent across the codebase, and we are using `teacher_api_id`
    to refer to the ID in the `ParticipantsController` and respective services.
    
    It makes sense to follow the same pattern here. It makes things clearer since we
    are passing an API ID around.

--- app/services/api/declarations/void.rb
+++ app/services/api/declarations/void.rb
@@ -14,20 +14,20 @@ module API::Declarations
   private
 
     def not_already_voided
-      return if errors[:declaration_id].any?
+      return if errors[:declaration_api_id].any?
       return unless declaration.payment_status_voided?
 
-      errors.add(:declaration_id, "The declaration has already been voided.")
+      errors.add(:declaration_api_id, "The declaration has already been voided.")
     end
 
     def voidable_payment
-      return if errors[:declaration_id].any?
+      return if errors[:declaration_api_id].any?
       return if declaration.voidable_payment?
 
       # This error message might seem strange since the error isn't related to
       # whether the payment has been clawed back or not, but it is consistent
       # with ECF.
-      errors.add(:declaration_id, "This declaration has been clawed-back, so you can only view it.")
+      errors.add(:declaration_api_id, "This declaration has been clawed-back, so you can only view it.")
     end
   end
 end
