a080fd516f7270ca9fd4b665d00ab383a94a1927
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2025-12-23 17:39:11 +0000

    [2847] POST participant-declarations swagger docs

---
+++ spec/swagger_schemas/requests/declaration_create.rb
@@ -0,0 +1,26 @@
+DECLARATION_CREATE_REQUEST = {
+  description: "An participant declaration request",
+  type: :object,
+  required: %i[data],
+  properties: {
+    data: {
+      type: :object,
+      anyOf: [
+        { "$ref": "#/components/schemas/DeclarationPre2025DataRequest" },
+        { "$ref": "#/components/schemas/DeclarationPost2024ECTDataRequest" },
+        { "$ref": "#/components/schemas/DeclarationPost2024MentorDataRequest" },
+      ],
+    },
+  },
+  example: {
+    data: {
+      type: "participant-declaration",
+      attributes: {
+        participant_id: "3452b1a6-cbaa-422f-9ca9-40afa28583a2",
+        declaration_type: "started",
+        declaration_date: "2021-05-31T02:21:32.000Z",
+        course_identifier: "ecf-induction",
+      },
+    },
+  },
+}.freeze
---
+++ spec/swagger_schemas/requests/declaration_post2024_mentor.rb
@@ -0,0 +1,95 @@
+DECLARATION_POST2024_MENTOR_STARTED_ATTRIBUTES = {
+  description: "An ECF started participant declaration",
+  type: :object,
+  required: %i[participant_id declaration_type declaration_date course_identifier],
+  additionalProperties: false,
+  properties: {
+    participant_id: {
+      description: "The unique ID of the participant",
+      type: :string,
+      format: :uuid,
+      example: "3452b1a6-cbaa-422f-9ca9-40afa28583a2",
+    },
+    declaration_type: {
+      description: "The event declaration type",
+      type: :string,
+      enum: %w[started],
+      example: "started",
+    },
+    declaration_date: {
+      description: "The event declaration date",
+      type: :string,
+      format: :"date-time",
+      example: "2021-05-31T02:21:32.000Z",
+    },
+    course_identifier: {
+      description: "The type of course the participant is enrolled in",
+      type: :string,
+      enum: %w[ecf-mentor],
+      example: "ecf-mentor",
+    },
+    evidence_held: {
+      description: "The type of evidence the lead provider holds on their platform to demonstrate the participant has met the retention criteria for the current milestone period.",
+      type: :string,
+      enum: %w[training-event-attended self-study-material-completed materials-engaged-with-offline other],
+      example: "training-event-attended",
+    },
+  },
+}.freeze
+
+DECLARATION_POST2024_MENTOR_COMPLETED_ATTRIBUTES = {
+  description: "An ECF completed participant declaration",
+  type: :object,
+  required: %i[participant_id declaration_type declaration_date course_identifier evidence_held],
+  additionalProperties: false,
+  properties: {
+    participant_id: {
+      description: "The unique ID of the participant",
+      type: :string,
+      format: :uuid,
+      example: "3452b1a6-cbaa-422f-9ca9-40afa28583a2",
+    },
+    declaration_type: {
+      description: "The event declaration type",
+      type: :string,
+      enum: %w[completed],
+      example: "completed",
+    },
+    declaration_date: {
+      description: "The event declaration date",
+      type: :string,
+      format: :"date-time",
+      example: "2021-05-31T02:21:32.000Z",
+    },
+    course_identifier: {
+      description: "The type of course the participant is enrolled in",
+      type: :string,
+      enum: %w[ecf-mentor],
+      example: "ecf-mentor",
+    },
+    evidence_held: {
+      description: "The type of evidence the lead provider holds on their platform to demonstrate the participant has met the retention criteria for the current milestone period.",
+      type: :string,
+      enum: %w[75-percent-engagement-met 75-percent-engagement-met-reduced-induction],
+      example: "75-percent-engagement-met",
+    },
+  },
+}.freeze
+
+DECLARATION_POST2024_MENTOR_DATA_REQUEST = {
+  description: "A participant declaration data request for mentor participants from cohort 2025 onwards",
+  type: :object,
+  properties: {
+    type: {
+      type: :string,
+      enum: %w[participant-declaration],
+    },
+    attributes: {
+      type: :object,
+      anyOf: [
+        { "$ref": "#/components/schemas/DeclarationPost2024MentorStartedAttributes" },
+        { "$ref": "#/components/schemas/DeclarationPost2024MentorCompletedAttributes" },
+      ],
+    },
+  },
+}.freeze
