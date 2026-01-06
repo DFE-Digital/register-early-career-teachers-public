92b11d360dfc3f7faf5c3fa6b7a19beee51c890b
Erica Porter <ericaporter@users.noreply.github.com>
2026-01-06 11:26:10 +0000

    Update factories to align with stricter domain validations

--- app/services/api_seed_data/unfunded_mentors.rb
+++ app/services/api_seed_data/unfunded_mentors.rb
@@ -59,11 +59,13 @@ module APISeedData
 
     def create_unfunded_mentors(mentee_lp, mentor_lp, mentee_partnerships, mentor_partnerships)
       mentee_partnerships_cycle = mentee_partnerships.cycle
-      mentor_partnerships_cycle = mentor_partnerships.cycle
 
       MIN_UNFUNDED_MENTORS_PER_LP.times do |i|
         mentee_school_partnership = mentee_partnerships_cycle.next
-        mentor_school_partnership = mentor_partnerships_cycle.next
+        mentor_lpdp = mentor_partnerships.first.lead_provider_delivery_partnership
+        mentor_school_partnership = FactoryBot.create(:school_partnership,
+                                                      school: mentee_school_partnership.school,
+                                                      lead_provider_delivery_partnership: mentor_lpdp)
 
         random_date = rand(1..100).days.ago
 
