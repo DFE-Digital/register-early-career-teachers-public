3af88161d542fedb571a806a15baa7de73800ac8
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-01-08 11:29:07 +0000

    Remove any validations around region and hub limits

--- app/models/region.rb
+++ app/models/region.rb
@@ -4,11 +4,4 @@ class Region < ApplicationRecord
   # Validations
   validates :code, presence: true, uniqueness: true
   validates :districts, presence: true
-  validate :region_limit, if: -> { teaching_school_hub.present? }
-
-  def region_limit
-    if teaching_school_hub.regions.count >= 3
-      errors.add(:teaching_school_hub, "#{teaching_school_hub.name} already has the maximum of 3 hub regions")
-    end
-  end
 end
