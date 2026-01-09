db5151a89df10ec956be62af8b15df9334297d0f
Tony Headford <tony@binarycircus.com>
2026-01-09 12:40:39 +0000

    first iteration of processing with stubs

--- app/migration/teacher_history_converter/ect/latest_induction_records.rb
+++ app/migration/teacher_history_converter/ect/latest_induction_records.rb
@@ -38,9 +38,24 @@ private
       # - mentor
       # - cohort/schedule
       # - email address
+      if school_period_covers_induction_record_period?(school_period, induction_record)
+        # build_stub
+      else
+        # incorporate into period
+      end
     else
-      # add a new school period
-      ect_at_school_periods << build_new_school_period_from_induction_record(induction_record)
+      # this is a new period but is it contained by an existing one?
+      covering_period = ect_at_school_periods.find do |period|
+        school_period_covers_induction_record_period?(period, induction_record)
+      end
+
+      if covering_period.present?
+        # add a stub school period
+        ect_at_school_periods << build_stub_school_period_prior_to(covering_period, induction_record)
+      else
+        # add a new school period
+        ect_at_school_periods << build_new_school_period_from_induction_record(induction_record)
+      end
     end
     # Step 2: <- this maybe incorporated into the above?
     #
@@ -49,7 +64,7 @@ private
     # - create a new training period
     # - do nothing
 
-    ect_at_school_periods
+    ect_at_school_periods.sort_by(&:started_on)
   end
 
   def build_new_school_period_from_induction_record(induction_record)
@@ -65,8 +80,26 @@ private
     )
   end
 
-  def build_new_training_period_from_induction_record(induction_record)
-    ECF2TeacherHistory::TrainingPeriodRow.new(
+  def build_stub_school_period_prior_to(school_period, induction_record)
+    started_on = [school_period.started_on - 2.days, induction_record.start_date.to_date].min
+    finished_on = school_period.started_on - 1.day
+
+    training_period = build_new_training_period_from_induction_record(induction_record, { started_on:, finished_on: })
+
+    ECF2TeacherHistory::ECTAtSchoolPeriodRow.new(
+      started_on:,
+      finished_on:,
+      school: induction_record.school,
+      email: induction_record.preferred_identity_email,
+      mentorship_period_rows: [],
+      training_period_rows: [
+        training_period
+      ]
+    )
+  end
+
+  def build_new_training_period_from_induction_record(induction_record, overrides = {})
+    training_attrs = {
       started_on: induction_record.start_date.to_date,
       finished_on: induction_record.end_date&.to_date,
       created_at: induction_record.created_at,
@@ -78,7 +111,26 @@ private
       is_ect: true,
       ecf_start_induction_record_id: induction_record.induction_record_id,
       schedule_info: induction_record.schedule_info
-    )
+    }.merge(overrides)
+
+    ECF2TeacherHistory::TrainingPeriodRow.new(**training_attrs)
+      # started_on: induction_record.start_date.to_date,
+      # finished_on: induction_record.end_date&.to_date,
+      # created_at: induction_record.created_at,
+      # school: induction_record.school,
+      # training_programme: convert_training_programme_name(induction_record.training_programme),
+      # lead_provider_info: induction_record.training_provider_info.lead_provider_info,
+      # delivery_partner_info: induction_record.training_provider_info.delivery_partner_info,
+      # contract_period_year: induction_record.cohort_year,
+      # is_ect: true,
+      # ecf_start_induction_record_id: induction_record.induction_record_id,
+      # schedule_info: induction_record.schedule_info
+    # )
+  end
+
+  def school_period_covers_induction_record_period?(school_period, induction_record)
+    school_period_range = Range.new(school_period.started_on, school_period.finished_on)
+    school_period_range.cover?(Range.new(induction_record.start_date, induction_record.end_date))
   end
 
   def build_training_period_rows(school_induction_records, all_induction_records)
