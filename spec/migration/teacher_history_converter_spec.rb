087dd9dbabdcbbc12659647f63fec46d7f636452
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-01-09 16:44:18 +0000

    [3043] Data migration sets corrected_name with user full_name

--- spec/migration/teacher_history_converter_spec.rb
+++ spec/migration/teacher_history_converter_spec.rb
@@ -1,5 +1,16 @@
 describe TeacherHistoryConverter do
   subject { TeacherHistoryConverter.new(ecf1_teacher_history:).convert_to_ecf2! }
 
-  pending "will test something"
+  describe "Teacher" do
+    describe "setting corrected_name" do
+      let(:ecf1_teacher_history_user) { FactoryBot.build(:ecf1_teacher_history_user, full_name: "Test User") }
+      let(:ecf1_teacher_history) { FactoryBot.build(:ecf1_teacher_history, user: ecf1_teacher_history_user) }
+
+      it "selects the user updated at timestamp" do
+        expect(subject.teacher_row.trs_first_name).to eql("Test")
+        expect(subject.teacher_row.trs_last_name).to eql("User")
+        expect(subject.teacher_row.corrected_name).to eql("Test User")
+      end
+    end
+  end
 end
