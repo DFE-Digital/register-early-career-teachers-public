a2d14e970cfa91aaa92bea7b874c0b5c4fdd4101
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-01-09 10:42:13 +0000

    [2847] Updated example to retained-1

--- spec/swagger_schemas/requests/declaration_create.rb
+++ spec/swagger_schemas/requests/declaration_create.rb
@@ -1,5 +1,5 @@
 DECLARATION_CREATE_REQUEST = {
-  description: "An participant declaration request",
+  description: "A participant declaration request",
   type: :object,
   required: %i[data],
   properties: {
@@ -17,9 +17,10 @@ DECLARATION_CREATE_REQUEST = {
       type: "participant-declaration",
       attributes: {
         participant_id: "3452b1a6-cbaa-422f-9ca9-40afa28583a2",
-        declaration_type: "started",
+        declaration_type: "retained-1",
         declaration_date: "2021-05-31T02:21:32.000Z",
         course_identifier: "ecf-induction",
+        evidence_held: "training-event-attended",
       },
     },
   },
