bfb0fdba8bf4fd0147bc3dee003687afe68a8e55
Erica Porter <ericaporter@users.noreply.github.com>
2026-01-13 09:36:02 +0000

    Ensure change start date doesn't impact reuse journey

--- app/wizards/schools/register_ect_wizard/change_start_date_step.rb
+++ app/wizards/schools/register_ect_wizard/change_start_date_step.rb
@@ -2,38 +2,22 @@ module Schools
   module RegisterECTWizard
     class ChangeStartDateStep < StartDateStep
       def next_step
-        if school.last_programme_choices? && wizard.use_previous_choices_allowed?
-          :use_previous_ect_choices
-        elsif school.independent?
-          :independent_school_appropriate_body
-        else
-          :state_school_appropriate_body
-        end
+        :check_answers
       end
 
       def previous_step
         :check_answers
       end
 
-      def save!
-        super.tap do |result|
-          next unless result
-
-          reset_reuse_state!
-        end
-      end
-
     private
 
-      def reset_reuse_state!
-        ect.use_previous_ect_choices = nil
-        wizard.store[:school_partnership_to_reuse_id] = nil
+      def persist
+        super
 
-        return if wizard.use_previous_choices_allowed?
+        ect.update!(use_previous_ect_choices: nil)
+        store[:school_partnership_to_reuse_id] = nil
 
-        ect.appropriate_body_id = nil
-        ect.training_programme = nil
-        ect.lead_provider_id = nil
+        true
       end
     end
   end
