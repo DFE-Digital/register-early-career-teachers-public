3756ac1b2bf6d67b4e50aebf3ab48dba714748cf
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-01-13 14:45:34 +0000

    [3043] Added test for TRS first_name and last_name

--- spec/migration/teacher_history_converter_spec.rb
+++ spec/migration/teacher_history_converter_spec.rb
@@ -2,11 +2,21 @@ describe TeacherHistoryConverter do
   subject { TeacherHistoryConverter.new(ecf1_teacher_history:).convert_to_ecf2! }
 
   describe "Teacher" do
+    describe "setting trs names" do
+      let(:ecf1_teacher_history_user) { FactoryBot.build(:ecf1_teacher_history_user, full_name: "Ms Test User") }
+      let(:ecf1_teacher_history) { FactoryBot.build(:ecf1_teacher_history, user: ecf1_teacher_history_user) }
+
+      it "sets trs_first_name and trs_last_name" do
+        expect(subject.teacher_row.trs_first_name).to eql("Test")
+        expect(subject.teacher_row.trs_last_name).to eql("User")
+      end
+    end
+
     describe "setting corrected_name" do
       let(:ecf1_teacher_history_user) { FactoryBot.build(:ecf1_teacher_history_user, full_name: "Test User") }
       let(:ecf1_teacher_history) { FactoryBot.build(:ecf1_teacher_history, user: ecf1_teacher_history_user) }
 
-      it "selects the user updated at timestamp" do
+      it "sets corrected_name" do
         expect(subject.teacher_row.trs_first_name).to eql("Test")
         expect(subject.teacher_row.trs_last_name).to eql("User")
         expect(subject.teacher_row.corrected_name).to eql("Test User")
