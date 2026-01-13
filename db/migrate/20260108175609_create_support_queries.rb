11d7a3da17193fb365cf11ee6327b3f093f842fd
Richard Worrall <richard@soupish.net>
2026-01-13 16:55:38 +0000

    Add support form for school users

---
+++ db/migrate/20260108175609_create_support_queries.rb
@@ -0,0 +1,17 @@
+class CreateSupportQueries < ActiveRecord::Migration[8.0]
+  def change
+    create_table :support_queries do |t|
+      t.string :state, null: false, default: "pending"
+      t.integer :zendesk_id
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :school_name, null: false
+      t.integer :school_urn, null: false
+      t.text :message, null: false
+
+      t.timestamps
+    end
+
+    add_index :support_queries, :zendesk_id, unique: true
+  end
+end
