5ee3f75bf99a716bbd10be30151aaa5111711fa1
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-01-14 16:46:47 +0000

    [2975] Add Notify mailer for ECT registration emails

---
+++ app/mailers/schools/ect_registration_mailer.rb
@@ -0,0 +1,28 @@
+class Schools::ECTRegistrationMailer < ApplicationMailer
+  SUBJECT = "You’ve been registered for early career teacher training and support"
+  ECT_GUIDANCE_URL = "https://www.gov.uk/government/publications/early-career-teachers-your-training-and-support-entitlement"
+  PRIVACY_NOTICE_URL = "https://www.gov.uk/government/publications/privacy-information-education-providers-workforce-including-teachers"
+
+  def school_led_confirmation
+    ect_at_school_period = params.fetch(:ect_at_school_period)
+
+    prepare_personalisation(ect_at_school_period)
+    view_mail(NOTIFY_TEMPLATE_ID, to: ect_at_school_period.email, subject: SUBJECT)
+  end
+
+  def provider_led_confirmation
+    ect_at_school_period = params.fetch(:ect_at_school_period)
+
+    prepare_personalisation(ect_at_school_period)
+    view_mail(NOTIFY_TEMPLATE_ID, to: ect_at_school_period.email, subject: SUBJECT)
+  end
+
+private
+
+  def prepare_personalisation(ect_at_school_period)
+    @early_career_teacher_name = Teachers::Name.new(ect_at_school_period.teacher).full_name
+    @school_name = ect_at_school_period.school.name
+    @ect_guidance_url = ECT_GUIDANCE_URL
+    @privacy_notice_url = PRIVACY_NOTICE_URL
+  end
+end
