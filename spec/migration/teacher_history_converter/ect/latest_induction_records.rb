f1b07bd9b227a4a5fd7365fdd468eef950753ca1
Lorenzo Tello <ltello8a@gmail.com>
2026-01-16 12:42:34 +0000

    reformat latest induction records mode specs

--- spec/migration/teacher_history_converter/ect/latest_induction_records.rb
+++ spec/migration/teacher_history_converter/ect/latest_induction_records.rb
@@ -19,128 +19,298 @@ describe "Latest induction records mode conversion" do
 
   {
     "two IRs share start date. The first ends later than the second" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-3-3..2024-5-5___222_222
+      #  From these induction records:
+      #                                             ┌───────────────────────┐
+      #                                             │         A             │
+      #                                             └───────────────────────┘
+      #                                             ┌───────────────┐
+      #                                             │       B       │
+      #                                             └───────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #                                        ┌───┐
+      #                                        │ A │
+      #                                        └───┘
+      #                                             ┌───────────────┐
+      #                                             │       B       │
+      #                                             └───────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-3-3", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-5-5" },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..2024-5-5___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-5-5" },
       ]
     },
+
     "two IRs share start date and end_date" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-3-3..2024-6-6___222_222
+      #  From these induction records:
+      #                                             ┌──────────────┐
+      #                                             │       A      │
+      #                                             └──────────────┘
+      #                                             ┌──────────────┐
+      #                                             │       B      │
+      #                                             └──────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #                                        ┌───┐
+      #                                        │ A │
+      #                                        └───┘
+      #                                             ┌───────────────┐
+      #                                             │       B       │
+      #                                             └───────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-3-3", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-6-6" },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..2024-6-6___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-6-6" },
       ]
     },
-    "two IRs. The second one starts and end before first one starts" => {
-      induction_records: %w[
-        2024-4-4..2024-6-6___111_111
-        2024-3-3..2024-5-5___222_222
+
+    "two IRs. The second one starts before the first one starts and ends before the first one ends" => {
+      #  From these induction records:
+      #                                             ┌────────────────────┐
+      #                                             │        A           │
+      #                                             └────────────────────┘
+      #                            ┌────────────────────┐
+      #                            │         B          │
+      #                            └────────────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #                       ┌───┐
+      #                       │ A │
+      #                       └───┘
+      #                            ┌────────────────────┐
+      #                            │         B          │
+      #                            └────────────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-4-4", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-5-5" },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..2024-5-5___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-5-5" },
       ]
     },
-    "two IRs. The second one starts and end before first one starts" => {
-      induction_records: %w[
-        2024-5-5..2024-6-6___111_111
-        2024-3-3..2024-4-4___222_222
+
+    "two IRs. The second one starts and end before the first one starts" => {
+      #  From these induction records:
+      #                                     ┌────────────────────┐
+      #                                     │        A           │
+      #                                     └────────────────────┘
+      #           ┌────────────────────┐
+      #           │         B          │
+      #           └────────────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #      ┌───┐
+      #      │ A │
+      #      └───┘
+      #           ┌────────────────────┐
+      #           │         B          │
+      #           └────────────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-5-5", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-4-4" },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..2024-4-4___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-4-4" },
       ]
     },
+
     "two IRs share start date. The second ends later than the first one" => {
-      induction_records: %w[
-        2024-3-3..2024-5-5___111_111
-        2024-3-3..2024-6-6___222_222
+      #  From these induction records:
+      #           ┌─────────────────┐
+      #           │        A        │
+      #           └─────────────────┘
+      #           ┌────────────────────┐
+      #           │         B          │
+      #           └────────────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #      ┌───┐
+      #      │ A │
+      #      └───┘
+      #           ┌────────────────────┐
+      #           │         B          │
+      #           └────────────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-3-3", end_date: "2024-5-5" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-6-6" },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..2024-6-6___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-6-6" },
       ]
     },
+
     "two IRs. The second starts later and ends earlier than the first one" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-4-4..2024-5-5___222_222
+      #  From these induction records:
+      #           ┌───────────────────────────────────────┐
+      #           │                     A                 │
+      #           └───────────────────────────────────────┘
+      #                       ┌────────────────────┐
+      #                       │         B          │
+      #                       └────────────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #           ┌──────────┐┌───────────────────────────┐
+      #           │     A    ││             ✂️            │
+      #           └──────────┘└───────────────────────────┘
+      #                       ┌────────────────────┐
+      #                       │         B          │
+      #                       └────────────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-1-1", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-5-5" },
       ],
-      at_school_periods: %w[
-        2024-3-3..2024-4-3___111_111
-        2024-4-4..2024-5-5___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-1-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-5-5" },
       ]
     },
+
     "two IRs. The second starts later and ends the same day the first one does" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-4-4..2024-6-6___222_222
+      #  From these induction records:
+      #           ┌────────────────────────────────┐
+      #           │                A               │
+      #           └────────────────────────────────┘
+      #                       ┌────────────────────┐
+      #                       │         B          │
+      #                       └────────────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #           ┌──────────┐┌────────────────────┐
+      #           │     A    ││         ✂️         │
+      #           └──────────┘└────────────────────┘
+      #                       ┌────────────────────┐
+      #                       │         B          │
+      #                       └────────────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-1-1", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-6-6" },
       ],
-      at_school_periods: %w[
-        2024-3-3..2024-4-3___111_111
-        2024-4-4..2024-6-6___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-1-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-6-6" },
       ]
     },
+
     "two IRs. The second starts and ends later than the first one" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-4-4..2024-7-7___222_222
+      #  From these induction records:
+      #           ┌──────────────────────┐
+      #           │           A          │
+      #           └──────────────────────┘
+      #                       ┌────────────────────┐
+      #                       │         B          │
+      #                       └────────────────────┘
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #           ┌──────────┐┌──────────┐
+      #           │     A    ││    ✂️    │
+      #           └──────────┘└──────────┘
+      #                       ┌────────────────────┐
+      #                       │         B          │
+      #                       └────────────────────┘
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-1-1", end_date: "2024-5-5" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: "2024-6-6" },
       ],
-      at_school_periods: %w[
-        2024-3-3..2024-4-3___111_111
-        2024-4-4..2024-7-7___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-1-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: "2024-6-6" },
       ]
     },
+
     "two IRs. The second starts later than the first one and never ends" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-4-4..:ignore___222_222
+      #  From these induction records:
+      #           ┌──────────────────────┐
+      #           │           A          │
+      #           └──────────────────────┘
+      #                       ┌────────────────────>
+      #                       │         B
+      #                       └────────────────────>
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #           ┌──────────┐┌──────────┐
+      #           │     A    ││    ✂️    │
+      #           └──────────┘└──────────┘
+      #                       ┌────────────────────>
+      #                       │         B
+      #                       └────────────────────>
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-1-1", end_date: "2024-5-5" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: :ignore },
       ],
-      at_school_periods: %w[
-        2024-3-3..2024-4-3___111_111
-        2024-4-4..:ignore___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-1-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: :ignore },
       ]
     },
+
     "two IRs starts the same day. The second one never ends" => {
-      induction_records: %w[
-        2024-3-3..2024-6-6___111_111
-        2024-3-3..:ignore___222_222
+      #  From these induction records:
+      #           ┌──────────────────────┐
+      #           │           A          │
+      #           └──────────────────────┘
+      #           ┌────────────────────────────>
+      #           │             B
+      #           └────────────────────────────>
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #      ┌───┐
+      #      │ A │
+      #      └───┘
+      #           ┌────────────────────────────>
+      #           │           B
+      #           └────────────────────────────>
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-3-3", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: :ignore },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..:ignore___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: :ignore },
       ]
     },
+
     "two IRs. The first starts later than the second one. The second one never ends" => {
-      induction_records: %w[
-        2024-4-4..2024-6-6___111_111
-        2024-3-3..:ignore___222_222
+      #  From these induction records:
+      #                 ┌──────────────────────┐
+      #                 │           A          │
+      #                 └──────────────────────┘
+      #           ┌────────────────────────────────────>
+      #           │                 B
+      #           └────────────────────────────────────>
+      # ------------------------------------------------------------------------------------------
+      #  To these ECT at school periods:
+      #      ┌───┐
+      #      │ A │
+      #      └───┘
+      #           ┌────────────────────────────────────>
+      #           │                 B
+      #           └────────────────────────────────────>
+      induction_records: [
+        { urn: "111_111",  start_date: "2024-4-4", end_date: "2024-6-6" },
+        { urn: "222_222",  start_date: "2024-3-3", end_date: :ignore },
       ],
-      at_school_periods: %w[
-        2024-3-1..2024-3-2___111_111
-        2024-3-3..:ignore___222_222
+      at_school_periods: [
+        { urn: "111_111",  started_on: "2024-3-1", finished_on: "2024-3-2" },
+        { urn: "222_222",  started_on: "2024-3-3", finished_on: :ignore },
       ]
-    },
+    }
   }.each do |description, data|
     context description do
       let(:induction_records) do
         data[:induction_records].map do |induction_record|
-          dates, urn = induction_record.split("___")
-          start_date, end_date = dates.split("..").map do |date_or_ignore|
-            date_or_ignore == ":ignore" ? :ignore : Time.zone.parse(date_or_ignore)
-          end
-
           {
-            start_date:,
-            end_date:,
-            school: schools.find { |school| school[:urn] == urn.to_i }
+            start_date: (induction_record[:start_date] == :ignore ? :ignore : Time.zone.parse(induction_record[:start_date])),
+            end_date: (induction_record[:end_date] == :ignore ? :ignore : Time.zone.parse(induction_record[:end_date])),
+            school: schools.find { |school| school[:urn] == induction_record[:urn].to_i }
           }
         end
       end
@@ -152,14 +322,12 @@ describe "Latest induction records mode conversion" do
       it "produces the expected ECT at school periods" do
         aggregate_failures do
           data[:at_school_periods].each_with_index do |at_school_period, i|
-            dates, urn = at_school_period.split("___")
-            started_on, finished_on = dates.split("..").map do |date_or_ignore|
-              date_or_ignore == ":ignore" ? nil : date_or_ignore.to_date
-            end
+            started_on = at_school_period[:started_on] == :ignore ? :ignore : Time.zone.parse(at_school_period[:started_on])
+            finished_on = at_school_period[:finished_on] == :ignore ? :ignore : Time.zone.parse(at_school_period[:finished_on])
 
             expect(subject.ect_at_school_period_rows[i].started_on).to eql(started_on)
             expect(subject.ect_at_school_period_rows[i].finished_on).to eql(finished_on)
-            expect(subject.ect_at_school_period_rows[i].school.urn).to eql(urn)
+            expect(subject.ect_at_school_period_rows[i].school.urn).to eql(at_school_period[:urn])
           end
         end
       end
