24ead636da8f8fbf432fc6631db88b8a2dce3991
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-01-19 15:02:37 +0000

    [3090] Course identifier validation error matching ecf1

--- spec/services/api/declarations/create_spec.rb
+++ spec/services/api/declarations/create_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe API::Declarations::Create, type: :model do
         it { is_expected.to be_valid }
         it { is_expected.to validate_presence_of(:lead_provider_id).with_message("Enter a '#/lead_provider_id'.") }
         it { is_expected.to validate_presence_of(:teacher_api_id).with_message("Enter a '#/teacher_api_id'.") }
-        it { is_expected.to validate_inclusion_of(:teacher_type).in_array(API::Concerns::Teachers::SharedAction::TEACHER_TYPES).with_message("The entered '#/teacher_type' is not recognised for the given participant. Check details and try again.") }
+        it { is_expected.to validate_presence_of(:teacher_type).with_message("Enter a '#/teacher_type'.") }
 
         context "when the `lead_provider` does not exist" do
           let(:lead_provider_id) { 9999 }
@@ -47,14 +47,14 @@ RSpec.describe API::Declarations::Create, type: :model do
           let(:teacher_type) { trainee_type == :ect ? :mentor : :ect }
 
           it { is_expected.to have_one_error_per_attribute }
-          it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
+          it { is_expected.to have_error(:teacher_type, "The entered '#/teacher_type' is not recognised for the given participant. Check details and try again.") }
         end
 
         context "when a matching training period does not exist (different lead provider)" do
           let(:lead_provider_id) { FactoryBot.create(:lead_provider, name: "Different to #{lead_provider.name}").id }
 
           it { is_expected.to have_one_error_per_attribute }
-          it { is_expected.to have_error(:teacher_api_id, "Your update cannot be made as the '#/teacher_api_id' is not recognised. Check participant details and try again.") }
+          it { is_expected.to have_error(:teacher_type, "The entered '#/teacher_type' is not recognised for the given participant. Check details and try again.") }
         end
 
         context "when the teacher does not exist" do
@@ -81,6 +81,19 @@ RSpec.describe API::Declarations::Create, type: :model do
           let(:teacher_type) { nil }
 
           it { is_expected.to have_error(:teacher_type, "Enter a '#/teacher_type'.") }
+        end
+
+        context "when wrong teacher type is provided for the trainee" do
+          # Set `teacher_type` param to :ect for Mentor or :mentor for ECT
+          let(:teacher_type) { trainee_type == :ect ? :mentor : :ect }
+
+          it { is_expected.to have_error(:teacher_type, "The entered '#/teacher_type' is not recognised for the given participant. Check details and try again.") }
+        end
+
+        context "when a non existing teacher type is used" do
+          let(:teacher_type) { :fake }
+
+          it { is_expected.to have_error(:teacher_type, "The entered '#/teacher_type' is not recognised for the given participant. Check details and try again.") }
         end
 
         context "when `declaration_date` is nil" do
