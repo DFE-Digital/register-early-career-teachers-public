9e44a10a33aaad872092369b87046bbd155877a6
thenapking <tom.giles@gmail.com>
2026-01-22 16:05:57 +0000

    Product review feedback

--- app/controllers/admin/teachers/trainings_controller.rb
+++ app/controllers/admin/teachers/trainings_controller.rb
@@ -7,7 +7,8 @@ module Admin
         @teacher = TeacherPresenter.new(Teacher.find(params[:teacher_id]))
         @navigation_items = helpers.admin_teacher_navigation_items(@teacher, :training)
         @breadcrumbs = teacher_breadcrumbs
-        @ect_training_periods = @teacher.ect_training_periods.includes(ect_at_school_period: :teacher).order(started_on: :desc)
+        @ect_at_school_periods = @teacher.ect_at_school_periods
+        @ect_training_periods = @teacher.ect_training_periods.includes(ect_at_school_period: :teacher).order(started_on: :desc).group_by(&:lead_provider_delivery_partnership)
         @mentor_training_periods = @teacher.mentor_training_periods.includes(mentor_at_school_period: :teacher).order(started_on: :desc)
       end
 
