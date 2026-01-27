4debef47f7daf7520be2d7ba9cf9df9bca2047c7
Erica Porter <ericaporter@users.noreply.github.com>
2026-01-27 14:32:17 +0000

    Update migration

--- db/migrate/20260107162705_rename_super_admin_to_user_manager_in_dfe_role_type.rb
+++ db/migrate/20260107162705_rename_super_admin_to_user_manager_in_dfe_role_type.rb
@@ -1,15 +1,5 @@
 class RenameSuperAdminToUserManagerInDfERoleType < ActiveRecord::Migration[8.0]
-  def up
-    execute <<~SQL
-      ALTER TYPE dfe_role_type
-      RENAME VALUE 'super_admin' TO 'user_manager';
-    SQL
-  end
-
-  def down
-    execute <<~SQL
-      ALTER TYPE dfe_role_type
-      RENAME VALUE 'user_manager' TO 'super_admin';
-    SQL
+  def change
+    rename_enum_value :dfe_role_type, from: "super_admin", to: "user_manager"
   end
 end
