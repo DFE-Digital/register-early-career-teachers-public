9aee3bbdd5e31fcb349059e44fd59dde91119e7f
thenapking <tom.giles@gmail.com>
2026-01-28 12:06:22 +0000

    Clean up scope

--- app/models/ect_at_school_period.rb
+++ app/models/ect_at_school_period.rb
@@ -66,11 +66,8 @@ class ECTAtSchoolPeriod < ApplicationRecord
     .where(expression_of_interest: { lead_provider_id: })
   }
   scope :induction_not_completed, -> {
-    joins(<<~SQL)
-      LEFT JOIN teachers AS ect_teachers
-        ON ect_teachers.id = ect_at_school_periods.teacher_id
-    SQL
-    .where(ect_teachers: { trs_induction_completed_date: nil })
+    joins("LEFT JOIN teachers AS induction_teachers ON induction_teachers.id = ect_at_school_periods.teacher_id")
+    .where(induction_teachers: { trs_induction_completed_date: nil })
   }
 
   def reported_leaving_by?(school)
