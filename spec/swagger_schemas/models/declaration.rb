b5a369309713986719b4dda32aee74f64c38733b
Leandro Alemão <leandro.costantini@gmail.com>
2026-01-29 15:45:12 +0000

    [3141] Fix evidence held description on GET and PUT participant declaration swagger docs

--- spec/swagger_schemas/models/declaration.rb
+++ spec/swagger_schemas/models/declaration.rb
@@ -107,7 +107,7 @@ DECLARATION = {
           example: false,
         },
         evidence_held: {
-          description: "The type of evidence the lead provider holds on their platform to demonstrate the participant has met the retention criteria for the current milestone period. For retained-2 declarations, providers will need to confirm if the engagement threshold has been reached and only accept either the ‘75-percent-engagement-met’ or ‘75-percent-engagement-met-reduced-induction’ values.",
+          description: "The type of evidence the lead provider holds on their platform to demonstrate the participant has met the retention criteria for the current milestone period.",
           type: :string,
           nullable: true,
           enum: Declaration.evidence_types.values,
