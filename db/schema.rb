3c61d7242e012f738cf440ed0c0063ce689dc1da
Lorenzo Tello <ltello8a@gmail.com>
2026-01-30 16:38:05 +0000

    create the table to persist combinations

--- db/schema.rb
+++ db/schema.rb
@@ -149,11 +149,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_29_161426) do
 
   create_table "data_migration_teacher_combinations", force: :cascade do |t|
     t.string "trn"
-    t.uuid "participant_id"
-    t.jsonb "ecf1_ect_combinations"
-    t.jsonb "ecf1_mentor_combinations"
-    t.jsonb "ecf2_ect_combinations"
-    t.jsonb "ecf2_mentor_combinations"
+    t.uuid "ecf1_ect_profile_id"
+    t.uuid "ecf1_mentor_profile_id"
+    t.jsonb "ecf1_ect_combinations", default: [], null: false
+    t.jsonb "ecf1_mentor_combinations", default: [], null: false
+    t.jsonb "ecf2_ect_combinations", default: [], null: false
+    t.jsonb "ecf2_mentor_combinations", default: [], null: false
     t.virtual "ecf1_ect_combinations_count", type: :integer, as: "jsonb_array_length(ecf1_ect_combinations)", stored: true
     t.virtual "ecf1_mentor_combinations_count", type: :integer, as: "jsonb_array_length(ecf1_mentor_combinations)", stored: true
     t.virtual "ecf2_ect_combinations_count", type: :integer, as: "jsonb_array_length(ecf2_ect_combinations)", stored: true
