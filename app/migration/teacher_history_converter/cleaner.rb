8eb0cae951ef2aa680eab7e6c2edd3718ed1e24a
Peter Yates <peter.yates@graphia.co.uk>
2026-02-03 10:55:25 +0000

    Add a zero day induction record cleaner step
    This will adjust the end date of any periods that start and end on the
    same day, so the end date is on the next day

--- app/migration/teacher_history_converter/cleaner.rb
+++ app/migration/teacher_history_converter/cleaner.rb
@@ -12,6 +12,7 @@ private
   def clean!
     fix_service_start_dates(@raw_induction_records)
       .then { |induction_records| fix_corrupted_dates(induction_records) }
+      .then { |induction_records| fix_zero_day_periods(induction_records) }
   end
 
   def fix_service_start_dates(induction_records)
@@ -20,5 +21,9 @@ private
 
   def fix_corrupted_dates(induction_records)
     TeacherHistoryConverter::Cleaner::CorruptedDates.new(induction_records).induction_records
+  end
+
+  def fix_zero_day_periods(induction_records)
+    TeacherHistoryConverter::Cleaner::ZeroDay.new(induction_records).induction_records
   end
 end
