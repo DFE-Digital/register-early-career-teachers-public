3fb0538494f868c11920e5af67c980018465f160
Craig Davidson <craigmdavidson@gmail.com>
2026-02-04 09:24:18 +0000

    Appropriate Body Unclaimed ECTs detail page
    This page further breaks down unclaimed ECTs into claimable ECT, unclaimable ECTs due to missing QTS, and unclaimable ECTs due to being claimed by another AB.

--- config/routes/appropriate_body.rb
+++ config/routes/appropriate_body.rb
@@ -1,12 +1,12 @@
 namespace :appropriate_bodies, path: "appropriate-body", as: :ab do
   resource :landing, only: :show, path: "", controller: :landing
 
+  resources :unclaimed, only: :index, path: "schools-data"
   resources :teachers, only: %i[show index] do
     match "closed", to: "teachers#index", via: :get, on: :collection, as: "closed", defaults: { status: "closed" }
     match "open", to: "teachers#index", via: :get, on: :collection, as: "open", defaults: { status: "open" }
 
     resources :induction_periods, only: %i[edit update], path: "induction-periods"
-
     scope module: :teachers do
       resource :release_ect, only: %i[new create show], path: "release", controller: :record_released_induction
       resource :record_passed_outcome, only: %i[new create show], path: "record-passed-outcome", controller: :record_passed_induction
---
+++ spec/components/unclaimed_index/detailed_review_section_component_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe UnclaimedIndex::DetailedReviewSectionComponent, type: :component do
+  let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
+  let(:component) { described_class.new(appropriate_body:) }
+
+  before do
+    allow(component).to receive_messages(
+      number_of_claimable_ect_records: 5,
+      number_of_missing_qts_records: 3,
+      number_of_records_claimed_by_another_appropriate_body: 2
+    )
+    render_inline(component)
+  end
+
+  it "displays the claim section heading" do
+    expect(page).to have_css("h2", text: "Check records and claim an ECT")
+  end
+
+  it "displays the action required section heading" do
+    expect(page).to have_css("h2", text: "ECT records that may need action")
+  end
+
+  it "displays the number of claimable ECT records" do
+    expect(page).to have_css("h3", text: "5")
+    expect(page).to have_css(".govuk-body", text: "Check and claim ECT")
+  end
+
+  it "displays the number of missing QTS records" do
+    expect(page).to have_css("h3", text: "3")
+    expect(page).to have_css(".govuk-body", text: "No QTS award")
+  end
+
+  it "displays the number of records claimed by another appropriate body" do
+    expect(page).to have_css("h3", text: "2")
+    expect(page).to have_css(".govuk-body", text: "Currently claimed by another AB")
+  end
+end
