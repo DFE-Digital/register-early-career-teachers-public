8d609a3633e7c04c209925f0fb77cf988837ae43
Leonidas Apostolidis <leoapost@gmail.com>
2026-02-06 09:28:05 +0000

    Validate the partnership uniqueness when selecting delivery partner

--- app/wizards/admin/schools/add_partnership_wizard/select_delivery_partner_step.rb
+++ app/wizards/admin/schools/add_partnership_wizard/select_delivery_partner_step.rb
@@ -6,6 +6,7 @@ module Admin
 
         validates :delivery_partner_id, presence: { message: "Select a delivery partner" }
         validate :delivery_partner_available
+        validate :partnership_unique
 
         def self.permitted_params = %i[delivery_partner_id]
 
@@ -25,6 +26,23 @@ module Admin
           return if wizard.delivery_partners.where(id: delivery_partner_id).exists?
 
           errors.add(:delivery_partner_id, "Select a delivery partner")
+        end
+
+        def partnership_unique
+          return if delivery_partner_id.blank?
+
+          partnership = LeadProviderDeliveryPartnership.find_by(
+            active_lead_provider: wizard.selected_active_lead_provider,
+            delivery_partner_id:
+          )
+          return if partnership.blank?
+
+          return unless SchoolPartnership.exists?(
+            school: wizard.school,
+            lead_provider_delivery_partnership: partnership
+          )
+
+          errors.add(:base, "Partnership already exists for this school and contract period")
         end
       end
     end
