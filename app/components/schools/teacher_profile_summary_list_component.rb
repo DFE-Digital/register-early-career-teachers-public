f2cc88ebd1868885543071417539019b73686d11
Erica Porter <ericaporter@users.noreply.github.com>
2026-02-09 12:59:31 +0000

    Improve readability of teacher profile component

--- app/components/schools/teacher_profile_summary_list_component.rb
+++ app/components/schools/teacher_profile_summary_list_component.rb
@@ -18,24 +18,33 @@ module Schools
     end
 
     def withdrawn_or_deferred_tag
+      return unless withdrawn? || deferred?
+
       if withdrawn?
         govuk_tag(text: "Action required", colour: "red")
-      elsif deferred?
+      else
         govuk_tag(text: "Training paused", colour: "orange")
       end
     end
 
     def withdrawn_or_deferred_message_text
-      lead_provider_name = @ect.latest_started_lead_provider_name
-      subject = lead_provider_name.presence || "The lead provider"
-      verb = lead_provider_name.present? ? "have" : "has"
+      return unless withdrawn? || deferred?
+
+      subject, verb = lead_provider_subject_and_verb
       name = teacher_full_name(@ect.teacher)
 
       if withdrawn?
         "#{subject} #{verb} told us that #{name} is no longer training with them. Contact them if you think this is an error."
-      elsif deferred?
+      else
         "#{subject} #{verb} told us that #{name}'s training is paused. Contact them if you think this is an error."
       end
+    end
+
+    def lead_provider_subject_and_verb
+      lead_provider_name = @ect.latest_started_lead_provider_name
+      subject = lead_provider_name.presence || "The lead provider"
+      verb = lead_provider_name.present? ? "have" : "has"
+      [subject, verb]
     end
 
     def training_status
@@ -58,7 +67,6 @@ module Schools
       @ect.teacher.trs_induction_status == "Exempt"
     end
 
-    # TODO: check if these are used?
     def current_mentor = mentorship.current_mentor
 
     def mentorship = @mentorship ||= ECTAtSchoolPeriods::Mentorship.new(@ect)
