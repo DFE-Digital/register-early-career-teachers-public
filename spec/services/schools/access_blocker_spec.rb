95d630b1305e2f7d45c664efe76d3809b5608825
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-02-09 17:14:30 +0000

    [3010] Add test for happy path + proposed_to_open

--- spec/services/schools/access_blocker_spec.rb
+++ spec/services/schools/access_blocker_spec.rb
@@ -12,10 +12,17 @@ RSpec.describe Schools::AccessBlocker do
     end
 
     context "when the gias school is closed" do
-      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :state_school_type) }
+      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :state_school_type, status: :closed) }
       let(:school_urn) { gias_school.urn }
 
-      before { gias_school.update!(status: :closed) }
+      it "blocks access" do
+        expect(blocker).to be_blocked
+      end
+    end
+
+    context "when the gias school is proposed to open" do
+      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :state_school_type, status: :proposed_to_open) }
+      let(:school_urn) { gias_school.urn }
 
       it "blocks access" do
         expect(blocker).to be_blocked
@@ -23,7 +30,7 @@ RSpec.describe Schools::AccessBlocker do
     end
 
     context "when the school is independent, not section 41 approved, and has no ongoing training" do
-      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :independent_school_type, :not_section_41) }
+      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :independent_school_type, :not_section_41, status: :open) }
       let(:school_urn) { gias_school.urn }
 
       it "blocks access" do
@@ -32,13 +39,22 @@ RSpec.describe Schools::AccessBlocker do
     end
 
     context "when the school is independent, not section 41 approved, and has ongoing training" do
-      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :independent_school_type, :not_section_41) }
+      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :independent_school_type, :not_section_41, status: :open) }
       let(:school) { gias_school.school }
       let(:school_urn) { school.urn }
 
       before do
         FactoryBot.create(:training_period, :ongoing, ect_at_school_period: FactoryBot.create(:ect_at_school_period, school:))
       end
+
+      it "does not block access" do
+        expect(blocker).not_to be_blocked
+      end
+    end
+
+    context "when the school is independent, section 41 approved, and has no training periods yet" do
+      let(:gias_school) { FactoryBot.create(:gias_school, :with_school, :independent_school_type, :section_41, status: :open) }
+      let(:school_urn) { gias_school.urn }
 
       it "does not block access" do
         expect(blocker).not_to be_blocked
