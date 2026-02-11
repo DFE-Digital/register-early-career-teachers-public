00deff713b1354838758cc7fdb68677ab6cc5697
Lorenzo Tello <ltello8a@gmail.com>
2026-02-11 13:44:47 +0000

    store mentorships migrated during the migration

--- app/migration/ecf2_teacher_history.rb
+++ app/migration/ecf2_teacher_history.rb
@@ -11,6 +11,7 @@ class ECF2TeacherHistory
               :mentor_at_school_periods,
               :ecf2_ect_combination_summaries,
               :ecf2_mentor_combination_summaries,
+              :ecf2_mentorship_summaries,
               :training_periods,
               :mentorship_periods
 
@@ -20,12 +21,13 @@ class ECF2TeacherHistory
     @mentor_at_school_periods = mentor_at_school_periods
     @ecf2_ect_combination_summaries = []
     @ecf2_mentor_combination_summaries = []
+    @ecf2_mentorship_summaries = []
   end
 
   def save_all_ect_data!
     find_or_create_teacher!.tap do |teacher|
       save_ect_periods!(teacher)
-      save_ect_combination_summaries!
+      save_ect_combination_and_mentorship_summaries!
     end
   end
 
@@ -67,6 +69,13 @@ class ECF2TeacherHistory
                                              .map(&:combination)
                                              .compact
                                              .map(&:summary)
+  end
+
+  def ecf1_mentorship_summaries
+    @ecf1_mentorship_summaries ||= ect_at_school_periods
+                                     .flat_map(&:mentorship_periods)
+                                     .compact
+                                     .map(&:summary)
   end
 
 private
@@ -115,6 +124,7 @@ private
 
     record_failed_combinations(at_school_period: model, message:) if model.respond_to?(:training_periods)
     record_failed_combination(combination: model.combination, message:) if model.respond_to?(:combination)
+    record_failed_mentorship(mentorship: model, message:) if model.is_a?(ECF2TeacherHistory::MentorshipPeriod)
     model_identifier = model.is_a?(Symbol) ? model : model.class.name.demodulize.underscore
 
     ::TeacherMigrationFailure.create!(
@@ -136,21 +146,31 @@ private
     DataMigrationFailedCombination.create!(**combination.to_h, failure_message: message)
   end
 
+  def record_failed_mentorship(mentorship:, message:)
+    DataMigrationFailedMentorship.create!(
+      ect_participant_profile_id: mentorship.api_ect_training_record_id,
+      mentor_participant_profile_id: mentorship.api_mentor_training_record_id,
+      started_on: mentorship.started_on,
+      finished_on: mentorship.finished_on,
+      ecf_start_induction_record_id: mentorship.ecf_start_induction_record_id,
+      ecf_end_induction_record_id: mentorship.ecf_end_induction_record_id,
+      failure_message: message
+    )
+  end
+
   def school_partnership_for(training_period)
     return { school_partnership: nil } unless training_period.training_programme.to_s == "provider_led"
 
     training_period.school_partnership
   end
 
-  def data_migration_teacher_combinations
-    @data_migration_teacher_combinations ||= DataMigrationTeacherCombination.find_or_initialize_by(api_id: teacher.api_id)
-  end
-
-  def save_ect_combination_summaries!
+  def save_ect_combination_and_mentorship_summaries!
     data_migration_teacher_combinations.update!(
       ecf1_ect_profile_id: teacher.api_ect_training_record_id,
       ecf1_ect_combinations: ecf1_ect_combination_summaries,
-      ecf2_ect_combinations: ecf2_ect_combination_summaries
+      ecf2_ect_combinations: ecf2_ect_combination_summaries,
+      ecf1_mentorships: ecf1_mentorship_summaries,
+      ecf2_mentorships: ecf2_mentorship_summaries
     )
   end
 
@@ -173,18 +193,17 @@ private
           with_failure_recording(teacher: found_teacher,
                                  model: training_period,
                                  migration_item_id: training_period.ecf_start_induction_record_id) do
-            ::TrainingPeriod.create!(
-              ect_at_school_period: created_ect_at_school_period,
-              **school_partnership_for(training_period),
-              **training_period
-            )
+            ::TrainingPeriod.create!(ect_at_school_period: created_ect_at_school_period,
+                                     **school_partnership_for(training_period),
+                                     **training_period)
             ecf2_ect_combination_summaries << training_period.combination.summary
           end
         end
 
         ect_at_school_period.mentorship_periods.each do |mentorship_period|
-          with_failure_recording(teacher: found_teacher, model: :mentorship_period, migration_item_id: mentorship_period.ecf_start_induction_record_id) do
-            ::MentorshipPeriod.create!(mentee: created_ect_at_school_period, **mentorship_period)
+          with_failure_recording(teacher: found_teacher, model: mentorship_period, migration_item_id: mentorship_period.ecf_start_induction_record_id) do
+            ::MentorshipPeriod.create!(mentee: created_ect_at_school_period, **mentorship_period.to_h)
+            ecf2_mentorship_summaries << mentorship_period.summary
           end
         end
       end
