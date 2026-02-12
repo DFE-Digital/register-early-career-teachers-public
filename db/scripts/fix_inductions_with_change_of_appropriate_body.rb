59745d863191becb31d766d47904d3f576000cbb
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-02-12 18:59:11 +0000

    Snags

--- db/scripts/fix_inductions_with_change_of_appropriate_body.rb
+++ db/scripts/fix_inductions_with_change_of_appropriate_body.rb
@@ -1,5 +1,5 @@
-west_lakes = AppropriateBody.find_by(name: "West Lakes Academy")
-one_cumbria = AppropriateBody.find_by(name: "One Cumbria Teaching School Hub")
+west_lakes = AppropriateBodyPeriod.find_by(name: "West Lakes Academy")
+one_cumbria = AppropriateBodyPeriod.find_by(name: "One Cumbria Teaching School Hub")
 cut_off_date = Date.new(2024, 9, 1)
 
 InductionPeriods::FullTransferInductionPeriods.new(from: west_lakes, to: one_cumbria, on: cut_off_date).call
