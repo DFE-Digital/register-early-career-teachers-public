2979c2013caf7b037bcbd298d5abdbf7282837c0
Peter Yates <peter.yates@graphia.co.uk>
2026-02-15 13:46:20 +0000

    Use factory bot associations instead of create
    The blanket creates are one of the reaosns we're generating too many
    records during test setup

--- spec/factories/migration/cpd_lead_provider_factory.rb
+++ spec/factories/migration/cpd_lead_provider_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :migration_cpd_lead_provider, class: "Migration::CpdLeadProvider" do
-    lead_provider { create(:migration_lead_provider) }
+    association :lead_provider, factory: :migration_lead_provider
 
     sequence(:name) { |n| "CPD Lead Provider #{n}" }
   end
