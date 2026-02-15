a55241dae21fca9e945a5995db1f7c6801591fd6
Peter Yates <peter.yates@graphia.co.uk>
2026-02-15 13:26:01 +0000

    Refine the withdrawal data class

--- app/migration/teacher_history_converter/withdrawal_data.rb
+++ app/migration/teacher_history_converter/withdrawal_data.rb
@@ -1,13 +1,22 @@
 class TeacherHistoryConverter::WithdrawalData
-  attr_reader :training_status, :states
+  attr_reader :training_status, :states, :lead_provider_id
 
-  def initialize(training_status:, states:)
+  def initialize(training_status:, states:, lead_provider_id:)
     @training_status = training_status
     @states = states.sort_by(&:created_at).reverse
+    @lead_provider_id = lead_provider_id
+  end
+
+  def cpd_lead_provider_id
+    return if lead_provider_id.nil?
+
+    Mappers::LeadProviderMapper.new(index_by: :id).get(lead_provider_id)&.cpd_lead_provider_id
   end
 
   def withdrawal_data
+    return {} if cpd_lead_provider_id.blank?
     return {} unless training_status == "withdrawn"
+    return {} unless states.any?
 
     # here, we want to find the relevant state from the ECT so the withdrawal reason/timestamp
     # are populated, but there are multiple approaches
@@ -22,15 +31,12 @@ class TeacherHistoryConverter::WithdrawalData
     # In ECF1 we're also finding the most recent 'withdrawn' profile state
     #
     # https://github.com/DFE-Digital/early-careers-framework/blob/main/app/serializers/api/v3/ecf/participant_serializer.rb#L37-L41
-    #
-    # FIXME: perhaps we should be linking to the LP here too? If we do, hardcode the list rather than do a DB lookup...
-    if (matching_state = states.find { it.state == "withdrawn" })
-      {
-        withdrawal_reason: matching_state.reason.to_s,
-        withdrawn_at: matching_state.created_at
-      }
-    else
-      {}
-    end
+
+    return {} unless (matching_state = states.find { it.state == "withdrawn" && it.cpd_lead_provider_id == cpd_lead_provider_id })
+
+    {
+      withdrawal_reason: matching_state.reason.to_s,
+      withdrawn_at: matching_state.created_at
+    }
   end
 end
