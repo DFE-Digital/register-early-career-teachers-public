2750fb6c0b7590a79885f7b036af9c6372fdf44e
Peter Yates <peter.yates@graphia.co.uk>
2026-02-17 16:56:03 +0000

    Add new step to the cleaner and rework the specs
    Now instead of testing the cleaning is actually cleaning, just make sure
    we're running each of the steps.

--- spec/migration/teacher_history_converter/cleaner_spec.rb
+++ spec/migration/teacher_history_converter/cleaner_spec.rb
@@ -1,36 +1,20 @@
 describe TeacherHistoryConverter::Cleaner do
-  let(:induction_records) do
+  let(:induction_records) { FactoryBot.build_list(:ecf1_teacher_history_induction_record_row, 5) }
+  let(:cleaner_steps) do
     [
-      FactoryBot.build(:ecf1_teacher_history_induction_record_row, start_date: Date.new(2024, 2, 2), end_date: Date.new(2015, 1, 1)),
-      FactoryBot.build(:ecf1_teacher_history_induction_record_row, start_date: Date.new(2024, 4, 4), end_date: Date.new(2024, 3, 3)),
-      FactoryBot.build(:ecf1_teacher_history_induction_record_row, start_date: Date.new(2024, 5, 5), end_date: Date.new(2024, 5, 5))
+      TeacherHistoryConverter::Cleaner::BritishSchoolsOverseas,
+      TeacherHistoryConverter::Cleaner::SchoolFundedFip,
+      TeacherHistoryConverter::Cleaner::IndependentNonSection41,
+      TeacherHistoryConverter::Cleaner::ServiceStartDate,
+      TeacherHistoryConverter::Cleaner::CorruptedDates,
+      TeacherHistoryConverter::Cleaner::ZeroDay,
+      TeacherHistoryConverter::Cleaner::FixFirstStartDate
     ]
   end
 
   it "calls both ServiceStartDate and CorruptedDates cleaners" do
-    allow(TeacherHistoryConverter::Cleaner::ServiceStartDate).to receive(:new).and_call_original
-    allow(TeacherHistoryConverter::Cleaner::CorruptedDates).to receive(:new).and_call_original
-    allow(TeacherHistoryConverter::Cleaner::ZeroDay).to receive(:new).and_call_original
-
+    cleaner_steps.each { allow(TeacherHistoryConverter::Cleaner::ServiceStartDate).to receive(:new).and_call_original }
     TeacherHistoryConverter::Cleaner.new(induction_records).induction_records
-
-    expect(TeacherHistoryConverter::Cleaner::ServiceStartDate).to have_received(:new).once
-    expect(TeacherHistoryConverter::Cleaner::CorruptedDates).to have_received(:new).once
-    expect(TeacherHistoryConverter::Cleaner::ZeroDay).to have_received(:new).once
-  end
-
-  it "returns a 'cleansed' set of induction records" do
-    cleansed_records = TeacherHistoryConverter::Cleaner.new(induction_records).induction_records
-
-    # changes first IR's end date to start date of next IR (TeacherHistoryConverter::Cleaner::ServiceStartDate)
-    expect(cleansed_records[0].end_date).to eql(Date.new(2024, 4, 4))
-
-    # converts second IR to a stub (TeacherHistoryConverter::Cleaner::CorruptedDates)
-    expect(cleansed_records[1].start_date).to eql(Date.new(2024, 3, 3))
-    expect(cleansed_records[1].end_date).to eql(Date.new(2024, 3, 4))
-
-    # converts third IR from 0 to 1 days (TeacherHistoryConverter::Cleaner::ZeroDay)
-    expect(cleansed_records[2].start_date).to eql(Date.new(2024, 5, 5))
-    expect(cleansed_records[2].end_date).to eql(Date.new(2024, 5, 6))
+    cleaner_steps.each { expect(TeacherHistoryConverter::Cleaner::ServiceStartDate).to have_received(:new).once }
   end
 end
