5b2a86e8f7526c8431852e8098b5ca2f01f82b2a
Peter Yates <peter.yates@graphia.co.uk>
2026-02-18 10:33:08 +0000

    Make date fixer more robust when no induction records

--- app/migration/teacher_history_converter/cleaner/fix_first_start_date.rb
+++ app/migration/teacher_history_converter/cleaner/fix_first_start_date.rb
@@ -5,9 +5,9 @@ class TeacherHistoryConverter::Cleaner::FixFirstStartDate
 
   def induction_records
     @raw_induction_records.dup.tap do |fixed_induction_records|
-      first_ir = fixed_induction_records[0]
-
-      first_ir.start_date = [first_ir.start_date, first_ir.created_at.to_date].min
+      if (first_ir = fixed_induction_records[0])
+        first_ir.start_date = [first_ir.start_date, first_ir.created_at.to_date].min
+      end
     end
   end
 end
