d08f4bf3c772f49f4151830c85c1edb8737cc6f8
Ross Oliver <ross.oliver36@gmail.com>
2026-02-18 15:01:53 +0000

    Add `uplift_fees_enabled` to `ContractPeriod`
    It turns out that the uplift fee logic is contract period dependent. The
    contract periods <= 2024 allow uplift fees, however contract periods >= 2025
    should not accept uplift fees.
    
    Add `uplift_fees_enabled` to `ContractPeriod` so that we can apply relevant
    validation and business logic.

--- app/services/api_seed_data/contract_periods.rb
+++ app/services/api_seed_data/contract_periods.rb
@@ -6,24 +6,29 @@ module APISeedData
         enabled: false,
         payments_frozen_at: Time.zone.local(2024, 6, 18),
         mentor_funding_enabled: false,
-        detailed_evidence_types_enabled: false },
+        detailed_evidence_types_enabled: false,
+        uplift_fees_enabled: true },
       { year: 2022,
         enabled: false,
         payments_frozen_at: Time.zone.local(2025, 6, 16),
         mentor_funding_enabled: false,
-        detailed_evidence_types_enabled: false },
+        detailed_evidence_types_enabled: false,
+        uplift_fees_enabled: true },
       { year: 2023,
         enabled: true,
         mentor_funding_enabled: false,
-        detailed_evidence_types_enabled: false },
+        detailed_evidence_types_enabled: false,
+        uplift_fees_enabled: true },
       { year: 2024,
         enabled: true,
         mentor_funding_enabled: false,
-        detailed_evidence_types_enabled: false },
+        detailed_evidence_types_enabled: false,
+        uplift_fees_enabled: true },
       { year: 2025,
         enabled: true,
         mentor_funding_enabled: true,
-        detailed_evidence_types_enabled: true },
+        detailed_evidence_types_enabled: true,
+        uplift_fees_enabled: false },
     ].freeze
 
     def plant
@@ -37,7 +42,8 @@ module APISeedData
                           enabled: data[:enabled],
                           payments_frozen_at: data[:payments_frozen_at],
                           mentor_funding_enabled: data[:mentor_funding_enabled],
-                          detailed_evidence_types_enabled: data[:detailed_evidence_types_enabled]).tap do |contract_period|
+                          detailed_evidence_types_enabled: data[:detailed_evidence_types_enabled],
+                          uplift_fees_enabled: data[:uplift_fees_enabled]).tap do |contract_period|
           log_seed_info("#{contract_period.year} (running from #{contract_period.started_on} until #{contract_period.finished_on})")
         end
       end
