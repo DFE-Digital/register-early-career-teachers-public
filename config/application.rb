7fab080bf93805e30d6ec92c5a6f23ad72a866f0
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-02-19 19:08:07 +0000

    [2238] Refactor to use env agnostic configuration

--- config/application.rb
+++ config/application.rb
@@ -52,6 +52,7 @@ module RegisterEarlyCareerTeachers
     config.enable_persona_avatars = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_PERSONA_AVATARS", true))
     config.enable_migration_testing = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_MIGRATION_TESTING", false))
     config.enable_schools_interface = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_SCHOOLS_INTERFACE", false))
+    config.enable_otp_school_sign_in = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_OTP_SCHOOL_SIGN_IN", false))
     config.enable_teaching_school_hubs = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_HUBS", false))
     config.enable_incident_banner = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_INCIDENT_BANNER", false))
     config.enable_appropriate_body_records_to_review = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ENABLE_APPROPRIATE_BODY_RECORDS_TO_REVIEW", false))
