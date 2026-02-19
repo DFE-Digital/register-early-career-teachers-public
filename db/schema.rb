e4191ecca0a94269f5790ef45469c4475cdfb7a8
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-02-19 20:05:30 +0000

    [2238] migration `AddOtpSchoolUrnToUsers`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_17_082651) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_19_194858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
@@ -1055,6 +1055,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_17_082651) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "role", default: "admin", null: false, enum_type: "dfe_role_type"
+    t.string "otp_school_urn"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
