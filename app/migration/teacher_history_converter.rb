065a78648aec93737bccd4cd29553668c0964d56
Tony Headford <tony@binarycircus.com>
2026-02-23 21:20:25 +0000

    adding cleaner

--- app/migration/teacher_history_converter.rb
+++ app/migration/teacher_history_converter.rb
@@ -61,7 +61,7 @@ private
     profile_id = ecf1_teacher_history.ect.participant_profile_id
     raw_induction_records = ecf1_teacher_history.ect.induction_records
     induction_completion_date = ecf1_teacher_history.ect.induction_completion_date
-    induction_records = TeacherHistoryConverter::Cleaner.new(raw_induction_records, induction_completion_date:).induction_records
+    induction_records = TeacherHistoryConverter::Cleaner.new(raw_induction_records, induction_completion_date:, participant_type: :ect).induction_records
     mentor_at_school_periods = ecf1_teacher_history.ect.mentor_at_school_periods
     states = ecf1_teacher_history.ect.states
 
@@ -86,7 +86,7 @@ private
     trn = ecf1_teacher_history.user.trn
     profile_id = ecf1_teacher_history.mentor.participant_profile_id
     raw_induction_records = ecf1_teacher_history.mentor.induction_records
-    induction_records = TeacherHistoryConverter::Cleaner.new(raw_induction_records).induction_records
+    induction_records = TeacherHistoryConverter::Cleaner.new(raw_induction_records, participant_type: :mentor).induction_records
     states = ecf1_teacher_history.mentor.states
     exclude_training_periods = exclude_ero_training_periods?
 
