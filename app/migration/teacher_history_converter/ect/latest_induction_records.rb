44f09cbcb21899ef2b04dc68cf58135ec5573d0e
Tony Headford <tony@binarycircus.com>
2026-02-23 14:14:53 +0000

    fix specs

--- app/migration/teacher_history_converter/ect/latest_induction_records.rb
+++ app/migration/teacher_history_converter/ect/latest_induction_records.rb
@@ -94,7 +94,9 @@ private
     training_provider_info = induction_record.training_provider_info
     school = induction_record.school
 
-    api_transfer_updated_at = transfers[training_provider_info.lead_provider_info.ecf1_id] if training_provider_info.present?
+    if training_provider_info&.lead_provider_info&.present?
+      api_transfer_updated_at = transfers[training_provider_info.lead_provider_info.ecf1_id]
+    end
 
     training_attrs = {
       started_on: induction_record.start_date,
