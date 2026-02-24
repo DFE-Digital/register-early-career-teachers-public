5a17f25e01c7f5b9aa8c5f61387e045d4efc1457
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-02-24 16:15:38 +0000

    [2238] Show OTP school URN on admin user details page
    If the `otp_school_sign_in_enabled` is enabled

--- spec/views/admin/users/show.html.erb_spec.rb
+++ spec/views/admin/users/show.html.erb_spec.rb
@@ -1,8 +1,10 @@
 describe "admin/users/show.html.erb" do
   let(:role) { :admin }
   let(:user) { FactoryBot.create(:user, role) }
+  let(:otp_school_sign_in_enabled) { false }
 
   before do
+    allow(Rails.application.config).to receive(:enable_otp_school_sign_in).and_return(otp_school_sign_in_enabled)
     assign(:user, user)
     render
   end
@@ -35,6 +37,10 @@ describe "admin/users/show.html.erb" do
     expect(rendered).to have_css("dd", text: "Admin")
   end
 
+  it "does not display the otp school URN row when otp school sign-in flag is disabled" do
+    expect(rendered).not_to have_css("dt", text: "School URN for OTP sign-in")
+  end
+
   context "when the user is a user manager user" do
     let(:role) { :user_manager }
 
@@ -50,6 +56,28 @@ describe "admin/users/show.html.erb" do
     it "displays the finance role" do
       expect(rendered).to have_css("dt", text: "Role")
       expect(rendered).to have_css("dd", text: "Finance")
+    end
+  end
+
+  context "when otp school sign-in flag is enabled" do
+    let(:otp_school_sign_in_enabled) { true }
+
+    context "when the user has an otp school URN" do
+      let(:user) { FactoryBot.create(:user, role, otp_school_urn: 123_456) }
+
+      it "displays the otp school URN row and value" do
+        expect(rendered).to have_css("dt", text: "School URN for OTP sign-in")
+        expect(rendered).to have_css("dd", text: user.otp_school_urn.to_s)
+      end
+    end
+
+    context "when the user has no otp school URN" do
+      let(:user) { FactoryBot.create(:user, role, otp_school_urn: nil) }
+
+      it "displays 'Not set' for the otp school URN row" do
+        expect(rendered).to have_css("dt", text: "School URN for OTP sign-in")
+        expect(rendered).to have_css("dd", text: "Not set")
+      end
     end
   end
 end
