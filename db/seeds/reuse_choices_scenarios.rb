affaa6127c2a79ca115dd5c171cda2fc2eae3924
Erica Porter <ericaporter@users.noreply.github.com>
2026-02-26 13:24:06 +0000

    Adding reuse seeds for review and staging envs

---
+++ db/seeds/reuse_choices_scenarios.rb
@@ -0,0 +1,11 @@
+if Rails.env.review? || Rails.env.staging?
+  require Rails.root.join("db/seeds/support/seeds/reuse_choices")
+
+  print_seed_info(
+    "Seeding reuse choices scenario schools (staging only)",
+    colour: :yellow,
+    blank_lines_before: 1
+  )
+
+  Seeds::ReuseChoices.new(contract_period_year: 2025).call
+end
