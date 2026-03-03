e3a009605d86d3225fdb6c564c41790f186a856a
thenapking <tom.giles@gmail.com>
2026-03-03 15:19:03 +0000

    Factory

--- spec/factories/schedule_factory.rb
+++ spec/factories/schedule_factory.rb
@@ -10,5 +10,24 @@ FactoryBot.define do
     trait :replacement_schedule do
       identifier { "ecf-replacement-september" }
     end
+
+    trait :with_milestones do
+      after(:build) do |schedule|
+        year = schedule.contract_period.year
+
+        milestone_data = [
+          { declaration_type: "started",    start_date: Date.new(year, 6, 1),     milestone_date: Date.new(year, 12, 31) },
+          { declaration_type: "retained-1", start_date: Date.new(year + 1, 1, 1), milestone_date: Date.new(year + 1, 3, 31) },
+          { declaration_type: "retained-2", start_date: Date.new(year + 1, 4, 1), milestone_date: Date.new(year + 1, 7, 31) },
+          { declaration_type: "retained-3", start_date: Date.new(year + 1, 8, 1), milestone_date: Date.new(year + 1, 12, 31) },
+          { declaration_type: "retained-4", start_date: Date.new(year + 2, 1, 1), milestone_date: Date.new(year + 2, 3, 31) },
+          { declaration_type: "completed",  start_date: Date.new(year + 2, 4, 1), milestone_date: Date.new(year + 2, 7, 31) }
+        ]
+
+        milestone_data.each do |attrs|
+          build(:milestone, attrs.merge(schedule: schedule))
+        end
+      end
+    end
   end
 end
