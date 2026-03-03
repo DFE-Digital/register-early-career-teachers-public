5d2324106aa7503edc8914729f080422fa128f53
thenapking <tom.giles@gmail.com>
2026-03-03 18:20:21 +0000

    Temp override contract periods

---
+++ db/seeds/zzz_override_contracts.rb
@@ -0,0 +1,6 @@
+# TODO: REMOVE THIS
+cp_2025 = ContractPeriod.find_by!(year: 2025)
+cp_2026 = ContractPeriod.find_by!(year: 2026)
+
+cp_2025.update!(finished_on: Date.new(2026, 3, 4))
+cp_2026.update!(started_on: Date.new(2026, 3, 5))
