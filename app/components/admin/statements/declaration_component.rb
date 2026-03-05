91b909c1b8850f73b0b620eba407e3e832e30120
thenapking <tom.giles@gmail.com>
2026-03-05 16:50:24 +0000

    Basic mocking for Flatrate works

--- app/components/admin/statements/declaration_component.rb
+++ app/components/admin/statements/declaration_component.rb
@@ -2,13 +2,24 @@ module Admin
   module Statements
     class DeclarationComponent < ApplicationComponent
       attr_accessor :statement
+
       delegate :contract, to: :statement
 
       def initialize(statement:)
         @statement = statement
       end
 
-      private
+      def head
+        ["", "Total"]
+      end
+
+      def rows
+        d = mapped_declarations || []
+        d << ["Voided", voided.to_s]
+        d
+      end
+
+    private
 
       def calculators
         PaymentCalculator::Resolver.new(statement:, contract:).calculators
@@ -18,11 +29,21 @@ module Admin
         @declaration_type_outputs ||= calculators.first.outputs.declaration_type_outputs
       end
 
+      def payments_count(declaration_type_output)
+        declaration_type_output.billable_count - declaration_type_output.refundable_count
+      end
+
       def payment_type(declaration_type_output)
         declaration_type_output.declaration_type.humanize
+      end
+
+      def mapped_declarations
+        @mapped_declarations ||= declaration_type_outputs.map { |dto| [payment_type(dto), payments_count(dto).to_s] }
+      end
+
+      def voided
+        @voided ||= statement.payment_declarations.where(payment_status: "voided").count
       end
     end
   end
 end
-
-
