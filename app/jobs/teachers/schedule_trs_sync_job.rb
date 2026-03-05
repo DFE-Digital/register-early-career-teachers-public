2effcdfd7ba9d68043af04d8bb1fbc5157bfcf7f
Craig Davidson <craigmdavidson@gmail.com>
2026-03-05 14:02:46 +0000

    Increase Teacher TRS sync throughput
    By increasing the job frequency from every hour to every minute,
    but decreasing the batch size from 1200 to 50.
    Then increasing the frequency of the individual refreshes from i.seconds * 3, to i.seconds (i.e. 1 per second)
    We can expect to increase the overall throughput from 1200 to 3000,
    while having a greater protection against hitting the rate limits (500 per minute).

--- app/jobs/teachers/schedule_trs_sync_job.rb
+++ app/jobs/teachers/schedule_trs_sync_job.rb
@@ -2,13 +2,13 @@ module Teachers
   class ScheduleTRSSyncJob < ApplicationJob
     queue_as :default
 
-    BATCH_SIZE = 1200
+    BATCH_SIZE = 50
 
     def perform
       teachers = Teacher.found_in_trs.active_in_trs.ordered_by_trs_data_last_refreshed_at_nulls_first.limit(BATCH_SIZE)
 
       teachers.each_with_index do |teacher, i|
-        Teachers::SyncTeacherWithTRSJob.set(wait: (i * 3).seconds).perform_later(teacher:)
+        Teachers::SyncTeacherWithTRSJob.set(wait: i.seconds).perform_later(teacher:)
       end
     end
   end
