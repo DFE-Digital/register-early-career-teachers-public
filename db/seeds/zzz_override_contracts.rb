2267a4ae4ab2f1793baa0b5092e4ee661116d19a
thenapking <tom.giles@gmail.com>
2026-03-05 13:06:21 +0000

    Today

--- db/seeds/zzz_override_contracts.rb
+++ db/seeds/zzz_override_contracts.rb
@@ -2,5 +2,5 @@
 cp_2025 = ContractPeriod.find_by!(year: 2025)
 cp_2026 = ContractPeriod.find_by!(year: 2026)
 
-cp_2025.update!(finished_on: Date.new(2026, 3, 4))
-cp_2026.update!(started_on: Date.new(2026, 3, 5))
+cp_2025.update!(finished_on: Date.new(2026, 3, 5))
+cp_2026.update!(started_on: Date.new(2026, 3, 6))
