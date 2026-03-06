ef56049136fff8f3a68cb891b470e38bf7a30fd5
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-03-06 16:36:36 +0000

    [3198] Make API guidance sidebar dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,7 +48,8 @@ module ApplicationHelper
 
     return unless parsed_yaml
 
-    page_data(**parsed_yaml)
+    page_data_keys = method(:page_data).parameters.map(&:last)
+    page_data(**parsed_yaml.slice(*page_data_keys))
   end
 
   def support_mailto_link(text = Rails.application.config.support_email_address)
