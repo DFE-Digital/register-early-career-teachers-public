a4369fd7e87c00a46657b7e66ed43355325fa78c
Tony Headford <tony@binarycircus.com>
2026-03-09 11:07:18 +0000

    selection criteria for premium

--- app/migration/teacher_history_converter/migration_strategy.rb
+++ app/migration/teacher_history_converter/migration_strategy.rb
@@ -6,7 +6,7 @@ class TeacherHistoryConverter::MigrationStrategy
   end
 
   def strategy
-    if mentor_induction_records_count <= 1 && ect_induction_records_count <= 1
+    if teacher_history_meets_premium_criteria?
       :all_induction_records
     else
       :latest_induction_records
@@ -14,6 +14,46 @@ class TeacherHistoryConverter::MigrationStrategy
   end
 
 private
+
+  def teacher_history_meets_premium_criteria?
+    below_threshold_for_induction_records &&
+      has_not_been_withdrawn_or_deferred? &&
+      is_not_induction_completed? &&
+      dates_are_in_the_right_order_for?(ecf1_teacher_history.ect&.induction_records) &&
+      dates_are_in_the_right_order_for?(ecf1_teacher_history.mentor&.induction_records)
+  end
+
+  def dates_are_in_the_right_order_for?(induction_records)
+    return true if induction_records.blank?
+
+    previous_induction_record = nil
+    induction_records.sort_by(&:created_at).all? do |induction_record|
+      result = if induction_record.dates_in_order?
+                 if previous_induction_record.present?
+                   previous_induction_record.end_date.present? && previous_induction_record.end_date < induction_record.start_date
+                 else
+                   true
+                 end
+               else
+                 false
+               end
+      previous_induction_record = induction_record
+      result
+    end
+  end
+
+  def is_not_induction_completed?
+    ecf1_teacher_history.ect&.induction_completion_date.blank?
+  end
+
+  def has_not_been_withdrawn_or_deferred?
+    ecf1_teacher_history.ect&.induction_records&.none? { |ir| ir.training_status.in? %w[withdrawn deferred] } &&
+      ecf1_teacher_history.mentor&.induction_records&.none? { |ir| ir.training_status.in? %w[withdrawn deferred] }
+  end
+
+  def below_threshold_for_induction_records
+    mentor_induction_records_count <= 2 && ect_induction_records_count <= 2
+  end
 
   def ect_induction_records_count
     ecf1_teacher_history.ect&.induction_records&.count || 0
