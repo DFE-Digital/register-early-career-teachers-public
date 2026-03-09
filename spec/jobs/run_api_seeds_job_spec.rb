d393a00d6c6c1eb635112e6c9e86225ab6732551
Leandro Alemão <leandro.costantini@gmail.com>
2026-03-09 10:13:18 +0000

    [3184] Prevent api seeds job from running in production

--- spec/jobs/run_api_seeds_job_spec.rb
+++ spec/jobs/run_api_seeds_job_spec.rb
@@ -1,10 +1,11 @@
 RSpec.describe RunAPISeedsJob, type: :job do
   describe "#perform" do
     let(:api_seed_task) { instance_double(Rake::Task, invoke: nil) }
+    let(:environment) { "review" }
 
     before do
       allow(Rails.application).to receive(:load_tasks)
-
+      allow(Rails).to receive(:env) { environment.inquiry }
       allow(Rake::Task).to receive(:[]).with("api_seed_data:generate").and_return(api_seed_task)
     end
 
@@ -18,6 +19,16 @@ RSpec.describe RunAPISeedsJob, type: :job do
       described_class.new.perform
 
       expect(api_seed_task).to have_received(:invoke)
+    end
+
+    context "when in the production environment" do
+      let(:environment) { "production" }
+
+      it "does not invoke the `api_seed_data:generate` rake task" do
+        described_class.new.perform
+
+        expect(api_seed_task).not_to have_received(:invoke)
+      end
     end
   end
 
