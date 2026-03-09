e5aa6e2d17f2ba26fe9da8d38ebdd7b53a550b13
Leandro Alemão <leandro.costantini@gmail.com>
2026-03-09 10:13:18 +0000

    [3184] Prevent api seeds job from running in production

--- app/jobs/run_api_seeds_job.rb
+++ app/jobs/run_api_seeds_job.rb
@@ -2,6 +2,8 @@ class RunAPISeedsJob < ApplicationJob
   queue_as :api_seeds
 
   def perform
+    return if Rails.env.production?
+
     Rails.application.load_tasks
 
     Rake::Task["api_seed_data:generate"].invoke
