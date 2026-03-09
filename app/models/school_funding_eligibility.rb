6b54373f371af81b8ccbebf3922d08f41b83e0ba
Leandro Alemão <leandro.costantini@gmail.com>
2026-03-09 18:07:48 +0000

    [3428] Refresh school metadata

--- app/models/school_funding_eligibility.rb
+++ app/models/school_funding_eligibility.rb
@@ -1,4 +1,6 @@
 class SchoolFundingEligibility < ApplicationRecord
+  include DeclarativeUpdates
+
   belongs_to :school, foreign_key: :school_urn, primary_key: :urn
   belongs_to :contract_period, foreign_key: :contract_period_year, primary_key: :year
 
@@ -6,4 +8,6 @@ class SchoolFundingEligibility < ApplicationRecord
   validates :contract_period, presence: true
   validates :pupil_premium_uplift, inclusion: { in: [true, false] }
   validates :sparsity_uplift, inclusion: { in: [true, false] }
+
+  refresh_metadata -> { school }, on_event: %i[create destroy update]
 end
