4db2f5a0aa708d9d7e012621d25ab8162a02b44f
Peter Yates <peter.yates@graphia.co.uk>
2026-03-09 12:11:41 +0000

    Add ECT premium->econonmy fallback logic

--- spec/migration/migrators/ect_spec.rb
+++ spec/migration/migrators/ect_spec.rb
@@ -31,6 +31,38 @@ RSpec.describe Migrators::ECT do
   end
 
   describe "#migrate_one!" do
+    let(:fake_ecf2_teacher_history) { double(ECF2TeacherHistory, save_all_ect_data!: true, success?: false) }
+    let(:fake_migration_strategy) { double(TeacherHistoryConverter::MigrationStrategy, strategy: :all_induction_records) }
+    let(:fake_converter) do
+      double(
+        TeacherHistoryConverter,
+        convert_to_ecf2!: fake_ecf2_teacher_history,
+        migration_mode: :all_induction_records,
+        set_migration_mode_to_latest_induction_records!: true
+      )
+    end
+
+    before do
+      allow(TeacherHistoryConverter::MigrationStrategy).to receive(:new).and_return(fake_migration_strategy)
+      allow(TeacherHistoryConverter).to receive(:new).and_return(fake_converter)
+    end
+
+    it "falls back to latest_induction_records (economy) if all_induction_records (premium) mode fails" do
+      teacher_profile = FactoryBot.create(:migration_teacher_profile)
+
+      participant_identity = FactoryBot.create(:migration_participant_identity, user: teacher_profile.user)
+      FactoryBot.create(:migration_participant_profile, :ect, teacher_profile:, user: teacher_profile.user, participant_identity:)
+
+      migrator = Migrators::ECT.new
+      migrator.migrate_one!(teacher_profile)
+
+      expect(fake_converter).to have_received(:convert_to_ecf2!).twice
+      expect(fake_converter).to have_received(:set_migration_mode_to_latest_induction_records!).once
+      expect(fake_ecf2_teacher_history).to have_received(:save_all_ect_data!).twice
+    end
+  end
+
+  describe "#migrate_one!" do
     subject(:migrator) { described_class.new }
 
     describe "mentorship period creation", skip: "Add back once we re-implement mentorship migration" do
