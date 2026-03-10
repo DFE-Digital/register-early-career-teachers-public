d199c9aba66386db0f4d1389a4f70ff43d18c5ff
Lorenzo Tello <ltello8a@gmail.com>
2026-03-10 11:37:25 +0000

    fix flaky test

--- spec/migration/migrators/declaration_spec.rb
+++ spec/migration/migrators/declaration_spec.rb
@@ -59,6 +59,8 @@ describe Migrators::Declaration do
     let(:instance) { described_class.new(worker: 0) }
     let!(:data_migration) { FactoryBot.create(:data_migration, model: :declaration, worker: 0) }
 
+    before { instance.send(:cache_manager).clear_all_caches! }
+
     context "when the declaration is not related to an ERO mentor" do
       let(:clawback_statement) { Statement.find_by_api_id(participant_declaration.clawback_statement.id) }
       let(:payment_statement) { Statement.find_by_api_id(participant_declaration.payment_statement.id) }
@@ -90,7 +92,12 @@ describe Migrators::Declaration do
       end
 
       context "when the declaration is a special one that can't be associated with any existing training_period" do
-        let!(:participant_declaration) { FactoryBot.create(:migration_participant_declaration, :billable, declaration_type: :completed, id: "05e09502-f3ef-4f89-aa1a-e17a120df7dc") }
+        let!(:school) { FactoryBot.create(:school, urn: 149_712, create_contract_period: false) }
+        let!(:ecf_school) { FactoryBot.create(:ecf_migration_school, urn: 149_712) }
+        let!(:cohort) { FactoryBot.create(:migration_cohort) }
+        let!(:school_cohort) { FactoryBot.create(:migration_school_cohort, cohort:, school: ecf_school) }
+        let!(:participant_profile) { FactoryBot.create(:migration_participant_profile, :ect, school_cohort:) }
+        let!(:participant_declaration) { FactoryBot.create(:migration_participant_declaration, :billable, id: "05e09502-f3ef-4f89-aa1a-e17a120df7dc", participant_profile:, cohort:, declaration_type: :completed) }
         let!(:ecf_lead_provider) { participant_declaration.cpd_lead_provider.lead_provider }
         let!(:teacher) { FactoryBot.create(:teacher, api_ect_training_record_id: participant_declaration.participant_profile_id) }
         let!(:lead_provider) { FactoryBot.create(:lead_provider, ecf_id: ecf_lead_provider.id, name: ecf_lead_provider.name) }
@@ -99,48 +106,48 @@ describe Migrators::Declaration do
         let!(:delivery_partner) { FactoryBot.create(:delivery_partner, id: 173, name: "DP173") }
         let!(:lead_provider_delivery_partnership) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider:, delivery_partner:) }
         let!(:schedule) { FactoryBot.create(:schedule, contract_period:) }
-        let!(:school) { FactoryBot.create(:school, urn: "149712") }
         let!(:payment_statement) { FactoryBot.create(:statement, :payable, api_id: participant_declaration.payment_statement.id, contract_period:) }
+
         let(:started_on) { Date.new(contract_period.year, 9, 1) }
         let(:finished_on) { Date.new(contract_period.year, 9, 2) }
 
-        it "sets the created declaration attributes correctly" do
-          instance.migrate!
+        context "when no school periods overlap the new one and no school partnership to reuse" do
+          it "sets the created declaration attributes correctly" do
+            instance.migrate!
 
-          declaration = Declaration.find_by(api_id: participant_declaration.id)
-          training_period = declaration.training_period
-          at_school_period = training_period.ect_at_school_period
-          school_partnership = training_period.school_partnership
+            declaration = Declaration.find_by(api_id: participant_declaration.id)
+            training_period = declaration.training_period
+            at_school_period = training_period.ect_at_school_period
+            school_partnership = training_period.school_partnership
 
-          aggregate_failures do
-            expect(declaration).to have_attributes(participant_declaration.attributes.slice("created_at", "declaration_date", "declaration_type", "evidence_type", "updated_at"))
-            expect(declaration.clawback_status).to eq(participant_declaration.clawback_status)
-            expect(declaration.delivery_partner_when_created.id).to eq(delivery_partner.id)
-            expect(declaration.lead_provider.ecf_id).to eq(ecf_lead_provider_id)
-            expect(declaration.payment_statement_id).to eq(payment_statement.id)
-            expect(declaration.payment_status).to eq(participant_declaration.payment_status)
-            expect(declaration.training_period_id).to be_present
-            expect(declaration.voided_by_user_at).to eq(participant_declaration.voided_at)
+            aggregate_failures do
+              expect(declaration).to have_attributes(participant_declaration.attributes.slice("created_at", "declaration_date", "declaration_type", "evidence_type", "updated_at"))
+              expect(declaration.clawback_status).to eq(participant_declaration.clawback_status)
+              expect(declaration.delivery_partner_when_created.id).to eq(delivery_partner.id)
+              expect(declaration.lead_provider.ecf_id).to eq(ecf_lead_provider_id)
+              expect(declaration.payment_statement_id).to eq(payment_statement.id)
+              expect(declaration.payment_status).to eq(participant_declaration.payment_status)
+              expect(declaration.training_period_id).to be_present
+              expect(declaration.voided_by_user_at).to eq(participant_declaration.voided_at)
 
-            expect(at_school_period.started_on).to eq(started_on)
-            expect(at_school_period.finished_on).to eq(finished_on)
-            expect(at_school_period.school).to eq(school)
+              expect(at_school_period.started_on).to eq(started_on)
+              expect(at_school_period.finished_on).to eq(finished_on)
+              expect(at_school_period.school).to eq(school)
 
-            expect(training_period.started_on).to eq(started_on)
-            expect(training_period.finished_on).to eq(finished_on)
-            expect(training_period.schedule.contract_period_year).to eq(contract_period.year)
-            expect(training_period.schedule.identifier).to eq("ecf-standard-september")
+              expect(training_period.started_on).to eq(started_on)
+              expect(training_period.finished_on).to eq(finished_on)
+              expect(training_period.schedule.contract_period_year).to eq(contract_period.year)
+              expect(training_period.schedule.identifier).to eq("ecf-standard-september")
 
-            expect(school_partnership.school).to eq(school)
-            expect(school_partnership.lead_provider).to eq(lead_provider)
-            expect(school_partnership.delivery_partner).to eq(delivery_partner)
-            expect(school_partnership.contract_period).to eq(contract_period)
+              expect(school_partnership.school).to eq(school)
+              expect(school_partnership.lead_provider).to eq(lead_provider)
+              expect(school_partnership.delivery_partner).to eq(delivery_partner)
+              expect(school_partnership.contract_period).to eq(contract_period)
+            end
           end
         end
 
         context "when an existing teacher at school period overlaps with the new one to be created" do
-          let(:school_partnership) { FactoryBot.create(:school_partnership, school:, lead_provider_delivery_partnership:) }
-
           before do
             FactoryBot.create(:ect_at_school_period, teacher:, school:, started_on:, finished_on:)
           end
@@ -160,6 +167,19 @@ describe Migrators::Declaration do
               expect(training_period.started_on).to eq(started_on - 2.days)
               expect(training_period.finished_on).to eq(finished_on - 2.days)
             end
+          end
+        end
+
+        context "when a school partnerships for the new training period already exists" do
+          let!(:school_partnership) { FactoryBot.create(:school_partnership, school:, lead_provider_delivery_partnership:) }
+
+          it "reuses that school partnership" do
+            instance.migrate!
+
+            declaration = Declaration.find_by(api_id: participant_declaration.id)
+            training_period = declaration.training_period
+
+            expect(training_period.school_partnership).to eq(school_partnership)
           end
         end
       end
