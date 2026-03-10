7265e73054206476a2afb5db4919c2f11d940257
thenapking <tom.giles@gmail.com>
2026-03-10 14:03:54 +0000

    Tests

--- app/components/admin/statements/adjustments_component.rb
+++ app/components/admin/statements/adjustments_component.rb
@@ -12,20 +12,38 @@ module Admin
         @statement = statement
       end
 
-      def total_amount
-        adjustments.sum(&:amount)
-      end
-
       def adjustments
         statement.adjustments.order(created_at: :asc)
       end
 
-      def change_or_remove_links(adjustment)
+      def change_or_remove_link(adjustment)
         return unless adjustment_editable?
 
-        change_link(adjustment)  + " | " + remove_link(adjustment)
+        change_link(adjustment) + " | " + remove_link(adjustment)
       end
 
+      def total
+        safe_join([
+          tag.div("Total", class: "govuk-!-text-align-right govuk-heading-s govuk-!-margin-bottom-0"),
+          tag.div(number_to_pounds(total_amount), class: "govuk-!-text-align-right govuk-heading-s")
+        ])
+      end
+
+      def add_adjustment_link
+        tag.p do
+          govuk_link_to(
+            "Add",
+            new_admin_finance_statement_adjustment_path(statement),
+            visually_hidden_suffix: "adjustment"
+          )
+        end
+      end
+
+    private
+
+      def total_amount
+        adjustments.sum(&:amount)
+      end
 
       def change_link(adjustment)
         govuk_link_to("Change", edit_admin_finance_statement_adjustment_path(statement, adjustment))
