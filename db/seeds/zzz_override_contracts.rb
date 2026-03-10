f6555b521330980db49c303582eae9ca1796b911
thenapking <tom.giles@gmail.com>
2026-03-10 11:02:11 +0000

    Remove seeds change to contract period

--- db/seeds/zzz_override_contracts.rb
+++
@@ -1,6 +0,0 @@
-# TODO: REMOVE THIS
-cp_2025 = ContractPeriod.find_by!(year: 2025)
-cp_2026 = ContractPeriod.find_by!(year: 2026)
-
-cp_2025.update!(finished_on: Date.new(2026, 3, 10))
-cp_2026.update!(started_on: Date.new(2026, 3, 11))
