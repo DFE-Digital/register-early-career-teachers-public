26571591ff6686a0386452111211be13eb955867
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-03-10 19:55:54 +0000

    [1831] Remove `Statements::DeclarationsSearch`

--- app/services/statements/declarations_csv.rb
+++ app/services/statements/declarations_csv.rb
@@ -64,7 +64,23 @@ module Statements
   private
 
     def declarations
-      @declarations ||= Statements::DeclarationsSearch.new(statement:).declarations
+      @declarations ||= Declaration.where(id: declaration_selection.selected_declaration_ids)
+        .preload(
+          :delivery_partner_when_created,
+          mentorship_period: { mentor: :teacher },
+          training_period: [
+            :lead_provider,
+            :schedule,
+            { ect_at_school_period: %i[school teacher] },
+            { mentor_at_school_period: %i[school teacher] }
+          ]
+        )
+        .order(:declaration_date, :created_at, :id)
+        .distinct
+    end
+
+    def declaration_selection
+      @declaration_selection ||= DeclarationSelection.new(statement:)
     end
 
     def row_for(declaration)
