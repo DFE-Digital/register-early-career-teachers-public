c7a33adb418a24fbc2f5e1263f3de49800ea3463
Ross Oliver <ross.oliver36@gmail.com>
2026-03-12 13:12:12 +0000

    Clear parity check request state before retying
    We're seeing occasional job failures where the worker is killed of after being
    presumed idle. This leaves the request it was handling in a state that it can't
    be retried (as its `in_progress` and with some responses already).
    
    When running a job on an in-progress request we want to reset it to `queued`
    and clear existing responses so that it can be cleanly retried.
    
    If this wasn't short lived code I would have added a proper state transition
    for this, but as its about to be binned I've hashed it togther quickly.

--- spec/jobs/parity_check_request_job_spec.rb
+++ spec/jobs/parity_check_request_job_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe ParityCheckRequestJob, type: :job do
   describe "#perform" do
+    let(:request_handler) { instance_double(ParityCheck::RequestHandler) }
+
     it "calls the ParityCheck::RequestHandler service with the request" do
       request = FactoryBot.create(:parity_check_request)
-      request_handler = instance_double(ParityCheck::RequestHandler)
 
       allow(ParityCheck::RequestHandler).to receive(:new).with(request).and_return(request_handler)
       expect(request_handler).to receive(:process_request)
@@ -14,6 +15,21 @@ RSpec.describe ParityCheckRequestJob, type: :job do
       expect(ParityCheck::RequestHandler).not_to receive(:new)
 
       described_class.new.perform(request_id: 123)
+    end
+
+    it "clears existing responses and resets the request status to queued if retrying a request" do
+      request = FactoryBot.create(:parity_check_request, :in_progress)
+      FactoryBot.create(:parity_check_response, request:)
+
+      allow(ParityCheck::RequestHandler).to receive(:new).with(request).and_return(request_handler)
+      expect(request_handler).to receive(:process_request)
+
+      described_class.new.perform(request_id: request.id)
+
+      request.reload
+
+      expect(request.responses).to be_empty
+      expect(request).to be_queued
     end
   end
 end
