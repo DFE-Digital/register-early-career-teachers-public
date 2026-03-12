8bb1f53b3f7aaa01a53c237a915c6f1ad4817cb5
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2026-03-12 11:41:37 +0000

    [3398] Submit event when mark as payable is run

--- app/services/statements/mark_as_payable.rb
+++ app/services/statements/mark_as_payable.rb
@@ -2,8 +2,8 @@ module Statements
   class MarkAsPayable
     attr_reader :statement
 
-    def self.mark_all!
-      deadline_past_statements = Statement.where(status: :open).where(deadline_date: ..Date.current)
+    def self.mark_all_eligible!
+      deadline_past_statements = Statement.where(status: :open).where(deadline_date: ...Date.current)
 
       deadline_past_statements.find_each do |statement|
         new(statement).mark!
@@ -16,15 +16,35 @@ module Statements
 
     def mark!
       ActiveRecord::Base.transaction do
-        eligible_declarations.find_each(&:mark_as_payable!)
+        eligible_declarations.find_each do |declaration|
+          declaration.mark_as_payable!
+          record_declaration_payable_event!(declaration)
+        end
         statement.mark_as_payable!
+        record_statement_payable_event!
       end
     end
 
   private
 
     def eligible_declarations
-      statement.payment_declarations.where(payment_status: :eligible)
+      statement.payment_declarations.payment_status_eligible
+    end
+
+    def record_declaration_payable_event!(declaration)
+      Events::Record.record_teacher_declaration_marked_payable!(
+        author: Events::SystemAuthor.new,
+        teacher: declaration.training_period.teacher,
+        training_period: declaration.training_period,
+        declaration:
+      )
+    end
+
+    def record_statement_payable_event!
+      Events::Record.record_statement_marked_payable!(
+        author: Events::SystemAuthor.new,
+        statement:
+      )
     end
   end
 end
