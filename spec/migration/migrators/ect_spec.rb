39da66bbcf572942227c13dfefd52f47b1bfde8a
Peter Yates <peter.yates@graphia.co.uk>
2026-03-16 11:39:07 +0000

    Ensure errors are recorded when falling back to economy
    Co-authored-by: Lorenzo Tello <<ltello8a@gmail.com>>
    Co-authored-by: tonyheadford <tony@binarycircus.com>

--- spec/migration/migrators/ect_spec.rb
+++ spec/migration/migrators/ect_spec.rb
@@ -69,4 +69,43 @@ RSpec.describe Migrators::ECT do
       expect(fake_ecf2_teacher_history).to have_received(:save_all_ect_data!).twice
     end
   end
+
+  describe "#migrate_one! (part 2)" do
+    subject(:teacher) { Teacher.find_by(trn: ecf1_teacher_profile.trn) }
+
+    # Timestamps we care about
+    let(:user_created_at) { 3.years.ago.round }
+
+    # ECF1 data
+    let(:ecf1_induction_programme) { FactoryBot.create(:migration_induction_programme, :provider_led) }
+    let(:ecf1_induction_record) { FactoryBot.create(:migration_induction_record, induction_programme: ecf1_induction_programme, created_at: 18.hours.ago.round) }
+    let(:ecf1_teacher_profile) { ecf1_induction_record.participant_profile.teacher_profile }
+    let(:ecf1_urn) { ecf1_induction_programme.school_cohort.school.urn.to_i }
+
+    # ECF2 data
+    let(:ecf2_school) { ecf2_gias_school.school }
+    let(:ecf2_contract_period) { FactoryBot.create(:contract_period, year: ecf1_induction_record.induction_programme.school_cohort.cohort.start_year) }
+    let(:ecf2_lead_provider) { FactoryBot.create(:lead_provider, name: ecf1_induction_programme.partnership.lead_provider.name, ecf_id: ecf1_induction_programme.partnership.lead_provider_id) }
+    let(:ecf2_delivery_partner) { FactoryBot.create(:delivery_partner, name: ecf1_induction_programme.partnership.delivery_partner.name, api_id: ecf1_induction_programme.partnership.delivery_partner_id) }
+    let(:ecf2_active_lead_provider) { FactoryBot.create(:active_lead_provider, lead_provider: ecf2_lead_provider, contract_period: ecf2_contract_period) }
+    let(:ecf2_lead_provider_delivery_partnership) { FactoryBot.create(:lead_provider_delivery_partnership, active_lead_provider: ecf2_active_lead_provider, delivery_partner: ecf2_delivery_partner) }
+
+    let!(:ecf2_gias_school) { FactoryBot.create(:gias_school, :with_school, urn: ecf1_urn) }
+    let!(:ecf2_schedule) { FactoryBot.create(:schedule, contract_period: ecf2_contract_period, identifier: ecf1_induction_record.schedule.schedule_identifier) }
+    # let!(:ecf2_school_partnership) { FactoryBot.create(:school_partnership, school: ecf2_school, lead_provider_delivery_partnership: ecf2_lead_provider_delivery_partnership) }
+
+    before do
+      DataMigration.create!(worker: 1, model: :ect)
+      migrator = Migrators::ECT.new(worker: 1)
+      migrator.migrate_one!(ecf1_teacher_profile)
+    end
+
+    context "when there are errors" do
+      let(:migration_mode) { :latest_induction_records }
+
+      it "records the errors" do
+        expect(DataMigrationFailedCombination.count).not_to be_zero
+      end
+    end
+  end
 end
