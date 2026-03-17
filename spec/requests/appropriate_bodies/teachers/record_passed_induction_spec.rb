c9d8345aa3730939062cdf2f696587c3c8542241
Craig Davidson <craigmdavidson@gmail.com>
2026-03-17 11:23:50 +0000

    Handle invalid dates cleanly
    By default we use a Day/Month/Year combination text entry for dates. This allows alpha characters to be entered. Currently when this happens Rails throws a ActiveRecord::MultiparameterAssignmentErrors and the user gets a 500 error, so instead we catch this and make it look like a date validation error.

--- spec/requests/appropriate_bodies/teachers/record_passed_induction_spec.rb
+++ spec/requests/appropriate_bodies/teachers/record_passed_induction_spec.rb
@@ -72,6 +72,24 @@ RSpec.describe "Appropriate body recording a passed induction outcome for a teac
         end
       end
 
+      context "when date is not valid" do
+        let(:params) do
+          {
+            appropriate_bodies_record_pass: {
+              "finished_on(3i)" => "aa",
+              "finished_on(2i)" => "bb",
+              "finished_on(1i)" => "cccc",
+              number_of_terms: 3,
+            }
+          }
+        end
+
+        it "returns error with the entered value" do
+          expect(response).to have_http_status(:unprocessable_content)
+          expect(response.body).to include("aa/bb/cccc is not a valid date")
+        end
+      end
+
       context "with missing params" do
         let(:params) do
           {
