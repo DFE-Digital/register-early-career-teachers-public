0c21b3aed6f234fa7a2daf743a07c89653380df8
thenapking <tom.giles@gmail.com>
2026-03-24 14:18:56 +0000

    PR feedback

---
+++ app/services/contract_periods/reassigner.rb
@@ -0,0 +1,29 @@
+module ContractPeriods
+  class Reassigner
+    SUCCESSOR_CONTRACT_YEAR = 2024
+    attr_reader :training_period
+
+    def initialize(training_period:)
+      @training_period = training_period
+    end
+
+    def contract_period_closed?
+      return false unless training_period&.provider_led_training_programme?
+
+      contract_period = assigned_contract_period
+      return false unless contract_period
+
+      contract_period.payments_frozen?
+    end
+
+    def successor_contract_period
+      @successor_contract_period ||= ContractPeriod.find_by!(year: SUCCESSOR_CONTRACT_YEAR)
+    end
+
+  private
+
+    def assigned_contract_period
+      @training_period.contract_period || @training_period.expression_of_interest_contract_period
+    end
+  end
+end
