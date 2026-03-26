530a301beaf5991186b666f9be622fac949a2515
thenapking <tom.giles@gmail.com>
2026-03-26 15:09:05 +0000

    Lead provider select

--- app/wizards/schools/ects/change_training_programme_wizard/lead_provider_step.rb
+++ app/wizards/schools/ects/change_training_programme_wizard/lead_provider_step.rb
@@ -33,7 +33,21 @@ module Schools
         end
 
         def contract_period
+          @contract_period ||= contract_period_reassignment.required? ? successor_contract_period : contract_period_on_start_date
+        end
+
+        def contract_period_on_start_date
           ContractPeriod.containing_date(ect_at_school_period.started_on)
+        end
+
+        def contract_period_reassignment
+          @contract_period_reassignment ||= ContractPeriods::Reassignment.new(training_period: last_confirmed_provider_led_training_period)
+        end
+
+        delegate :successor_contract_period, to: :contract_period_reassignment
+
+        def last_confirmed_provider_led_training_period
+          @last_confirmed_provider_led_training_period ||= ect_at_school_period.training_periods.where.not(school_partnership: nil).last
         end
       end
     end
