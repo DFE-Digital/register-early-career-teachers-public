a33798d15ea21ce69a30a2f1d8b7face8a473325
thenapking <tom.giles@gmail.com>
2026-03-27 13:14:32 +0000

    Feature test

--- spec/features/schools/ects/change/edge_cases/changing_training_programme_for_ects_in_closed_contract_periods_spec.rb
+++ spec/features/schools/ects/change/edge_cases/changing_training_programme_for_ects_in_closed_contract_periods_spec.rb
@@ -1,34 +1,68 @@
-describe "School user can change ECTs training programme to provider lead", :enable_schools_interface do
-  include_context "safe_schedules"
-
-  it "changes the training programme from school-led to provider-led" do
+describe "Changing training programme for ECTs who started provider-led training in a closed contract period", :enable_schools_interface do
+  before do
     given_there_is_a_school
     and_there_is_a_closed_contract_period
     and_there_is_an_open_contract_period_with_extended_schedules
     and_there_was_a_school_partnership_in_the_closed_period
+    and_there_is_another_lead_provider
+
     and_there_is_an_ect
     with_a_mentor
     and_the_ect_started_provider_led_training_in_a_closed_contract_period
-    and_switched_to_school_led_training
-    
+    and_the_ect_switched_to_school_led_training
+  end
+
+  it "changes the training programme from school-led to provider-led" do
+    and_there_is_an_active_lead_provider_in_the_open_contract_period
+    and_there_is_a_third_active_lead_provider
+
     when_i_am_logged_in_as_a_school_user
     and_i_visit_the_ect_page
-    then_i_can_change_the_training_programme_to_provider_led
+    then_i_can_change_the_training_programme
 
-    when_i_change_the_training_programme_to_provider_led
+    when_i_change_the_training_programme
     then_i_can_choose_from_lead_providers_who_are_active_in_the_open_contract_period
-    
+
     when_i_choose_the_lead_provider
     and_i_continue
     then_i_am_asked_to_check_and_confirm_the_change
 
     when_i_confirm_the_change
     then_i_see_the_provider_led_confirmation_message
-    and_a_training_period_has_been_created_in_the_open_contract_period
+
+    when_i_visit_the_ect_page
+    then_the_ects_training_is_awaiting_confirmation_from_the_new_lead_provider
+    and_a_training_period_has_been_created_with_an_expression_of_interest_in_the_open_contract_period
     and_the_school_led_training_period_has_been_finished
+    and_the_provider_led_training_period_has_been_left_in_the_original_contract_period
     and_the_ect_has_payments_frozen_year_set
   end
 
+  it "changes the training programme from school-led to provider-led, with an existing school partnership" do
+    and_there_is_a_school_partnership_in_the_open_contract_period
+    and_there_is_a_third_active_lead_provider
+
+    when_i_am_logged_in_as_a_school_user
+    and_i_visit_the_ect_page
+    then_i_can_change_the_training_programme
+
+    when_i_change_the_training_programme
+    then_i_can_choose_from_lead_providers_who_are_active_in_the_open_contract_period
+
+    when_i_choose_the_lead_provider
+    and_i_continue
+    then_i_am_asked_to_check_and_confirm_the_change
+
+    when_i_confirm_the_change
+    then_i_see_the_provider_led_confirmation_message
+
+    when_i_visit_the_ect_page
+    then_the_ects_training_is_confirmed_with_the_existing_school_partnership
+    and_a_training_period_has_been_created_with_a_school_partnership_in_the_open_contract_period
+    and_the_school_led_training_period_has_been_finished
+    and_the_provider_led_training_period_has_been_left_in_the_original_contract_period
+    and_the_ect_has_payments_frozen_year_set
+  end
 
 private
 
@@ -49,16 +83,47 @@ private
     @school_partnership = FactoryBot.create(:school_partnership, :with_active_lead_provider, :for_year, year: 2021, school: @school)
   end
 
+  def and_there_is_another_lead_provider
+    @lead_provider = FactoryBot.create(:lead_provider, name: "Other Lead Provider")
+  end
+
+  def and_there_is_an_active_lead_provider_in_the_open_contract_period
+    @other_active_lead_provider = FactoryBot.create(
+      :active_lead_provider,
+      :for_year,
+      year: 2024,
+      lead_provider: @lead_provider
+    )
+  end
+
+  def and_there_is_a_third_active_lead_provider
+    lead_provider = FactoryBot.create(:lead_provider, name: "A Third Testing Provider")
+    @third_active_lead_provider = FactoryBot.create(
+      :active_lead_provider,
+      :for_year,
+      year: 2025,
+      lead_provider:
+    )
+  end
+
+  def and_there_is_a_school_partnership_in_the_open_contract_period
+    @other_school_partnership = FactoryBot.create(:school_partnership,
+                                                  :with_active_lead_provider,
+                                                  :for_year, year: 2024,
+                                                             school: @school, lead_provider: @lead_provider)
+    @other_active_lead_provider = @other_school_partnership.active_lead_provider
+  end
+
   def and_there_is_an_ect
     @teacher = FactoryBot.create(
       :teacher,
       trs_first_name: "John",
       trs_last_name: "Doe"
     )
-    @ect = FactoryBot.create(
+    @ect_at_school_period = FactoryBot.create(
       :ect_at_school_period,
       :ongoing,
-      started_on: 1.day.ago,
+      started_on: Date.new(2021, 9, 1),
       teacher: @teacher,
       school: @school,
       email: "ect@example.com"
@@ -70,84 +135,48 @@ private
       :mentor_at_school_period,
       :ongoing,
       school: @school,
-      started_on: @ect.started_on
+      started_on: @ect_at_school_period.started_on
     )
-    FactoryBot.create(:mentorship_period, mentee: @ect, mentor: mentor_at_school_period)
-  end
-
-  def and_there_is_a_contract_period
-    @contract_period = FactoryBot.create(:contract_period, :current, :with_schedules)
-  end
-
-  def and_there_is_an_active_lead_provider
-    lead_provider = FactoryBot.create(:lead_provider, name: "Testing Provider")
-    @active_lead_provider = FactoryBot.create(
-      :active_lead_provider,
-      contract_period: @contract_period,
-      lead_provider:
-    )
+    FactoryBot.create(:mentorship_period, mentee: @ect_at_school_period, mentor: mentor_at_school_period)
   end
 
   def and_the_ect_started_provider_led_training_in_a_closed_contract_period
-    @first_training_period = FactoryBot.create(:training_period, :for_ect, :finished, :provider_led, ect_at_school_period:, school_partnership:, started_on:)
-    
+    @provider_led_training_period = FactoryBot.create(:training_period, :for_ect, :finished, :provider_led,
+                                                      ect_at_school_period: @ect_at_school_period,
+                                                      school_partnership: @school_partnership,
+                                                      started_on: @ect_at_school_period.started_on)
   end
 
-  def and_then_the_ect_switched_to_school_led_training
-    FactoryBot.create(:training_period, :for_ect, :ongoing, :school_led, ect_at_school_period:, started_on: @first_training_period.finished_on + 1.day)
+  def and_the_ect_switched_to_school_led_training
+    @school_led_training_period = FactoryBot.create(:training_period, :for_ect, :ongoing, :school_led, ect_at_school_period: @ect_at_school_period, started_on: @provider_led_training_period.finished_on + 1.day)
   end
 
-  def with_provider_led_training
-    FactoryBot.create(
-      :training_period,
-      :provider_led,
-      :for_ect,
-      :ongoing,
-      ect_at_school_period: @ect,
-      started_on: @ect.started_on
-    )
-  end
-
-  def with_school_led_training
-    FactoryBot.create(
-      :training_period,
-      :school_led,
-      :for_ect,
-      :ongoing,
-      ect_at_school_period: @ect,
-      started_on: @ect.started_on
-    )
-  end
-
-  def and_i_am_logged_in_as_a_school_user
+  def when_i_am_logged_in_as_a_school_user
     sign_in_as_school_user(school: @school)
   end
 
-  def when_i_visit_the_ect_page
-    page.goto(schools_ect_path(@ect))
+  def and_i_visit_the_ect_page
+    page.goto(schools_ect_path(@ect_at_school_period))
   end
+
+  alias_method :when_i_visit_the_ect_page, :and_i_visit_the_ect_page
 
   def then_i_can_change_the_training_programme
     row = page.locator(".govuk-summary-list__row", hasText: "Training programme")
     row.get_by_role("link", name: "Change").click
   end
 
-  def and_i_can_change_the_training_programme_to_school_led
-    heading = page.locator("h1", hasText: "Change John Doe’s training programme to school-led")
-    expect(heading).to be_visible
-  end
-
-  def and_i_can_change_the_training_programme_to_provider_led
-    heading = page.locator("h1", hasText: "Change John Doe’s training programme to provider-led")
-    expect(heading).to be_visible
-  end
-
   def when_i_change_the_training_programme
     page.get_by_role("button", name: "Change training programme").click
   end
 
-  def and_i_choose_the_lead_provider
-    page.get_by_label("Testing Provider").check
+  def then_i_can_choose_from_lead_providers_who_are_active_in_the_open_contract_period
+    expect(page.get_by_label("Other Lead Provider")).to be_visible
+    expect(page.get_by_label("A Third Lead Provider")).not_to be_visible
+  end
+
+  def when_i_choose_the_lead_provider
+    page.get_by_label("Other Lead Provider").check
   end
 
   def when_i_continue
@@ -161,46 +190,49 @@ private
     expect(heading).to be_visible
   end
 
-  def when_i_navigate_back_to_the_form
-    page.get_by_role("link", name: "Back", exact: true).click
-  end
-
-  def then_the_lead_provider_is_selected
-    lead_provider_radio = page.get_by_label("Testing Provider")
-    expect(lead_provider_radio).to be_checked
-  end
-
-  def and_i_confirm_the_change
+  def when_i_confirm_the_change
     page.get_by_role("button", name: "Confirm change").click
   end
 
   def then_i_see_the_provider_led_confirmation_message
     success_panel = page.locator(".govuk-panel")
     expect(success_panel).to have_text(
-      "You have changed John Doe’s training programme to provider-led with Testing Provider"
+      "You have changed John Doe’s training programme to provider-led with Other Lead Provider"
     )
   end
 
-  def then_i_see_the_school_led_confirmation_message
-    success_panel = page.locator(".govuk-panel")
-    expect(success_panel).to have_text(
-      "You have changed John Doe’s training programme to school-led"
-    )
-  end
-
-  def and_a_training_period_has_been_created_in_the_open_contract_period
+  def and_a_training_period_has_been_created_with_an_expression_of_interest_in_the_open_contract_period
     @ect_at_school_period.reload
     new_training_period = @ect_at_school_period.training_periods.last
     expect(new_training_period.expression_of_interest_contract_period.year).to eq(2024)
   end
 
-  def and_the_school_led_training_period_has_been_finished
-    @provider_led_training_period.reload
-    expect(@provider_led_training_period.finished_on).to eq(Date.current)
+  def and_a_training_period_has_been_created_with_a_school_partnership_in_the_open_contract_period
+    @ect_at_school_period.reload
+    new_training_period = @ect_at_school_period.training_periods.last
+    expect(new_training_period.contract_period.year).to eq(2024)
   end
 
   def and_the_ect_has_payments_frozen_year_set
-    @ect_at_school_period.teacher.reload
-    expect(@ect_at_school_period.teacher.ect_payments_frozen_year).to eq(2021)
+    teacher = @ect_at_school_period.teacher.reload
+    expect(teacher.ect_payments_frozen_year).to eq(2021)
+  end
+
+  def and_the_school_led_training_period_has_been_finished
+    @school_led_training_period.reload
+    expect(@school_led_training_period.finished_on).to eq(Date.current)
+  end
+
+  def and_the_provider_led_training_period_has_been_left_in_the_original_contract_period
+    @provider_led_training_period.reload
+    expect(@provider_led_training_period.contract_period.year).to eq(2021)
+  end
+
+  def then_the_ects_training_is_confirmed_with_the_existing_school_partnership
+    expect(page.get_by_text("Confirmed by Other Lead Provider")).to be_visible
+  end
+
+  def then_the_ects_training_is_awaiting_confirmation_from_the_new_lead_provider
+    expect(page.get_by_text("Awaiting confirmation by Other Lead Provider")).to be_visible
   end
 end
