0f31769845454c64a903ff155f98b3b6e2d23b1d
thenapking <tom.giles@gmail.com>
2026-04-01 13:35:30 +0100

    Add missing tests

--- spec/services/contract_periods/reassignment_spec.rb
+++ spec/services/contract_periods/reassignment_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe ContractPeriods::Reassignment do
   describe "#required?" do
-    subject(:result) { described_class.new(training_period:).required? }
+    subject { described_class.new(training_period:).required? }
 
     context "when training period is provider-led in a payments-frozen contract period" do
       let(:contract_period) do
@@ -97,6 +97,69 @@ RSpec.describe ContractPeriods::Reassignment do
       successor_contract_period = described_class.new(training_period: nil).successor_contract_period
 
       expect(successor_contract_period.year).to eq(2024)
+    end
+  end
+
+  describe "assigned_contract_period" do
+    subject { described_class.new(training_period:).assigned_contract_period }
+
+    context "when training period has a contract period" do
+      let(:contract_period) { instance_double(ContractPeriod) }
+      let(:training_period) do
+        instance_double(
+          TrainingPeriod,
+          contract_period:,
+          expression_of_interest_contract_period: nil
+        )
+      end
+
+      it { is_expected.to eq(contract_period) }
+    end
+
+    context "when training period does not have a contract period but has an expression of interest contract period" do
+      let(:expression_of_interest_contract_period) { instance_double(ContractPeriod) }
+      let(:training_period) do
+        instance_double(
+          TrainingPeriod,
+          contract_period: nil,
+          expression_of_interest_contract_period:
+        )
+      end
+
+      it { is_expected.to eq(expression_of_interest_contract_period) }
+    end
+
+    context "when training period does not have a contract period or an expression of interest contract period" do
+      let(:training_period) do
+        instance_double(
+          TrainingPeriod,
+          contract_period: nil,
+          expression_of_interest_contract_period: nil
+        )
+      end
+
+      it { is_expected.to be_nil }
+    end
+
+    context "when a training period has both a contract period and an expression of interest contract period" do
+      let(:contract_period) { instance_double(ContractPeriod) }
+      let(:expression_of_interest_contract_period) { instance_double(ContractPeriod) }
+
+      let(:training_period) do
+        instance_double(
+          TrainingPeriod,
+          contract_period:,
+          expression_of_interest_contract_period:
+        )
+      end
+
+      it { is_expected.to eq(contract_period) }
+    end
+
+    context "when there is no training period" do
+      let(:training_period) { nil }
+
+      it { is_expected.to be_nil }
     end
   end
 end
