031099b05cc413b949806a86e707ea3e93268c83
Connor McQuillan <me@cpjmcquillan.com>
2026-04-02 16:07:11 +0100

    WIP - Spike using redis for session storage
    We've seen some `ActionDispatch::Cookies::CookieOverflow` [errors] in Sentry, so
    it seems pragmatic to start thinking of alternative session storage solutions
    before we run in to issues in Production.
    
    Since sessions are such a key part of the wizard journeys, this would be
    particularly bad if we saw widespread errors once we're live!
    
    This spikes out using redis for session storage. I'm not completely sure this is
    the right configuration, but there is nothing quite like sucking it and seeing
    🤞.
    
    [errors]: https://dfe-teacher-services.sentry.io/issues/7253144469

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,13 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_CACHE_URL") }
+  config.session_store :cache_store,
+                       {
+                         key: "_ecf2_session",
+                         servers: ENV.fetch("REDIS_CACHE_URL"),
+                         expire_after: 6.hours,
+                         secure: true
+                       }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :solid_queue
