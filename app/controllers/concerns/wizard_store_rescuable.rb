e3da398842cfcbb345202d8a3ad3b9c502f19867
Richard Worrall <richard@soupish.net>
2026-04-08 16:24:48 +0100

    Tweak error message

--- app/controllers/concerns/wizard_store_rescuable.rb
+++ app/controllers/concerns/wizard_store_rescuable.rb
@@ -1,13 +1,19 @@
 module WizardStoreRescuable
   extend ActiveSupport::Concern
 
+  EMPTY_STORE_FLASH_MESSAGE = <<~TXT
+    There was a problem saving your changes. Please close any other tabs or
+    windows for this service, and try again. If the problem persists, please
+    contact support.
+  TXT
+
   included do
-    rescue_from ApplicationWizardStep::StoreEmptyError, with: :redirect_to_first_step
+    rescue_from ApplicationWizardStep::EmptyStoreError, with: :redirect_to_first_step
 
   private
 
     def redirect_to_first_step
-      redirect_to @wizard.first_step_path, alert: ApplicationWizardStep::STORE_EMPTY_FLASH_MESSAGE
+      redirect_to @wizard.first_step_path, notice: EMPTY_STORE_FLASH_MESSAGE
     end
   end
 end
