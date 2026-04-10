746c638f36237d77730b71977ddaba0c9c590efb
Peter Yates <peter.yates@graphia.co.uk>
2026-04-10 14:25:19 +0100

    Add AppropriateBodyData#ecf2_id for easy mapping

--- app/migration/types.rb
+++ app/migration/types.rb
@@ -1,5 +1,11 @@
 module Types
-  AppropriateBodyData = Data.define(:ecf1_id, :name)
+  AppropriateBodyData = Data.define(:ecf1_id, :name) do
+    def ecf2_id
+      return unless ecf1_id
+
+      Mappers::AppropriateBodyMapper.new.get_ecf2_id(ecf1_id)
+    end
+  end
   DeliveryPartnerInfo = Data.define(:ecf1_id, :name)
   LeadProviderInfo = Data.define(:ecf1_id, :name)
   SchoolData = Struct.new("SchoolData", :urn, :name, :school_type_name) do
