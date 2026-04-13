62330a3997d9cf089e05222ccff379bcf0959aec
Peter Yates <peter.yates@graphia.co.uk>
2026-04-13 15:17:39 +0100

    Move session store config to envs
    Instead of adding env-based logic to the initializer, move the session
    store logic to the env-specific config files. I prefer this as now the
    cache/session storage (now both Redis-based) are next to each other.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,15 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_CACHE_URL") }
+  config.session_store(
+    :redis_session_store,
+    serializer: :json,
+    redis: {
+      expire_after: ENV.fetch("MAX_SESSION_IDLE_TIME", 7200.seconds).to_i,
+      key_prefix: "_ecf2_session:",
+      url: ENV.fetch("REDIS_CACHE_URL")
+    }
+  )
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :solid_queue
