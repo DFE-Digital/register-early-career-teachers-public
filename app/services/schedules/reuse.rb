13839a4bf9434c4203c0276986871a91bd263efa
thenapking <tom.giles@gmail.com>
2026-04-13 14:22:14 +0100

    More refactoring

--- app/services/schedules/reuse.rb
+++ app/services/schedules/reuse.rb
@@ -1,7 +1,7 @@
 module Schedules
   module Reuse
     def contract_period
-      if contract_period_reassignment_required? && current_training_period_confirmed?
+      if confirmed_training_reassignment_required?
         successor_contract_period
       elsif reuse_existing_schedule?
         existing_schedule.contract_period
@@ -27,10 +27,8 @@ module Schedules
       existing_schedule.contract_period != contract_period_at_transition
     end
 
-    def contract_period_reassignment_required?
-      return false unless period.is_a?(ECTAtSchoolPeriod)
-
-      contract_period_reassignment.required?
+    def confirmed_training_reassignment_required?
+      contract_period_reassignment_required? && current_or_next_training_period_confirmed?
     end
 
     def contract_period_reassignment
@@ -41,10 +39,8 @@ module Schedules
       @most_recent_provider_led_period ||= training_periods&.provider_led_training_programme&.latest_first&.first
     end
 
-    delegate :training_periods, to: :period
-
-    def current_training_period_confirmed?
-      current_or_next_training_period.school_partnership.present?
+    def current_or_next_training_period_confirmed?
+      current_or_next_training_period&.school_partnership.present?
     end
 
     def current_or_next_training_period
@@ -55,9 +51,11 @@ module Schedules
       most_recent_provider_led_period&.schedule
     end
 
-    def previous_contract_period = contract_period_reassignment.assigned_contract_period
-
+    delegate :training_periods, to: :period
+    delegate :required?, to: :contract_period_reassignment, prefix: true
     delegate :successor_contract_period, to: :contract_period_reassignment
+    delegate :assigned_contract_period, to: :contract_period_reassignment
+    alias_method :previous_contract_period, :assigned_contract_period
     alias_method :started_on, :date_of_transition
   end
 end
