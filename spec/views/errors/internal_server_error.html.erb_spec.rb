d7582da5908d674b62ab78a6a2af18fe48c5c0e6
Jas Johal <22663194+JSJohal@users.noreply.github.com>
2026-04-13 15:18:52 +0000

    Removed trailing whitespace... 😒

--- spec/views/errors/internal_server_error.html.erb_spec.rb
+++ spec/views/errors/internal_server_error.html.erb_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe "errors/internal_server_error.html.erb" do
   before { render }
-  
+
   it "display support email" do
     expect(rendered).to have_link("teacher.induction@education.gov.uk", href: "mailto:teacher.induction@education.gov.uk")
     expect(rendered).to have_link("continuing-professional-development@digital.education.gov.uk", href: "mailto:continuing-professional-development@digital.education.gov.uk")
