74b8df07070ced2793278bd21b7177e7fdba48c4
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-04-14 16:22:45 +0100

    Add school_sign_in_cta helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,15 @@
 module PagesHelper
+  def school_sign_in_cta(text)
+    return govuk_button_to(text, "/auth/dfe_sign_in") if direct_school_dfe_sign_in?
+
+    govuk_button_link_to(text, sign_in_path)
+  end
+
+private
+
+  def direct_school_dfe_sign_in?
+    Rails.env.production? &&
+      Rails.application.config.dfe_sign_in_enabled &&
+      !Rails.application.config.enable_personas
+  end
 end
