3f9986132629aaa067fc31aa4c56fe039a9686ad
Craig Davidson <craigmdavidson@gmail.com>
2026-04-24 11:37:28 +0100

    Return :active status for ECTs and mentors who have completed with a finished training period

--- app/services/api/training_periods/teacher_status.rb
+++ app/services/api/training_periods/teacher_status.rb
@@ -1,20 +1,34 @@
 module API::TrainingPeriods
   class TeacherStatus
-    attr_reader :started_on, :finished_on
+    attr_reader :training_period
+
+    delegate :teacher, :started_on, :finished_on, :for_ect?, :for_mentor?, to: :training_period, allow_nil: true
+    delegate :mentor_became_ineligible_for_funding_on, to: :teacher, allow_nil: true
 
     def initialize(latest_training_period:)
-      @started_on = latest_training_period.started_on
-      @finished_on = latest_training_period.finished_on
+      @training_period = latest_training_period
     end
 
     def status
-      if finished_on.present?
+      if teacher_is_ect_and_completed_induction? || teacher_is_mentor_and_completed_training?
+        :active # longer term we would prefer something like :complete
+      elsif finished_on.present?
         finished_on.future? ? :leaving : :left
       elsif started_on&.future?
         :joining
       else
-        :active # includes complete
+        :active
       end
+    end
+
+  private
+
+    def teacher_is_ect_and_completed_induction?
+      for_ect? && API::Teachers::InductionStatus.new(teacher:).completed_induction?
+    end
+
+    def teacher_is_mentor_and_completed_training?
+      for_mentor? && mentor_became_ineligible_for_funding_on.present?
     end
   end
 end
