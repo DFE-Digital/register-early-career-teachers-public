f76209f4058345064a21a4243d4ad82e784e199a
Ross Oliver <ross.oliver36@gmail.com>
2026-04-26 22:41:55 +0100

    Temp reinstate legacy query

--- app/services/api/declarations/query.rb
+++ app/services/api/declarations/query.rb
@@ -12,7 +12,7 @@ module API::Declarations
       updated_since: :ignore
     )
       @lead_provider_id = lead_provider_id
-      @scope = join_school_period(Declaration.all)
+      @scope = Declaration.distinct
 
       where_lead_provider_is(lead_provider_id)
       where_contract_period_year_in(contract_period_years)
@@ -58,11 +58,14 @@ module API::Declarations
     def where_lead_provider_is(lead_provider_id)
       return if ignore?(filter: lead_provider_id)
 
-      declarations_matching_lead_provider(lead_provider_id)
+      @scope = scope.where(id: declarations_matching_lead_provider(lead_provider_id).select(:id))
     end
 
+    # Keeps the lead-provider join chain in a sub-relation so the outer scope
+    # is filtered via WHERE id IN (subquery). This makes pagination COUNT(*)
+    # cheaper and lets other filters compose against a clean outer scope.
     def declarations_matching_lead_provider(lead_provider_id)
-      @scope = scope
+      Declaration
         # Join the lead provider for the declaration.
         .joins(
           training_period: {
@@ -71,8 +74,11 @@ module API::Declarations
             }
           }
         )
-        # Join the metadata for the lead provider and teacher, where the teacher has been
-        # trained by the lead provider at some point.
+        # Join the ECT and mentor school periods (left join as its one or the other).
+        .left_joins(training_period: %i[ect_at_school_period mentor_at_school_period])
+        # Join the latest ECT and mentor training period for the lead provider we are filtering by.
+        # This will restrict declarations to only those for teachers associated with the lead provider we are filtering by.
+        # The manual sanitisation is needed as Rails does not support binding parameters in JOINs.
         .joins(
           ActiveRecord::Base.send(
             :sanitize_sql_array,
@@ -81,53 +87,51 @@ module API::Declarations
                 JOIN metadata_teachers_lead_providers
                   ON metadata_teachers_lead_providers.lead_provider_id = ?
                 AND (
-                  (
-                    metadata_teachers_lead_providers.latest_ect_training_period_id IS NOT NULL
-                    OR metadata_teachers_lead_providers.latest_mentor_training_period_id IS NOT NULL
-                  )
-                  AND metadata_teachers_lead_providers.teacher_id = COALESCE(
-                    ect_at_school_periods.teacher_id,
-                    mentor_at_school_periods.teacher_id
-                  )
+                  (metadata_teachers_lead_providers.teacher_id = ect_at_school_periods.teacher_id
+                    AND latest_ect_training_period_id IS NOT NULL)
+                OR (metadata_teachers_lead_providers.teacher_id = mentor_at_school_periods.teacher_id
+                    AND latest_mentor_training_period_id IS NOT NULL)
                 )
               SQL
               lead_provider_id
             ]
           )
         )
-        # Join latest ECT/mentor training period for the lead provider; this will ensure that:
-        #   * If the declaration is for an ECT, the ECT has been trained by the lead provider.
-        #   * If the declaration is for a mentor, the mentor has been trained by the lead provider.
-        .joins(<<~SQL)
-          JOIN training_periods latest_training_period
-            ON latest_training_period.id = CASE
-              WHEN ect_at_school_periods.id IS NOT NULL
-                THEN metadata_teachers_lead_providers.latest_ect_training_period_id
-              WHEN mentor_at_school_periods.id IS NOT NULL
-                THEN metadata_teachers_lead_providers.latest_mentor_training_period_id
-            END
+        .joins(<<-SQL)
+          LEFT JOIN training_periods latest_mentor_training_period
+          ON latest_mentor_training_period.id = metadata_teachers_lead_providers.latest_mentor_training_period_id
+        SQL
+        .joins(<<-SQL)
+          LEFT JOIN training_periods latest_ect_training_period ON
+          latest_ect_training_period.id = metadata_teachers_lead_providers.latest_ect_training_period_id
         SQL
         # Restrict to either:
-        #   * Declarations directly associated with the lead provider.
-        #   * Billable declarations dated earlier than the latest ECT/mentor training period for the lead provider.
+        #   * Declarations directly associated with the lead provider we are filtering by.
+        #   * Billable declarations dated earlier than the latest ECT/mentor training period for the lead provider we are filtering by.
         .where(<<-SQL, payment_statuses: Declaration::BILLABLE_OR_CHANGEABLE_PAYMENT_STATUSES, lead_provider_id:)
           active_lead_providers.lead_provider_id = :lead_provider_id
-          OR (
-            declarations.payment_status IN (:payment_statuses)
-            AND declarations.clawback_status = 'no_clawback'
-            AND (
-              latest_training_period.finished_on IS NULL
-              OR declarations.declaration_date <= latest_training_period.finished_on
+          OR
+          (
+            (
+              CASE
+                WHEN ect_at_school_periods.id IS NOT NULL THEN
+                  latest_ect_training_period.finished_on
+                ELSE
+                  latest_mentor_training_period.finished_on
+              END IS NULL
+              OR
+              declarations.declaration_date <=
+              CASE
+                WHEN ect_at_school_periods.id IS NOT NULL THEN
+                  latest_ect_training_period.finished_on
+                ELSE
+                  latest_mentor_training_period.finished_on
+              END
             )
+            AND declarations.payment_status IN (:payment_statuses)
+            AND declarations.clawback_status = 'no_clawback'
           )
         SQL
-    end
-
-    def join_school_period(scope)
-      # Join the ECT or mentor school period (this will only ever be one or the other).
-      scope
-        .joins(:training_period)
-        .left_joins(training_period: %i[ect_at_school_period mentor_at_school_period])
     end
 
     def where_contract_period_year_in(contract_period_years)
@@ -141,19 +145,21 @@ module API::Declarations
     def where_teacher_is(teacher_api_ids)
       return if ignore?(filter: teacher_api_ids, ignore_empty_array: false)
 
-      teacher_subquery = Teacher.where(api_id: teacher_api_ids).select(:id)
+      teacher_ids = Teacher.where(api_id: teacher_api_ids).ids
+
       @scope = scope
+        .left_joins(training_period: %i[ect_at_school_period mentor_at_school_period])
         .where(
           "ect_at_school_periods.teacher_id IN (:ids) OR mentor_at_school_periods.teacher_id IN (:ids)",
-          ids: teacher_subquery
+          ids: teacher_ids
         )
     end
 
     def where_delivery_partner_is(delivery_partner_api_ids)
       return if ignore?(filter: delivery_partner_api_ids, ignore_empty_array: false)
 
-      delivery_partner_subquery = DeliveryPartner.where(api_id: delivery_partner_api_ids).select(:id)
-      @scope = scope.where(delivery_partner_when_created_id: delivery_partner_subquery)
+      delivery_partner_ids = DeliveryPartner.where(api_id: delivery_partner_api_ids).ids
+      @scope = scope.where(delivery_partner_when_created_id: delivery_partner_ids)
     end
 
     def where_updated_since(updated_since)
