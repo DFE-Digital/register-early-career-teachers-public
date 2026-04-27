20dc9cf1c49ff09f9b844c2fbfd3006c31757ed1
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-04-27 11:25:57 +0100

    Harden test coverage for release notes

--- spec/components/api/guidance/release_notes/tags_component_spec.rb
+++ spec/components/api/guidance/release_notes/tags_component_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe API::Guidance::ReleaseNotes::TagsComponent, type: :component do
+  describe "TAG_MAPPINGS" do
+    let(:tags) { described_class::TAG_MAPPINGS.keys.sort }
+
+    it "maps all tags" do
+      notes_file_path = Rails.root.join("app/views/api/release_notes/release_notes.yml")
+      notes_data = YAML.load_file(notes_file_path, permitted_classes: [Date])
+      all_tags = notes_data.flat_map { |note| note["tags"] || [] }.uniq
+
+      expect(all_tags).to all be_in(tags)
+    end
+  end
+
   describe "initialization" do
     subject(:component) do
-      described_class.new(
-        tags:
-      )
+      described_class.new(tags:)
     end
 
     let(:tags) { %w[bug-fix new-feature] }
@@ -28,13 +38,13 @@ RSpec.describe API::Guidance::ReleaseNotes::TagsComponent, type: :component do
         expect(rendered_content).to eql(expected_output)
       end
 
-      context "when a tag is are not mapped" do
+      context "when a tag is unknown" do
         let(:tags) { %w[new-tag] }
 
-        it "raises an error" do
+        it "raises an error with guidance" do
           expect {
             render_inline(component)
-          }.to raise_error(described_class::UnknownTagError, "Tag not recognised: new-tag")
+          }.to raise_error(described_class::UnknownTagError, /Tag not recognised: 'new-tag'. Valid tags are: breaking-change/)
         end
       end
 
