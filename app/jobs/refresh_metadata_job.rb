5d68412b18d6cea32ceaf598202cf1949e82efbb
Ross Oliver <ross.oliver36@gmail.com>
2026-04-28 10:24:23 +0100

    Rework the queues/workers
    We are finding in production that the volume of workers is overloading the
    database; we currently have a maximum of 3 processes * 3 threads * 10 workers,
    so 90 concurrent jobs.
    
    As we have an `*` in one of our workers it will end up picking up any pending
    job potentially blocking the more critical/time-sensitive queues such as
    `mailer`.
    
    Scale the number of physical workers down to 2.
    
    Ensure we have a dedicated workers for mailers/default/metadata/everything else
    and scale the metadata workers down to a maximum of 2 at any given time (1
    thread, 1 process and 2 workers). This is also enforced in `limits_concurrency`
    on the job in case we scale the workers up from 2 in the future.
    
    Only run parity check/migration/seed jobs in relevant environments.

--- app/jobs/refresh_metadata_job.rb
+++ app/jobs/refresh_metadata_job.rb
@@ -1,6 +1,8 @@
 class RefreshMetadataJob < ApplicationJob
   queue_as :metadata
 
+  limits_concurrency to: 2, key: :refresh_metadata
+
   def perform(object_type:, object_ids:, track_changes: false)
     objects = object_type.where(id: object_ids)
     Metadata::Manager.new.refresh_metadata!(objects, track_changes:)
