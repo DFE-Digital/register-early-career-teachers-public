326fc0e5ccbf52af0a41065f4b168247dab573ad
Tony Headford <tony@binarycircus.com>
2026-05-05 09:31:02 +0100

    ensure we don't change the timestamps

--- db/scripts/deferred_training_periods.rb
+++ db/scripts/deferred_training_periods.rb
@@ -6,7 +6,7 @@ csv_file = Rails.root.join("db/scripts/deferred_training_periods.csv")
 
 CSV.foreach(csv_file, headers: true, header_converters: :symbol) do |row|
   training_period = TrainingPeriod.find(row[:training_period_id])
-  deferred_at = Date.parse(row[:deferred_at])
+  deferred_at = Time.find_zone("UTC").parse(row[:deferred_at])
   deferral_reason = row[:deferral_reason].underscore
 
   MigrationFixes::DeferTrainingPeriod.new(training_period:, deferred_at:, deferral_reason:).defer!
