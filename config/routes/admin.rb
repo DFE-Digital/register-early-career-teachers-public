af546df5721356ac49adf97f8f646f1b557e4c65
Erica Porter <ericaporter@users.noreply.github.com>
2026-05-11 17:10:43 +0100

    Add ability for admin users to create new delivery partners

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -29,7 +29,7 @@ namespace :admin do
       end
 
       resources :lead_providers, only: %i[index], path: "lead-providers"
-      resources :delivery_partners, only: %i[index show edit update], path: "delivery-partners" do
+      resources :delivery_partners, only: %i[index show new create edit update], path: "delivery-partners" do
         resource :delivery_partnerships, only: %i[new create], path: ":year", as: :delivery_partnership, controller: "delivery_partners/delivery_partnerships"
       end
     end
