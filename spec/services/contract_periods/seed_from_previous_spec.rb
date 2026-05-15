10c2946b229eda2022b3a195eef71dda121f6237
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-05-15 14:07:18 +0100

    Expand test cases

--- spec/services/contract_periods/seed_from_previous_spec.rb
+++ spec/services/contract_periods/seed_from_previous_spec.rb
@@ -42,7 +42,19 @@ RSpec.describe ContractPeriods::SeedFromPrevious do
                           identifier: "ecf-standard-september")
       end
 
-      let(:new_schedule) { Schedule.find_by(contract_period_year: current_year) }
+      let!(:previous_schedule_january) do
+        FactoryBot.create(:schedule,
+                          contract_period: previous_contract_period,
+                          identifier: "ecf-standard-january")
+      end
+
+      let!(:previous_april_schedule) do
+        FactoryBot.create(:schedule,
+                          contract_period: previous_contract_period,
+                          identifier: "ecf-standard-april")
+      end
+
+      let(:new_schedules) { Schedule.where(contract_period_year: current_year) }
 
       before do
         previous_schedule_september.milestones.create!(
@@ -55,9 +67,23 @@ RSpec.describe ContractPeriods::SeedFromPrevious do
           start_date: Date.new(previous_year, 9, 1),
           milestone_date: nil
         )
+        previous_schedule_january.milestones.create!(
+          declaration_type: "started",
+          start_date: Date.new(current_year, 1, 1),
+          milestone_date: Date.new(current_year, 6, 30)
+        )
+        previous_schedule_january.milestones.create!(
+          declaration_type: "completed",
+          start_date: Date.new(current_year, 3, 1),
+          milestone_date: nil
+        )
       end
 
       context "when the contract period has not started yet" do
+        let(:new_september_schedule) { new_schedules.find_by(identifier: "ecf-standard-september") }
+        let(:new_january_schedule) { new_schedules.find_by(identifier: "ecf-standard-january") }
+        let(:new_april_schedule) { new_schedules.find_by(identifier: "ecf-standard-april") }
+
         before do
           travel_to 1.day.before(contract_period.started_on)
         end
@@ -67,7 +93,7 @@ RSpec.describe ContractPeriods::SeedFromPrevious do
         end
 
         it "clones from the previous contract period" do
-          expect { service.schedule! }.to change(Schedule, :count).by(1).and change(Milestone, :count).by(2)
+          expect { service.schedule! }.to change(Schedule, :count).by(3).and change(Milestone, :count).by(4)
         end
 
         it "raises an error if called repeatedly" do
@@ -80,26 +106,38 @@ RSpec.describe ContractPeriods::SeedFromPrevious do
 
         it "advances dates by one year if set" do
           service.schedule!
-          expect(new_schedule.milestones.find_by(declaration_type: "started").start_date).to eq(Date.new(current_year, 6, 1))
 
-          expect(new_schedule.milestones.find_by(declaration_type: "started").milestone_date).to eq(Date.new(current_year, 11, 30))
-          expect(new_schedule.milestones.find_by(declaration_type: "retained-1").milestone_date).to be_nil
+          expect(new_september_schedule.milestones.find_by(declaration_type: "started").start_date).to eq(Date.new(current_year, 6, 1))
+          expect(new_september_schedule.milestones.find_by(declaration_type: "retained-1").milestone_date).to be_nil
+
+          expect(new_january_schedule.milestones.find_by(declaration_type: "started").milestone_date).to eq(Date.new(next_year, 6, 30))
         end
 
         it "rolls back changes if an error occurs" do
           allow(Milestone).to receive(:create!).and_raise(ActiveRecord::RecordInvalid)
           expect { service.schedule! }.to raise_error(ActiveRecord::RecordInvalid)
-          expect(new_schedule).to be_nil
+          expect(new_schedules).to be_empty
         end
 
-        it "copies the schedule identifier" do
+        it "copies schedule identifiers" do
           service.schedule!
-          expect(new_schedule.identifier).to eq("ecf-standard-september")
+          expect(new_schedules.pluck(:identifier)).to contain_exactly(
+            "ecf-standard-september",
+            "ecf-standard-january",
+            "ecf-standard-april"
+          )
         end
 
-        it "creates milestones with declaration types copied" do
+        it "copies milestone declaration types" do
           service.schedule!
-          expect(new_schedule.milestones.pluck(:declaration_type)).to contain_exactly("started", "retained-1")
+          expect(new_september_schedule.milestones.pluck(:declaration_type)).to contain_exactly("started", "retained-1")
+          expect(new_january_schedule.milestones.pluck(:declaration_type)).to contain_exactly("started", "completed")
+        end
+
+        it "copies schedules without milestones" do
+          service.schedule!
+          expect(new_april_schedule).not_to be_nil
+          expect(new_april_schedule.milestones).to be_empty
         end
       end
 
