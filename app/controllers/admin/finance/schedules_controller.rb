69093ada0e7a8c52615b71ad065c2e883d69613e
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-05-20 14:18:49 +0100

    Address comments

--- app/controllers/admin/finance/schedules_controller.rb
+++ app/controllers/admin/finance/schedules_controller.rb
@@ -1,6 +1,12 @@
 module Admin::Finance
   class SchedulesController < Admin::Finance::BaseController
+    before_action :set_contract_period
     def index
+    end
+
+  private
+
+    def set_contract_period
       @contract_period = ContractPeriod.find(params[:contract_period_id])
     end
   end
