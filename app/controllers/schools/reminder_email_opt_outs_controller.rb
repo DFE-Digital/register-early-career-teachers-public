c785ef164654223ed3e4d333d31626711250287d
Richard Worrall <richard@soupish.net>
2026-05-21 16:10:30 +0100

    [3966] Ensure opt out params are always strings

--- app/controllers/schools/reminder_email_opt_outs_controller.rb
+++ app/controllers/schools/reminder_email_opt_outs_controller.rb
@@ -13,9 +13,9 @@ module Schools
   private
 
     def authenticate_token!
-      @school = School.find(params[:school_id])
+      @school = School.find(params[:school_id].to_s)
       raise ActiveRecord::RecordNotFound unless
-        ReminderEmailOptOutToken.valid?(school_id: @school.id, token: params[:token])
+        ReminderEmailOptOutToken.valid?(school_id: @school.id, token: params[:token].to_s)
     end
   end
 end
