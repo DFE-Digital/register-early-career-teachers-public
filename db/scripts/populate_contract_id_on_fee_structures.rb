43f7cfd6ba10fdc75e29c059b79594e2b9342e40
Tony Headford <tony@binarycircus.com>
2026-05-21 15:59:08 +0100

    Correct the script name in usage comment

--- db/scripts/populate_contract_id_on_fee_structures.rb
+++ db/scripts/populate_contract_id_on_fee_structures.rb
@@ -1,6 +1,6 @@
 # Populate contract_id value on fee structures so we can reverse the relationship
 #
-# $ kubectl exec -it <pod-name> -- bin/rails runner db/scripts/move_contract_id_to_fee_structures.rb
+# $ kubectl exec -it <pod-name> -- bin/rails runner db/scripts/populate_contract_id_on_fee_structures.rb
 
 Contract.find_each do |contract|
   contract.banded_fee_structure&.update!(contract_id: contract.id)
