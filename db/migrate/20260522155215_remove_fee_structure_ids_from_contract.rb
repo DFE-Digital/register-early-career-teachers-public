dc18c1614d883b96d69ac0380a6c185b84d4582f
Tony Headford <tony@binarycircus.com>
2026-05-26 14:17:25 +0100

    db change to remove fee structure ids from contract

---
+++ db/migrate/20260522155215_remove_fee_structure_ids_from_contract.rb
@@ -0,0 +1,6 @@
+class RemoveFeeStructureIdsFromContract < ActiveRecord::Migration[8.1]
+  def change
+    remove_reference :contracts, :flat_rate_fee_structure, foreign_key: { to_table: :contract_flat_rate_fee_structures }, index: { unique: true }
+    remove_reference :contracts, :banded_fee_structure, foreign_key: { to_table: :contract_banded_fee_structures }, index: { unique: true }
+  end
+end
