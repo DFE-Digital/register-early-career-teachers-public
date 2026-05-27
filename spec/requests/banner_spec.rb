35a83944106372a187b250ce6b1032f13bafbe3d
Erica Porter <ericaporter@users.noreply.github.com>
2026-05-27 14:08:06 +0100

    Add registration window closed banner to notification banners

--- spec/requests/banner_spec.rb
+++ spec/requests/banner_spec.rb
@@ -62,4 +62,50 @@ RSpec.describe "Banner", type: :request do
       end
     end
   end
+
+  context "when registration window is closed" do
+    let(:content) { "Registration not currently open" }
+
+    context "between 1-14 June 2026" do
+      before { travel_to Date.new(2026, 6, 7) }
+
+      it "displays on default layout" do
+        get "/support"
+        expect(response.body).to include(content)
+      end
+
+      it "displays on full layout" do
+        get "/access-denied"
+        expect(response.body).to include(content)
+      end
+    end
+
+    context "before 1 June 2026" do
+      before { travel_to Date.new(2026, 5, 31) }
+
+      it "does not display on default layout" do
+        get "/support"
+        expect(response.body).not_to include(content)
+      end
+
+      it "does not display on full layout" do
+        get "/access-denied"
+        expect(response.body).not_to include(content)
+      end
+    end
+
+    context "from 15 June 2026" do
+      before { travel_to Date.new(2026, 6, 15) }
+
+      it "does not display on default layout" do
+        get "/support"
+        expect(response.body).not_to include(content)
+      end
+
+      it "does not display on full layout" do
+        get "/access-denied"
+        expect(response.body).not_to include(content)
+      end
+    end
+  end
 end
