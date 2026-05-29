c0999d98c39ee1af9697895aa1e0da1d654f3254
Ross Oliver <ross.oliver36@gmail.com>
2026-05-29 09:21:01 +0100

    Make fee structures non-nullable
    Remove redundant `reload` from band consistency check.
    
    Switch contract association strategy to `build` so we can save together and
    avoid validation issues.
    
    Ensure bands are created consistently in factories.
    
    Drop migration to remove old foreign keys (will re-add in follow up PR to avoid
    errors due to inconsistent app/DB state on deploy).
    
    Switch validation rules back to `with_options`.

--- spec/factories/contract_factory.rb
+++ spec/factories/contract_factory.rb
@@ -2,18 +2,21 @@ FactoryBot.define do
   factory(:contract) do
     association :active_lead_provider
 
+    for_ittecf_ectp
+
     trait(:for_ecf) do
       contract_type { "ecf" }
       ecf_contract_version { "1" }
-      association :banded_fee_structure, :with_bands, factory: :contract_banded_fee_structure
+      association :banded_fee_structure, :with_bands, factory: :contract_banded_fee_structure, strategy: :build
+      flat_rate_fee_structure { nil }
     end
 
     trait(:for_ittecf_ectp) do
       contract_type { "ittecf_ectp" }
       ecf_contract_version { "1" }
       ecf_mentor_contract_version { "2" }
-      association :banded_fee_structure, :with_bands, factory: :contract_banded_fee_structure
-      association :flat_rate_fee_structure, factory: :contract_flat_rate_fee_structure
+      association :banded_fee_structure, :with_bands, factory: :contract_banded_fee_structure, strategy: :build
+      association :flat_rate_fee_structure, factory: :contract_flat_rate_fee_structure, strategy: :build
     end
   end
 end
