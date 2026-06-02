63cdd208efe94987b5bc5b226262d910c853e599
Craig Davidson <craigmdavidson@gmail.com>
2026-06-02 14:54:00 +0100

    Replace contracts_helper with contract.description

--- app/helpers/admin/contracts_helper.rb
+++
@@ -1,7 +0,0 @@
-module Admin::ContractsHelper
-  def contract_heading(contract)
-    contract_name = contract.ittecf_ectp_contract_type? ? "ITTECF ECTP" : "ECF"
-    vat_rate = number_to_percentage(contract.applicable_vat_rate * 100, precision: 0)
-    "#{contract_name} (#{vat_rate} VAT)"
-  end
-end
