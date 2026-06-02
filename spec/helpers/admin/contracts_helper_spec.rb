3d14d0720a3c7eab336b7de2c4d3d64578fc744f
Craig Davidson <craigmdavidson@gmail.com>
2026-06-02 09:52:01 +0100

    Rubocop

--- spec/helpers/admin/contracts_helper_spec.rb
+++ spec/helpers/admin/contracts_helper_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Admin::ContractsHelper, type: :helper do
 
     context "when ECF type" do
       let(:contract) { FactoryBot.create(:contract, :for_ecf) }
+
       it { expect(helper.contract_heading(contract)).to eq("ECF (20% VAT)") }
     end
 
@@ -13,6 +14,7 @@ RSpec.describe Admin::ContractsHelper, type: :helper do
       let(:lead_provider) { FactoryBot.create(:lead_provider, vat_registered: false) }
       let(:active_lead_provider) { FactoryBot.create(:active_lead_provider, lead_provider:) }
       let(:contract) { FactoryBot.create(:contract, :for_ecf, active_lead_provider:) }
+
       it { expect(helper.contract_heading(contract)).to eq("ECF (0% VAT)") }
     end
   end
