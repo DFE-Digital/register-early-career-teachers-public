433cb47d6de92d6ef41c9c0e050fdb1722932189
Peter Yates <peter.yates@graphia.co.uk>
2026-06-02 16:26:38 +0100

    Add active_lead_providers method to active service
    This method returns a list of the active lead providers for the given
    contract period

--- spec/services/lead_providers/active_spec.rb
+++ spec/services/lead_providers/active_spec.rb
@@ -15,14 +15,31 @@ describe LeadProviders::Active do
     let(:lead_provider) { FactoryBot.create(:lead_provider) }
     let(:contract_period) { FactoryBot.create(:contract_period) }
 
-    context "when an active_lead_provider record exists for the registration period" do
+    context "when an active_lead_provider record exists for the contract period" do
       let!(:active_lead_provider) { FactoryBot.create(:active_lead_provider, lead_provider:, contract_period:) }
 
       it { is_expected.to be(true) }
     end
 
-    context "when no active_lead_provider record exists for the registration period" do
+    context "when no active_lead_provider record exists for the contract period" do
       it { is_expected.to be(false) }
+    end
+  end
+
+  describe "#active_lead_providers" do
+    subject { LeadProviders::Active.new(lead_provider).active_lead_providers(contract_period) }
+
+    let(:lead_provider) { FactoryBot.create(:lead_provider) }
+    let(:contract_period) { FactoryBot.create(:contract_period) }
+
+    context "when an active_lead_provider record exists for the contract period" do
+      let!(:active_lead_provider) { FactoryBot.create(:active_lead_provider, lead_provider:, contract_period:) }
+
+      it { is_expected.to include(active_lead_provider) }
+    end
+
+    context "when no active_lead_provider record exists for the contract period" do
+      it { is_expected.to be_empty }
     end
   end
 
