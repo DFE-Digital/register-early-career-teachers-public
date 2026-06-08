dbd88f0baae32c7f39ea3e52a69e791ca3e8217c
Tony Headford <tony@binarycircus.com>
2026-06-08 15:19:50 +0100

    fix rubocop offences

--- db/scripts/increase_band_b_for_edt.rb
+++ db/scripts/increase_band_b_for_edt.rb
@@ -3,20 +3,18 @@
 #
 # $ kubectl exec -it <pod-name> -- bin/rails runner db/scripts/increase_band_b_for_edt.rb
 
-begin
-  name = "Education Development Trust"
-  contract_period_year = 2025
+name = "Education Development Trust"
+contract_period_year = 2025
 
-  lead_provider = LeadProvider.find_by!(name:)
-  active_lead_provider = ActiveLeadProvider.find_by!(lead_provider:, contract_period_year:)
+lead_provider = LeadProvider.find_by!(name:)
+active_lead_provider = ActiveLeadProvider.find_by!(lead_provider:, contract_period_year:)
 
-  contract = Contract.find_by!(active_lead_provider:)
+contract = Contract.find_by!(active_lead_provider:)
 
-  band = contract.banded_fee_structure.bands.order(min_declarations: :desc).second
+band = contract.banded_fee_structure.bands.order(min_declarations: :desc).second
 
-  raise "Max declarations #{band.max_declarations} is not 3102!" if band.max_declarations != 3102
+raise "Max declarations #{band.max_declarations} is not 3102!" if band.max_declarations != 3102
 
-  band.update!(max_declarations: 4000)
+band.update!(max_declarations: 4000)
 
-  puts "Band B updated: #{band.min_declarations} - #{band.max_declarations}"
-end
+Rails.logger.info "EDT 2025 contract band B updated: #{band.min_declarations} - #{band.max_declarations}"
