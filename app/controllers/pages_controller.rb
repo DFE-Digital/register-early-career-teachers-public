849cceb45a76bf8d8774ff6be9663495b71f9032
Erica Porter <ericaporter@users.noreply.github.com>
2026-06-15 12:18:42 +0100

    Configure DfE Sign-in access request URLs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
   after_action :allow_search_engine_indexing, only: :home
 
-  DFE_SIGN_IN_REQUEST_ORGANISATION_URL =
-    "https://services.signin.education.gov.uk/request-organisation/search"
-
-  DFE_SIGN_IN_MY_SERVICES_URL =
-    "https://services.signin.education.gov.uk/my-services"
-
   def home
     redirect_to(post_login_redirect_path) and return if authenticated?
   end
@@ -14,8 +8,10 @@ class PagesController < ApplicationController
   # Unrecognised DfE Sign In user (org/role)
   def access_denied
     @organisation_name = session[:invalid_user_organisation_name]
-    @dfe_sign_in_request_organisation_url = DFE_SIGN_IN_REQUEST_ORGANISATION_URL
-    @dfe_sign_in_my_services_url = DFE_SIGN_IN_MY_SERVICES_URL
+    @dfe_sign_in_request_organisation_url =
+      Rails.application.config.dfe_sign_in_request_organisation_url
+    @dfe_sign_in_my_services_url =
+      Rails.application.config.dfe_sign_in_my_services_url
   end
 
   def support
