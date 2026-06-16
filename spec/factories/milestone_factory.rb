fbf2c6f842a5703741adca9726fab1ba45cee9d8
Peter Yates <peter.yates@graphia.co.uk>
2026-06-16 18:00:17 +0100

    Add specs covering TrainingPeriodBuilder

--- spec/factories/milestone_factory.rb
+++ spec/factories/milestone_factory.rb
@@ -9,5 +9,12 @@ FactoryBot.define do
     declaration_type { "started" }
     start_date { schedule&.contract_period&.started_on }
     milestone_date { 1.month.after(start_date) }
+
+    trait(:started) { declaration_type { "started" } }
+    trait(:retained_1) { declaration_type { "retained-1" } }
+    trait(:retained_2) { declaration_type { "retained-2" } }
+    trait(:retained_3) { declaration_type { "retained-3" } }
+    trait(:retained_4) { declaration_type { "retained-4" } }
+    trait(:completed) { declaration_type { "completed" } }
   end
 end
