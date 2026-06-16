889c159bc23fefc028afbaa5398cac581788f4b6
Richard Worrall <richard@soupish.net>
2026-06-16 12:52:26 +0100

    Avoid potentially broken reverse migration

--- db/migrate/20260615120000_remove_school_urn_from_school_funding_eligibilities.rb
+++ db/migrate/20260615120000_remove_school_urn_from_school_funding_eligibilities.rb
@@ -8,17 +8,6 @@ class RemoveSchoolURNFromSchoolFundingEligibilities < ActiveRecord::Migration[8.
   end
 
   def down
-    add_column :school_funding_eligibilities, :school_urn, :bigint
-    add_index :school_funding_eligibilities, :school_urn
-
-    execute(<<~SQL)
-      UPDATE school_funding_eligibilities
-      SET school_urn = gias_school_urn
-      WHERE school_urn IS NULL
-    SQL
-
-    add_foreign_key :school_funding_eligibilities, :schools,
-                    column: :school_urn,
-                    primary_key: :urn
+    raise ActiveRecord::IrreversibleMigration
   end
 end
