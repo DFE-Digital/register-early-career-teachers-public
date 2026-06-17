a60ccd1eefb02a1cb948d648a7a6134148c809f8
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-06-17 21:40:11 +0100

    [3404] Hook the check answers step up to
    the current active period change service.

--- app/wizards/admin/teachers/training_periods/change_contract_period_wizard/check_answers_step.rb
+++ app/wizards/admin/teachers/training_periods/change_contract_period_wizard/check_answers_step.rb
@@ -3,11 +3,33 @@ module Admin
     module TrainingPeriods
       module ChangeContractPeriodWizard
         class CheckAnswersStep < Step
+          CurrentActivePeriodChange = ::Admin::Teachers::TrainingPeriods::ChangeContractPeriod::CurrentActivePeriod
+
           self.expected_store_keys = %i[contract_period_year school_partnership_id]
 
           def previous_step = :select_partnership
 
-          def save! = true
+          def save!
+            service.change_contract_period!
+            true
+          rescue CurrentActivePeriodChange::UnsupportedTrainingPeriodError
+            errors.add(:base, "Training period is not eligible for contract period change")
+            false
+          rescue CurrentActivePeriodChange::ScheduleNotFoundError
+            errors.add(:base, "A matching schedule could not be found for the selected contract period")
+            false
+          end
+
+        private
+
+          def service
+            CurrentActivePeriodChange.new(
+              training_period: wizard.training_period,
+              contract_period: wizard.selected_contract_period,
+              school_partnership: wizard.selected_school_partnership,
+              author: wizard.author
+            )
+          end
         end
       end
     end
