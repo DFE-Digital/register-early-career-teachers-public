2c13d3fb56042e3786f0770f167bf94112765011
Erica Porter <ericaporter@users.noreply.github.com>
2026-06-18 16:07:00 +0100

    Add migration to backfill data

---
+++ db/migrate/20260618144509_backfill_uplift_target_ratio_on_contract_banded_fee_structures.rb
@@ -0,0 +1,21 @@
+class BackfillUpliftTargetRatioOnContractBandedFeeStructures < ActiveRecord::Migration[8.1]
+  UPLIFT_TARGET_RATIOS_BY_YEAR = {
+    2021 => 0.33,
+    2022 => 0.33,
+    2023 => 0.33,
+    2024 => 0.4,
+    2025 => 0.0
+  }.freeze
+
+  def up
+    UPLIFT_TARGET_RATIOS_BY_YEAR.each do |year, uplift_target_ratio|
+      Contract::BandedFeeStructure
+        .joins(contract: :active_lead_provider)
+        .where(active_lead_providers: { contract_period_year: year })
+        .where(contracts: { contract_type: "ecf" })
+        .update_all(uplift_target_ratio:)
+    end
+  end
+
+  def down = nil
+end
