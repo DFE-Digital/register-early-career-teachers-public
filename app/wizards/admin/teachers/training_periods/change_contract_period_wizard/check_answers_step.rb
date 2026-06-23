4a2f38c98b2d950cc0870db7ed941ee8b55d5924
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-06-23 14:29:59 +0100

    [3404] Support contract period changes for future periods
    Allow eligible future training periods through the contract period change
    journey and route check answers submissions to the `FuturePeriod` service.
    
    Keep future periods with different LP/DP combinations ineligible.

--- app/wizards/admin/teachers/training_periods/change_contract_period_wizard/check_answers_step.rb
+++ app/wizards/admin/teachers/training_periods/change_contract_period_wizard/check_answers_step.rb
@@ -4,6 +4,7 @@ module Admin
       module ChangeContractPeriodWizard
         class CheckAnswersStep < Step
           CurrentActivePeriodChange = ::Admin::Teachers::TrainingPeriods::ChangeContractPeriod::CurrentActivePeriod
+          FuturePeriodChange = ::Admin::Teachers::TrainingPeriods::ChangeContractPeriod::FuturePeriod
 
           self.expected_store_keys = %i[contract_period_year]
 
@@ -14,10 +15,12 @@ module Admin
           def save!
             service.change_contract_period!
             true
-          rescue CurrentActivePeriodChange::UnsupportedTrainingPeriodError
+          rescue CurrentActivePeriodChange::UnsupportedTrainingPeriodError,
+                 FuturePeriodChange::UnsupportedTrainingPeriodError
             errors.add(:base, "Training period is not eligible for contract period change")
             false
-          rescue CurrentActivePeriodChange::ScheduleNotFoundError
+          rescue CurrentActivePeriodChange::ScheduleNotFoundError,
+                 FuturePeriodChange::ScheduleNotFoundError
             errors.add(:base, "A matching schedule could not be found for the selected contract period")
             false
           rescue CurrentActivePeriodChange::ActiveLeadProviderNotFoundError
@@ -28,12 +31,18 @@ module Admin
         private
 
           def service
-            CurrentActivePeriodChange.new(
+            service_class.new(
               training_period: wizard.training_period,
               contract_period: wizard.selected_contract_period,
               school_partnership: wizard.selected_school_partnership,
               author: wizard.author
             )
+          end
+
+          def service_class
+            return FuturePeriodChange if wizard.training_period.started_on > Time.zone.today
+
+            CurrentActivePeriodChange
           end
         end
       end
