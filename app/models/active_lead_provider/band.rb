3e1ed55dcfe6f6e02a6fa0da7a0c5a5bed61b550
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-06-24 09:42:54 +0100

    Fix snags

--- app/models/active_lead_provider/band.rb
+++ app/models/active_lead_provider/band.rb
@@ -4,7 +4,7 @@ class ActiveLeadProvider::Band < ApplicationRecord
   attr_readonly :allocation_order
 
   # Associations
-  belongs_to :active_lead_provider, optional: false
+  belongs_to :active_lead_provider
 
   has_many :terms,
            class_name: "Contract::BandedFeeStructure::Band", # TODO: rename to BandTerms
@@ -28,61 +28,64 @@ class ActiveLeadProvider::Band < ApplicationRecord
               message: "Capacity must be a number greater than zero"
             }
 
-  validate :first_band_allocation_order_is_one, on: :create
-
   # Callbacks
+  before_update :prevent_update, unless: :last?
+  before_destroy :abort_destruction, unless: :last?
   before_validation :assign_allocation_order,
                     on: :create,
-                    if: -> { active_lead_provider.present? && allocation_order.blank? }
+                    if: -> { active_lead_provider.present? }
 
-  # @return [Boolean]
-  def editable?
-    active_lead_provider.bands.last == self
-  end
-
-  # @return [Boolean]
-  def allocated?
-    allocation_order.present? && persisted?
-  end
-
-  # @return [Boolean]
-  def first_band?
-    return false unless allocated?
-
-    allocation_order == 1
-  end
-
-  def previous_band
-    return nil unless allocated?
-    return nil if first_band?
-
-    active_lead_provider.bands.find_by(allocation_order: allocation_order - 1)
-  end
-
+  # @return [Integer, nil]
   def min_declarations
-    return nil unless allocated?
-    return 1 if first_band?
+    return nil unless has_allocation_order?
+    return 1 if first?
 
     previous_band.max_declarations + 1
   end
 
+  # @return [Integer, nil]
   def max_declarations
-    return nil unless allocated?
-    return capacity if first_band?
+    return nil unless has_allocation_order?
+    return capacity if first?
 
     min_declarations + capacity - 1
   end
 
 private
 
+  # Read-only
   def assign_allocation_order
     self.allocation_order = active_lead_provider.bands.count + 1
   end
 
-  def first_band_allocation_order_is_one
-    return unless allocation_order && allocation_order > 1
-    return if active_lead_provider.bands.exists?
+  def prevent_update
+    raise ActiveRecord::RecordNotSaved.new("Only the last band can be updated", self)
+  end
 
-    errors.add(:allocation_order, "The first band's allocation order must be 1")
+  def abort_destruction
+    raise ActiveRecord::RecordNotDestroyed.new("Only the last band can be destroyed", self)
+  end
+
+  # @return [Boolean]
+  def has_allocation_order?
+    allocation_order.present? && persisted?
+  end
+
+  # @return [Boolean]
+  def first?
+    allocation_order == 1
+  end
+
+  # @return [Boolean]
+  def last?
+    active_lead_provider.bands.last == self
+  end
+
+  # @return [ActiveLeadProvider::Band]
+  def previous_band
+    return nil unless has_allocation_order?
+    return nil if first?
+
+    active_lead_provider.bands.find_by(allocation_order: allocation_order - 1)
   end
 end
--- spec/factories/active_lead_provider/active_lead_provider_band_factory.rb
+++ spec/factories/active_lead_provider/active_lead_provider_band_factory.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :active_lead_provider_band, class: "ActiveLeadProvider::Band" do
     association :active_lead_provider
-    allocation_order { nil }
     capacity { 100 }
   end
 end
