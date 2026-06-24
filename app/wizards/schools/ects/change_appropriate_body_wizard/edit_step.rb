7d3ecf73faccc5389d0e4b084861819706e675db
thenapking <tom.giles@gmail.com>
2026-06-24 15:02:23 +0100

    [2105] Filter current AB from list

--- app/wizards/schools/ects/change_appropriate_body_wizard/edit_step.rb
+++ app/wizards/schools/ects/change_appropriate_body_wizard/edit_step.rb
@@ -7,8 +7,6 @@ module Schools
         validates :appropriate_body_id,
                   presence: { message: "Select the appropriate body which will be supporting the ECT's induction" }
 
-        validate :appropriate_body_id_must_change, if: -> { appropriate_body_id.present? }
-
         def self.permitted_params = [:appropriate_body_id]
 
         def next_step = :check_answers
@@ -17,16 +15,21 @@ module Schools
           store.appropriate_body_id = appropriate_body_id if valid_step?
         end
 
+        def appropriate_bodies_except_current
+          AppropriateBodyPeriod
+          .active
+          .where.not(id: current_appropriate_body_id)
+          .select(:id, :name)
+        end
+
       private
 
         def pre_populate_attributes
           self.appropriate_body_id = store.appropriate_body_id
         end
 
-        def appropriate_body_id_must_change
-          return unless appropriate_body_id == ect_at_school_period.school_reported_appropriate_body_id.to_s
-
-          errors.add(:appropriate_body_id, "You must select a different appropriate body")
+        def current_appropriate_body_id
+          ect_at_school_period.school_reported_appropriate_body_id
         end
       end
     end
