888e813c89a8541017aafc208a0c6d9e2018f5ff
thenapking <tom.giles@gmail.com>
2026-06-30 17:26:13 +0100

    [4038] Add events when teachers moved

--- app/services/gias/schools/replace.rb
+++ app/services/gias/schools/replace.rb
@@ -18,17 +18,54 @@ module GIAS::Schools
 
     def replace_school!
       ActiveRecord::Base.transaction do
-        school.update!(urn: gias_school.successor.urn)
+        ect_at_school_periods_to_be_moved =
+          school.ect_at_school_periods.includes(:teacher).load
+
+        mentor_at_school_periods_to_be_moved =
+          school.mentor_at_school_periods.includes(:teacher).load
+
+        old_school_name = Schools::Name.new(school).name_and_urn
+
+        school.update!(urn: successor.urn)
         record_school_replaced_event!
+
+        record_ect_moved_to_new_school_event!(ect_at_school_periods_to_be_moved, old_school_name)
+        record_mentor_moved_to_new_school_event!(mentor_at_school_periods_to_be_moved, old_school_name)
+      end
+    end
+
+    def record_ect_moved_to_new_school_event!(periods, old_school_name)
+      periods.each do |ect_at_school_period|
+        Events::Record.record_teacher_ect_at_school_period_moved_school!(
+          teacher: ect_at_school_period.teacher,
+          ect_at_school_period:,
+          new_school: successor.school,
+          old_school_name:,
+          happened_at: successor.opened_on,
+          author:
+        )
+      end
+    end
+
+    def record_mentor_moved_to_new_school_event!(periods, old_school_name)
+      periods.each do |mentor_at_school_period|
+        Events::Record.record_teacher_mentor_at_school_period_moved_school!(
+          teacher: mentor_at_school_period.teacher,
+          mentor_at_school_period:,
+          new_school: successor.school,
+          old_school_name:,
+          happened_at: successor.opened_on,
+          author:
+        )
       end
     end
 
     def record_school_replaced_event!
       Events::Record.record_school_changed_event!(
         school:,
-        new_gias_school: gias_school.successor,
+        new_gias_school: successor,
         old_gias_school: gias_school,
-        happened_at: gias_school.successor.opened_on,
+        happened_at: successor.opened_on,
         author:
       )
     end
@@ -37,6 +74,9 @@ module GIAS::Schools
       Events::SystemAuthor.new
     end
 
+    def new_school = successor.school
+
+    delegate :successor, to: :gias_school
     delegate :school, to: :gias_school
   end
 end
