924e3bd9dd6c7f58fc7a1b6fb08842f63bcdc221
Ross Oliver <ross.oliver36@gmail.com>
2026-07-02 10:33:16 +0100

    Add `/mentorship-periods` endpoint
    Returns `MentorshipPeriod` records for all ECTs of a lead provider, linking
    back to the ECT/mentor participants in each

---
+++ app/services/api/mentorship_periods/query.rb
@@ -0,0 +1,38 @@
+module API::MentorshipPeriods
+  class Query
+    include Queries::FilterIgnorable
+
+    attr_reader :scope
+
+    def initialize(lead_provider_id:)
+      @scope = MentorshipPeriod.current_or_future
+
+      where_lead_provider(lead_provider_id)
+    end
+
+    def mentorship_periods
+      preload_associations(block_given? ? yield(scope) : scope)
+    end
+
+  private
+
+    def preload_associations(results)
+      results
+        .strict_loading
+        .includes(
+          mentee: %i[teacher school],
+          mentor: :teacher
+        )
+    end
+
+    def where_lead_provider(lead_provider_id)
+      @scope = scope
+        .joins(mentee: { training_periods: :lead_provider })
+        .where(
+          lead_providers: {
+            id: lead_provider_id,
+          }
+        )
+    end
+  end
+end
