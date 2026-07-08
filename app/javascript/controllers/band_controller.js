dab91c40355fbcfe6a3b2bec9ef20093bb522c27
Peter David Hamilton <pete@peterdavidhamilton.com>
2026-07-08 07:17:31 +0100

    - round % to whole number - restrict % to 0..100 - test JS - move attributes and style to ERB and CSS

--- app/javascript/controllers/band_controller.js
+++ app/javascript/controllers/band_controller.js
@@ -3,17 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class BandController extends Controller {
   static targets = ["outputPercentage", "servicePercentage"]
 
-  connect() {
-    const target = this.servicePercentageTarget
-    target.readOnly = true
-    target.tabIndex = -1
-    target.style.opacity = "0.5"
-    target.style.cursor = "not-allowed"
-    target.style.backgroundColor = "transparent"
-  }
-
+  // Ensure output_fee_percentage is 0..100
+  // Update service_fee_percentage with delta
   updateServiceFee() {
-    const output = Number.parseFloat(this.outputPercentageTarget.value)
-    this.servicePercentageTarget.value = Number.isNaN(output) ? "" : (100 - output).toFixed(2)
+    let output = Number.parseFloat(this.outputPercentageTarget.value)
+
+    if (Number.isNaN(output)) {
+      this.servicePercentageTarget.value = ""
+      return
+    }
+
+    output = Math.max(0, Math.min(100, Math.round(output)))
+    this.outputPercentageTarget.value = output
+    this.servicePercentageTarget.value = (100 - output)
   }
 }
