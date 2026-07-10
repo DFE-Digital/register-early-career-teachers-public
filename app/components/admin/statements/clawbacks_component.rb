7982b2bc95db37dc2673b86e7cae35e692f59834
Richard Worrall <richard@soupish.net>
2026-07-10 16:58:25 +0100

    [3726] Extract standardised statement component presenter_for method

--- app/components/admin/statements/clawbacks_component.rb
+++ app/components/admin/statements/clawbacks_component.rb
@@ -1,6 +1,8 @@
 module Admin
   module Statements
     class ClawbacksComponent < ApplicationComponent
+      include CalculatorPresenters
+
       attr_reader :statement
 
       def initialize(statement:)
@@ -14,14 +16,6 @@ module Admin
 
       def clawback_tables
         @clawback_tables ||= statement.calculators.map { presenter_for(it) }
-      end
-
-      def presenter_for(calculator)
-        return ECFPresenter.new(calculator) if contract.ecf_contract_type?
-        return FlatRatePresenter.new(calculator) if calculator.flat_rate?
-        return BandedPresenter.new(calculator) if calculator.banded?
-
-        raise ArgumentError, "Unknown calculator type: #{calculator.class}"
       end
     end
   end
--- app/components/admin/statements/output_payments_component.rb
+++ app/components/admin/statements/output_payments_component.rb
@@ -1,6 +1,8 @@
 module Admin
   module Statements
     class OutputPaymentsComponent < ApplicationComponent
+      include CalculatorPresenters
+
       attr_reader :statement
 
       def initialize(statement:)
@@ -18,14 +20,6 @@ module Admin
 
       def output_payment_tables
         @output_payment_tables ||= statement.calculators.map { presenter_for(it) }
-      end
-
-      def presenter_for(calculator)
-        return ECFPresenter.new(calculator) if contract.ecf_contract_type?
-        return FlatRatePresenter.new(calculator) if calculator.flat_rate?
-        return BandedPresenter.new(calculator) if calculator.banded?
-
-        raise ArgumentError, "Unknown calculator type: #{calculator.class}"
       end
     end
   end
