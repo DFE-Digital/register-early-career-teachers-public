81e9bbd6ff1bbae9f6230f8c3f594c33e6e9c7e9
Erica Porter <ericaporter@users.noreply.github.com>
2026-07-14 16:38:50 +0100

    Make variable names clearer

--- spec/services/teachers/ect_withdrawal_history_correction_spec.rb
+++ spec/services/teachers/ect_withdrawal_history_correction_spec.rb
@@ -2,9 +2,9 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
   subject(:correct_withdrawal_history) do
     described_class.new(
       ect_at_school_period:,
-      source_training_period:,
-      target_training_period:,
-      corrected_finished_on:,
+      original_training_period:,
+      erroneous_withdrawn_training_period:,
+      corrected_end_date:,
       author:
     ).correct!
   end
@@ -12,11 +12,11 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
   let(:author) { Events::SystemAuthor.new }
 
   let(:ect_started_on) { Date.new(2023, 9, 11) }
-  let(:target_finished_on) { Date.new(2023, 11, 23) }
-  let(:corrected_finished_on) { Date.new(2023, 11, 10) }
+  let(:original_finished_on) { Date.new(2023, 11, 23) }
+  let(:corrected_end_date) { Date.new(2023, 11, 10) }
 
-  let(:source_started_on) { Date.new(2025, 10, 7) }
-  let(:source_finished_on) { Date.new(2025, 11, 6) }
+  let(:erroneous_started_on) { Date.new(2025, 10, 7) }
+  let(:erroneous_finished_on) { Date.new(2025, 11, 6) }
   let(:withdrawn_at) { Time.zone.parse("2025-11-06T12:20:55Z") }
 
   let(:ect_at_school_period) do
@@ -46,53 +46,55 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
     )
   end
 
-  let!(:target_training_period) do
+  let!(:original_training_period) do
     FactoryBot.create(
       :training_period,
       :for_ect,
       :provider_led,
       ect_at_school_period:,
       started_on: ect_started_on,
-      finished_on: target_finished_on,
+      finished_on: original_finished_on,
       withdrawn_at: nil,
       withdrawal_reason: nil
     )
   end
 
-  let!(:source_training_period) do
+  let!(:erroneous_withdrawn_training_period) do
     FactoryBot.create(
       :training_period,
       :for_ect,
       :provider_led,
       ect_at_school_period:,
-      started_on: source_started_on,
-      finished_on: source_finished_on,
+      started_on: erroneous_started_on,
+      finished_on: erroneous_finished_on,
       withdrawn_at:,
       withdrawal_reason: "other"
     )
   end
 
   describe "#correct!" do
-    it "moves the withdrawal details onto the target training period" do
+    it "moves the withdrawal details onto the original training period" do
       correct_withdrawal_history
 
-      expect(target_training_period.reload).to have_attributes(
+      expect(original_training_period.reload).to have_attributes(
         withdrawn_at:,
         withdrawal_reason: "other"
       )
     end
 
-    it "changes the target training period end date" do
+    it "changes the original training period end date" do
       correct_withdrawal_history
 
-      expect(target_training_period.reload.finished_on)
-        .to eq(corrected_finished_on)
+      expect(original_training_period.reload.finished_on)
+        .to eq(corrected_end_date)
     end
 
-    it "removes the source training period" do
+    it "removes the erroneous withdrawn training period" do
       expect { correct_withdrawal_history }
         .to change {
-          TrainingPeriod.exists?(source_training_period.id)
+          TrainingPeriod.exists?(
+            erroneous_withdrawn_training_period.id
+          )
         }
         .from(true)
         .to(false)
@@ -102,14 +104,14 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
       correct_withdrawal_history
 
       expect(ect_at_school_period.reload.finished_on)
-        .to eq(corrected_finished_on)
+        .to eq(corrected_end_date)
     end
 
     it "finishes the linked mentorship period" do
       correct_withdrawal_history
 
       expect(mentorship_period.reload.finished_on)
-        .to eq(corrected_finished_on)
+        .to eq(corrected_end_date)
     end
 
     it "does not record events" do
@@ -117,13 +119,14 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         .not_to change(Event, :count)
     end
 
-    context "when the source and target training periods are the same" do
+    context "when the original and erroneous periods are the same" do
       subject(:correct_withdrawal_history) do
         described_class.new(
           ect_at_school_period:,
-          source_training_period: target_training_period,
-          target_training_period:,
-          corrected_finished_on:,
+          original_training_period:,
+          erroneous_withdrawn_training_period:
+            original_training_period,
+          corrected_end_date:,
           author:
         ).correct!
       end
@@ -132,43 +135,12 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Source and target training periods must be different"
+            "Original and erroneous training periods must be different"
           )
       end
     end
 
-    context "when the source training period belongs to another ECT-at-school period" do
-      let(:other_ect_at_school_period) do
-        FactoryBot.create(
-          :ect_at_school_period,
-          started_on: source_started_on,
-          finished_on: nil
-        )
-      end
-
-      let(:source_training_period) do
-        FactoryBot.create(
-          :training_period,
-          :for_ect,
-          :provider_led,
-          ect_at_school_period: other_ect_at_school_period,
-          started_on: source_started_on,
-          finished_on: source_finished_on,
-          withdrawn_at:,
-          withdrawal_reason: "other"
-        )
-      end
-
-      it "raises an error" do
-        expect { correct_withdrawal_history }
-          .to raise_error(
-            RuntimeError,
-            "Source training period does not belong to the ECT-at-school period"
-          )
-      end
-    end
-
-    context "when the target training period belongs to another ECT-at-school period" do
+    context "when the original training period belongs to another ECT-at-school period" do
       let(:other_ect_at_school_period) do
         FactoryBot.create(
           :ect_at_school_period,
@@ -177,14 +149,14 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         )
       end
 
-      let(:target_training_period) do
+      let(:original_training_period) do
         FactoryBot.create(
           :training_period,
           :for_ect,
           :provider_led,
           ect_at_school_period: other_ect_at_school_period,
           started_on: ect_started_on,
-          finished_on: target_finished_on,
+          finished_on: original_finished_on,
           withdrawn_at: nil,
           withdrawal_reason: nil
         )
@@ -194,7 +166,38 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Target training period does not belong to the ECT-at-school period"
+            "Original training period does not belong to the ECT-at-school period"
+          )
+      end
+    end
+
+    context "when the erroneous training period belongs to another ECT-at-school period" do
+      let(:other_ect_at_school_period) do
+        FactoryBot.create(
+          :ect_at_school_period,
+          started_on: erroneous_started_on,
+          finished_on: nil
+        )
+      end
+
+      let(:erroneous_withdrawn_training_period) do
+        FactoryBot.create(
+          :training_period,
+          :for_ect,
+          :provider_led,
+          ect_at_school_period: other_ect_at_school_period,
+          started_on: erroneous_started_on,
+          finished_on: erroneous_finished_on,
+          withdrawn_at:,
+          withdrawal_reason: "other"
+        )
+      end
+
+      it "raises an error" do
+        expect { correct_withdrawal_history }
+          .to raise_error(
+            RuntimeError,
+            "Erroneous training period does not belong to the ECT-at-school period"
           )
       end
     end
@@ -202,7 +205,7 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
     context "when the ECT-at-school period is already finished" do
       before do
         ect_at_school_period.update_columns(
-          finished_on: corrected_finished_on
+          finished_on: corrected_end_date
         )
       end
 
@@ -215,22 +218,22 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
       end
     end
 
-    context "when the corrected finish date is before the ECT-at-school period starts" do
-      let(:corrected_finished_on) { ect_started_on - 1.day }
+    context "when the corrected end date is before the ECT-at-school period starts" do
+      let(:corrected_end_date) { ect_started_on - 1.day }
 
       it "raises an error" do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Corrected finish date is before the ECT-at-school period start date"
+            "Corrected end date is before the ECT-at-school period start date"
           )
       end
     end
 
-    context "when the target training period starts after the corrected finish date" do
+    context "when the original training period starts after the corrected end date" do
       before do
-        target_training_period.update!(
-          started_on: corrected_finished_on + 1.day
+        original_training_period.update!(
+          started_on: corrected_end_date + 1.day
         )
       end
 
@@ -238,15 +241,15 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Target training period starts after the corrected finish date"
+            "Original training period starts after the corrected end date"
           )
       end
     end
 
-    context "when the target training period finishes before the corrected finish date" do
+    context "when the original training period finishes before the corrected end date" do
       before do
-        target_training_period.update!(
-          finished_on: corrected_finished_on - 1.day
+        original_training_period.update!(
+          finished_on: corrected_end_date - 1.day
         )
       end
 
@@ -254,15 +257,15 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Target training period finishes before the corrected finish date"
+            "Original training period finishes before the corrected end date"
           )
       end
     end
 
-    context "when the source training period starts before the corrected finish date" do
+    context "when the erroneous training period starts before the corrected end date" do
       before do
-        source_training_period.update_columns(
-          started_on: corrected_finished_on - 1.day
+        erroneous_withdrawn_training_period.update_columns(
+          started_on: corrected_end_date - 1.day
         )
       end
 
@@ -270,14 +273,14 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Source training period starts before the corrected finish date"
+            "Erroneous training period starts before the corrected end date"
           )
       end
     end
 
-    context "when the target training period already has withdrawal details" do
+    context "when the original training period already has withdrawal details" do
       before do
-        target_training_period.update!(
+        original_training_period.update!(
           withdrawn_at: Time.zone.parse("2024-01-01T12:00:00Z"),
           withdrawal_reason: "other"
         )
@@ -287,14 +290,14 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Target training period already has withdrawal details"
+            "Original training period already has withdrawal details"
           )
       end
     end
 
-    context "when the source training period has no withdrawal timestamp" do
+    context "when the erroneous period has no withdrawal timestamp" do
       before do
-        source_training_period.update_columns(
+        erroneous_withdrawn_training_period.update_columns(
           withdrawn_at: nil
         )
       end
@@ -303,14 +306,14 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Source training period has incomplete withdrawal details"
+            "Erroneous training period has incomplete withdrawal details"
           )
       end
     end
 
-    context "when the source training period has no withdrawal reason" do
+    context "when the erroneous period has no withdrawal reason" do
       before do
-        source_training_period.update_columns(
+        erroneous_withdrawn_training_period.update_columns(
           withdrawal_reason: nil
         )
       end
@@ -319,17 +322,17 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Source training period has incomplete withdrawal details"
+            "Erroneous training period has incomplete withdrawal details"
           )
       end
     end
 
-    context "when the target training period has declarations" do
+    context "when the original training period has declarations" do
       before do
         FactoryBot.create(
           :declaration,
           :eligible,
-          training_period: target_training_period
+          training_period: original_training_period
         )
       end
 
@@ -337,16 +340,16 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Target training period has declarations"
+            "Original training period has declarations"
           )
 
         expect_records_to_remain_unchanged
       end
     end
 
-    context "when the target training period has events" do
+    context "when the original training period has events" do
       before do
-        allow(target_training_period.events)
+        allow(original_training_period.events)
           .to receive(:exists?)
           .and_return(true)
       end
@@ -355,19 +358,19 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Target training period has events"
+            "Original training period has events"
           )
 
         expect_records_to_remain_unchanged
       end
     end
 
-    context "when the source training period has declarations" do
+    context "when the erroneous training period has declarations" do
       before do
         FactoryBot.create(
           :declaration,
           :eligible,
-          training_period: source_training_period
+          training_period: erroneous_withdrawn_training_period
         )
       end
 
@@ -375,16 +378,16 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Source training period has declarations"
+            "Erroneous training period has declarations"
           )
 
         expect_records_to_remain_unchanged
       end
     end
 
-    context "when the source training period has events" do
+    context "when the erroneous training period has events" do
       before do
-        allow(source_training_period.events)
+        allow(erroneous_withdrawn_training_period.events)
           .to receive(:exists?)
           .and_return(true)
       end
@@ -393,7 +396,7 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
         expect { correct_withdrawal_history }
           .to raise_error(
             RuntimeError,
-            "Source training period has events"
+            "Erroneous training period has events"
           )
 
         expect_records_to_remain_unchanged
@@ -420,19 +423,19 @@ RSpec.describe Teachers::ECTWithdrawalHistoryCorrection do
   end
 
   def expect_records_to_remain_unchanged
-    expect(target_training_period.reload).to have_attributes(
-      finished_on: target_finished_on,
+    expect(original_training_period.reload).to have_attributes(
+      finished_on: original_finished_on,
       withdrawn_at: nil,
       withdrawal_reason: nil
     )
 
-    expect(TrainingPeriod.exists?(source_training_period.id))
-      .to be(true)
+    expect(
+      TrainingPeriod.exists?(
+        erroneous_withdrawn_training_period.id
+      )
+    ).to be(true)
 
-    expect(ect_at_school_period.reload.finished_on)
-      .to be_nil
-
-    expect(mentorship_period.reload.finished_on)
-      .to be_nil
+    expect(ect_at_school_period.reload.finished_on).to be_nil
+    expect(mentorship_period.reload.finished_on).to be_nil
   end
 end
