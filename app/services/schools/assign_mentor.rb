98422a6111c1200954fa8f72893a50a01020a1b6
Connor McQuillan <me@cpjmcquillan.com>
2026-07-14 17:58:23 +0100

    Introduce `Schools::MentorAssignment::MentorshipPeriods::DateResolver`
    This introduces a small class responsible for resolving the
    `earliest_possible_start` and `latest_possible_finish` dates when assigning a
    mentor.
    
    So much of the complexity in `Schools::AssignMentor` was tied up in the logic to
    determine dates, and this was highlighted by the volume of unit tests required
    to exercise each branch of the conditionals.
    
    By abstracting this complexity away to its own class we can significantly
    simplify the tests for the `Schools::AssignMentor` service, as well as making
    it easier to understand in isolation.

--- app/services/schools/assign_mentor.rb
+++ app/services/schools/assign_mentor.rb
@@ -52,15 +52,16 @@ module Schools
       )
     end
 
-    def earliest_possible_start
-      possible_dates = [ect_at_school_period.started_on, mentor_at_school_period.started_on]
-      possible_dates.push(Date.current) if @mentorship_can_start_today
-      possible_dates.compact.max
+    def dates_resolver
+      MentorAssignment::MentorshipPeriods::DatesResolver.new(
+        ect_at_school_period:,
+        mentor_at_school_period:,
+        mentorship_can_start_today: @mentorship_can_start_today
+      )
     end
 
-    def latest_possible_finish
-      [ect_at_school_period.finished_on, mentor_at_school_period.finished_on].compact.min
-    end
+    def earliest_possible_start = dates_resolver.earliest_possible_start
+    def latest_possible_finish = dates_resolver.latest_possible_finish
 
     def record_events!
       Events::Record.record_teacher_starts_being_mentored_event!(
