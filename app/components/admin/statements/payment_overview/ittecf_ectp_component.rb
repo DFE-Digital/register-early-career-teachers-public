3eefea3b2ca1ef483c83268c51e382fee5b70d5a
Richard Worrall <richard@soupish.net>
2026-07-23 13:56:28 +0100

    [3726] Clearer method names in PaymentOverviewComponent

--- app/components/admin/statements/payment_overview/ittecf_ectp_component.rb
+++ app/components/admin/statements/payment_overview/ittecf_ectp_component.rb
@@ -4,8 +4,8 @@ module Admin
       class IttecfEctpComponent < PaymentOverviewComponent
         def rows
           [
-            ["ECTs output payment", { text: number_to_pounds(outputs), numeric: true }],
-            ["Mentors output payment", { text: number_to_pounds(mentors_outputs), numeric: true }],
+            ["ECTs output payment", { text: number_to_pounds(output_payment), numeric: true }],
+            ["Mentors output payment", { text: number_to_pounds(mentors_output_payment), numeric: true }],
             ["Service fee", { text: number_to_pounds(monthly_service_fee), numeric: true }],
             ["ECTs clawbacks", { text: number_to_pounds(clawbacks), numeric: true }],
             ["Mentors clawbacks", { text: number_to_pounds(mentors_clawbacks), numeric: true }],
@@ -17,13 +17,14 @@ module Admin
       private
 
         delegate :flat_rate_calculator, to: :calculators, private: true
+        delegate :outputs, to: :flat_rate_calculator, prefix: :mentors, private: true
 
-        def mentors_outputs
-          flat_rate_calculator.outputs.total_billable_amount
+        def mentors_output_payment
+          mentors_outputs.total_billable_amount
         end
 
         def mentors_clawbacks
-          -flat_rate_calculator.outputs.total_refundable_amount
+          -mentors_outputs.total_refundable_amount
         end
       end
     end
