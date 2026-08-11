3e19f8eba82aec4095f7d661060f73dbeb5c168a
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-08-11 13:26:27 +0100

    [3544] Fix heading semantics on teacher timeline page
    Allow timeline heading levels to be configured so teacher events sit below
    the Timeline h2 while appropriate body timelines retain their existing
    heading hierarchy.

--- spec/views/admin/teachers/timeline/show.html.erb_spec.rb
+++ spec/views/admin/teachers/timeline/show.html.erb_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe "admin/teachers/timeline/show.html.erb" do
   let(:teacher) { FactoryBot.create(:teacher) }
+  let(:events) { [] }
 
   before do
     assign(:teacher, Admin::TeacherPresenter.new(teacher))
-    assign(:events, [])
+    assign(:events, events)
     assign(:breadcrumbs, {
       "Teachers" => admin_teachers_path,
       "Floella Benjamin" => admin_teacher_path(teacher),
@@ -14,5 +15,13 @@ RSpec.describe "admin/teachers/timeline/show.html.erb" do
 
   it "includes a breadcrumb to admin teachers list" do
     expect(view.content_for(:backlink_or_breadcrumb)).to have_link("Teachers", href: admin_teachers_path)
+  end
+
+  context "with timeline events" do
+    let(:events) { [FactoryBot.build(:event, :with_body)] }
+
+    it "renders event titles as level 3 headings" do
+      expect(rendered).to have_css("h3.app-timeline__title", text: events.first.heading)
+    end
   end
 end
