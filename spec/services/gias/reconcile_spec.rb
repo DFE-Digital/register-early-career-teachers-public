953f0c5acf8df4339540554436112e0b0633ff46
thenapking <tom.giles@gmail.com>
2026-08-13 12:40:17 +0100

    [4144] PR Feedback: many to one merges

--- spec/services/gias/reconcile_spec.rb
+++ spec/services/gias/reconcile_spec.rb
@@ -86,6 +86,49 @@ RSpec.describe GIAS::Reconcile, type: :service do
         end
       end
 
+      context "when two schools are merging into the same successor" do
+        let(:urns) { [20_003, 20_007, 20_008] }
+        let(:merged_gias_school) { FactoryBot.create(:gias_school, :with_school, :closed, closed_on: Date.current, urn: 20_003) }
+        let(:other_merged_gias_school) { FactoryBot.create(:gias_school, :with_school, :closed, closed_on: Date.current, urn: 20_008) }
+        let(:successor_gias_school_2) { FactoryBot.create(:gias_school, :with_school, status: "open", urn: 20_007) }
+
+        before do
+          FactoryBot.create(:gias_school_link, :successor_merged, from_gias_school: merged_gias_school, to_gias_school: successor_gias_school_2)
+          FactoryBot.create(:gias_school_link, :successor_merged, from_gias_school: other_merged_gias_school, to_gias_school: successor_gias_school_2)
+          allow(GIAS::Reconciliation::Merge).to receive(:merge!).with(other_merged_gias_school).and_return(true)
+        end
+
+        it "merges both schools" do
+          expect(GIAS::Reconciliation::Merge).to receive(:merge!).with(merged_gias_school)
+          expect(GIAS::Reconciliation::Merge).to receive(:merge!).with(other_merged_gias_school)
+
+          service
+        end
+
+        context "when a teacher has overlapping mentor periods at the predecessor schools" do
+          let(:mentor_teacher) { FactoryBot.create(:teacher) }
+
+          before do
+            FactoryBot.create(:mentor_at_school_period, teacher: mentor_teacher, school: merged_gias_school.school, started_on: Date.new(2025, 1, 1), finished_on: Date.new(2025, 6, 30))
+            FactoryBot.create(:mentor_at_school_period, teacher: mentor_teacher, school: other_merged_gias_school.school, started_on: Date.new(2025, 5, 1), finished_on: Date.new(2025, 10, 31))
+            FactoryBot.create(:mentor_at_school_period, teacher: mentor_teacher, school: successor_gias_school_2.school, started_on: Date.new(2025, 8, 1), finished_on: Date.new(2025, 12, 31))
+
+            allow(GIAS::Reconciliation::Merge).to receive(:merge!).and_call_original
+          end
+
+          it "merges the mentor periods correctly" do
+            service
+
+            merged_mentor_at_school_periods = mentor_teacher.reload.mentor_at_school_periods
+
+            expect(merged_mentor_at_school_periods.count).to eq(1)
+            expect(merged_mentor_at_school_periods.first.started_on).to eq(Date.new(2025, 1, 1))
+            expect(merged_mentor_at_school_periods.first.finished_on).to eq(Date.new(2025, 12, 31))
+            expect(merged_mentor_at_school_periods.first.school).to eq(successor_gias_school_2.school)
+          end
+        end
+      end
+
       context "when there are schools that cannot be reconciled" do
         let(:urns) { [20_001, 20_002, 20_003, 20_004, 20_005, 20_006, 20_007] }
         let(:split_gias_school) { FactoryBot.create(:gias_school, :closed, closed_on: Date.current, urn: 20_004) }
