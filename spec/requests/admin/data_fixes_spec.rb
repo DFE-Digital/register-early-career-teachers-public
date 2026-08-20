d30fa207cc637c1dd3016c045e6326b203556fa6
Connor McQuillan <me@cpjmcquillan.com>
2026-08-20 15:19:02 +0100

    Product team users can preview data fixes
    Before, in #3377 users could input a CSV of data fixes which were parsed and
    presented to the user.
    
    But we didn't do anything with those changes.
    
    This builds on #3393 and processes the proposed changes in a preview step. Any
    errors are presented to the user. If there are no errors, we display the
    "changes".
    
    This is executed in a transaction and rolled back, so no changes are persisted
    yet.
    
    Both the parsed rows and the processed changes are displayed to users in the
    most rudimentary way possible. We intend to iterate on this soon!
    
    In order to display *all* the errors when processing the CSV, we need to pass a
    custom `ErrorPresenter` to the form builder. This ensures each error message is
    displayed, rather than only the first.

--- spec/requests/admin/data_fixes_spec.rb
+++ spec/requests/admin/data_fixes_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe "Admin::DataFixesController" do
+  before do
+    allow(Rails.application.config)
+      .to receive(:enable_admin_data_fixes)
+      .and_return(enable_admin_data_fixes)
+  end
+
   describe "GET #new" do
     subject do
       get path_for_step("csv")
       response
-    end
-
-    before do
-      allow(Rails.application.config)
-        .to receive(:enable_admin_data_fixes)
-        .and_return(enable_admin_data_fixes)
     end
 
     context "when `enable_admin_data_fixes` is true" do
@@ -66,23 +66,17 @@ RSpec.describe "Admin::DataFixesController" do
 
   describe "POST #create" do
     subject do
-      post(path_for_step("csv"), params:)
+      post(path_for_step("csv"), params: csv_params)
       response
     end
 
-    let(:params) { { csv: { csv_string: } } }
+    let(:csv_params) { { csv: { csv_string: } } }
     let(:csv_string) do
       <<~ROWS
         object_type,object_id,action,attributes
         something,1,create,"attribute1,value1,attribute2,value2"
         another_thing,2,destroy,""
       ROWS
-    end
-
-    before do
-      allow(Rails.application.config)
-        .to receive(:enable_admin_data_fixes)
-        .and_return(enable_admin_data_fixes)
     end
 
     context "when `enable_admin_data_fixes` is true" do
@@ -107,8 +101,47 @@ RSpec.describe "Admin::DataFixesController" do
       context "when signed in as a product team user" do
         include_context "sign in as product_team DfE user"
 
-        context "when the CSV is valid" do
-          it { is_expected.to redirect_to(path_for_step("preview")) }
+        let(:teacher) { FactoryBot.create(:teacher, trn: "123456") }
+
+        context "when the CSV is valid and changes can be processed" do
+          let(:csv_string) do
+            <<~ROWS
+              object_type,object_id,action,attributes
+              Teacher,#{teacher.id},update,"trn,345678"
+            ROWS
+          end
+
+          it "redirects to preview step, then redirects to verify step " \
+             "without persisting any changes" do
+            expect(subject).to redirect_to(path_for_step("preview"))
+
+            follow_redirect!
+
+            expect { post path_for_step("preview") }
+              .not_to(change { teacher.reload.trn })
+
+            expect(response).to redirect_to(path_for_step("verify"))
+          end
+        end
+
+        context "when the CSV is valid but the changes cannot be processed" do
+          let(:csv_string) do
+            <<~ROWS
+              object_type,object_id,action,attributes
+              Teacher,#{teacher.id},update,"trn,123"
+            ROWS
+          end
+
+          it "redirects to preview step, then returns unprocessable_content" do
+            expect(subject).to redirect_to(path_for_step("preview"))
+
+            follow_redirect!
+
+            expect { post path_for_step("preview") }
+              .not_to(change { teacher.reload.trn })
+
+            expect(response).to have_http_status(:unprocessable_content)
+          end
         end
 
         context "when the CSV is invalid" do
