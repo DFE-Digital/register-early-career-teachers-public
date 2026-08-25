cfedd7affe47c1cc8e821bda351e9f9e72a23c6f
Erica Porter <ericaporter@users.noreply.github.com>
2026-08-25 10:55:03 +0100

    Validate ECT school start date changes

--- app/wizards/schools/ects/change_start_date_wizard/edit_step.rb
+++ app/wizards/schools/ects/change_start_date_wizard/edit_step.rb
@@ -4,15 +4,12 @@ module Schools
       class EditStep < ECTs::Step
         attr_accessor :start_date
 
-        validate :start_date_valid
+        validates :start_date, ect_start_date: true
+        validate :start_date_after_previous_school_or_training_period_start
+        validate :start_date_within_4_months
 
         def self.permitted_params
-          %i[
-            start_date
-            start_date(1i)
-            start_date(2i)
-            start_date(3i)
-          ]
+          %i[start_date]
         end
 
         def next_step
@@ -42,13 +39,68 @@ module Schools
             .date_as_hash
         end
 
-        def start_date_valid
-          return if start_date_input.valid?
+        def start_date_after_previous_school_or_training_period_start
+          return if skip_additional_start_date_validations?
+          return if start_date_boundary_validator.valid?
 
           errors.add(
             :start_date,
-            start_date_input.error_message
+            invalid_period_error_message
           )
+        end
+
+        def invalid_period_error_message
+          "Our records show that " \
+            "#{name_for(ect_at_school_period.teacher)} started " \
+            "#{invalid_period_type} at #{previous_period.school.name} on " \
+            "#{invalid_period_started_on_formatted}. " \
+            "Enter a start date after " \
+            "#{earliest_valid_input_date_formatted}."
+        end
+
+        def start_date_within_4_months
+          return if skip_additional_start_date_validations?
+          return if registrations_closed_for_contract_period?
+          return if start_date_as_date < earliest_invalid_start_date
+
+          errors.add(
+            :start_date,
+            "Start date must be before " \
+              "#{earliest_invalid_start_date.to_fs(:govuk)}. " \
+              "You cannot register the ECT this far in advance."
+          )
+        end
+
+        def registrations_closed_for_contract_period?
+          start_date_as_date.future? &&
+            !start_date_contract_period&.enabled?
+        end
+
+        def skip_additional_start_date_validations?
+          start_date.blank? || errors[:start_date].any?
+        end
+
+        def earliest_invalid_start_date
+          @earliest_invalid_start_date ||=
+            (4.months + 1.day).from_now.to_date
+        end
+
+        def previous_period
+          @previous_period ||=
+            ect_at_school_period
+              .teacher
+              .ect_at_school_periods
+              .where.not(id: ect_at_school_period.id)
+              .latest_first
+              .first
+        end
+
+        def start_date_boundary_validator
+          @start_date_boundary_validator ||=
+            Schools::Validation::PeriodBoundary.new(
+              input_period: previous_period,
+              input_date: start_date_as_date
+            )
         end
 
         def start_date_input
@@ -66,6 +118,14 @@ module Schools
           @start_date_contract_period ||=
             ContractPeriod.containing_date(start_date_as_date)
         end
+
+        delegate :type,
+                 :started_on_formatted,
+                 to: :start_date_boundary_validator,
+                 prefix: :invalid_period
+
+        delegate :earliest_valid_input_date_formatted,
+                 to: :start_date_boundary_validator
       end
     end
   end
