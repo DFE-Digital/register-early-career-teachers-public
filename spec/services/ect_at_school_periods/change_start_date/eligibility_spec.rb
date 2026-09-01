4db31e93de124e537a5c664ee2cc836ec6cdda8f
Erica Porter <ericaporter@users.noreply.github.com>
2026-09-01 15:49:54 +0100

    Simplify change start date eligibility specs

--- spec/services/ect_at_school_periods/change_start_date/eligibility_spec.rb
+++ spec/services/ect_at_school_periods/change_start_date/eligibility_spec.rb
@@ -4,172 +4,107 @@ RSpec.describe ECTAtSchoolPeriods::ChangeStartDate::Eligibility do
   end
 
   describe "#eligible?" do
-    let(:school) { FactoryBot.create(:school) }
-    let(:teacher) { FactoryBot.create(:teacher) }
-
     let(:ect_at_school_period) do
-      FactoryBot.create(
-        :ect_at_school_period,
-        :unfinished,
-        school:,
-        teacher:,
-        started_on: Date.new(2026, 9, 1)
-      )
+      FactoryBot.create(:ect_at_school_period, :unfinished)
     end
 
     context "when this is the latest ECT at-school period" do
       context "with exactly one training period" do
-        before do
+        let!(:training_period) do
           FactoryBot.create(
             :training_period,
             :school_led,
             :for_ect,
             :unfinished,
-            ect_at_school_period:,
-            started_on: ect_at_school_period.started_on
+            ect_at_school_period:
           )
         end
 
         context "without a mentorship period" do
-          it "is eligible" do
-            expect(eligibility).to be_eligible
-          end
+          it { is_expected.to be_eligible }
         end
 
         context "with one mentorship period" do
-          let(:mentor_at_school_period) do
-            FactoryBot.create(
-              :mentor_at_school_period,
-              :unfinished,
-              school:,
-              started_on: Date.new(2026, 9, 1)
-            )
+          let!(:mentorship_period) do
+            FactoryBot.create(:mentorship_period, mentee: ect_at_school_period)
           end
 
-          before do
-            FactoryBot.create(
-              :mentorship_period,
-              :unfinished,
-              mentee: ect_at_school_period,
-              mentor: mentor_at_school_period,
-              started_on: Date.new(2026, 9, 1)
-            )
-          end
-
-          it "is eligible" do
-            expect(eligibility).to be_eligible
-          end
+          it { is_expected.to be_eligible }
         end
 
         context "with multiple mentorship periods" do
-          let(:first_mentor_at_school_period) do
+          let!(:mentorship_period) do
+            FactoryBot.create(:mentorship_period, mentee: ect_at_school_period)
+          end
+          let!(:second_mentor_at_school_period) do
             FactoryBot.create(
               :mentor_at_school_period,
               :unfinished,
-              school:,
-              started_on: Date.new(2026, 9, 1)
+              school: ect_at_school_period.school
             )
           end
-
-          let(:second_mentor_at_school_period) do
-            FactoryBot.create(
-              :mentor_at_school_period,
-              :unfinished,
-              school:,
-              started_on: Date.new(2026, 9, 1)
-            )
-          end
-
-          before do
-            FactoryBot.create(
-              :mentorship_period,
-              mentee: ect_at_school_period,
-              mentor: first_mentor_at_school_period,
-              started_on: Date.new(2026, 9, 1),
-              finished_on: Date.new(2026, 9, 30)
-            )
-
+          let!(:second_mentorship_period) do
             FactoryBot.create(
               :mentorship_period,
               :unfinished,
               mentee: ect_at_school_period,
               mentor: second_mentor_at_school_period,
-              started_on: Date.new(2026, 10, 1)
+              started_on: mentorship_period.finished_on.next_day
             )
           end
 
-          it "is not eligible" do
-            expect(eligibility).not_to be_eligible
-          end
+          it { is_expected.not_to be_eligible }
         end
       end
 
       context "without a training period" do
-        it "is not eligible" do
-          expect(eligibility).not_to be_eligible
-        end
+        it { is_expected.not_to be_eligible }
       end
 
       context "with multiple training periods" do
-        before do
+        let!(:training_period) do
           FactoryBot.create(
             :training_period,
             :school_led,
             :for_ect,
-            ect_at_school_period:,
-            started_on: Date.new(2026, 9, 1),
-            finished_on: Date.new(2026, 9, 30)
+            ect_at_school_period:
           )
-
+        end
+        let!(:second_training_period) do
           FactoryBot.create(
             :training_period,
             :school_led,
             :for_ect,
             :unfinished,
             ect_at_school_period:,
-            started_on: Date.new(2026, 10, 1)
+            started_on: training_period.finished_on.next_day
           )
         end
 
-        it "is not eligible" do
-          expect(eligibility).not_to be_eligible
-        end
+        it { is_expected.not_to be_eligible }
       end
     end
 
     context "when this is not the latest ECT at-school period" do
-      let(:ect_at_school_period) do
-        FactoryBot.create(
-          :ect_at_school_period,
-          school:,
-          teacher:,
-          started_on: Date.new(2025, 9, 1),
-          finished_on: Date.new(2026, 8, 31)
-        )
-      end
-
-      before do
+      let(:ect_at_school_period) { FactoryBot.create(:ect_at_school_period) }
+      let!(:training_period) do
         FactoryBot.create(
           :training_period,
           :school_led,
           :for_ect,
-          ect_at_school_period:,
-          started_on: Date.new(2025, 9, 1),
-          finished_on: Date.new(2026, 8, 31)
+          ect_at_school_period:
         )
-
+      end
+      let!(:latest_ect_at_school_period) do
         FactoryBot.create(
           :ect_at_school_period,
           :unfinished,
-          school:,
-          teacher:,
-          started_on: Date.new(2026, 9, 1)
+          teacher: ect_at_school_period.teacher,
+          started_on: ect_at_school_period.finished_on.next_day
         )
       end
 
-      it "is not eligible" do
-        expect(eligibility).not_to be_eligible
-      end
+      it { is_expected.not_to be_eligible }
     end
   end
 end
