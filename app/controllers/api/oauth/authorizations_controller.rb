a06266e332281dd8ac6b359ebab4372fd804a548
Craig Davidson <craigmdavidson@gmail.com>
2026-09-04 17:44:37 +0100

    AuthorizationRequest handles session storage

--- app/controllers/api/oauth/authorizations_controller.rb
+++ app/controllers/api/oauth/authorizations_controller.rb
@@ -2,14 +2,13 @@ module API
   module OAuth
     class AuthorizationsController < ::AppropriateBodiesController
       def new
-        @authorization_request = build_authorization_request
+        @authorization_request = AuthorizationRequest.build(authorization_request_params)
         return render(:invalid_request, status: :bad_request) unless @authorization_request.redirectable?
 
         if @authorization_request.valid?
-          store_authorization_request
-          @client = @authorization_request.client
+          @authorization_request.store_in(session)
         else
-          destroy_authorization_request
+          AuthorizationRequest.clear_from(session)
           redirect_to(
             @authorization_request.redirect_uri_with_params,
             allow_other_host: true
@@ -18,47 +17,33 @@ module API
       end
 
       def create
-        head :not_implemented
+        @authorization_request = AuthorizationRequest.from(session)
+        render json: @authorization_request
       end
 
       def destroy
-        @authorization_request = resume_authorization_request
+        @authorization_request = AuthorizationRequest.from(session)
         return render(:invalid_request, status: :bad_request) unless @authorization_request&.redirectable?
 
-        destroy_authorization_request
-
+        AuthorizationRequest.clear_from(session)
         redirect_to(
-          @authorization_request.redirect_uri_with_params(error: :access_denied, error_description: "User refused connection"),
+          @authorization_request.redirect_uri_with_params(
+            error: :access_denied, error_description: "User refused connection"
+          ),
           allow_other_host: true
         )
       end
 
     private
 
-      def build_authorization_request
-        AuthorizationRequest.new(
-          logged_in_appropriate_body_period_id: @appropriate_body.id,
-          **authorization_request_params
-        )
-      end
-
-      def resume_authorization_request
-        return if session[:oauth_authorization_request].blank?
-
-        AuthorizationRequest.new(**session[:oauth_authorization_request].symbolize_keys)
-      end
-
-      def store_authorization_request
-        session[:oauth_authorization_request] = @authorization_request.attributes
-      end
-
-      def destroy_authorization_request
-        session.delete(:oauth_authorization_request)
-      end
-
       def authorization_request_params
-        params.permit(:response_type, :client_id, :appropriate_body_period_id, :redirect_uri,
-                      :code_challenge, :code_challenge_method, :state).to_h.symbolize_keys
+        params.permit(
+          :response_type, :client_id, :appropriate_body_period_id, :redirect_uri,
+          :code_challenge, :code_challenge_method, :state
+        )
+          .to_h
+          .symbolize_keys
+          .merge(logged_in_appropriate_body_period_id: @appropriate_body.id)
       end
     end
   end
--- app/models/api/oauth/authorization_request.rb
+++ app/models/api/oauth/authorization_request.rb
@@ -3,12 +3,10 @@ module API
     class AuthorizationRequest
       include ActiveModel::Model
       include ActiveModel::Attributes
+      include SessionStorable
+      include Redirectable
 
       RESPONSE_TYPE = "code"
-      ERROR_CODES = {
-        %i[response_type inclusion] => :unsupported_response_type,
-        %i[appropriate_body_period_id equal_to] => :access_denied,
-      }.freeze
 
       attribute :response_type, :string
       attribute :client_id, :string
@@ -33,26 +31,6 @@ module API
       delegate :name, to: :client, prefix: true, allow_nil: true
 
       def client = @client ||= Client.find_by(client_id:)
-
-      def redirectable? = redirect_uri.present? && client&.redirect_uris&.include?(redirect_uri)
-
-      def error_code
-        return if errors.empty?
-
-        ERROR_CODES.find { |(attribute, kind), _| errors.of_kind?(attribute, kind) }&.last || :invalid_request
-      end
-
-      def error_messages_description = errors.full_messages.join(", ")
-
-      def redirect_uri_with_params(error: error_code, error_description: error_messages_description)
-        uri = URI.parse(redirect_uri)
-        existing_query_params = uri.query.present? ? URI.decode_www_form(uri.query).to_h : {}
-        error_params = error.present? ? { error:, error_description: } : {}
-        state_params = { state: state.presence }
-        query_params = existing_query_params.merge(error_params).merge(state_params).compact
-        uri.query = URI.encode_www_form(query_params)
-        uri.to_s
-      end
     end
   end
 end
