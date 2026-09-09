0a95a9e508a1b2b28bd822f26485201954cf8c10
thenapking <tom.giles@gmail.com>
2026-09-09 17:41:53 +0100

    [4381]  Add banner to admin teachers index

--- app/controllers/admin/teachers_controller.rb
+++ app/controllers/admin/teachers_controller.rb
@@ -15,6 +15,7 @@ module Admin
 
       @pagy, teachers = pagy(teacher_search.teacher_scope)
       @teacher_rows = rows.rows(teachers)
+      @show_failed_trn_merges_banner = Teacher.trs_response_permanent_redirect.exists?
     end
 
     def show
