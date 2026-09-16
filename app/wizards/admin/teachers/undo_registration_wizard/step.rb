242749731546c0bb040537eb4d9bd36a8455c202
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-09-16 18:53:25 +0100

    [4245] Add undo registration wizard steps

---
+++ app/wizards/admin/teachers/undo_registration_wizard/step.rb
@@ -0,0 +1,29 @@
+module Admin
+  module Teachers
+    module UndoRegistrationWizard
+      class Step < ApplicationWizardStep
+        def self.permitted_params = []
+
+        def save!
+          return false unless wizard.valid_step?
+
+          persist
+          true
+        end
+
+      private
+
+        def pre_populate_attributes
+          self.class.permitted_params.each do |key|
+            value = wizard.store.public_send(key)
+            public_send("#{key}=", value) if value.present?
+          end
+        end
+
+        def step_params
+          @step_params ||= wizard.step_params.to_h.slice(*self.class.permitted_params.map(&:to_s))
+        end
+      end
+    end
+  end
+end
