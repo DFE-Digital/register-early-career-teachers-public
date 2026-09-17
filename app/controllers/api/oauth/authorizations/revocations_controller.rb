d6974cf6e61f7aba6f31fe175d7bcbb107c76a6d
Tony Headford <tony@binarycircus.com>
2026-09-17 12:21:45 +0100

    move revocations controller into authorizations module

---
+++ app/controllers/api/oauth/authorizations/revocations_controller.rb
@@ -0,0 +1,23 @@
+module API
+  module OAuth
+    module Authorizations
+      class RevocationsController < ActionController::API
+        include API::OAuth::ClientAuthenticable
+
+        before_action :set_authorization
+
+        def create
+          API::OAuth::Authorizations::RevocationRequest.new(authorization: @authorization).revoke!
+
+          head :ok
+        end
+
+      private
+
+        def set_authorization
+          @authorization = current_client.authorization_for_token(token: params.require(:token))
+        end
+      end
+    end
+  end
+end
